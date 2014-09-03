6ba69a16e7c9caddb6d4d7865a20c62c0200b546
Nino Padrutt <nino.padrutt@hotmail.com>
2014-09-03 09:47:27 +0200

    make it build

---
+++ MoneyManager/MoneyManager.Shared/Converter/BooleanToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Helper.Converter
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+        }
+    }
+}
