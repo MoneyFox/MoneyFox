81270a87eec278ab2559a6525ceee974da9fe432
Nino Padrutt <nino.padrutt@hotmail.com>
2014-09-22 23:29:42 +0200

    set default value for add transaction

--- MoneyManager/MoneyManager.Shared/Src/TransactionHelper.cs
+++ MoneyManager/MoneyManager.Shared/Src/TransactionHelper.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Linq;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Microsoft.Practices.ServiceLocation;
 using MoneyManager.DataAccess;
@@ -9,21 +10,43 @@ namespace MoneyManager.Src
 {
     public class TransactionHelper
     {
-        public FinancialTransaction SelectedTransaction
+        public static FinancialTransaction SelectedTransaction
         {
             get { return ServiceLocator.Current.GetInstance<TransactionDataAccess>().SelectedTransaction; }
             set { ServiceLocator.Current.GetInstance<TransactionDataAccess>().SelectedTransaction = value; }
         }
 
+        public static AccountDataAccess AccountData
+        {
+            get { return ServiceLocator.Current.GetInstance<AccountDataAccess>(); }
+        }
+
         public static void GoToAddTransaction(TransactionType transactionType)
         {
-            ServiceLocator.Current.GetInstance<TransactionDataAccess>().SelectedTransaction 
-                = new FinancialTransaction
-            {
-                Type = (int)transactionType,
-                Currency = "CHF"
-            };
+            SetDefaultTransaction(transactionType);
+            SetDefaultAccount();
+
+
             ((Frame)Window.Current.Content).Navigate(typeof(AddTransaction));
         }
+
+        private static void SetDefaultTransaction(TransactionType transactionType)
+        {
+            ServiceLocator.Current.GetInstance<TransactionDataAccess>().SelectedTransaction
+                = new FinancialTransaction
+                {
+                    Type = (int) transactionType,
+                    Currency = "CHF"
+                };
+        }
+
+        private static void SetDefaultAccount()
+        {
+            if (AccountData.AllAccounts.Any())
+            {
+                SelectedTransaction.ChargedAccount = AccountData.AllAccounts.First();
+            }
+        }
+
     }
 }
