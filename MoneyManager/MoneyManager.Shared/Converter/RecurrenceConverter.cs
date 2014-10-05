2c8a0e7f24f5aad4e08f7773a5cc63bdf29ee560
Nino Padrutt <nino.padrutt@hotmail.com>
2014-10-05 21:28:51 +0200

    adjust template

---
+++ MoneyManager/MoneyManager.Shared/Converter/RecurrenceConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+using MoneyManager.Src;
+
+namespace MoneyManager.Converter
+{
+    public class RecurrenceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return Utilities.GetTranslation(Int32.Parse(value.ToString()) == (int)TransactionRecurrence.Monthly 
+                ? "MonthlyLabel" 
+                : "WeeklyLabel");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
