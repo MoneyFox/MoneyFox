10fa8ae8b1c051f9e41f4c606fcc176ff9c8c583
Nino Padrutt <nino.padrutt@hotmail.com>
2014-10-20 16:38:55 +0200

    add new recurrence intervalls to converter

--- MoneyManager/MoneyManager.Shared/Converter/RecurrenceConverter.cs
+++ MoneyManager/MoneyManager.Shared/Converter/RecurrenceConverter.cs
@@ -8,9 +8,24 @@ namespace MoneyManager.Converter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return Utilities.GetTranslation(Int32.Parse(value.ToString()) == (int)TransactionRecurrence.Monthly
-                ? "MonthlyLabel"
-                : "WeeklyLabel");
+            var enumInt = Int32.Parse(value.ToString());
+            
+            Switch(enumInt){
+                case (int)TransactionRecurrence.Daily:
+                    return Utilities.GetTranslation("DailyLabel");
+                    
+                case (int)TransactionRecurrence.Weekly:
+                    return Utilities.GetTranslation("WeeklyLabel");
+                    
+                case (int)TransactionRecurrence.Monthly:
+                    return Utilities.GetTranslation("MonthlyLabel");
+                    
+                case (int)TransactionRecurrence.Yearly:
+                    return Utilities.GetTranslation("YearlyLabel");
+                    
+                Default:
+                    return Utilities.GetTranslation("NoneLabel"); 
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
