642ba89e9f8af35340e503b1e9871b309e1fd8d5
Nino Padrutt <nino.padrutt@hotmail.com>
2014-10-22 20:11:41 +0200

    renmae language to general

--- MoneyManager/MoneyManager.WindowsPhone/Views/SettingsGeneral.xaml.cs
+++ MoneyManager/MoneyManager.WindowsPhone/Views/SettingsGeneral.xaml.cs
@@ -23,12 +23,12 @@ namespace MoneyManager.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class SettingsLanguage : Page
+    public sealed partial class SettingsGeneral : Page
     {
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
 
-        public SettingsLanguage()
+        public SettingsGeneral()
         {
             this.InitializeComponent();
 
