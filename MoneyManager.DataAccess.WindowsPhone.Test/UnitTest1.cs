cc31d4d8ec25337968ea1036e62741172ce72334
Nino Padrutt <nino.padrutt@hotmail.com>
2014-10-23 17:26:39 +0200

    new testproject

---
+++ MoneyManager.DataAccess.WindowsPhone.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace MoneyManager.DataAccess.WindowsPhone.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
