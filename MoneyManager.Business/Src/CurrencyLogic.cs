fb8bdf1d148a617aebfc59cf578bed6db7c4e1e2
Nino Padrutt <nino.padrutt@hotmail.com>
2014-10-27 17:02:04 +0100

    some adjustments for Currency support

--- MoneyManager.Business/Src/CurrencyLogic.cs
+++ MoneyManager.Business/Src/CurrencyLogic.cs
@@ -8,8 +8,10 @@ namespace MoneyManager.Business.Src
 {
     internal class CurrencyLogic
     {
-        private const string currencyServiceUrl = "http://www.freecurrencyconverterapi.com/api/convert?q={0}&compact=y";
-        private static readonly HttpClient httpClient = new HttpClient();
+                private const string CURRENCY_SERVICE_URL = "http://www.freecurrencyconverterapi.com/api/convert?q={0}&compact=y";
+        private const string COUNTRIES_SERVICE_URL = "http://www.freecurrencyconverterapi.com/api/v2/countries";
+
+        private static HttpClient httpClient = new HttpClient();
 
         public CurrencyLogic()
         {
@@ -17,46 +19,52 @@ namespace MoneyManager.Business.Src
                 "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
         }
 
-        public static void GetSupportedCurrencies()
+        public async static Task<string> GetSupportedCurrencies()
         {
+            return await GetJsonFromService(COUNTRIES_SERVICE_URL);
         }
 
-        public static double GetCurrencyRatio(string currencyFrom, string currencyTo)
+        public async static Task<double> GetCurrencyRatio(string currencyFrom, string currencyTo)
         {
-            var httpClient = new HttpClient {BaseAddress = new Uri("https://api.SmallInvoice.com/")};
-            httpClient.DefaultRequestHeaders.Add("user-agent",
-                "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
-
             string currencyFromTo = string.Format("{0}-{1}", currencyFrom.ToUpper(), currencyTo.ToUpper());
-            string currenciesFromTo = string.Format(currencyServiceUrl, currencyFromTo);
+            string url = string.Format(CURRENCY_SERVICE_URL, currencyFromTo);
 
-            //Get Value from Service
-            //
+            string jsonString = await GetJsonFromService(url); // "{\"CHF-EUR\":{\"val\":1.2212}}";
+            jsonString = jsonString.Replace(currencyFromTo, "Conversion");
 
-            string jsonString = "{\"EUR-CHF\":{\"val\":1.2212}}";
+            return ParseToExchangeRate(jsonString);
+        }
 
-            jsonString = jsonString.Replace(currenciesFromTo, "Conversion");
-
+        private static double ParseToExchangeRate(string jsonString)
+        {
             var typeExample =
                 new
                 {
                     Conversion = new
-                    {
-                        val = ""
-                    }
+                                 {
+                                     val = ""
+                                 }
                 };
+
             var currency = JsonConvert.DeserializeAnonymousType(jsonString, typeExample);
             return Double.Parse(currency.Conversion.val, CultureInfo.CurrentCulture);
         }
 
         private static async Task<string> GetJsonFromService(string url)
         {
+            PrepareHttpClient();
             var req = new HttpRequestMessage(HttpMethod.Get, url);
             HttpResponseMessage response = await httpClient.SendAsync(req);
             response.EnsureSuccessStatusCode();
 
-            string content = await response.Content.ReadAsStringAsync();
-            return content;
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private static void PrepareHttpClient()
+        {
+            httpClient = new HttpClient { BaseAddress = new Uri("https://api.SmallInvoice.com/") };
+            httpClient.DefaultRequestHeaders.Add("user-agent",
+                "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
         }
     }
 }
