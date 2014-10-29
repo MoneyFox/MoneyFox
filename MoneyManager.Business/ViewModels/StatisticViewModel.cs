0abbc09aac7bf03946bc2de7432c5e920c72f0f4
Nino Padrutt <nino.padrutt@hotmail.com>
2014-10-29 17:45:59 +0100

    remove stuff since moved to logic class

--- MoneyManager.Business/ViewModels/StatisticViewModel.cs
+++ MoneyManager.Business/ViewModels/StatisticViewModel.cs
@@ -14,47 +14,9 @@ namespace MoneyManager.Business.ViewModels
     [ImplementPropertyChanged]
     public class StatisticViewModel : ViewModelBase
     {
-        private IEnumerable<FinancialTransaction> allTransaction
-        {
-            get { return ServiceLocator.Current.GetInstance<TransactionDataAccess>().AllTransactions; }
-        }
-
         public ObservableCollection<StatisticItem> MonthlyCashFlow
         {
-            get { return LoadMonthlyCashFlow(); }
-        }
-
-        public ObservableCollection<StatisticItem> LoadMonthlyCashFlow()
-        {
-            var transactionList = allTransaction
-                .Where(x => x.Date.Month == DateTime.Now.Month)
-                .ToList();
-
-            var itemList = new ObservableCollection<StatisticItem>();
-
-            var income = new StatisticItem
-            {
-                Category = Translation.GetTranslation("IncomeLabel"),
-                Value = transactionList.Where(x => x.Type == (int) TransactionType.Income).Sum(x => x.Amount)
-            };
-
-            var spent = new StatisticItem
-            {
-                Category = Translation.GetTranslation("SpentLabel"),
-                Value = transactionList.Where(x => x.Type == (int) TransactionType.Spending).Sum(x => x.Amount)
-            };
-
-            var increased = new StatisticItem
-            {
-                Category = Translation.GetTranslation("IncreasedLabel"),
-                Value = income.Value - spent.Value
-            };
-
-            itemList.Add(income);
-            itemList.Add(spent);
-            itemList.Add(increased);
-
-            return itemList;
+            get { return StatisticLogic.GetMonthlyCashFlow(); }
         }
     }
 }
