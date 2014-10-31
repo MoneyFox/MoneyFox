139318ce7081f166fc79ce991338dbee4d0ba0d0
Nino Padrutt <nino.padrutt@hotmail.com>
2014-10-31 15:32:10 +0100

    code cleanup

--- MoneyManager/MoneyManager.Shared/UserControls/SelectCategoryListUserControl.xaml.cs
+++ MoneyManager/MoneyManager.Shared/UserControls/SelectCategoryListUserControl.xaml.cs
@@ -1,16 +1,10 @@
-﻿using Windows.UI.Xaml.Controls;
-using Microsoft.Practices.ServiceLocation;
-using MoneyManager.Business.ViewModels;
-
-namespace MoneyManager.UserControls
+﻿namespace MoneyManager.UserControls
 {
     public sealed partial class SelectCategoryUserControl
     {
         public SelectCategoryUserControl()
         {
             InitializeComponent();
-
-            ListViewCategories.SelectedItem = null;
         }
     }
 }
