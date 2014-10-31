18104fa8f658ef75166d6f6e9617d62e87e07c54
Nino Padrutt <nino.padrutt@hotmail.com>
2014-10-31 23:23:48 +0100

    add end of month value to total balance control

--- MoneyManager.Business/ViewModels/TotalBalanceViewModel.cs
+++ MoneyManager.Business/ViewModels/TotalBalanceViewModel.cs
@@ -2,6 +2,7 @@
 using Microsoft.Practices.ServiceLocation;
 using MoneyManager.DataAccess.DataAccess;
 using MoneyManager.DataAccess.Model;
+using MoneyManager.Foundation;
 using PropertyChanged;
 using System.Collections.ObjectModel;
 using System.Globalization;
@@ -17,7 +18,13 @@ namespace MoneyManager.Business.ViewModels
             get { return ServiceLocator.Current.GetInstance<AccountDataAccess>().AllAccounts; }
         }
 
+        public TransactionDataAccess TransactionData
+        {
+            get { return ServiceLocator.Current.GetInstance<TransactionDataAccess>(); }
+        }
+
         public double TotalBalance { get; set; }
+        public double EndOfMonthBalance { get; set; }
 
         public string CurrencyCulture
         {
@@ -29,6 +36,29 @@ namespace MoneyManager.Business.ViewModels
             TotalBalance = AllAccounts != null
                 ? AllAccounts.Sum(x => x.CurrentBalance)
                 : 0;
+
+            EndOfMonthBalance = GetEndOfMonthValue();
+        }
+
+        private double GetEndOfMonthValue()
+        {
+            var balance = TotalBalance;
+            var unclearedTransactions = TransactionData.GetUnclearedTransactions();
+
+            foreach (var transaction in unclearedTransactions)
+            {
+                switch (transaction.Type)
+                {
+                    case (int) TransactionType.Spending:
+                        balance -= transaction.Amount;
+                        break;
+                    case (int) TransactionType.Income:
+                        balance += transaction.Amount;
+                        break;
+                }
+            }
+
+            return balance;
         }
     }
 }
