3eba84152e0e1acee6e081678ea4345de8a7d18b
Nino Padrutt <nino.padrutt@hotmail.com>
2014-11-01 14:47:56 +0100

    make aboutpage  to user control

---
+++ MoneyManager/MoneyManager.Shared/UserControls/AboutUserControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.ApplicationModel.Email;
+using Windows.System;
+using Windows.UI.Xaml.Input;
+using MoneyManager.Business.Helper;
+using MoneyManager.Foundation;
+
+namespace MoneyManager.UserControls
+{
+    public sealed partial class AboutUserControl 
+    {
+        public AboutUserControl()
+        {
+            InitializeComponent();
+
+            lblVersion.Text = Utilities.GetVersion();
+        }
+
+        private async void ComposeMail_OnTap(object sender, TappedRoutedEventArgs e)
+        {
+            var sendTo = new EmailRecipient
+            {
+                Address = Translation.GetTranslation("SupportMail")
+            };
+
+            var mail = new EmailMessage { Subject = Translation.GetTranslation("Feedback") };
+            mail.To.Add(sendTo);
+            await EmailManager.ShowComposeNewEmailAsync(mail);
+        }
+
+        private async void GoToWebsite_OnTap(object sender, TappedRoutedEventArgs e)
+        {
+            string url = "http://www.apply-solutions.ch/moneyfoxbeta";
+            await Launcher.LaunchUriAsync(new Uri(url));
+        }
+
+        private async void GoToTwitter_OnTap(object sender, TappedRoutedEventArgs e)
+        {
+            string url = "http://twitter.com/npadrutt";
+            await Launcher.LaunchUriAsync(new Uri(url));
+        }
+S
+    }
+}
--- MoneyManager/MoneyManager.WindowsPhone/Views/About.xaml.cs
+++ MoneyManager/MoneyManager.WindowsPhone/Views/About.xaml.cs
@@ -1,20 +1,10 @@
-﻿using MoneyManager.Business;
-using MoneyManager.Business.Helper;
-using MoneyManager.Common;
-using MoneyManager.Foundation;
-using System;
-using System.Reflection;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Email;
-using Windows.System;
-using Windows.UI.Xaml.Input;
+﻿using MoneyManager.Common;
 using Windows.UI.Xaml.Navigation;
 
 namespace MoneyManager.Views
 {
     public sealed partial class About
     {
-        private readonly ObservableDictionary defaultViewModel = new ObservableDictionary();
         private readonly NavigationHelper navigationHelper;
 
         public About()
@@ -22,41 +12,11 @@ namespace MoneyManager.Views
             InitializeComponent();
 
             navigationHelper = new NavigationHelper(this);
-            lblVersion.Text = Utilities.GetVersion();
         }
 
         public NavigationHelper NavigationHelper
         {
             get { return navigationHelper; }
-        }
-
-        public ObservableDictionary DefaultViewModel
-        {
-            get { return defaultViewModel; }
-        }
-
-        private async void ComposeMail_OnTap(object sender, TappedRoutedEventArgs e)
-        {
-            var sendTo = new EmailRecipient
-            {
-                Address = Translation.GetTranslation("SupportMail")
-            };
-
-            var mail = new EmailMessage { Subject = Translation.GetTranslation("Feedback") };
-            mail.To.Add(sendTo);
-            await EmailManager.ShowComposeNewEmailAsync(mail);
-        }
-
-        private async void GoToWebsite_OnTap(object sender, TappedRoutedEventArgs e)
-        {
-            string url = "http://www.apply-solutions.ch/moneyfoxbeta";
-            await Launcher.LaunchUriAsync(new Uri(url));
-        }
-
-        private async void GoToTwitter_OnTap(object sender, TappedRoutedEventArgs e)
-        {
-            string url = "http://twitter.com/npadrutt";
-            await Launcher.LaunchUriAsync(new Uri(url));
         }
 
         #region NavigationHelper registration
