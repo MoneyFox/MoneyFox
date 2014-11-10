fe1e5e8672cd65b9dab4ddf3a1587debf6bebea4
Nino Padrutt <nino.padrutt@hotmail.com>
2014-11-10 21:55:00 +0100

    create backup control

---
+++ MoneyManager/MoneyManager.Shared/UserControls/BackupUserControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Microsoft.Live;
+
+namespace MoneyManager.UserControls
+{
+    public sealed partial class BackupUserControl
+    {
+        private LiveConnectClient liveClient;
+
+        public BackupUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginToOneDrive(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var authClient = new LiveAuthClient();
+                LiveLoginResult result = await authClient.LoginAsync(new [] { "wl.signin", "wl.skydrive" });
+
+                if (result.Status == LiveConnectSessionStatus.Connected)
+                {
+                    liveClient = new LiveConnectClient(result.Session);
+                }
+            }
+            catch (LiveAuthException ex)
+            {
+                // Display an error message.
+            }
+            catch (LiveConnectException ex)
+            {
+                // Display an error message.
+            }
+        }
+
+        private void CreateBackup(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RestoreBackup(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
