96c77079df8f17ca29476c3ce239367423cf4711
Nino Padrutt <nino.padrutt@hotmail.com>
2014-11-19 23:15:58 +0100

    change title of transaction list

--- MoneyManager.Business/ViewModels/TransactionListViewModel.cs
+++ MoneyManager.Business/ViewModels/TransactionListViewModel.cs
@@ -6,6 +6,7 @@ using GalaSoft.MvvmLight;
 using Microsoft.Practices.ServiceLocation;
 using MoneyManager.DataAccess.DataAccess;
 using MoneyManager.DataAccess.Model;
+using MoneyManager.Foundation;
 using PropertyChanged;
 using QKit.JumpList;
 
@@ -16,16 +17,26 @@ namespace MoneyManager.Business.ViewModels
     [ImplementPropertyChanged]
     public class TransactionListViewModel : ViewModelBase
     {
-        private TransactionDataAccess TransactionData
+        private TransactionDataAccess transactionData
         {
             get { return ServiceLocator.Current.GetInstance<TransactionDataAccess>(); }
+        }
+
+        private AccountDataAccess accountData
+        {
+            get { return ServiceLocator.Current.GetInstance<AccountDataAccess>(); }
+        }
+
+        public string Title
+        {
+            get { return accountData.SelectedAccount.Name; }
         }
 
         public List<JumpListGroup<FinancialTransaction>> RelatedTransactions { set; get; }
 
         public void SetRelatedTransactions(int accountId)
         {
-            var related = TransactionData.GetRelatedTransactions(accountId);
+            var related = transactionData.GetRelatedTransactions(accountId);
 
             var dateInfo = new DateTimeFormatInfo();
             RelatedTransactions = related.ToGroups(x => x.Date, x => dateInfo.GetMonthName(x.Date.Month));
