405630ca6cb864b77d716ac1262b81b7d8a3065f
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2014-11-23 16:48:04 +0100

    add select statistic dialog

---
+++ MoneyManager/MoneyManager.WindowsPhone/Dialogs/SelectStatisticDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace MoneyManager.Dialogs
+{
+    public sealed partial class SelectStatisticDialog
+    {
+        public SelectStatisticDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadStatistic(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
