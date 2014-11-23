227af189bdb70469ef51df8750ba7aa4b15d5909
NPadrutt <nino.padrutt@apply-solutions.ch>
2014-11-23 18:10:19 +0100

    deploy  + version

--- MoneyManager/MoneyManager.WindowsPhone/Properties/AssemblyInfo.cs
+++ MoneyManager/MoneyManager.WindowsPhone/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.5.8.0")]
-[assembly: AssemblyFileVersion("0.5.8.0")]
+[assembly: AssemblyVersion("0.5.9.0")]
+[assembly: AssemblyFileVersion("0.5.9.0")]
 [assembly: ComVisible(false)]
