1232ddfb57f7389514c9ad152582244db4a95ad3
Nino Padrutt <nino.padrutt@hotmail.com>
2014-11-26 10:23:21 +0100

    add stub for transactionlogic test methods

---
+++ MoneyManager.Business.WindowsPhone.Test/Logic/TransactionLogicTest.cs
@@ -0,0 +1,46 @@
+namespace MoneyManager.Business.WindowsPhone.Test.Logic
+{
+  [TestMethod]
+  public class TransactionLogicTest{
+    [TestInitialize]
+    public void TestInit(){
+      new ViewModelLocator();
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async void SaveTransactionTest(){
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public void PrepareEditTest(){
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async void DeleteTransactionTest(){
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public void DeleteAssociatedTransactionsFromDatabaseTest(){
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async void UpdateTransactionTest(){
+      Assert.IsTrue(false);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async void ClearTransactionsTest(){
+      Assert.IsTrue(false);
+    }
+  }
+}
