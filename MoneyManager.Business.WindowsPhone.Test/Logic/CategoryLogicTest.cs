8974dff73ea9e0f906d5781044e13afbaf2ebd8f
Nino Padrutt <nino.padrutt@hotmail.com>
2014-11-26 10:16:22 +0100

    Delete category test

---
+++ MoneyManager.Business.WindowsPhone.Test/Logic/CategoryLogicTest.cs
@@ -0,0 +1,39 @@
+namespace MoneyManager.Business.WindowsPhone.Test.Logic
+{
+  [TestClass]
+  public class CategoryLogicTest{
+
+    [TestInitialize]
+    pulic void InitTest(){
+      new ViewModelLocator();
+    }
+
+    [TestMethod]
+    public void DeleteCategoryTest(){
+      var category1 = new Category {
+        Name = "Einkaufen"
+      };
+      var category2 = new Category {
+        Name = "Sparen"
+      };
+      var category3 = new Category {
+        Name = "Schule"
+      };
+
+      using(var db = SqlConnectionFactory.GetSqlConnection()){
+        db.Insert(category1);
+        db.Insert(category2);
+        db.Insert(category3);
+      }
+
+      CategoryLogic.DeleteCategory(category1);
+      CategoryLogic.DeleteCategory(category3);
+
+      using(var db = SqlConnectionFactory.GetSqlConnection()){
+        Assert.IsFalse(db.Table<Category>().Any(x => x.Id == category1.Id));
+        Assert.IsTrue(db.Table<Category>().Any(x => x.Id == category2.Id));
+        Assert.IsFalse(db.Table<Category>().Any(x => x.Id == category3.Id));
+      }
+    }
+  }
+}
