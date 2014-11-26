d1ec9549872bd8fe40025b92772c214c3c235f55
NPadrutt <nino.padrutt@hotmail.com>
2014-11-27 00:42:57 +0100

    version + pubilsh

--- MoneyManager/MoneyManager.Shared/App.xaml.cs
+++ MoneyManager/MoneyManager.Shared/App.xaml.cs
@@ -34,7 +34,7 @@ namespace MoneyManager
         {
             BugSenseHandler.Instance.InitAndStartSession(new ExceptionManager(Current), "298c818d");
 #if DEBUG
-            BugSenseHandler.Instance.HandleWhileDebugging = false;
+            //BugSenseHandler.Instance.HandleWhileDebugging = false;
             if (Debugger.IsAttached)
             {
                 DebugSettings.EnableFrameRateCounter = true;
