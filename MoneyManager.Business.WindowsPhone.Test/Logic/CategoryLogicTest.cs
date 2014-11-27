113bdb7d97184465c1556ba479abdb12692b06f0
NPadrutt <nino.padrutt@hotmail.com>
2014-11-27 23:33:53 +0100

    update fody

--- MoneyManager.Business.WindowsPhone.Test/Logic/CategoryLogicTest.cs
+++ MoneyManager.Business.WindowsPhone.Test/Logic/CategoryLogicTest.cs
@@ -3,45 +3,53 @@ using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using MoneyManager.Business.Logic;
 using MoneyManager.DataAccess;
 using MoneyManager.DataAccess.Model;
+using SQLite.Net;
 
 namespace MoneyManager.Business.WindowsPhone.Test.Logic
 {
-  [TestClass]
-  public class CategoryLogicTest{
+    [TestClass]
+    public class CategoryLogicTest
+    {
+        [TestInitialize]
+        public void InitTest()
+        {
+            new ViewModelLocator();
 
-    [TestInitialize]
-    public void InitTest(){
-      new ViewModelLocator();
+            DatabaseLogic.CreateDatabase();
+        }
 
-        DatabaseLogic.CreateDatabase();
+        [TestMethod]
+        public void DeleteCategoryTest()
+        {
+            var category1 = new Category
+            {
+                Name = "Einkaufen"
+            };
+            var category2 = new Category
+            {
+                Name = "Sparen"
+            };
+            var category3 = new Category
+            {
+                Name = "Schule"
+            };
+
+            using (SQLiteConnection db = SqlConnectionFactory.GetSqlConnection())
+            {
+                db.Insert(category1);
+                db.Insert(category2);
+                db.Insert(category3);
+            }
+
+            CategoryLogic.DeleteCategory(category1, true);
+            CategoryLogic.DeleteCategory(category3, true);
+
+            using (SQLiteConnection db = SqlConnectionFactory.GetSqlConnection())
+            {
+                Assert.IsFalse(db.Table<Category>().Any(x => x.Id == category1.Id));
+                Assert.IsTrue(db.Table<Category>().Any(x => x.Id == category2.Id));
+                Assert.IsFalse(db.Table<Category>().Any(x => x.Id == category3.Id));
+            }
+        }
     }
-
-    [TestMethod]
-    public void DeleteCategoryTest(){
-      var category1 = new Category {
-        Name = "Einkaufen"
-      };
-      var category2 = new Category {
-        Name = "Sparen"
-      };
-      var category3 = new Category {
-        Name = "Schule"
-      };
-
-      using(var db = SqlConnectionFactory.GetSqlConnection()){
-        db.Insert(category1);
-        db.Insert(category2);
-        db.Insert(category3);
-      }
-
-      CategoryLogic.DeleteCategory(category1, true);
-      CategoryLogic.DeleteCategory(category3, true);
-
-      using(var db = SqlConnectionFactory.GetSqlConnection()){
-        Assert.IsFalse(db.Table<Category>().Any(x => x.Id == category1.Id));
-        Assert.IsTrue(db.Table<Category>().Any(x => x.Id == category2.Id));
-        Assert.IsFalse(db.Table<Category>().Any(x => x.Id == category3.Id));
-      }
-    }
-  }
 }
