9938d19682c26793e3f22f318e43d73bfbdaf3a6
NPadrutt <nino.padrutt@hotmail.com>
2014-12-03 23:07:33 +0100

    publish and version

--- MoneyManager/MoneyManager.WindowsPhone/Properties/AssemblyInfo.cs
+++ MoneyManager/MoneyManager.WindowsPhone/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.6.3.2")]
-[assembly: AssemblyFileVersion("0.6.3.2")]
+[assembly: AssemblyVersion("0.6.3.3")]
+[assembly: AssemblyFileVersion("0.6.3.3")]
 [assembly: ComVisible(false)]
