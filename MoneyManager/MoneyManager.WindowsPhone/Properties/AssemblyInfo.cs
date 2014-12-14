98500908c7ca200d0966f4e71e93f2345449661d
NPadrutt <nino.padrutt@hotmail.com>
2014-12-15 00:11:57 +0100

    bugfix for in app purchase

--- MoneyManager/MoneyManager.WindowsPhone/Properties/AssemblyInfo.cs
+++ MoneyManager/MoneyManager.WindowsPhone/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.7.0.5")]
-[assembly: AssemblyFileVersion("0.7.0.5")]
+[assembly: AssemblyVersion("0.7.0.6")]
+[assembly: AssemblyFileVersion("0.7.0.6")]
 [assembly: ComVisible(false)]
