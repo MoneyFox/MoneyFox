a616ed9cc23247c52836f16839fc17607d02e3dd
NPadrutt <nino.padrutt@hotmail.com>
2015-01-27 20:13:09 +0100

    add tests for title

---
+++ MoneyManager.Business.WindowsPhone.Test/ViewModels/AddTransactionViewModelTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MoneyManager.Business.ViewModels;
+
+namespace MoneyManager.Business.WindowsPhone.Test.ViewModels
+{
+    [TestClass]
+    public class AddTransactionViewModelTest
+    {
+        [TestMethod]
+        public void ReturnEditTitle_Test()
+        {
+            var viewModel = new AddTransactionViewModel { IsEdit = true };
+
+            Assert.AreEqual("edit", viewModel.Title);
+        }
+
+        [TestMethod]
+        public void ReturnAddTitle_Test()
+        {
+            var viewModel = new AddTransactionViewModel { IsEdit = false };
+
+            Assert.AreEqual("add", viewModel.Title);
+        }
+
+        [TestMethod]
+        public void ReturnDefault_Title()
+        {
+            var viewModel = new AddTransactionViewModel();
+
+            Assert.AreEqual("add", viewModel.Title);
+        }
+
+    }
+}
