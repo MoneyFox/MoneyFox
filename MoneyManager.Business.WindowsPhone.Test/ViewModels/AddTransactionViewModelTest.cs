ed1605af39f382db60abd7fec63fcd5ca7f0ed48
NPadrutt <nino.padrutt@hotmail.com>
2015-02-06 19:19:43 +0100

    add and fix tests

--- MoneyManager.Business.WindowsPhone.Test/ViewModels/AddTransactionViewModelTest.cs
+++ MoneyManager.Business.WindowsPhone.Test/ViewModels/AddTransactionViewModelTest.cs
@@ -1,33 +1,90 @@
-﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+﻿using Microsoft.Practices.ServiceLocation;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using MoneyManager.Business.ViewModels;
+using MoneyManager.DataAccess.DataAccess;
+using MoneyManager.DataAccess.Model;
+using MoneyManager.Foundation;
 
 namespace MoneyManager.Business.WindowsPhone.Test.ViewModels
 {
     [TestClass]
     public class AddTransactionViewModelTest
     {
-        [TestMethod]
-        public void ReturnEditTitle_Test()
+        public FinancialTransaction SelectedTransaction
         {
-            var viewModel = new AddTransactionViewModel { IsEdit = true };
+            get { return ServiceLocator.Current.GetInstance<TransactionDataAccess>().SelectedTransaction; }
+            set { ServiceLocator.Current.GetInstance<TransactionDataAccess>().SelectedTransaction = value; }
+        }
 
-            Assert.AreEqual("edit", viewModel.Title);
+        [TestInitialize]
+        public void Init()
+        {
+            new ViewModelLocator();
         }
 
         [TestMethod]
-        public void ReturnAddTitle_Test()
+        public void ReturnEditSpendingTitle_Test()
         {
-            var viewModel = new AddTransactionViewModel { IsEdit = false };
+            var viewModel = new AddTransactionViewModel { IsEdit = true, IsTransfer = true };
+            SelectedTransaction.Type = (int)TransactionType.Spending;
 
-            Assert.AreEqual("add", viewModel.Title);
+            Assert.AreEqual("edit spending", viewModel.Title);
         }
 
         [TestMethod]
-        public void ReturnDefault_Title()
+        public void ReturnEditIncomeTitle_Test()
+        {
+            var viewModel = new AddTransactionViewModel { IsEdit = true, IsTransfer = true };
+            SelectedTransaction.Type = (int)TransactionType.Income;
+
+            Assert.AreEqual("edit income", viewModel.Title);
+        }
+
+
+        [TestMethod]
+        public void ReturnEditTransferTitle_Test()
+        {
+            var viewModel = new AddTransactionViewModel { IsEdit = true, IsTransfer = true};
+            SelectedTransaction.Type = (int)TransactionType.Transfer;
+
+            Assert.AreEqual("edit transfer", viewModel.Title);
+        }
+
+        [TestMethod]
+        public void ReturnAddTransferTitle_Test()
+        {
+            var viewModel = new AddTransactionViewModel { IsEdit = false, IsTransfer = true };
+
+            Assert.AreEqual("add transfer", viewModel.Title);
+        }
+
+        [TestMethod]
+        public void ReturnSpendingDefault_Title()
         {
             var viewModel = new AddTransactionViewModel();
 
-            Assert.AreEqual("add", viewModel.Title);
+            SelectedTransaction.Type = (int) TransactionType.Spending;
+
+            Assert.AreEqual("add spending", viewModel.Title);
+        }
+        [TestMethod]
+        public void ReturnIncomeDefault_Title()
+        {
+            var viewModel = new AddTransactionViewModel();
+
+            SelectedTransaction.Type = (int) TransactionType.Income;
+
+            Assert.AreEqual("add income", viewModel.Title);
+        }
+        
+        [TestMethod]
+        public void ReturnTransferDefault_Title()
+        {
+            var viewModel = new AddTransactionViewModel();
+
+            SelectedTransaction.Type = (int)TransactionType.Transfer;
+
+            Assert.AreEqual("add transfer", viewModel.Title);
         }
 
     }
