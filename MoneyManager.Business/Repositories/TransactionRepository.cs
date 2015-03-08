96a62cc084f9df274a08cafe30b41e6625da2be2
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-03-08 01:05:29 +0100

    add some repo interface

---
+++ MoneyManager.Business/Repositories/TransactionRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using MoneyManager.DataAccess.Model;
+using MoneyManager.Foundation.OperationContracts;
+
+namespace MoneyManager.Business.Repositories {
+    public class TransactionRepository : IRepository<FinancialTransaction> {
+        public ObservableCollection<FinancialTransaction> Data {
+            get { throw new System.NotImplementedException(); }
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public FinancialTransaction Selected {
+            get { throw new System.NotImplementedException(); }
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public void Save(FinancialTransaction item) {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(FinancialTransaction item) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ MoneyManager.Foundation/OperationContracts/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MoneyManager.Foundation.OperationContracts {
+    public interface IRepository<T>{
+        /// <summary>
+        ///     All item loaded from the database
+        /// </summary>
+        ObservableCollection<T> Data { get; set; }
+
+        /// <summary>
+        ///     The selected Item
+        /// </summary>
+        T Selected { get; set; }
+
+        /// <summary>
+        ///     Will update an existing entry and add a non existing
+        /// </summary>
+        /// <param name="item">Item.</param>
+        void Save(T item);
+
+        /// <summary>
+        ///     Delete the specified item.
+        /// </summary>
+        /// <param name="item">Item.</param>
+        void Delete(T item);
+    }
+}
