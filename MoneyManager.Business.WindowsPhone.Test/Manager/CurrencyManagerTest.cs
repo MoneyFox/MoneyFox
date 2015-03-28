b86833405fd7208d2d425c4be0e4a386c7cef681
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-03-28 10:00:54 +0100

    add test data and currency service tests

--- MoneyManager.Business.WindowsPhone.Test/Manager/CurrencyManagerTest.cs
+++ MoneyManager.Business.WindowsPhone.Test/Manager/CurrencyManagerTest.cs
@@ -3,10 +3,15 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using MoneyManager.Business.Manager;
 using MoneyManager.Business.Services;
+using MoneyManager.Business.WindowsPhone.Test.Mocks;
 
 namespace MoneyManager.Business.WindowsPhone.Test.Manager {
     [TestClass]
     public class CurrencyManagerTest {
+        /// <summary>
+        /// Checks if the currency service is reachable
+        /// </summary>
+        /// <returns>async task</returns>
         [TestMethod]
         [TestCategory("Integration")]
         public async Task CurrencyManager_GetSupportedCountries_Integration() {
@@ -18,9 +23,12 @@ namespace MoneyManager.Business.WindowsPhone.Test.Manager {
         }
 
         [TestMethod]
-        [Ignore]
-        public void CurrencyManager_GetSupportedCountries() {
-            
+        public async Task CurrencyManager_GetSupportedCountries() {
+            var currencyManager = new CurrencyManager(new JsonServiceMock());
+            var list = await currencyManager.GetSupportedCountries();
+
+            Assert.IsTrue(list.Any(x => x.ID == "CH"));
+            Assert.IsTrue(list.Any(x => x.ID == "US"));
         }        
         
         [TestMethod]
---
+++ MoneyManager.Business.WindowsPhone.Test/Mocks/JsonServiceMock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+using MoneyManager.Foundation.OperationContracts;
+
+namespace MoneyManager.Business.WindowsPhone.Test.Mocks {
+    public class JsonServiceMock : IJsonService {
+        public async Task<string> GetJsonFromService(string url) {
+            string fileContent;
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(@"ms-appx:///TestData/CountryTestData.txt"));
+            using (StreamReader sRead = new StreamReader(await file.OpenStreamForReadAsync())) {
+                fileContent = await sRead.ReadToEndAsync();
+            }
+            return fileContent;
+        }
+    }
+}
