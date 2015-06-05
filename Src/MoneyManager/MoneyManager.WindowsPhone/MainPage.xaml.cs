dae867a13579d260f45bda45d4d87255fa0e44b0
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-06-05 18:41:37 +0200

    move some things

--- Src/MoneyManager/MoneyManager.WindowsPhone/MainPage.xaml.cs
+++ Src/MoneyManager/MoneyManager.WindowsPhone/MainPage.xaml.cs
@@ -6,59 +6,71 @@ using MoneyManager.Common;
 using MoneyManager.Foundation;
 using MoneyManager.Views;
 
-namespace MoneyManager {
-    public sealed partial class MainPage {
+namespace MoneyManager
+{
+    public sealed partial class MainPage
+    {
         private readonly NavigationHelper _navigationHelper;
 
-        public MainPage() {
+        public MainPage()
+        {
             InitializeComponent();
 
-            _navigationHelper = new NavigationHelper(this);
-
             ReviewHelper.AskUserForReview();
+            _navigationHelper = new NavigationHelper(this);
         }
 
-        private void AddAccountClick(object sender, RoutedEventArgs e) {
+        private void AddAccountClick(object sender, RoutedEventArgs e)
+        {
             AccountLogic.PrepareAddAccount();
             Frame.Navigate(typeof (AddAccount));
         }
 
-        private void SettingsClick(object sender, RoutedEventArgs e) {
+        private void SettingsClick(object sender, RoutedEventArgs e)
+        {
             Frame.Navigate(typeof (SettingsOverview));
         }
 
-        private void GoToAbout(object sender, RoutedEventArgs e) {
+        private void GoToAbout(object sender, RoutedEventArgs e)
+        {
             Frame.Navigate(typeof (About));
         }
 
-        private void AddIncomeClick(object sender, RoutedEventArgs e) {
+        private void AddIncomeClick(object sender, RoutedEventArgs e)
+        {
             AddTransaction(TransactionType.Income);
         }
 
-        private void AddSpendingClick(object sender, RoutedEventArgs e) {
+        private void AddSpendingClick(object sender, RoutedEventArgs e)
+        {
             AddTransaction(TransactionType.Spending);
         }
 
-        private void AddTransferClick(object sender, RoutedEventArgs e) {
+        private void AddTransferClick(object sender, RoutedEventArgs e)
+        {
             AddTransaction(TransactionType.Transfer);
         }
 
-        private static void AddTransaction(TransactionType type) {
+        private static void AddTransaction(TransactionType type)
+        {
             TransactionLogic.GoToAddTransaction(type);
             ((Frame) Window.Current.Content).Navigate(typeof (AddTransaction));
         }
 
-        private void OpenStatisticClick(object sender, RoutedEventArgs e) {
+        private void OpenStatisticClick(object sender, RoutedEventArgs e)
+        {
             ((Frame) Window.Current.Content).Navigate(typeof (StatisticView));
         }
 
         #region NavigationHelper registration
 
-        protected override void OnNavigatedTo(NavigationEventArgs e) {
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
             _navigationHelper.OnNavigatedTo(e);
         }
 
-        protected override void OnNavigatedFrom(NavigationEventArgs e) {
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
             _navigationHelper.OnNavigatedFrom(e);
         }
 
