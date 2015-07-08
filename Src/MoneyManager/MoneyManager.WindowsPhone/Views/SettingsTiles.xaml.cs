23547d29e2ae100e5829ee4a1e49f246f9b40c27
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-07-08 14:46:19 +0200

    move user control code to page.

--- Src/MoneyManager/MoneyManager.WindowsPhone/Views/SettingsTiles.xaml.cs
+++ Src/MoneyManager/MoneyManager.WindowsPhone/Views/SettingsTiles.xaml.cs
@@ -17,7 +17,60 @@ namespace MoneyManager.Views
         }
 
         public NavigationHelper NavigationHelper { get; }
-        public ObservableDictionary DefaultViewModel { get; } = new ObservableDictionary();
+
+        public TileSettingsViewModel TileSettingsView
+        {
+            get { return ServiceLocator.Current.GetInstance<TileSettingsViewModel>(); }
+        }
+
+        private void CreateIncomeTile(object sender, RoutedEventArgs e)
+        {
+            TileSettingsView.IncomeTile.Create();
+            ButtonRemoveIncomeTile.Visibility = Visibility.Visible;
+        }
+
+        private async void RemoveIncomeTile(object sender, RoutedEventArgs e)
+        {
+            TileSettingsView.IncomeTile.Remove();
+            ButtonRemoveIncomeTile.Visibility = Visibility.Collapsed;
+            await ShowUnpinnNotification();
+        }
+
+        private void CreateSpendingTile(object sender, RoutedEventArgs routedEventArgs)
+        {
+            TileSettingsView.SpendingTile.Create();
+            ButtonRemoveSpendingTile.Visibility = Visibility.Visible;
+        }
+
+        private async void RemoveSpendingTile(object sender, RoutedEventArgs e)
+        {
+            TileSettingsView.SpendingTile.Remove();
+            ButtonRemoveSpendingTile.Visibility = Visibility.Collapsed;
+            await ShowUnpinnNotification();
+        }
+
+        private void CreateTransferTile(object sender, RoutedEventArgs e)
+        {
+            TileSettingsView.TransferTile.Create();
+            ButtonRemoveTransferTile.Visibility = Visibility.Visible;
+        }
+
+        private async void RemoveTransferTile(object sender, RoutedEventArgs e)
+        {
+            TileSettingsView.TransferTile.Remove();
+            ButtonRemoveTransferTile.Visibility = Visibility.Collapsed;
+            await ShowUnpinnNotification();
+        }
+
+        private async Task ShowUnpinnNotification()
+        {
+            var dialog = new MessageDialog(Translation.GetTranslation("TileUnpinnedText"),
+                Translation.GetTranslation("TileUnpinnedTitle"));
+            dialog.Commands.Add(new UICommand(Translation.GetTranslation("OkLabel")));
+            dialog.DefaultCommandIndex = 1;
+
+            await dialog.ShowAsync();
+        }
 
         #region NavigationHelper registration
 
