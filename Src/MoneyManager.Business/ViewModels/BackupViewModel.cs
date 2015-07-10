e08a0439af695fb667691f905b4467b92f88abcc
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-07-10 13:20:30 +0200

    some modification to the backup view

--- Src/MoneyManager.Business/ViewModels/BackupViewModel.cs
+++ Src/MoneyManager.Business/ViewModels/BackupViewModel.cs
@@ -19,27 +19,25 @@ namespace MoneyManager.Business.ViewModels
             _repositoryManager = repositoryManager;
 
             LoadedCommand = new RelayCommand(Loaded);
-            LoginCommand = new RelayCommand(Login);
             BackupCommand = new RelayCommand(CreateBackup);
             RestoreCommand = new RelayCommand(RestoreBackup);
         }
 
         public RelayCommand LoadedCommand { get; private set; }
-        public RelayCommand LoginCommand { get; private set; }
         public RelayCommand BackupCommand { get; private set; }
         public RelayCommand RestoreCommand { get; private set; }
-        public bool IsConnected { get; private set; }
         public bool IsLoading { get; private set; }
         public string CreationDate { get; private set; }
 
         private async void Loaded()
         {
-            //Login();
             CreationDate = await _backup.GetCreationDateLastBackup();
         }
 
         private async void CreateBackup()
         {
+            Login();
+
             if (!await ShowOverwriteInfo())
             {
                 return;
@@ -51,16 +49,10 @@ namespace MoneyManager.Business.ViewModels
             IsLoading = false;
         }
 
-        private void Login()
-        {
-            IsLoading = true;
-            _backup.Login();
-            IsConnected = true;
-            IsLoading = false;
-        }
-
         private async void RestoreBackup()
         {
+            Login();
+
             if (!await ShowOverwriteInfo()) {
                 return;
             }
@@ -73,21 +65,23 @@ namespace MoneyManager.Business.ViewModels
             await ShowCompletionNote();
             IsLoading = false;
         }
+        
+        private void Login() {
+            IsLoading = true;
+            _backup.Login();
+            IsLoading = false;
+        }
 
         private async Task<bool> ShowOverwriteInfo()
         {
-            if (!string.IsNullOrEmpty(CreationDate))
-            {
-                var dialog = new MessageDialog(Translation.GetTranslation("OverwriteBackupMessage"),
-                    Translation.GetTranslation("OverwriteBackup"));
-                dialog.Commands.Add(new UICommand(Translation.GetTranslation("YesLabel")));
-                dialog.Commands.Add(new UICommand(Translation.GetTranslation("NoLabel")));
+            var dialog = new MessageDialog(Translation.GetTranslation("OverwriteBackupMessage"),
+                Translation.GetTranslation("OverwriteBackup"));
+            dialog.Commands.Add(new UICommand(Translation.GetTranslation("YesLabel")));
+            dialog.Commands.Add(new UICommand(Translation.GetTranslation("NoLabel")));
 
-                var result = await dialog.ShowAsync();
+            var result = await dialog.ShowAsync();
 
-                return result.Label == Translation.GetTranslation("YesLabel");
-            }
-            return true;
+            return result.Label == Translation.GetTranslation("YesLabel");
         }
 
         private async Task ShowCompletionNote()
