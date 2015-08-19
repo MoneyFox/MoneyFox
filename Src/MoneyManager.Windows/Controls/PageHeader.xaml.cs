efb290930eb06bcc5963a3b3b58807f1b0d27265
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-08-19 17:57:11 +0200

    add uap project

---
+++ Src/MoneyManager.Windows/Controls/PageHeader.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MoneyManager.Windows.Controls
+{
+    public sealed partial class PageHeader : UserControl
+    {
+        // Using a DependencyProperty as the backing store for HeaderContent.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderContentProperty =
+            DependencyProperty.Register("HeaderContent", typeof (UIElement), typeof (PageHeader),
+                new PropertyMetadata(DependencyProperty.UnsetValue));
+
+        public PageHeader()
+        {
+            InitializeComponent();
+
+            Loaded += (s, a) =>
+            {
+                AppShell.Current.TogglePaneButtonRectChanged += Current_TogglePaneButtonSizeChanged;
+                titleBar.Margin = new Thickness(AppShell.Current.TogglePaneButtonRect.Right, 0, 0, 0);
+            };
+        }
+
+        public UIElement HeaderContent
+        {
+            get { return (UIElement) GetValue(HeaderContentProperty); }
+            set { SetValue(HeaderContentProperty, value); }
+        }
+
+        private void Current_TogglePaneButtonSizeChanged(AppShell sender, Rect e)
+        {
+            titleBar.Margin = new Thickness(e.Right, 0, 0, 0);
+        }
+    }
+}
---
+++ Src/MoneyManager.Windows/NavMenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace MoneyManager.Windows
+{
+    /// <summary>
+    ///     Data to represent an item in the nav menu.
+    /// </summary>
+    public class NavMenuItem
+    {
+        public string Label { get; set; }
+        public Symbol Symbol { get; set; }
+
+        public char SymbolAsChar
+        {
+            get { return (char) Symbol; }
+        }
+
+        public Type DestPage { get; set; }
+        public object Arguments { get; set; }
+    }
+}
