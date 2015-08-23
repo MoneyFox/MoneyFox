d5b473bdddc66f3c9dee7fb7d3380f17bbc9d750
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-08-23 18:58:03 +0200

    remove no longer needed code.

--- Src/MoneyManager.Windows/Controls/SelectCategoryUserControl.xaml.cs
+++ Src/MoneyManager.Windows/Controls/SelectCategoryUserControl.xaml.cs
@@ -1,20 +1,10 @@
-﻿using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using MoneyManager.Windows.Views;
-
-namespace MoneyManager.Windows.Controls
+﻿namespace MoneyManager.Windows.Controls
 {
     public sealed partial class SelectCategoryUserControl
     {
         public SelectCategoryUserControl()
         {
             InitializeComponent();
-        }
-
-        //TODO: Handle in View Model
-        private void OpenSelectCategoryDialog(object sender, RoutedEventArgs routedEventArgs)
-        {
-            ((Frame) Window.Current.Content).Navigate(typeof (SelectCategoryView));
         }
     }
 }
