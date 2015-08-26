a84a449114c208f9964a29f9c5c56c5d7ecb82bf
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-08-26 10:41:31 +0200

    add binding properties

--- Src/MoneyManager.Core/App.cs
+++ Src/MoneyManager.Core/App.cs
@@ -1,6 +1,7 @@
 ﻿using Cirrious.CrossCore;
 using Cirrious.CrossCore.IoC;
 using Cirrious.MvvmCross.ViewModels;
+using Microsoft.Practices.ServiceLocation;
 using MoneyManager.Core.ViewModels;
 using MoneyManager.Foundation.OperationContracts;
 
@@ -44,5 +45,29 @@ namespace MoneyManager.Core
             RegisterAppStart<MainViewModel>();
         }
 
+        //This properties are used to bind the view model to the datacontext directly in xaml (windows only)
+        public MainViewModel MainViewModel => Mvx.Resolve<MainViewModel>();
+
+        public AddAccountViewModel AddAccountViewModel => Mvx.Resolve<AddAccountViewModel>();
+
+        public AccountListUserControlViewModel AccountListUserControlViewModel => Mvx.Resolve<AccountListUserControlViewModel>();
+
+        public AddTransactionViewModel AddTransactionViewModel => Mvx.Resolve<AddTransactionViewModel>();
+
+        public BalanceViewModel BalanceViewModel => Mvx.Resolve<BalanceViewModel>();
+
+        public CategoryListViewModel CategoryListViewModel => Mvx.Resolve<CategoryListViewModel>();
+
+        public TransactionListViewModel TransactionListViewModel => Mvx.Resolve<TransactionListViewModel>();
+
+        public TileSettingsViewModel TileSettingsViewModel => Mvx.Resolve<TileSettingsViewModel>();
+
+        public SettingDefaultsViewModel SettingDefaultsViewModel => Mvx.Resolve<SettingDefaultsViewModel>();
+
+        public StatisticViewModel StatisticViewModel => Mvx.Resolve<StatisticViewModel>();
+
+        public BackupViewModel BackupViewModel => Mvx.Resolve<BackupViewModel>();
+
+        public SelectCategoryViewModel SelectCategoryViewModel => Mvx.Resolve<SelectCategoryViewModel>();
     }
 }
