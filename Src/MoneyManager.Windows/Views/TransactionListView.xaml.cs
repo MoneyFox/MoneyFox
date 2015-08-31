b766e3cc46b835564ef4b4f51cb19b31e035ded3
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-08-31 22:30:38 +0200

    remove unused code

--- Src/MoneyManager.Windows/Views/TransactionListView.xaml.cs
+++ Src/MoneyManager.Windows/Views/TransactionListView.xaml.cs
@@ -1,9 +1,5 @@
-﻿using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Cirrious.CrossCore;
-using MoneyManager.Core.Logic;
+﻿using Cirrious.CrossCore;
 using MoneyManager.Core.ViewModels;
-using MoneyManager.Foundation;
 
 namespace MoneyManager.Windows.Views
 {
@@ -13,30 +9,6 @@ namespace MoneyManager.Windows.Views
         {
             InitializeComponent();
             DataContext = Mvx.Resolve<TransactionListViewModel>();
-        }
-
-        //TODO: move to view model
-        private void AddSpendingClick(object sender, RoutedEventArgs e)
-        {
-            AddTransaction(TransactionType.Spending);
-        }
-
-        //TODO: move to view model
-        private void AddIncomeClick(object sender, RoutedEventArgs e)
-        {
-            AddTransaction(TransactionType.Income);
-        }
-
-        //TODO: move to view model
-        private void AddTransferClick(object sender, RoutedEventArgs e)
-        {
-            AddTransaction(TransactionType.Transfer);
-        }
-
-        private static void AddTransaction(TransactionType type)
-        {
-            TransactionLogic.GoToAddTransaction(type, true);
-            ((Frame) Window.Current.Content).Navigate(typeof (ModifyTransactionView));
         }
     }
 }
