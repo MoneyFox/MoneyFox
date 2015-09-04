ea76ce361153f32464f0f1d020947c1f2fb4a3bc
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-09-04 13:30:36 +0200

    add an easy test

---
+++ Src/MoneyManager.Core.Tests/ViewModels/ModifyAccountViewModelTests.cs
@@ -0,0 +1,43 @@
+﻿using MoneyManager.Core.DataAccess;
+using MoneyManager.Core.ViewModels;
+using MoneyManager.Foundation;
+using MoneyManager.Foundation.Model;
+using MoneyManager.Foundation.OperationContracts;
+using Moq;
+using Xunit;
+
+namespace MoneyManager.Core.Tests.ViewModels
+{
+    public class ModifyAccountViewModelTests
+    {
+        [Fact]
+        public void Title_EditAccount_CorrectTitle()
+        {
+            string accountname = "Sparkonto";
+
+            var accountRepositorySetup = new Mock<IRepository<Account>>();
+            accountRepositorySetup.SetupGet(x => x.Selected).Returns(new Account {Id = 2, Name = accountname});
+
+            var viewmodel = new ModifyAccountViewModel(accountRepositorySetup.Object, new BalanceViewModel(
+                accountRepositorySetup.Object, new Mock<ITransactionRepository>().Object, new SettingDataAccess())) {IsEdit = true};
+
+            viewmodel.Title.ShouldBe("Edit " + accountname);
+        }
+
+        [Fact]
+        public void Title_AddAccount_CorrectTitle()
+        {
+            string accountname = "Sparkonto";
+
+            var accountRepositorySetup = new Mock<IRepository<Account>>();
+            accountRepositorySetup.SetupGet(x => x.Selected).Returns(new Account { Id = 2, Name = accountname });
+
+            var viewmodel = new ModifyAccountViewModel(accountRepositorySetup.Object, new BalanceViewModel(
+                accountRepositorySetup.Object, new Mock<ITransactionRepository>().Object, new SettingDataAccess()))
+            { IsEdit = false };
+
+            viewmodel.Title.ShouldBe(Strings.AddAccountTitle);
+        }
+
+    }
+}
