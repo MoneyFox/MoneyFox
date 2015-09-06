6905edd9ee878b47402371e00432b90e0618930c
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-09-06 17:20:01 +0200

    add transaction list

---
+++ Src/MoneyManager.Droid/Activities/TransactionListActivity.cs
@@ -0,0 +1,49 @@
+using Android.App;
+using Android.OS;
+using Android.Views;
+using Cirrious.MvvmCross.Droid.FullFragging.Views;
+using MoneyManager.Core.ViewModels;
+
+namespace MoneyManager.Droid.Activities
+{
+    [Activity(Label = "TransactionListActivity")]
+    public class TransactionListActivity : MvxActivity
+    {
+
+        public new TransactionListViewModel ViewModel
+        {
+            get { return (TransactionListViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+
+        /// <summary>
+        ///     Raises the create event.
+        /// </summary>
+        /// <param name="bundle">Saved instance state.</param>
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.TransactionListLayout);
+            ActionBar.SetDisplayHomeAsUpEnabled(true);
+            ActionBar.Title = ViewModel.Title;
+        }
+
+        /// <summary>
+        ///     This hook is called whenever an item in your options menu is selected.
+        /// </summary>
+        /// <param name="item">The menu item that was selected.</param>
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    Finish();
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+    }
+}
