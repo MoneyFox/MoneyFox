fcc3947006a3264a4166870e80aefea9ee0fe73f
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-09-09 18:41:44 +0200

    add some unit tests for converter

---
+++ Src/MoneyManager.Core.Tests/Converter/TransactionAmountConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using MoneyManager.Core.Converter;
+using MoneyManager.Foundation;
+using MoneyManager.Foundation.Model;
+using Xunit;
+
+namespace MoneyManager.Core.Tests.Converter
+{
+    public class TransactionAmountConverterTests
+    {
+        [Theory]
+        [InlineData(TransactionType.Spending, "-")]
+        [InlineData(TransactionType.Income, "+")]
+        public void Converter_Transaction_AmountSign(TransactionType type, string result)
+        {
+            new TransactionAmountConverter().Convert(new FinancialTransaction {Amount = 80, Type = (int) type}, null, null, null).ShouldBe(result);
+        }
+
+        [Fact]
+        public void Converter_TransferSameAccount_Minus()
+        {
+            var account = new Account
+            {
+                Id = 4,
+                CurrentBalance = 400
+            };
+
+            new TransactionAmountConverter()
+                .Convert(new FinancialTransaction
+                {
+                    Amount = 80,
+                    Type = (int) TransactionType.Transfer,
+                    ChargedAccount = account
+                }, null, account, null)
+                .ShouldBe("-");
+        }
+
+        [Fact]
+        public void Converter_TransferSameAccount_Plus()
+        {
+            new TransactionAmountConverter()
+                .Convert(new FinancialTransaction
+                {
+                    Amount = 80,
+                    Type = (int) TransactionType.Transfer,
+                    ChargedAccount = new Account
+                    {
+                        Id = 4,
+                        CurrentBalance = 400
+                    }
+                }, null, new Account(), null)
+                .ShouldBe("+");
+        }
+    }
+}
--- Src/MoneyManager.Core/Converter/TransactionAmountConverter.cs
+++ Src/MoneyManager.Core/Converter/TransactionAmountConverter.cs
@@ -1,25 +1,20 @@
 ﻿using System;
 using System.Globalization;
-using Cirrious.CrossCore;
 using Cirrious.CrossCore.Converters;
 using MoneyManager.Foundation;
 using MoneyManager.Foundation.Model;
-using MoneyManager.Foundation.OperationContracts;
 
 namespace MoneyManager.Core.Converter
 {
     public class TransactionAmountConverter : IMvxValueConverter
     {
-        //TODO: remove this and refactor with converter parameter
-        private Account selectedAccount => Mvx.Resolve<IRepository<Account>>().Selected;
-
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var transaction = (FinancialTransaction) value;
 
             if (transaction.Type == (int)TransactionType.Transfer)
             {
-                return selectedAccount == transaction.ChargedAccount
+                return (Account) parameter == transaction.ChargedAccount
                     ? "-"
                     : "+";
             }
