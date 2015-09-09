9973ea19610692798fc1ab011cb8d195acb5cdb0
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-09-09 18:25:10 +0200

    add converter to format date nicely

---
+++ Src/MoneyManager.Core.Tests/Converter/DateFormatConverterTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MoneyManager.Core.Converter;
+using MoneyManager.Foundation;
+using Xunit;
+
+namespace MoneyManager.Core.Tests.Converter
+{
+    public class DateFormatConverterTests
+    {
+        [Fact]
+        public void Convert_DateTime_ValidString()
+        {
+            new DateTimeFormatConverter().Convert(new DateTime(2015,09,15,14,56,48), null, null, null).ShouldBe("Dienstag, 15. September 2015");
+        }
+    }
+}
