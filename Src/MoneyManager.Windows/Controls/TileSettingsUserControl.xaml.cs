2d2bad9c945491b63f604eb1822cdaef4bd0a7cf
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-09-10 00:04:13 +0200

    start with settings, try to publish a new android build

---
+++ Src/MoneyManager.Windows/Controls/TileSettingsUserControl.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyManager.Windows.Controls
+{
+    public sealed partial class TileSettingsUserControl
+    {
+        public TileSettingsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
