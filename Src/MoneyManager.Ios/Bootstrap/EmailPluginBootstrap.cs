3fb6d0a5a5ac7fe434967f26720b5a15bbf8976f
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-09-12 00:46:35 +0200

    Revert "revert"
    This reverts commit c58036982cab659dee09b3e8d60af22225e790b7.

---
+++ Src/MoneyManager.Ios/Bootstrap/EmailPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace MoneyManager.Ios.Bootstrap
+{
+    public class EmailPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.Email.PluginLoader, MvvmCross.Plugins.Email.Touch.Plugin>
+    {
+    }
+}
