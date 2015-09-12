d07a1d4c608d32fc1ee93e34ee77da98fbb66382
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-09-12 13:01:28 +0200

    remove unused plughin

--- Src/MoneyManager.Droid/Bootstrap/VisibilityPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace MoneyManager.Droid.Bootstrap
-{
-    public class VisibilityPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Visibility.PluginLoader>
-    {
-    }
-}
