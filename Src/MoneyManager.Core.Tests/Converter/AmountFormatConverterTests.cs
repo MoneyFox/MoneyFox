e885fc14909c476db1ece62329ae4c651466b3e3
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-09-15 23:39:32 +0200

    change tests

--- Src/MoneyManager.Core.Tests/Converter/AmountFormatConverterTests.cs
+++ Src/MoneyManager.Core.Tests/Converter/AmountFormatConverterTests.cs
@@ -7,8 +7,8 @@ namespace MoneyManager.Core.Tests.Converter
     public class AmountFormatConverterTests
     {
         [Theory]
-        [InlineData(123.45, "Fr. 123.45")]
-        [InlineData(123, "Fr. 123.00")]
+        [InlineData(123.45, "CHF 123.45")]
+        [InlineData(123, "CHF 123.00")]
         public void Convert_DateTime_ValidString(double amount, string result)
         {
             new AmountFormatConverter().Convert(amount, null, null, null).ShouldBe(result);
