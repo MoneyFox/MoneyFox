679dafd0701637802ff48ad77ac6605ac77abe28
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-09-29 16:31:06 +0200

    add some comments

--- Src/MoneyManager.Foundation/Interfaces/ILocalize.cs
+++ Src/MoneyManager.Foundation/Interfaces/ILocalize.cs
@@ -4,6 +4,10 @@ namespace MoneyManager.Foundation.Interfaces
 {
     public interface ILocalize
     {
+        /// <summary>
+        ///     Returns the current culture in a standardised.
+        /// </summary>
+        /// <returns>Current Culutre.</returns>
         CultureInfo GetCurrentCultureInfo();
     }
 }
--- Src/MoneyManager.Foundation/Interfaces/IStoreFeatures.cs
+++ Src/MoneyManager.Foundation/Interfaces/IStoreFeatures.cs
@@ -2,6 +2,9 @@ namespace MoneyManager.Foundation.Interfaces
 {
     public interface IStoreFeatures
     {
+        /// <summary>
+        ///     Opens the store and allows rating the app.
+        /// </summary>
         void RateApp();
     }
 }
