93b6682332cda4caa61f07fe3e753deb595a52fe
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-09-30 13:06:54 +0200

    add secure storage plugin.

---
+++ Src/MoneyManager.Droid/Bootstrap/SecureStoragePluginBootstrap.cs
@@ -0,0 +1,11 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace MoneyManager.Droid.Bootstrap
+{
+	public class SecureStoragePluginBootstrap 
+		: MvxLoaderPluginBootstrapAction<Beezy.MvvmCross.Plugins.SecureStorage.PluginLoader>
+
+	{
+	}
+}
+
