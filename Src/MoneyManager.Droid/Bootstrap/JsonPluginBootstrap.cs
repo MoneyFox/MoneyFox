410f6e5d3ac4f2d31c58cc1034c023b76a4d32e0
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-10-21 11:09:27 +0200

    update dependencies

---
+++ Src/MoneyManager.Droid/Bootstrap/JsonPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace MoneyManager.Droid.Bootstrap
+{
+    public class JsonPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Json.PluginLoader>
+    {
+    }
+}
