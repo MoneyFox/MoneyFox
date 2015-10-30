7a45dda11b82b963520ba89aa0d20e2bae4fa509
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-10-30 18:49:51 +0100

    tweaking the password settings

--- Src/MoneyManager.Core/ViewModels/PasswordUserControlViewModel.cs
+++ Src/MoneyManager.Core/ViewModels/PasswordUserControlViewModel.cs
@@ -1,5 +1,9 @@
-﻿using Cirrious.MvvmCross.ViewModels;
+﻿using System;
+using Beezy.MvvmCross.Plugins.SecureStorage;
+using Cirrious.MvvmCross.ViewModels;
 using MoneyManager.DataAccess;
+using MoneyManager.Foundation.Interfaces;
+using MoneyManager.Localization;
 using PropertyChanged;
 
 namespace MoneyManager.Core.ViewModels
@@ -7,11 +11,17 @@ namespace MoneyManager.Core.ViewModels
     [ImplementPropertyChanged]
     public class PasswordUserControlViewModel : BaseViewModel
     {
+        private const string PASSWORD_KEY = "password";
+        private readonly IDialogService dialogService;
+        private readonly IMvxProtectedData protectedData;
         private readonly SettingDataAccess settings;
 
-        public PasswordUserControlViewModel(SettingDataAccess settings)
+        public PasswordUserControlViewModel(SettingDataAccess settings,
+            IMvxProtectedData protectedData, IDialogService dialogService)
         {
             this.settings = settings;
+            this.protectedData = protectedData;
+            this.dialogService = dialogService;
         }
 
         /// <summary>
@@ -38,9 +48,43 @@ namespace MoneyManager.Core.ViewModels
         /// </summary>
         public MvxCommand SavePasswordCommand => new MvxCommand(SavePassword);
 
+        /// <summary>
+        ///     Loads the password from the secure storage
+        /// </summary>
+        public MvxCommand LoadCommand => new MvxCommand(LoadData);
+
+        /// <summary>
+        ///     Remove the password from the secure storage
+        /// </summary>
+        public MvxCommand UnloadedCommand => new MvxCommand(RemovePassword);
+
         private void SavePassword()
         {
-            throw new System.NotImplementedException();
+            if (Password != PasswordConfirmation)
+            {
+                dialogService.ShowMessage(Strings.PasswordConfirmationWrongTitle,
+                    Strings.PasswordConfirmationWrongMessage);
+                return;
+            }
+
+            IsPasswortActive = true;
+            protectedData.Protect(PASSWORD_KEY, Password);
+
+            dialogService.ShowMessage(Strings.PasswordSavedTitle, Strings.PasswordSavedMessage);
+        }
+
+        private void LoadData()
+        {
+            if (IsPasswortActive)
+            {
+                Password = protectedData.Unprotect(PASSWORD_KEY);
+                PasswordConfirmation = protectedData.Unprotect(PASSWORD_KEY);
+            }
+        }
+
+        private void RemovePassword()
+        {
+            protectedData.Remove(PASSWORD_KEY);
         }
     }
 }
