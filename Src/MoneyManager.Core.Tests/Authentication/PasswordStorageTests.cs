6a9b4230159653f6b1330ceb21831438ba8199f9
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-11-03 17:01:33 +0100

    add unit tests

---
+++ Src/MoneyManager.Core.Tests/Authentication/PasswordStorageTests.cs
@@ -0,0 +1,42 @@
+﻿using Beezy.MvvmCross.Plugins.SecureStorage;
+using MoneyManager.Core.Authentication;
+using MoneyManager.TestFoundation;
+using Moq;
+using Xunit;
+
+namespace MoneyManager.Core.Tests.Authentication
+{
+    public class PasswordStorageTests
+    {
+        [Theory]
+        [InlineData("This is a password")]
+        [InlineData("+\"*%&(()=")]
+        public void SavePassword_StringPassword_SavedPassword(string passwordString)
+        {
+            var resultPassword = string.Empty;
+            var resultKey = string.Empty;
+
+            var mockSetup = new Mock<IMvxProtectedData>();
+            mockSetup.Setup(x => x.Protect(It.IsAny<string>(), It.IsAny<string>())).Callback((string key, string password) =>
+            {
+                resultKey = key;
+                resultPassword = password;
+            });
+
+            new PasswordStorage(mockSetup.Object).SavePassword(passwordString);
+
+            resultKey.ShouldBe("password");
+            resultPassword.ShouldBe(passwordString);
+        }
+
+        [Fact]
+        public void LoadPassword_ReturnSavedPassword()
+        {
+            const string expectedPassword = "fooo";
+            var mockSetup = new Mock<IMvxProtectedData>();
+            mockSetup.Setup(x => x.Unprotect(It.Is<string>(y => y == "password"))).Returns(expectedPassword);
+
+            new PasswordStorage(mockSetup.Object).LoadPassword().ShouldBe(expectedPassword);
+        }
+    }
+}
