3e9cbaa6308fc0c228f2923c1069f25e96654c6a
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-12-15 19:00:23 +0100

    style code

--- Src/MoneyManager.Core/Helpers/StreamExtension.cs
+++ Src/MoneyManager.Core/Helpers/StreamExtension.cs
@@ -17,9 +17,9 @@ namespace MoneyManager.Core.Helpers
 
             try
             {
-                byte[] readBuffer = new byte[4096];
+                var readBuffer = new byte[4096];
 
-                int totalBytesRead = 0;
+                var totalBytesRead = 0;
                 int bytesRead;
 
                 while ((bytesRead = stream.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0)
@@ -28,26 +28,27 @@ namespace MoneyManager.Core.Helpers
 
                     if (totalBytesRead == readBuffer.Length)
                     {
-                        int nextByte = stream.ReadByte();
+                        var nextByte = stream.ReadByte();
                         if (nextByte != -1)
                         {
-                            byte[] temp = new byte[readBuffer.Length * 2];
+                            var temp = new byte[readBuffer.Length*2];
                             Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
-                            Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            Buffer.SetByte(temp, totalBytesRead, (byte) nextByte);
                             readBuffer = temp;
                             totalBytesRead++;
                         }
                     }
                 }
 
-                byte[] buffer = readBuffer;
+                var buffer = readBuffer;
                 if (readBuffer.Length != totalBytesRead)
                 {
                     buffer = new byte[totalBytesRead];
                     Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
                 }
                 return buffer;
-            } finally
+            }
+            finally
             {
                 if (stream.CanSeek)
                 {
