5d679d82cca0b8b45bd578f2a06601bad56c1930
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-12-17 22:14:16 +0100

    update onedrive sdk, again remove obsolet code

--- Src/MoneyManager.Ios/Setup.cs
+++ Src/MoneyManager.Ios/Setup.cs
@@ -1,16 +1,12 @@
 ﻿using System.Reflection;
 using Autofac;
-using Cirrious.CrossCore;
 using Cirrious.CrossCore.IoC;
 using Cirrious.MvvmCross.Touch.Platform;
 using Cirrious.MvvmCross.Touch.Views.Presenters;
 using Cirrious.MvvmCross.ViewModels;
 using MoneyManager.Core;
 using MoneyManager.Core.ViewModels;
-using MoneyManager.Foundation.Interfaces;
 using MoneyManager.Localization;
-using SQLite.Net.Interop;
-using SQLite.Net.Platform.XamarinIOS;
 using UIKit;
 
 namespace MoneyManager.Ios
@@ -25,18 +21,6 @@ namespace MoneyManager.Ios
         public Setup(IMvxApplicationDelegate applicationDelegate, IMvxTouchViewPresenter presenter)
             : base(applicationDelegate, presenter)
         {
-        }
-
-        protected override void InitializeFirstChance()
-        {
-            base.InitializeFirstChance();
-
-            Mvx.RegisterType<ISQLitePlatform, SQLitePlatformIOS>();
-            Mvx.RegisterType<IDatabasePath, DatabasePath>();
-            Mvx.RegisterType<IDialogService, DialogService>();
-            Mvx.RegisterType<IAppInformation, AppInformation>();
-            Mvx.RegisterType<IStoreFeatures, StoreFeatures>();
-            Mvx.RegisterType<ILocalSettings, LocalSettings>();
         }
 
         protected override IMvxIoCProvider CreateIocProvider()
