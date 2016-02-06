028f5a43384dfddb6b638ad420ced68067dfa1bc
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-06 15:51:14 +0100

    remove migration logic

--- Src/MoneyManager.Foundation/SqliteConnectionCreator.cs
+++ Src/MoneyManager.Foundation/SqliteConnectionCreator.cs
@@ -23,66 +23,7 @@ namespace MoneyManager.Foundation
         /// <returns>Established SQLiteConnection.</returns>
         public SQLiteConnection GetConnection()
         {
-            var db = connectionFactory.GetConnection(new SqLiteConfig(OneDriveAuthenticationConstants.DB_NAME, false));
-
-            var info = db.GetTableInfo("FinancialTransactions");
-            if (info.Any())
-            {
-                CreateDb();
-                MigrateTransactions(db);
-
-                db.DropTable<FinancialTransaction>();
-                db.DropTable<RecurringTransaction>();
-            }
-
-            return db;
-        }
-
-        private void MigrateTransactions(SQLiteConnection db)
-        {
-            var transactions = db.Table<FinancialTransaction>().ToList();
-            var recurruringTransactions = db.Table<RecurringTransaction>().ToList();
-
-            foreach (var trans in transactions)
-            {
-                var payment = new Payment
-                {
-                    Id = trans.Id,
-                    ChargedAccountId = trans.ChargedAccountId,
-                    TargetAccountId = trans.TargetAccountId,
-                    CategoryId = trans.CategoryId,
-                    Date = trans.Date,
-                    Amount = trans.Amount,
-                    IsCleared = trans.IsCleared,
-                    Type = trans.Type,
-                    Note = trans.Note,
-                    IsRecurring = trans.IsRecurring,
-                    RecurringPaymentId = trans.ReccuringTransactionId
-                };
-
-                if (trans.IsRecurring)
-                {
-                    trans.RecurringTransaction = recurruringTransactions.First(x => x.Id == trans.ReccuringTransactionId);
-
-                    payment.RecurringPayment = new RecurringPayment
-                    {
-                        Id = trans.RecurringTransaction.Id,
-                        ChargedAccountId = trans.RecurringTransaction.ChargedAccountId,
-                        TargetAccountId = trans.RecurringTransaction.TargetAccountId,
-                        CategoryId = trans.RecurringTransaction.CategoryId,
-                        StartDate = trans.RecurringTransaction.StartDate,
-                        EndDate = trans.RecurringTransaction.EndDate,
-                        IsEndless = trans.RecurringTransaction.IsEndless,
-                        Amount = trans.RecurringTransaction.Amount,
-                        Type = trans.RecurringTransaction.Type,
-                        Recurrence = trans.RecurringTransaction.Recurrence,
-                        Note = trans.RecurringTransaction.Note
-                    };
-                }
-
-                db.Insert(payment.RecurringPayment);
-                db.Insert(payment);
-            }
+            return connectionFactory.GetConnection(new SqLiteConfig(OneDriveAuthenticationConstants.DB_NAME, false));
         }
 
         private void CreateDb()
