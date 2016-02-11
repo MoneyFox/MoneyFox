a210b93101ae40fc08a34b9b6c45026e25851836
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-11 01:27:05 +0100

    add a selector view for windows

--- Src/MoneyManager.Windows/Views/StatisticSelectorView.xaml.cs
+++ Src/MoneyManager.Windows/Views/StatisticSelectorView.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace MoneyManager.Windows.Views
+﻿using MoneyManager.Core.ViewModels;
+using MvvmCross.Platform;
+
+namespace MoneyManager.Windows.Views
 {
     public sealed partial class StatisticSelectorView
     {
         public StatisticSelectorView()
         {
             InitializeComponent();
+
+            DataContext = Mvx.Resolve<StatisticSelectorViewModel>();
         }
     }
 }
