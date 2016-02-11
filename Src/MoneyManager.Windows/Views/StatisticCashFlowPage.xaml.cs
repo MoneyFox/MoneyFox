1642aa9398fd6420278e3019602635ba6a21a03b
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-12 00:41:39 +0100

    implement separat pages

---
+++ Src/MoneyManager.Windows/Views/StatisticCashFlowPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml;
+using MoneyManager.Windows.Dialogs;
+
+namespace MoneyManager.Windows.Views
+{
+    public sealed partial class StatisticCashFlowPage
+    {
+        public StatisticCashFlowPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SetDate(object sender, RoutedEventArgs e)
+        {
+            await new SelectDateRangeDialog().ShowAsync();
+        }
+    }
+}
---
+++ Src/MoneyManager.Windows/Views/StatisticCategorySpreadingPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml;
+using MoneyManager.Windows.Dialogs;
+
+namespace MoneyManager.Windows.Views
+{
+    public sealed partial class StatisticCategorySpreadingPage
+    {
+        public StatisticCategorySpreadingPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SetDate(object sender, RoutedEventArgs e)
+        {
+            await new SelectDateRangeDialog().ShowAsync();
+        }
+    }
+}
