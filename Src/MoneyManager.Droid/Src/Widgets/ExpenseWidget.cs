bdc2d9c7adbab50fa1ea9a42b9e6af9314dd916d
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-11 16:11:09 +0100

    add widget classes on android

---
+++ Src/MoneyManager.Droid/Src/Widgets/ExpenseWidget.cs
@@ -0,0 +1,25 @@
+﻿using MoneyManager.Foundation.Interfaces.Shotcuts;
+using System.Threading.Tasks;
+
+namespace MoneyManager.Droid.Src.Widgets
+{
+    public class ExpenseWidget : ISpendingShortcut
+    {
+        public Task CreateShortCut()
+        {
+            throw new System.NotImplementedException();
+        }
+        public Task RemoveShortcut()
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool IsShortcutExisting
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
+
