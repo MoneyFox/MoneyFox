b8e497f0c2cdb723c4ce876d12363c14d0f91e6e
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-11 09:15:29 +0100

    fox bug with registering fragments

--- Src/MoneyManager.Droid/Fragments/SettingsShortcutsFragment.cs
+++ Src/MoneyManager.Droid/Fragments/SettingsShortcutsFragment.cs
@@ -6,7 +6,7 @@ using MoneyManager.Droid.Fragments;
 namespace MoneyManager.Droid
 {
     [MvxFragment(typeof(MainViewModel), Resource.Id.content_frame)]
-    [Register("moneymanager.droid.fragments.SettingsShortcutsFragment")]
+	[Register("moneymanager.droid.fragments.SettingsShortcutsFragment")]
     public class SettingsShortcutsFragment : BaseFragment<TileSettingsViewModel>
     {
         protected override int FragmentId => Resource.Layout.fragment_settings_shortcuts;
