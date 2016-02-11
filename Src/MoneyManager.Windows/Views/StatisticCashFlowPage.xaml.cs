291b3c306a9979017086925e0841af290a5534ab
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-12 00:57:23 +0100

    sett plot view null when leaving

--- Src/MoneyManager.Windows/Views/StatisticCashFlowPage.xaml.cs
+++ Src/MoneyManager.Windows/Views/StatisticCashFlowPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
 using MoneyManager.Windows.Dialogs;
 
 namespace MoneyManager.Windows.Views
@@ -14,6 +15,13 @@ namespace MoneyManager.Windows.Views
         private async void SetDate(object sender, RoutedEventArgs e)
         {
             await new SelectDateRangeDialog().ShowAsync();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            CashFlowPlotView = null;
+
+            base.OnNavigatedFrom(e);
         }
     }
 }
