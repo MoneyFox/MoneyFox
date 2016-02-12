884ca5c8bfc1e2232390a9fa55613b5962044094
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-12 14:14:52 +0100

    some statistic fixes

--- Src/MoneyManager.Droid/Fragments/StatisticCashFlowFragment.cs
+++ Src/MoneyManager.Droid/Fragments/StatisticCashFlowFragment.cs
@@ -1,6 +1,9 @@
 using Android.Runtime;
 using MoneyManager.Core.ViewModels;
 using MvvmCross.Droid.Support.V7.Fragging.Attributes;
+using Android.Views;
+using Android.OS;
+using OxyPlot.Xamarin.Android;
 
 namespace MoneyManager.Droid.Fragments
 {
@@ -9,15 +12,25 @@ namespace MoneyManager.Droid.Fragments
     public class StatisticCashFlowFragment :  BaseFragment<StatisticCashFlowViewModel>
     {
         protected override int FragmentId => Resource.Layout.fragment_graphical_statistic;
+      
+        private PlotView plotModel;
 
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = base.OnCreateView(inflater, container, savedInstanceState);
 
+            plotModel = view.FindViewById<PlotView>(Resource.Id.plotViewModel);
+            plotModel.Model = ViewModel.CashFlowModel;
+
+            return view;
+        }
 
         public override void OnStart()
         {
             OnResume();
 
             ViewModel.LoadCommand.Execute();
+            plotModel.Model = ViewModel.CashFlowModel;
         }
-
     }
 }
