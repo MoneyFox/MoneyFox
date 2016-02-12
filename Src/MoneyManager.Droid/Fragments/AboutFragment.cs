369e821ca125d2e6251404e87bd30e0119a8df7e
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-12 11:42:06 +0100

    try to fix #764

--- Src/MoneyManager.Droid/Fragments/AboutFragment.cs
+++ Src/MoneyManager.Droid/Fragments/AboutFragment.cs
@@ -1,6 +1,8 @@
 using Android.Runtime;
 using MoneyManager.Core.ViewModels;
 using MvvmCross.Droid.Support.V7.Fragging.Attributes;
+using Android.Views;
+using Android.OS;
 
 namespace MoneyManager.Droid.Fragments
 {
@@ -9,5 +11,14 @@ namespace MoneyManager.Droid.Fragments
     public class AboutFragment : BaseFragment<AboutViewModel>
     {
         protected override int FragmentId => Resource.Layout.fragment_about;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = base.OnViewCreated(inflater, container, savedInstanceState);
+
+            RetainInstance = true;
+
+            return view;
+        }
     }
 }
