167cb2ba5e93ef263c11cf117845a3737c40ab85
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-22 00:00:14 +0100

    startet implement a general settings tab with backup settings

---
+++ Src/MoneyManager.Core/ViewModels/SettingsGeneralViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MoneyManager.DataAccess;
+
+namespace MoneyManager.Core.ViewModels
+{
+    public class SettingsGeneralViewModel : BaseViewModel
+    {
+        private readonly SettingDataAccess settingsDataAccess;
+
+        public SettingsGeneralViewModel(SettingDataAccess settingsDataAccess)
+        {
+            this.settingsDataAccess = settingsDataAccess;
+        }
+    }
+}
---
+++ Src/MoneyManager.Windows/Views/UserControls/SettingsGeneralUserControl.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyManager.Windows.Views.UserControls
+{
+    public sealed partial class SettingsGeneralUserControl
+    {
+        public SettingsGeneralUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
