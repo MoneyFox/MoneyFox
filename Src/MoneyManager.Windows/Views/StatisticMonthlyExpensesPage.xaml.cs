1f36424569e0a01005a36a335ea3b1249843cec2
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-23 19:30:50 +0100

    add page for windows for monthly expenses

---
+++ Src/MoneyManager.Windows/Views/StatisticMonthlyExpensesPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+using MoneyManager.Core.ViewModels;
+using MoneyManager.Windows.Dialogs;
+
+namespace MoneyManager.Windows.Views
+{
+    public sealed partial class StatisticMonthlyExpensesPage : IDisposable
+    {
+        public StatisticMonthlyExpensesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            ExpensePlotView.Model = (DataContext as StatisticExpensesMonthlyViewModel)?.MonthlyExpensesModel;
+        }
+
+        private async void SetDate(object sender, RoutedEventArgs e)
+        {
+            await new SelectDateRangeDialog().ShowAsync();
+            (DataContext as StatisticCashFlowViewModel)?.LoadCommand.Execute();
+        }
+
+        public void Dispose()
+        {
+            ExpensePlotView.Model = null;
+        }
+    }
+}
