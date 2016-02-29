43f08c1a3aa6e3b5b0220ab54ff92d8403f1d434
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-29 21:51:05 +0100

    moved string resources to foundation, remove xamarin insihts

--- Src/MoneyManager.DataAccess/PaymentDataAccess.cs
+++ Src/MoneyManager.DataAccess/PaymentDataAccess.cs
@@ -6,6 +6,7 @@ using MoneyManager.Foundation;
 using MoneyManager.Foundation.Interfaces;
 using MoneyManager.Foundation.Model;
 using PropertyChanged;
+using SQLite.Net;
 
 namespace MoneyManager.DataAccess
 {
