744aa0c95dec6433588d07f2154f5ce44e58b86d
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-03-10 18:25:15 +0100

    update insights

--- Src/MoneyFox.Windows/App.xaml.cs
+++ Src/MoneyFox.Windows/App.xaml.cs
@@ -41,8 +41,6 @@ namespace MoneyFox.Windows
 #if !DEBUG
             WindowsAppInitializer.InitializeAsync();
 #endif
-            WindowsAppInitializer.InitializeAsync();
-
 
             Suspending += OnSuspending;
         }
