d7fccf07394152d7edc1c3c5854c5d8108c94cb4
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-03-31 22:18:49 +0200

    fix wrong types and typo

--- Src/MoneyFox.Core/DataAccess/MoneyFoxDataContext.cs
+++ Src/MoneyFox.Core/DataAccess/MoneyFoxDataContext.cs
@@ -9,13 +9,13 @@ namespace MoneyFox.Core.DataAccess
     public class MoneyFoxDataContext : DbContext
     {
         public DbSet<Account> Accounts { get; set; }
-        public DbSet<PaymentViewModel> PaymentViewModels { get; set; }
+        public DbSet<Payment> Payments { get; set; }
         public DbSet<RecurringPayment> RecurringPaymentViewModels { get; set; }
         public DbSet<Category> Categories { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Filename=monefox.sqlite");
+            optionsBuilder.UseSqlite("Filename=moneyfox.sqlite");
         }
     }
 }
