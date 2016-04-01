4e74d45e2d89c23de8b542c71507be5ef31c0d0e
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-04-02 00:27:33 +0200

    add account and category viewmodels

---
+++ Src/MoneyFox.Core/ViewModels/Models/CategoryViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MoneyFox.Core.DatabaseModels;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MoneyFox.Core.ViewModels.Models
+{
+    public class CategoryViewModel : INotifyPropertyChanged
+    {
+        private Category category;
+
+        public CategoryViewModel() 
+            : this(new Category()) { }
+
+        public CategoryViewModel(Category category)
+        {
+            this.category = category;
+        }
+
+        public int Id => category.Id;
+
+        public string Name
+        {
+            get { return category.Name; }
+            set { category.Name = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
