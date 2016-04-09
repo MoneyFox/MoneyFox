02a50952693abe15ac62d50ed8e473cac3a45e3c
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-04-09 10:02:21 +0200

    add constraints and migration, also unit test

--- Src/MoneyFox.Shared/SqliteConnectionCreator.cs
+++ Src/MoneyFox.Shared/SqliteConnectionCreator.cs
@@ -1,6 +1,7 @@
 ﻿using MoneyFox.Shared.Constants;
 using MoneyFox.Shared.Interfaces;
 using MoneyFox.Shared.Model;
+using MvvmCross.Plugins.File;
 using MvvmCross.Plugins.Sqlite;
 using SQLite.Net;
 
@@ -8,13 +9,17 @@ namespace MoneyFox.Shared
 {
     public class SqliteConnectionCreator : ISqliteConnectionCreator
     {
-        private readonly IMvxSqliteConnectionFactory connectionFactory;
 
-        public SqliteConnectionCreator(IMvxSqliteConnectionFactory connectionFactory)
+        private readonly IMvxSqliteConnectionFactory connectionFactory;
+        private readonly IMvxFileStore fileStore;
+
+        public SqliteConnectionCreator(IMvxSqliteConnectionFactory connectionFactory, IMvxFileStore fileStore)
         {
             this.connectionFactory = connectionFactory;
+            this.fileStore = fileStore;
 
             CreateDb();
+            MigrateDb();
         }
 
         /// <summary>
@@ -31,10 +36,66 @@ namespace MoneyFox.Shared
             using (var db = connectionFactory.GetConnection(OneDriveAuthenticationConstants.DB_NAME))
             {
                 db.CreateTable<Account>();
-                db.CreateTable<Payment>();
-                db.CreateTable<RecurringPayment>();
                 db.CreateTable<Category>();
+                db.CreateCommand(RECURRING_PAYMENT_CREATE_SCRIPT).ExecuteNonQuery();
+                db.CreateCommand(PAYMENT_TABLE_CREATE_SCRIPT).ExecuteNonQuery();
             }
         }
+
+        private void MigrateDb()
+        {
+            if (fileStore.Exists(OneDriveAuthenticationConstants.DB_NAME_OLD))
+            {
+                using (var dbOld = connectionFactory.GetConnection(OneDriveAuthenticationConstants.DB_NAME_OLD))
+                {
+                    using (var db = connectionFactory.GetConnection(OneDriveAuthenticationConstants.DB_NAME))
+                    {
+                        db.InsertAll(dbOld.Table<Account>());
+                        db.InsertAll(dbOld.Table<Category>());
+                        db.InsertAll(dbOld.Table<RecurringPayment>());
+                        db.InsertAll(dbOld.Table<Payment>());
+                    }
+                }
+
+                fileStore.DeleteFile(OneDriveAuthenticationConstants.DB_NAME_OLD);
+            }
+        }
+
+        private const string RECURRING_PAYMENT_CREATE_SCRIPT =
+            "CREATE TABLE IF NOT EXISTS RecurringPayments( " +
+            "Id INTEGER NOT NULL CONSTRAINT PK_RecurringPayment PRIMARY KEY AUTOINCREMENT, " +
+            "Amount REAL NOT NULL, " +
+            "CategoryId INTEGER, " +
+            "ChargedAccountId INTEGER NOT NULL, " +
+            "EndDate TEXT NOT NULL, " +
+            "IsEndless INTEGER NOT NULL, " +
+            "Note TEXT, " +
+            "Recurrence INTEGER NOT NULL, " +
+            "StartDate TEXT NOT NULL, " +
+            "TargetAccountId INTEGER, " +
+            "Type INTEGER NOT NULL, " +
+            "CONSTRAINT FK_RecurringPayment_Category_CategoryId FOREIGN KEY (CategoryId) REFERENCES Categories(Id) ON DELETE RESTRICT, " +
+            "CONSTRAINT FK_RecurringPayment_Account_ChargedAccountId FOREIGN KEY(ChargedAccountId) REFERENCES Accounts(Id) ON DELETE CASCADE, " +
+            "CONSTRAINT FK_RecurringPayment_Account_TargetAccountId FOREIGN KEY(TargetAccountId) REFERENCES Accounts(Id) ON DELETE RESTRICT " +
+            ") ";
+
+        private const string PAYMENT_TABLE_CREATE_SCRIPT =
+            "CREATE TABLE IF NOT EXISTS Payments( " +
+            "Id INTEGER NOT NULL CONSTRAINT PK_Payment PRIMARY KEY AUTOINCREMENT, " +
+            "Amount REAL NOT NULL, " +
+            "CategoryId INTEGER, " +
+            "ChargedAccountId INTEGER NOT NULL, " +
+            "Date TEXT NOT NULL, " +
+            "IsCleared INTEGER NOT NULL, " +
+            "IsRecurring INTEGER NOT NULL, " +
+            "Note TEXT, " +
+            "RecurringPaymentId INTEGER, " +
+            "TargetAccountId INTEGER, " +
+            "Type INTEGER NOT NULL, " +
+            "CONSTRAINT FK_Payment_Category_CategoryId FOREIGN KEY(CategoryId) REFERENCES Categories(Id) ON DELETE RESTRICT, " +
+            "CONSTRAINT FK_Payment_Account_ChargedAccountId FOREIGN KEY(ChargedAccountId) REFERENCES Accounts(Id) ON DELETE CASCADE, " +
+            "CONSTRAINT FK_Payment_RecurringPayment_RecurringPaymentId FOREIGN KEY(RecurringPaymentId) REFERENCES RecurringPayments(Id) ON DELETE RESTRICT, " +
+            "CONSTRAINT FK_Payment_Account_TargetAccountId FOREIGN KEY(TargetAccountId) REFERENCES Accounts(Id) ON DELETE RESTRICT " +
+            ")";
     }
 }
---
+++ Src/MoneyFox.Windows.Tests/SqliteConnectionCreatorTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MoneyFox.Shared;
+using MoneyFox.Shared.Constants;
+using MvvmCross.Plugins.File.WindowsCommon;
+using MvvmCross.Plugins.Sqlite.WindowsUWP;
+using SQLite.Net;
+
+namespace MoneyFox.Windows.Tests
+{
+    [TestClass]
+    public class SqliteConnectionCreatorTests
+    {
+        [TestMethod]
+        public void GetConnection_CorrectType()
+        {
+            Assert.IsInstanceOfType(new SqliteConnectionCreator(new WindowsSqliteConnectionFactory()).GetConnection(),
+                typeof (SQLiteConnection));
+        }
+
+        [TestMethod]
+        public void GetConnection_DbFileCreated()
+        {
+            new SqliteConnectionCreator(new WindowsSqliteConnectionFactory());
+            
+            Assert.IsTrue(new MvxWindowsCommonFileStore().Exists(OneDriveAuthenticationConstants.DB_NAME));
+        }
+    }
+}
