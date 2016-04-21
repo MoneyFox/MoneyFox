7b747a438d9239603c18ffeadf523501a6053249
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2016-04-21 16:47:40 +0200

    Added placeholder when there are no accounts in main page. Fixed #594

---
+++ Src/MoneyFox.Windows/Converter/AccountListVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace MoneyFox.Windows.Converter
+{
+    class AccountListVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                var list = (ObservableCollection<Shared.Model.Account>)value;
+                return list.Count == 0 ? Visibility.Visible : Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
