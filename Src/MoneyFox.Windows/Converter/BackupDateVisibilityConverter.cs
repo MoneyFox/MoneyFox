63b390eb5f72eac3780c4b4ec86f0af436b46b41
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-04-28 17:38:47 +0200

    create windows update

--- Src/MoneyFox.Windows/Converter/BackupDateVisibilityConverter.cs
+++ Src/MoneyFox.Windows/Converter/BackupDateVisibilityConverter.cs
@@ -13,10 +13,7 @@ namespace MoneyFox.Windows.Converter
             {
                 return Visibility.Collapsed;
             }
-            else
-            {
-                return Visibility.Visible;
-            }
+            return Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
