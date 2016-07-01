6de7515433aa46e3b0035bda5134a0342dbcf420
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-07-01 09:55:25 +0200

    create android update

--- Src/MoneyFox.Shared/Repositories/PaymentRepository.cs
+++ Src/MoneyFox.Shared/Repositories/PaymentRepository.cs
@@ -4,7 +4,6 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Linq.Expressions;
 using MoneyFox.Shared.Exceptions;
-using MoneyFox.Shared.Helpers;
 using MoneyFox.Shared.Interfaces;
 using MoneyFox.Shared.Model;
 using MoneyFox.Shared.Resources;
