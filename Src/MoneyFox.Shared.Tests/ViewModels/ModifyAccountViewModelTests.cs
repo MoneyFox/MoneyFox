18e5eea9cb2a8b9cb93b294ed8f85e36a5b52f28
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-07-02 15:56:34 +0200

    styling

--- Src/MoneyFox.Shared.Tests/ViewModels/ModifyAccountViewModelTests.cs
+++ Src/MoneyFox.Shared.Tests/ViewModels/ModifyAccountViewModelTests.cs
@@ -10,11 +10,9 @@ using MvvmCross.Test.Core;
 
 namespace MoneyFox.Shared.Tests.ViewModels {
     [TestClass]
-    public class ModifyAccountViewModelTests : MvxIoCSupportingTest
-    {
+    public class ModifyAccountViewModelTests : MvxIoCSupportingTest {
         [TestInitialize]
-        public void Init()
-        {
+        public void Init() {
             MvxSingleton.ClearAllSingletons();
             Setup();
         }
@@ -25,10 +23,9 @@ namespace MoneyFox.Shared.Tests.ViewModels {
 
             var accountRepositorySetup = new Mock<IAccountRepository>();
 
-            var viewmodel = new ModifyAccountViewModel(accountRepositorySetup.Object, new Mock<IDialogService>().Object)
-            {
+            var viewmodel = new ModifyAccountViewModel(accountRepositorySetup.Object, new Mock<IDialogService>().Object) {
                 IsEdit = true,
-                SelectedAccount = new Account() { Id = 3, Name = accountname}
+                SelectedAccount = new Account {Id = 3, Name = accountname}
             };
 
             viewmodel.Title.ShouldBe(string.Format(Strings.EditAccountTitle, accountname));
@@ -36,66 +33,54 @@ namespace MoneyFox.Shared.Tests.ViewModels {
 
         [TestMethod]
         public void Title_AddAccount_CorrectTitle() {
-
-            var viewmodel = new ModifyAccountViewModel(new Mock<IAccountRepository>().Object, new Mock<IDialogService>().Object)
+            var viewmodel = new ModifyAccountViewModel(new Mock<IAccountRepository>().Object,
+                new Mock<IDialogService>().Object)
             {IsEdit = false};
 
             viewmodel.Title.ShouldBe(Strings.AddAccountTitle);
         }
 
         [TestMethod]
-        public void SaveCommand_Does_Not_Allow_Duplicate_Names()
-        {
+        public void SaveCommand_Does_Not_Allow_Duplicate_Names() {
             var accountRepo = new Mock<IAccountRepository>();
             accountRepo.SetupAllProperties();
-            accountRepo.Setup(c => c.Save(It.IsAny<Account>())).Callback((Account acc) =>
-            {
-                accountRepo.Object.Data.Add(acc);
-            });
+            accountRepo.Setup(c => c.Save(It.IsAny<Account>()))
+                .Callback((Account acc) => { accountRepo.Object.Data.Add(acc); });
             accountRepo.Object.Data = new ObservableCollection<Account>();
-            var account = new Account()
-            {
+            var account = new Account {
                 Id = 1,
                 Name = "Test Account"
             };
-            var newAccount = new Account()
-            {
+            var newAccount = new Account {
                 Name = "Test Account"
             };
             accountRepo.Object.Data.Add(account);
 
-            var viewmodel = new ModifyAccountViewModel(accountRepo.Object, new Mock<IDialogService>().Object)
-            {
-                IsEdit = false
+            var viewmodel = new ModifyAccountViewModel(accountRepo.Object, new Mock<IDialogService>().Object) {
+                IsEdit = false,
+                SelectedAccount = newAccount
             };
-            viewmodel.SelectedAccount = newAccount;
 
             viewmodel.SaveCommand.Execute();
             Assert.AreEqual(1, accountRepo.Object.Data.Count);
-
         }
 
         [TestMethod]
-        public void SaveCommand_SavesAccount()
-        {
+        public void SaveCommand_SavesAccount() {
             var accountRepo = new Mock<IAccountRepository>();
             accountRepo.SetupAllProperties();
-            accountRepo.Setup(c => c.Save(It.IsAny<Account>())).Callback((Account acc) =>
-            {
-                accountRepo.Object.Data.Add(acc);
-            });
+            accountRepo.Setup(c => c.Save(It.IsAny<Account>()))
+                .Callback((Account acc) => { accountRepo.Object.Data.Add(acc); });
             accountRepo.Object.Data = new ObservableCollection<Account>();
-            var account = new Account()
-            {
+            var account = new Account {
                 Id = 1,
                 Name = "Test Account"
             };
 
-            var viewmodel = new ModifyAccountViewModel(accountRepo.Object, new Mock<IDialogService>().Object)
-            {
-                IsEdit = false
+            var viewmodel = new ModifyAccountViewModel(accountRepo.Object, new Mock<IDialogService>().Object) {
+                IsEdit = false,
+                SelectedAccount = account
             };
-            viewmodel.SelectedAccount = account;
 
             viewmodel.SaveCommand.Execute();
             Assert.AreEqual(1, accountRepo.Object.Data.Count);
