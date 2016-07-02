e7b3395a2a8387dea4af91e16692bd4faf11360b
Michael Ai <michael.ai@outlook.com>
2016-07-02 15:37:18 +0000

    updated save_updatetimestamp test

--- Src/MoneyFox.Shared.Tests/ViewModels/ModifyAccountViewModelTests.cs
+++ Src/MoneyFox.Shared.Tests/ViewModels/ModifyAccountViewModelTests.cs
@@ -1,31 +1,30 @@
-﻿using Cheesebaron.MvxPlugins.Settings.Interfaces;
+﻿using System.Collections.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MoneyFox.Shared.Interfaces;
 using MoneyFox.Shared.Model;
-using MoneyFox.Shared.Repositories;
 using MoneyFox.Shared.Resources;
 using MoneyFox.Shared.ViewModels;
 using Moq;
-using MvvmCross.Platform;
-using MvvmCross.Plugins.Messenger;
+using MvvmCross.Platform.Core;
 using MvvmCross.Test.Core;
 using System;
-using System.Collections.ObjectModel;
-using System.Linq.Expressions;
+using Cheesebaron.MvxPlugins.Settings.Interfaces;
+using MvvmCross.Platform;
 
-namespace MoneyFox.Shared.Tests.ViewModels {
+namespace MoneyFox.Shared.Tests.ViewModels
+{
     [TestClass]
-    public class ModifyAccountViewModelTests : MvxIoCSupportingTest {
-
+    public class ModifyAccountViewModelTests : MvxIoCSupportingTest
+    {
         private DateTime localDateSetting;
+
 
         [TestInitialize]
         public void Init()
         {
-            ClearAll();
+            MvxSingleton.ClearAllSingletons();
             Setup();
 
-            // We setup the static setting classes here for the general usage in the app
             var settingsMockSetup = new Mock<ISettings>();
             settingsMockSetup.SetupAllProperties();
             settingsMockSetup.Setup(x => x.AddOrUpdateValue(It.IsAny<string>(), It.IsAny<DateTime>(), false))
@@ -36,51 +35,34 @@ namespace MoneyFox.Shared.Tests.ViewModels {
         }
 
         [TestMethod]
-        public void Title_EditAccount_CorrectTitle() {
+        public void Title_EditAccount_CorrectTitle()
+        {
             var accountname = "Sparkonto";
+
             var accountRepositorySetup = new Mock<IAccountRepository>();
 
             var viewmodel = new ModifyAccountViewModel(accountRepositorySetup.Object, new Mock<IDialogService>().Object)
             {
                 IsEdit = true,
-                SelectedAccount = new Account() { Id = 3, Name = accountname}
+                SelectedAccount = new Account() { Id = 3, Name = accountname }
             };
 
             viewmodel.Title.ShouldBe(string.Format(Strings.EditAccountTitle, accountname));
         }
 
         [TestMethod]
-        public void Title_AddAccount_CorrectTitle() {
+        public void Title_AddAccount_CorrectTitle()
+        {
 
             var viewmodel = new ModifyAccountViewModel(new Mock<IAccountRepository>().Object, new Mock<IDialogService>().Object)
-            {IsEdit = false};
+            { IsEdit = false };
 
             viewmodel.Title.ShouldBe(Strings.AddAccountTitle);
         }
 
         [TestMethod]
-        public void Save_UpdateTimeStamp()
+        public void SaveCommand_Does_Not_Allow_Duplicate_Names()
         {
-<<<<<<< HEAD
-            var accountname = "Sparkonto";
-
-            var accountRepositorySetup = new Mock<IAccountRepository>();
-            accountRepositorySetup.Setup(x => x.Load(It.IsAny<Expression<Func<Account, bool>>>()));
-            accountRepositorySetup.SetupAllProperties();
-           
-
-            ObservableCollection<Account> accountData = new ObservableCollection<Account>();
-            accountData.Add(new Account { Id = 2, Name = accountname });
-            accountRepositorySetup.SetupGet(x => x.Data).Returns(accountData);
-            accountRepositorySetup.Setup(x => x.AddPaymentAmount(new Payment())).Returns(true);
-            accountRepositorySetup.SetupGet(x => x.Selected).Returns(new Account { Id = 2, Name = "Sparkonto2" });
-            accountRepositorySetup.Setup(x => x.Save(accountRepositorySetup.Object.Selected)).Returns(true);
-
-            var accountRepo = accountRepositorySetup.Object;
-            var viewmodel = new ModifyAccountViewModel(accountRepo, new Mock<IDialogService>().Object)
-            { IsEdit = false };
-            viewmodel.SelectedAccount = new Account { Id = 2, Name = "Sparkonto2" };
-=======
             var accountRepo = new Mock<IAccountRepository>();
             accountRepo.SetupAllProperties();
             accountRepo.Setup(c => c.Save(It.IsAny<Account>())).Callback((Account acc) =>
@@ -131,12 +113,33 @@ namespace MoneyFox.Shared.Tests.ViewModels {
                 IsEdit = false
             };
             viewmodel.SelectedAccount = account;
->>>>>>> refs/remotes/MoneyFox/master
+
+            viewmodel.SaveCommand.Execute();
+            Assert.AreEqual(1, accountRepo.Object.Data.Count);
+        }
+
+        [TestMethod]
+        public void Save_UpdateTimeStamp()
+        {
+            var account = new Account { Id = 0, Name = "account" };
+
+            var accountRepoMock = new Mock<IAccountRepository>();
+            var accountRepositorySetup = new Mock<IAccountRepository>();
+            accountRepositorySetup.SetupAllProperties();
+            accountRepositorySetup.Setup(x => x.AddPaymentAmount(new Payment())).Returns(true);
+            accountRepositorySetup.Setup(x => x.Save(account)).Returns(true);
+            accountRepositorySetup.Setup(x => x.Data).Returns(() => new ObservableCollection<Account>());
+            var accountRepo = accountRepositorySetup.Object;
+
+            var viewmodel = new ModifyAccountViewModel(accountRepo, new Mock<IDialogService>().Object)
+            { IsEdit = false };
+            viewmodel.SelectedAccount = account;
 
             viewmodel.SaveCommand.Execute();
 
             localDateSetting.ShouldBeGreaterThan(DateTime.Now.AddSeconds(-1));
             localDateSetting.ShouldBeLessThan(DateTime.Now.AddSeconds(1));
         }
+
     }
 }
