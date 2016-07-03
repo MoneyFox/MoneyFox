7d6c94629dd7d7871e9df9f68851ad07ee68828e
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-07-03 17:31:16 +0200

    add test project

---
+++ Src/MoneyFox.Droid.Tests/TestSample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using Xunit;
+using Assert = NUnit.Framework.Assert;
+
+
+namespace MoneyFox.Droid.Tests {
+    [TestFixture]
+    public class TestsSample {
+
+        [SetUp]
+        public void Setup() { }
+
+
+        [TearDown]
+        public void Tear() { }
+
+        [Fact]
+        public void Pass() {
+            Console.WriteLine("test1");
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Fail() {
+            Assert.False(true);
+        }
+
+        [Test]
+        [Ignore("another time")]
+        public void Ignore() {
+            Assert.True(false);
+        }
+
+        [Test]
+        public void Inconclusive() {
+            Assert.Inconclusive("Inconclusive");
+        }
+    }
+}
