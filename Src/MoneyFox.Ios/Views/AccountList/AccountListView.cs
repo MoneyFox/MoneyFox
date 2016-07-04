1dae083da7f5cbbea90481597e4cb7a673916a4e
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-07-04 18:39:51 +0200

    add basic account list

--- Src/MoneyFox.Ios/Views/AccountList/AccountListView.cs
+++ Src/MoneyFox.Ios/Views/AccountList/AccountListView.cs
@@ -1,12 +1,28 @@
+using MoneyFox.Shared.Resources;
 using MoneyFox.Shared.ViewModels;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Binding.iOS.Views;
 using MvvmCross.iOS.Support.SidePanels;
 using MvvmCross.iOS.Views;
 
 namespace MoneyFox.Ios.Views.AccountList {
     [MvxPanelPresentation(MvxPanelEnum.Center, MvxPanelHintType.ResetRoot, true)]
     public partial class AccountListView : MvxViewController<AccountListViewModel> {
-        public AccountListView() : base("AccountListView", null)
-        {
+
+        public override void ViewDidLoad() {
+            base.ViewDidLoad();
+
+            var source = new MvxStandardTableViewSource(AccountList, "TitleText Name;DetailText Iban");
+            this.CreateBinding(source).To<AccountListViewModel>(vm => vm.AllAccounts).Apply();
+            this.CreateBinding(source).For(s => s.SelectionChangedCommand).To<AccountListViewModel>(vm => vm.OpenOverviewCommand).Apply();
+            AccountList.Source = source;
+            AccountList.ReloadData();
+        }
+
+        public override void ViewDidAppear(bool animated) {
+            base.ViewDidAppear(animated);
+
+            Title = Strings.AccountsLabel;
         }
     }
 }
