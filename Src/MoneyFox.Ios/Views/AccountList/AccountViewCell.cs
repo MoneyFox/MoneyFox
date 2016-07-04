cdeff6974bc7739e360ec35dd034fd4dad213cc4
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-07-04 18:58:50 +0200

    add a custom table cell.

---
+++ Src/MoneyFox.Ios/Views/AccountList/AccountViewCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace MoneyFox.Ios
+{
+	public partial class AccountViewCell : UITableViewCell
+	{
+		public static readonly NSString Key = new NSString("AccountViewCell");
+		public static readonly UINib Nib;
+
+		static AccountViewCell()
+		{
+			Nib = UINib.FromName("AccountViewCell", NSBundle.MainBundle);
+		}
+
+		protected AccountViewCell(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+	}
+}
