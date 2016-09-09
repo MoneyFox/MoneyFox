4e9ab5dbc60eeb2ed67845830d0c71c9c3e3d885
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-09-09 09:53:23 +0200

    add dependency unit test

---
+++ Src/MoneyFox.Windows.Tests/Dependencies/WindowsCommonSettingsTests.cs
@@ -0,0 +1,31 @@
+﻿using Cheesebaron.MvxPlugins.Settings.WindowsCommon;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace MoneyFox.Windows.Tests.Dependencies {
+    [TestClass]
+    public class WindowsCommonSettingsTests {
+        [TestMethod]
+        public void Ctor()
+        {
+            var settings = new WindowsCommonSettings();
+            Assert.IsNotNull(settings);
+        }
+
+        [TestMethod]
+        public void GetValues_NoValue_ReturnDefault() {
+            Assert.AreEqual(true, new WindowsCommonSettings().GetValue("Fooo", true));
+        }
+
+        [TestMethod]
+        public void GetValues_HasValue_ReturnValue()
+        {
+            string key = "Foo";
+            string value = "abcd";
+
+            var settings = new WindowsCommonSettings();
+            settings.AddOrUpdateValue(key, value);
+
+            Assert.AreEqual(value, settings.GetValue(key, "123"));
+        }
+    }
+}
