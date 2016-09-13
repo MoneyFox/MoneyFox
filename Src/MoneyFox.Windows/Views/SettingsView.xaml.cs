b228c5419a2e18cedf87399be7211c278064a4c2
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-09-13 10:23:14 +0200

    remove modify category dialog and associated stuff

--- Src/MoneyFox.Windows/Views/SettingsView.xaml.cs
+++ Src/MoneyFox.Windows/Views/SettingsView.xaml.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using Windows.UI.Xaml;
-using MoneyFox.Windows.Views.Dialogs;
-
-namespace MoneyFox.Windows.Views
+﻿namespace MoneyFox.Windows.Views
 {
     public sealed partial class SettingsView
     {
         public SettingsView()
         {
             InitializeComponent();
-        }
-
-        private async void AddCategory(object sender, RoutedEventArgs e)
-        {
-            await new ModifyCategoryDialog().ShowAsync();
         }
     }
 }
