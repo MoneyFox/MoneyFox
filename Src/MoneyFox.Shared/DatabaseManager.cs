af8403b0d7974bbb5a6da496c528079ed39194cc
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-10-02 14:03:40 +0200

    fix manager

--- Src/MoneyFox.Shared/DatabaseManager.cs
+++ Src/MoneyFox.Shared/DatabaseManager.cs
@@ -10,43 +10,6 @@ namespace MoneyFox.Shared
     /// </summary>
     public class DatabaseManager : IDatabaseManager
     {
-        private const string RECURRING_PAYMENT_CREATE_SCRIPT =
-            "CREATE TABLE IF NOT EXISTS RecurringPayments( " +
-            "Id INTEGER NOT NULL CONSTRAINT PK_RecurringPayment PRIMARY KEY, " +
-            "Amount REAL NOT NULL, " +
-            "CategoryId INTEGER, " +
-            "ChargedAccountId INTEGER NOT NULL, " +
-            "EndDate TEXT NOT NULL, " +
-            "IsEndless INTEGER NOT NULL, " +
-            "Note TEXT, " +
-            "Recurrence INTEGER NOT NULL, " +
-            "StartDate TEXT NOT NULL, " +
-            "TargetAccountId INTEGER, " +
-            "Type INTEGER NOT NULL, " +
-            "CONSTRAINT FK_RecurringPayment_Category_CategoryId FOREIGN KEY (CategoryId) REFERENCES Categories(Id) ON DELETE RESTRICT, " +
-            "CONSTRAINT FK_RecurringPayment_Account_ChargedAccountId FOREIGN KEY(ChargedAccountId) REFERENCES Accounts(Id) ON DELETE CASCADE, " +
-            "CONSTRAINT FK_RecurringPayment_Account_TargetAccountId FOREIGN KEY(TargetAccountId) REFERENCES Accounts(Id) ON DELETE RESTRICT " +
-            ") ";
-
-        private const string PAYMENT_TABLE_CREATE_SCRIPT =
-            "CREATE TABLE IF NOT EXISTS Payments( " +
-            "Id INTEGER NOT NULL CONSTRAINT PK_Payment PRIMARY KEY AUTOINCREMENT, " +
-            "Amount REAL NOT NULL, " +
-            "CategoryId INTEGER, " +
-            "ChargedAccountId INTEGER NOT NULL, " +
-            "Date TEXT NOT NULL, " +
-            "IsCleared INTEGER NOT NULL, " +
-            "IsRecurring INTEGER NOT NULL, " +
-            "Note TEXT, " +
-            "RecurringPaymentId INTEGER, " +
-            "TargetAccountId INTEGER, " +
-            "Type INTEGER NOT NULL, " +
-            "CONSTRAINT FK_Payment_Category_CategoryId FOREIGN KEY(CategoryId) REFERENCES Categories(Id) ON DELETE RESTRICT, " +
-            "CONSTRAINT FK_Payment_Account_ChargedAccountId FOREIGN KEY(ChargedAccountId) REFERENCES Accounts(Id) ON DELETE CASCADE, " +
-            "CONSTRAINT FK_Payment_RecurringPayment_RecurringPaymentId FOREIGN KEY(RecurringPaymentId) REFERENCES RecurringPayments(Id) ON DELETE RESTRICT, " +
-            "CONSTRAINT FK_Payment_Account_TargetAccountId FOREIGN KEY(TargetAccountId) REFERENCES Accounts(Id) ON DELETE RESTRICT " +
-            ")";
-
         private readonly IMvxSqliteConnectionFactory connectionFactory;
 
         /// <summary>
@@ -79,8 +42,8 @@ namespace MoneyFox.Shared
             {
                 db.CreateTable<Account>();
                 db.CreateTable<Category>();
-                db.CreateCommand(RECURRING_PAYMENT_CREATE_SCRIPT).ExecuteNonQuery();
-                db.CreateCommand(PAYMENT_TABLE_CREATE_SCRIPT).ExecuteNonQuery();
+                db.CreateTable<Payment>();
+                db.CreateTable<RecurringPayment>();
             }
         }
 
