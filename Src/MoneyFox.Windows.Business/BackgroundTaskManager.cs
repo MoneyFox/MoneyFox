ec830a7c2248dacb26aa693aa12c2a9c864f1b49
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-10-15 10:10:22 +0200

    refactor task registration

--- Src/MoneyFox.Windows.Business/BackgroundTaskManager.cs
+++ Src/MoneyFox.Windows.Business/BackgroundTaskManager.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
 using MoneyFox.Shared.Interfaces;
 
@@ -9,42 +7,72 @@ namespace MoneyFox.Windows.Business
 {
     public class BackgroundTaskManager : IBackgroundTaskManager
     {
-        private List<TimeTaskConfig> TimeTriggeredTasks => new List<TimeTaskConfig>
-        {
-            // Task will be executed all 6 hours
-            // 360 = 6 * 60 Minutes
-            new TimeTaskConfig {Namespace = "MoneyFox.Windows.Tasks", Taskname = "ClearPaymentTask", Interval = 360 },
-            new TimeTaskConfig {Namespace = "MoneyFox.Windows.Tasks", Taskname = "RecurringPaymentTask", Interval = 60}, //MinutesTilMidnight()},
-            new TimeTaskConfig {Namespace = "MoneyFox.Windows.Tasks", Taskname = "SyncBackupTask", Interval = 180 }
-        };
+        private const string TASK_NAMESPACE = "MoneyFox.Windows.Tasks";
+
+        private const string CLEAR_PAYMENT_TASK = "ClearPaymentTask";
+        private const string RECURRING_PAYMENT_TASK = "RecurringPaymentTask";
+        private const string SYNC_BACKUP_TASK = "SyncBackupTask";
 
         public async void StartBackgroundTask()
         {
-            foreach (var timeTask in TimeTriggeredTasks)
-            {
-                if (BackgroundTaskRegistration.AllTasks.Any(task => task.Value.Name == timeTask.Taskname))
-                {
-                    break;
-                }
+            var requestAccess = await BackgroundExecutionManager.RequestAccessAsync();
 
-                await RegisterTimeTaskAsync(timeTask);
+            if (requestAccess == BackgroundAccessStatus.DeniedByUser 
+                || requestAccess == BackgroundAccessStatus.DeniedBySystemPolicy) return;
+
+
+            RegisterClearPaymentTask();
+            RegisterRecurringPaymentTask();
+            RegisterSyncBackupTask();
+        }
+
+        private static void RegisterClearPaymentTask()
+        {
+            if (BackgroundTaskRegistration.AllTasks.All(task => task.Value.Name != CLEAR_PAYMENT_TASK))
+            {
+                var builder = new BackgroundTaskBuilder
+                {
+                    Name = CLEAR_PAYMENT_TASK,
+                    TaskEntryPoint = string.Format("{0}.{1}", TASK_NAMESPACE, CLEAR_PAYMENT_TASK)
+                };
+                // Task will be executed all 6 hours
+                // 360 = 6 * 60 Minutes
+                builder.SetTrigger(new TimeTrigger(360, false));
+                builder.Register();
             }
         }
 
-        private async Task RegisterTimeTaskAsync(TimeTaskConfig timeTaskConfig)
+        private static void RegisterRecurringPaymentTask()
         {
-            var requestAccess = await BackgroundExecutionManager.RequestAccessAsync();
-
-            if (requestAccess == BackgroundAccessStatus.DeniedByUser) return;
-
-            var taskBuilder = new BackgroundTaskBuilder
+            if (BackgroundTaskRegistration.AllTasks.All(task => task.Value.Name != RECURRING_PAYMENT_TASK))
             {
-                Name = timeTaskConfig.Taskname,
-                TaskEntryPoint = string.Format("{0}.{1}", timeTaskConfig.Namespace, timeTaskConfig.Taskname)
-            };
-            taskBuilder.SetTrigger(new TimeTrigger(timeTaskConfig.Interval, false));
+                var builder = new BackgroundTaskBuilder
+                {
+                    Name = RECURRING_PAYMENT_TASK,
+                    TaskEntryPoint = string.Format("{0}.{1}", TASK_NAMESPACE, RECURRING_PAYMENT_TASK)
+                };
 
-            taskBuilder.Register();
+                //  Task will be executed every day at midnight.
+                builder.SetTrigger(new TimeTrigger(60, false));
+                //builder.SetTrigger(new TimeTrigger(MinutesTilMidnight(), false));
+                builder.Register();
+            }
+        }
+
+        private static void RegisterSyncBackupTask()
+        {
+            if (BackgroundTaskRegistration.AllTasks.All(task => task.Value.Name != SYNC_BACKUP_TASK))
+            {
+                var builder = new BackgroundTaskBuilder
+                {
+                    Name = SYNC_BACKUP_TASK,
+                    TaskEntryPoint = string.Format("{0}.{1}", TASK_NAMESPACE, SYNC_BACKUP_TASK)
+                };
+                // Task will be executed all 3 hours
+                // 180 = 3 * 60 Minutes
+                builder.SetTrigger(new TimeTrigger(180, false));
+                builder.Register();
+            }
         }
 
         /// <summary>
@@ -55,13 +83,6 @@ namespace MoneyFox.Windows.Business
             var tommorowMidnight = DateTime.Today.AddDays(1);
             var timeTilMidnight = tommorowMidnight - DateTime.Now;
             return (uint)timeTilMidnight.TotalMinutes + 5;
-        }
-
-        public struct TimeTaskConfig
-        {
-            public string Namespace { get; set; }
-            public string Taskname { get; set; }
-            public uint Interval { get; set; }
         }
     }
 }
