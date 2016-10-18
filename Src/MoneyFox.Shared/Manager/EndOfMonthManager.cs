c1e72ed164ea20df1f4bc742e6a63a22dfe7034c
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-10-18 18:19:39 +0200

    implement custom binding for android account warning

--- Src/MoneyFox.Shared/Manager/EndOfMonthManager.cs
+++ Src/MoneyFox.Shared/Manager/EndOfMonthManager.cs
@@ -69,7 +69,9 @@ namespace MoneyFox.Shared.Manager
         {
             foreach (var account in accounts)
             {
-                account.EndMonthWarning = GetEndOfMonthBalanceForAccount(account) < 0
+                account.IsOverdrawn = GetEndOfMonthBalanceForAccount(account) < 0;
+
+                account.EndMonthWarning =account.IsOverdrawn 
                     ? "Negative at end of month"
                     : " ";
             }
