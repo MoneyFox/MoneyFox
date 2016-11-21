e91771278b663b166e7ffcb59b85d267cc18495e
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-11-22 00:10:14 +0100

    remove test code for background task

--- Src/MoneyFox.Windows.Business/BackgroundTaskManager.cs
+++ Src/MoneyFox.Windows.Business/BackgroundTaskManager.cs
@@ -39,13 +39,7 @@ namespace MoneyFox.Windows.Business
                 // Task will be executed all 6 hours
                 // 360 = 6 * 60 Minutes
                 builder.SetTrigger(new TimeTrigger(360, false));
-                var task = builder.Register();
-
-                task.Completed += (sender, args) =>
-                {
-                    var settings = ApplicationData.Current.LocalSettings;
-                    settings.Values["CLEAR_PAYMENT"] = "true";
-                };
+                builder.Register();
             }
         }
 
@@ -61,13 +55,7 @@ namespace MoneyFox.Windows.Business
 
                 //  Task will be executed every day at midnight.
                 builder.SetTrigger(new TimeTrigger(MinutesTilMidnight(), false));
-                var task = builder.Register();
-
-                task.Completed += (sender, args) =>
-                {
-                    var settings = ApplicationData.Current.LocalSettings;
-                    settings.Values["RECURRING_PAYMENT"] = "true";
-                };
+                builder.Register();
             }
         }
 
@@ -83,13 +71,7 @@ namespace MoneyFox.Windows.Business
                 // Task will be executed all 3 hours
                 // 180 = 3 * 60 Minutes
                 builder.SetTrigger(new TimeTrigger(180, false));
-                var task = builder.Register();
-
-                task.Completed += (sender, args) =>
-                {
-                    var settings = ApplicationData.Current.LocalSettings;
-                    settings.Values["SYNC_BACKUP"] = "true";
-                };
+                builder.Register();
             }
         }
 
