7ad1df03a605b86e1e59f5f85e0d3ecf0ca2a9d3
unknown <gabrielppwalton@gmail.com>
2016-12-09 14:54:45 +0000

    AB/GW - Created ChangeLanguage Command in SettingsGeneralViewModel and started to implement the function of the command

--- Src/MoneyFox.Business/ViewModels/SettingsGeneralViewModel.cs
+++ Src/MoneyFox.Business/ViewModels/SettingsGeneralViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using MoneyFox.Foundation.Interfaces;
+using MvvmCross.Core.ViewModels;
 using MvvmCross.Localization;
-
 namespace MoneyFox.Business.ViewModels
 {
     public class SettingsGeneralViewModel : BaseViewModel
@@ -17,6 +17,8 @@ namespace MoneyFox.Business.ViewModels
         /// </summary>
         public IMvxLanguageBinder TextSource => new MvxLanguageBinder("", GetType().Name);
 
+        public MvxCommand ChangeLanguageCommand => new MvxCommand(ChangeLanguage);
+
         public bool IsAutoBackupEnabled
         {
             get { return settingsManager.IsBackupAutouploadEnabled; }
@@ -24,6 +26,46 @@ namespace MoneyFox.Business.ViewModels
             {
                 settingsManager.IsBackupAutouploadEnabled = value;
                 RaisePropertyChanged();
+            }
+        }
+
+        public void ChangeLanguage()
+        {
+            //    if (shell.Language.Contains("de"))
+            //        DLS.setIndex(0);
+            //    if (shell.Language.Contains("de"))
+            //        DLS.setIndex(1);
+            //    if (shell.Language.Contains("es"))
+            //        DLS.setIndex(2);
+            //    if (shell.Language.Contains("pt"))
+            //        DLS.setIndex(3);
+            //   if (shell.Language.Contains("ru"))
+            //        DLS.setIndex(4);
+            //   if (shell.Language.Contains("cn"))
+            //        DLS.setIndex(5);
+            if (default_language_box.Index == 0)
+            {
+                DefaultLanguageSetter.ChosenLang = "de";
+            }
+            if (default_language_box.Index == 1)
+            {
+                DefaultLanguageSetter.ChosenLang = "de";
+            }
+            if (default_language_box.Index == 2)
+            {
+                DefaultLanguageSetter.ChosenLang = "es";
+            }
+            if (default_language_box.Index == 3)
+            {
+                DefaultLanguageSetter.ChosenLang = "pt";
+            }
+            if (default_language_box.Index == 4)
+            {
+                DefaultLanguageSetter.ChosenLang = "ru";
+            }
+            if (default_language_box.Index == 5)
+            {
+                DefaultLanguageSetter.ChosenLang = "cn";
             }
         }
     }
