d70c2c1f9b0b6dc95b54e92cf771b05534ce6a9d
Alex Billington <Afbillington96@gmail.com>
2016-12-12 18:21:28 +0000

    AB/DS/GW - Attempt to correct previous push errors.

---
+++ ReplaceEmailScript.sh
@@ -0,0 +1,15 @@
+git filter-branch --env-filter '
+OLD_EMAIL="henrywallis1996@gmail.com"
+CORRECT_NAME="Alex Billington"
+CORRECT_EMAIL="afbillington96@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
