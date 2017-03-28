c639b570761d71a0112519af6094229e1884da3a
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2017-03-28 23:21:17 +0200

    add legend control

--- Src/MoneyFox.Business/ViewModels/DesignTime/DesignTimeStatisticCategorySpreadingViewModel.cs
+++ Src/MoneyFox.Business/ViewModels/DesignTime/DesignTimeStatisticCategorySpreadingViewModel.cs
@@ -13,6 +13,9 @@ namespace MoneyFox.Business.ViewModels.DesignTime
             new StatisticItem {Label = "Essen", Value = 1234},
             new StatisticItem {Label = "Bier", Value = 1465},
             new StatisticItem {Label = "Boooze", Value = 543},
+            new StatisticItem {Label = "Rent", Value = 462},
+            new StatisticItem {Label = "Clothes", Value = 1112},
+            new StatisticItem {Label = "Eating or so?", Value = 512},
         };
     }
 }
