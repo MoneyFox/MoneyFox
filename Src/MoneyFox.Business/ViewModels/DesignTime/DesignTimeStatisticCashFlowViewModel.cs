29e682500638c5bc771c620f58fb3d1f7dd351ad
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2017-03-28 23:15:43 +0200

    move cashflow and cat spreading chart to telerik

---
+++ Src/MoneyFox.Business/ViewModels/DesignTime/DesignTimeStatisticCashFlowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MoneyFox.Foundation.Interfaces.ViewModels;
+using MoneyFox.Foundation.Models;
+using MvvmCross.Core.ViewModels;
+
+namespace MoneyFox.Business.ViewModels.DesignTime
+{
+    public class DesignTimeStatisticCashFlowViewModel : IStatisticCashFlowViewModel
+    {
+        public string Title => "I AM A MIGHTY TITLE";
+
+        public CashFlow CashFlow => new CashFlow
+        {
+            Expense = new StatisticItem { Label = "Expense", Category = "Expense",  Value = 1234},
+            Income = new StatisticItem { Label = "Income", Category = "Income",  Value = 1465},
+            Revenue = new StatisticItem { Label = "Revenue", Category = "Revenue",  Value = 543},
+        };
+
+        public MvxObservableCollection<StatisticItem> StatisticItems => new MvxObservableCollection<StatisticItem>
+        {
+            new StatisticItem {Label = "Expense", Value = 1234},
+            new StatisticItem {Label = "Income", Value = 1465},
+            new StatisticItem {Label = "Revenue", Value = 543},
+        };
+
+    }
+}
