05a2d5676b00651b21835eb536a1e046e1907a4a
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2017-03-30 15:09:00 +0200

    some adjustments and cleanup

--- Src/MoneyFox.Droid/BackgroundTaskManager.cs
+++ Src/MoneyFox.Droid/BackgroundTaskManager.cs
@@ -28,44 +28,13 @@ namespace MoneyFox.Droid
             var alarmmanager = (AlarmManager)currentActivity.GetSystemService(Context.AlarmService);
 
             // The task will be executed all 6 hours.
-            alarmmanager.SetInexactRepeating(AlarmType.RtcWakeup, 10000, 10000, pendingIntentClearPayments);
-            alarmmanager.SetInexactRepeating(AlarmType.RtcWakeup, 10000, 10000, pendingIntentRecurringPayments);
+            alarmmanager.SetInexactRepeating(AlarmType.RtcWakeup, 10000, 21600000, pendingIntentClearPayments);
+            alarmmanager.SetInexactRepeating(AlarmType.RtcWakeup, 10000, 21600000, pendingIntentRecurringPayments);
 
             if (settingsManager.IsBackupAutouploadEnabled)
             {
-                alarmmanager.SetInexactRepeating(AlarmType.RtcWakeup, 10000, 10000, pendingIntentSyncBackups);
+                alarmmanager.SetInexactRepeating(AlarmType.RtcWakeup, 21600000, 21600000, pendingIntentSyncBackups);
             }
-        }
-
-        private void StartClearingService()
-        {
-            var intent = new Intent(currentActivity, typeof(ClearPaymentService));
-            var pendingIntent = PendingIntent.GetService(currentActivity, 0, intent, 0);
-
-            var alarmmanager = (AlarmManager)currentActivity.GetSystemService(Context.AlarmService);
-
-            // The task will be executed all 6 hours.
-            alarmmanager.SetInexactRepeating(AlarmType.RtcWakeup, 21600000, 21600000, pendingIntent);
-        }
-
-        private void StartRecurringPaymentService()
-        {
-            var intent = new Intent(currentActivity, typeof(ClearPaymentService));
-            var pendingIntent = PendingIntent.GetService(currentActivity, 0, intent, 0);
-
-            var alarmmanager = (AlarmManager)currentActivity.GetSystemService(Context.AlarmService);
-
-            alarmmanager.SetRepeating(AlarmType.RtcWakeup, DateTime.Today.AddHours(2).Millisecond, AlarmManager.IntervalDay, pendingIntent);
-        }
-
-        private void StartSyncBackupService() {
-            var intent = new Intent(currentActivity, typeof(SyncBackupService));
-            var pendingIntent = PendingIntent.GetService(currentActivity, 0, intent, 0);
-
-            var alarmmanager = (AlarmManager)currentActivity.GetSystemService(Context.AlarmService);
-
-            // The task will be executed all 6 hours.
-            alarmmanager.SetInexactRepeating(AlarmType.RtcWakeup, 10800000, 10800000, pendingIntent);
         }
     }
 }
