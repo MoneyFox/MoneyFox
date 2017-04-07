a43267b938005823a5e85aca137833be0e59711c
Nino Padrutt <nino.padrutt@hotmail.com>
2017-04-07 10:30:03 +0200

    remove unused code

--- Src/MoneyFox.Droid/Fragments/AccountListFragment.cs
+++ Src/MoneyFox.Droid/Fragments/AccountListFragment.cs
@@ -28,11 +28,6 @@ namespace MoneyFox.Droid.Fragments
             view = base.OnCreateView(inflater, container, savedInstanceState);
             LoadBalancePanel();
 
-            //var includedAccountsList = view.FindViewById<ListView>(Resource.Id.included_account_list);
-            //RegisterForContextMenu(includedAccountsList);
-            //var excludedAccountList = view.FindViewById<ListView>(Resource.Id.excluded_account_list);
-            //RegisterForContextMenu(excludedAccountList);
-
             view.FindViewById<FloatingActionMenu>(Resource.Id.fab_menu_add_element).SetClosedOnTouchOutside(true);
 
             return view;
@@ -54,51 +49,6 @@ namespace MoneyFox.Droid.Fragments
             FragmentManager.BeginTransaction()
                 .Replace(Resource.Id.account_list_balance_frame, fragment)
                 .Commit();
-        }
-
-        public override void OnCreateContextMenu(IContextMenu menu, View v, IContextMenuContextMenuInfo menuInfo)
-        {
-            if (v.Id == Resource.Id.included_account_list || v.Id == Resource.Id.excluded_account_list)
-            {
-                var groupId = v.Id == Resource.Id.included_account_list ? GROUP_ID_INCLUDED_ACCOUNTS : GROUP_ID_EXCLUDED_ACCOUNTS;
-
-                menu.SetHeaderTitle(Strings.SelectOperationLabel);
-                menu.Add(groupId, 0, 0, Strings.EditLabel);
-                menu.Add(groupId, 1, 1, Strings.DeleteLabel);
-            }
-        }
-
-        public override bool OnContextItemSelected(IMenuItem item)
-        {
-            var accounts = new List<AccountViewModel>();
-            var menuInfos = (AdapterView.AdapterContextMenuInfo) item.MenuInfo;
-
-            // Determine which listview was clicked
-            switch (item.GroupId)
-            {
-                case GROUP_ID_INCLUDED_ACCOUNTS:
-                    accounts = ViewModel.IncludedAccounts.ToList();
-                    break;
-                case GROUP_ID_EXCLUDED_ACCOUNTS:
-                    accounts = ViewModel.ExcludedAccounts.ToList();
-                    break;
-            }
-
-            var selected = accounts[menuInfos.Position];
-
-            switch (item.ItemId)
-            {
-                case 0:
-                    ViewModel.EditAccountCommand.Execute(selected);
-                    return true;
-
-                case 1:
-                    ViewModel.DeleteAccountCommand.Execute(selected);
-                    return true;
-
-                default:
-                    return false;
-            }
         }
     }
 }
