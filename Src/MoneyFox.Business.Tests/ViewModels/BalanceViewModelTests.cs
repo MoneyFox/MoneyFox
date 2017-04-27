a4513d7bc3ead69b73655db6b76409e43e7edb0d
Nino Padrutt <nino.padrutt@hotmail.com>
2017-04-27 11:32:11 +0200

    include vm Tests again

--- Src/MoneyFox.Business.Tests/ViewModels/BalanceViewModelTests.cs
+++ Src/MoneyFox.Business.Tests/ViewModels/BalanceViewModelTests.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
+﻿using MoneyFox.Business.Manager;
 using MoneyFox.Business.ViewModels;
-using MoneyFox.Foundation.DataModels;
-using MoneyFox.Foundation.Interfaces;
-using MoneyFox.Foundation.Interfaces.Repositories;
 using MoneyFox.Foundation.Tests;
+using MoneyFox.Service.Pocos;
 using Moq;
 using MvvmCross.Test.Core;
 using Xunit;
@@ -17,11 +13,11 @@ namespace MoneyFox.Business.Tests.ViewModels
         [Fact]
         public void GetTotalBalance_Zero()
         {
-            var endOfMonthManagerSetup = new Mock<IEndOfMonthManager>();
-            endOfMonthManagerSetup.Setup(x => x.GetEndOfMonthBalanceForAccount(It.IsAny<AccountViewModel>())).Returns(() => 0);
-            endOfMonthManagerSetup.Setup(x => x.GetTotalEndOfMonthBalance(It.IsAny<IEnumerable<AccountViewModel>>())).Returns(() => 0);
+            var balanceCalculationManager = new Mock<IBalanceCalculationManager>();
+            balanceCalculationManager.Setup(x => x.GetEndOfMonthBalanceForAccount(It.IsAny<Account>())).ReturnsAsync(() => 0);
+            balanceCalculationManager.Setup(x => x.GetTotalEndOfMonthBalance()).ReturnsAsync(() => 0);
 
-            var vm = new BalanceViewModel(new Mock<IAccountRepository>().Object, endOfMonthManagerSetup.Object);
+            var vm = new BalanceViewModel(balanceCalculationManager.Object);
 
             vm.UpdateBalanceCommand.Execute();
 
@@ -32,7 +28,7 @@ namespace MoneyFox.Business.Tests.ViewModels
         [Fact]
         public void GetTotalBalance_TwoPayments_SumOfPayments()
         {
-            var vm = new BalanceViewModel(new Mock<IAccountRepository>().Object, new Mock<IEndOfMonthManager>().Object);
+            var vm = new BalanceViewModel(new Mock<IBalanceCalculationManager>().Object);
             vm.UpdateBalanceCommand.Execute();
 
             vm.TotalBalance.ShouldBe(0);
@@ -41,15 +37,11 @@ namespace MoneyFox.Business.Tests.ViewModels
         [Fact]
         public void GetTotalBalance_TwoAccounts_SumOfAccounts()
         {
-            var accountMockSetup = new Mock<IAccountRepository>();
-            accountMockSetup.Setup(x => x.GetList(It.IsAny<Expression<Func<AccountViewModel, bool>>>()))
-                .Returns(() => new List<AccountViewModel>
-            {
-                new AccountViewModel {CurrentBalance = 500, IsExcluded = false},
-                new AccountViewModel {CurrentBalance = 200, IsExcluded = false}
-            });
+            var balanceCalculationManager = new Mock<IBalanceCalculationManager>();
+            balanceCalculationManager.Setup(x => x.GetTotalEndOfMonthBalance())
+                .ReturnsAsync(() => 700);
 
-            var vm = new BalanceViewModel(accountMockSetup.Object, new Mock<IEndOfMonthManager>().Object);
+            var vm = new BalanceViewModel(balanceCalculationManager.Object);
             vm.UpdateBalanceCommand.Execute();
 
             vm.TotalBalance.ShouldBe(700);
