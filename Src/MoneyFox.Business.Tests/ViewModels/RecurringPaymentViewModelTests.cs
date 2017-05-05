eb34e865fa2927e4d6c95f0c28cc14eb2ab77c32
Nino Padrutt <nino.padrutt@hotmail.com>
2017-05-06 00:35:11 +0200

    Add new unit test classes

---
+++ Src/MoneyFox.Business.Tests/ViewModels/RecurringPaymentViewModelTests.cs
@@ -0,0 +1,28 @@
+﻿
+using MoneyFox.Business.ViewModels;
+using Xunit;
+
+namespace MoneyFox.Business.Tests.ViewModels
+{
+    public class RecurringPaymentViewModelTests
+    {
+        [Fact]
+        public void ChargedAccount_Default_Null()
+        {
+            Assert.Null(new RecurringPaymentViewModel().ChargedAccount);
+        }
+
+        [Fact]
+        public void TargetAccount_Default_Null()
+        {
+            Assert.Null(new RecurringPaymentViewModel().TargetAccount);
+        }
+
+        [Fact]
+        public void RecurringPayment_Default_Null()
+        {
+            Assert.Null(new RecurringPaymentViewModel().RecurringPayment);
+        }
+
+    }
+}
