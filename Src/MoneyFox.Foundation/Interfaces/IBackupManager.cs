25a47425d16acddcbd1f8a7cde9a7f9a479523bd
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2017-05-09 22:23:39 +0200

    add comments, catch exception

--- Src/MoneyFox.Foundation/Interfaces/IBackupManager.cs
+++ Src/MoneyFox.Foundation/Interfaces/IBackupManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using MoneyFox.Foundation.Exceptions;
 
 namespace MoneyFox.Foundation.Interfaces
 {
@@ -11,6 +12,7 @@ namespace MoneyFox.Foundation.Interfaces
         /// <summary>
         ///     Login user.
         /// </summary>
+        /// <exception cref="BackupAuthenticationFailedException">Thrown when the user couldn't be logged in.</exception>
         Task Login();        
         
         /// <summary>
@@ -22,33 +24,41 @@ namespace MoneyFox.Foundation.Interfaces
         ///     Checks if there are backups to restore.
         /// </summary>
         /// <returns>Backups available or not.</returns>
+        /// <exception cref="BackupAuthenticationFailedException">Thrown when the user couldn't be logged in.</exception>
         Task<bool> IsBackupExisting();
 
         /// <summary>
         ///     Returns the date when the last backup was created.
         /// </summary>
         /// <returns>Creation date of the last backup.</returns>
+        /// <exception cref="BackupAuthenticationFailedException">Thrown when the user couldn't be logged in.</exception>
         Task<DateTime> GetBackupDate();
 
         /// <summary>
         ///     Creates a new backup.
         /// </summary>
+        /// <exception cref="BackupAuthenticationFailedException">Thrown when the user couldn't be logged in.</exception>
         Task<bool> CreateNewBackup();
 
         /// <summary>
         ///     Restores an existing backup.
         /// </summary>
+        /// <exception cref="BackupAuthenticationFailedException">Thrown when the user couldn't be logged in.</exception>
+        /// <exception cref="NoBackupFoundException">Thrown when no backup with the right name is found.</exception>
         Task RestoreBackup();
 
         /// <summary>
         ///     Enqueues a new backup task
         /// </summary>
+        /// <exception cref="NetworkConnectionException">Thrown if there is no internet connection.</exception>
         Task EnqueueBackupTask(int attempts = 0);
 
         /// <summary>
         ///     Syncs the local database with the Backupservice and
         ///     restores it if the one on the Backupservice is newer.
         /// </summary>
+        /// <exception cref="BackupAuthenticationFailedException">Thrown when the user couldn't be logged in.</exception>
+        /// <exception cref="NoBackupFoundException">Thrown when no backup with the right name is found.</exception>
         Task DownloadBackup();
     }
 }
