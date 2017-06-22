eaaa59737814d971638b5e66818a54d658131774
Nino Padrutt <nino.padrutt@hotmail.com>
2017-06-22 22:30:53 +0200

    move stuff to interface and fix warnings

--- Src/MoneyFox.Business/ViewModels/DesignTime/DesignTimeAccountListViewModel.cs
+++ Src/MoneyFox.Business/ViewModels/DesignTime/DesignTimeAccountListViewModel.cs
@@ -3,13 +3,19 @@ using System.Threading.Tasks;
 using MoneyFox.Business.ViewModels.Interfaces;
 using MoneyFox.Service.Pocos;
 using MvvmCross.Core.ViewModels;
+using MvvmCross.Localization;
 
 namespace MoneyFox.Business.ViewModels.DesignTime
 {
+    /// <inheritdoc />
     public class DesignTimeAccountListViewModel : IAccountListViewModel
     {
-        public DesignTimeAccountListViewModel()
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        public DesignTimeAccountListViewModel(IMvxLanguageBinder textSource)
         {
+            TextSource = textSource;
             IncludedAccounts = new ObservableCollection<AccountViewModel>
             {
                 new AccountViewModel(new Account())
@@ -20,19 +26,58 @@ namespace MoneyFox.Business.ViewModels.DesignTime
                 }
             };
 
+            ViewActionViewModel = new AccountListViewActionViewModel(null, null);
             BalanceViewModel = new DesignTimeBalanceViewModel();
         }
 
+        /// <inheritdoc />
         public AccountViewModel SelectedAccountViewModel { get; set; }
 
+        /// <inheritdoc />
         public ObservableCollection<AccountViewModel> IncludedAccounts { get; set; }
+
+        /// <inheritdoc />
         public ObservableCollection<AccountViewModel> ExcludedAccounts { get; set; }
+
+        /// <inheritdoc />
         public bool IsAllAccountsEmpty { get; set; }
+
+        /// <inheritdoc />
         public bool IsExcludedAccountsEmpty { get; set; }
+
+        /// <inheritdoc />
         public IBalanceViewModel BalanceViewModel { get; }
+
+        /// <inheritdoc />
         public IViewActionViewModel ViewActionViewModel { get; }
+
+        /// <inheritdoc />
+        public IMvxLanguageBinder TextSource { get; }
+
+        /// <inheritdoc />
         public MvxAsyncCommand LoadedCommand => new MvxAsyncCommand(() => Task.CompletedTask);
-        public MvxAsyncCommand<AccountViewModel> EditAccountCommand => new MvxAsyncCommand<AccountViewModel>(async vm => { });
-        public MvxAsyncCommand<AccountViewModel> DeleteAccountCommand => new MvxAsyncCommand<AccountViewModel>(async vm => { });
+
+        /// <inheritdoc />
+        public MvxAsyncCommand<AccountViewModel> OpenOverviewCommand => new MvxAsyncCommand<AccountViewModel>(async vm => { await Task.CompletedTask; });
+
+        /// <inheritdoc />
+        public MvxAsyncCommand<AccountViewModel> EditAccountCommand =>
+            new MvxAsyncCommand<AccountViewModel>(async vm => { await Task.CompletedTask; });
+
+        /// <inheritdoc />
+        public MvxAsyncCommand<AccountViewModel> DeleteAccountCommand =>
+            new MvxAsyncCommand<AccountViewModel>(async vm => { await Task.CompletedTask; });
+
+        /// <inheritdoc />
+        public MvxAsyncCommand GoToAddAccountCommand => new MvxAsyncCommand(async vm => { await Task.CompletedTask; });
+        
+        /// <inheritdoc />
+        public MvxAsyncCommand<AccountViewModel> OpenContextMenuCommand => new MvxAsyncCommand<AccountViewModel>(async vm => { await Task.CompletedTask; });
+        
+        /// <inheritdoc />
+        public Task ShowMenu()
+        {
+            return Task.CompletedTask;
+        }
     }
 }
