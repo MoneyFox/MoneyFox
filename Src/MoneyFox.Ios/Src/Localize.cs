077248ad97333806af355e7fcb82008f81f73b43
Nino Padrutt <nino.padrutt@hotmail.com>
2017-07-20 22:50:50 +0200

    add comment

--- Src/MoneyFox.Ios/Src/Localize.cs
+++ Src/MoneyFox.Ios/Src/Localize.cs
@@ -1,15 +1,27 @@
 using System.Globalization;
 using Foundation;
 
-namespace MoneyFox.Ios {
-    public class Localize {
-        public CultureInfo GetCurrentCultureInfo() {
+namespace MoneyFox.Ios
+{
+    /// <summary>
+    ///     Localizatino helper
+    /// </summary>
+    public class Localize
+    {
+        /// <summary>
+        ///     Returns the current culture.
+        /// </summary>
+        /// <returns>Current culture.</returns>
+        public CultureInfo GetCurrentCultureInfo()
+        {
             var netLanguage = "en";
-            if (NSLocale.PreferredLanguages.Length > 0) {
+            if (NSLocale.PreferredLanguages.Length > 0)
+            {
                 var pref = NSLocale.PreferredLanguages[0];
                 netLanguage = pref.Replace("_", "-"); // turns es_ES into es-ES
 
-                if (pref == "pt") {
+                if (pref == "pt")
+                {
                     // get the correct Brazilian language strings from the PCL RESX
                     //(note the local iOS folder is still "pt")
                     pref = "pt-BR";
