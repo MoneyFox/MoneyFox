14b5f5b251bcdc50489ba085e8e906a041bd1372
Nino Padrutt <nino.padrutt@hotmail.com>
2017-09-03 00:54:25 +0200

    create windows update

--- Src/MoneyFox.Windows/App.xaml.cs
+++ Src/MoneyFox.Windows/App.xaml.cs
@@ -104,7 +104,7 @@ namespace MoneyFox.Windows
                 BackgroundTaskHelper.Register(typeof(RecurringPaymentTask), new TimeTrigger(15, false));
 
                 shell.ViewModel = Mvx.Resolve<ShellViewModel>();
-                await Mvx.Resolve<IBackupManager>().DownloadBackup();
+                Mvx.Resolve<IBackupManager>().DownloadBackup();
 
                 //If Jump Lists are supported, add them
                 if (ApiInformation.IsTypePresent("Windows.UI.StartScreen.JumpList"))
