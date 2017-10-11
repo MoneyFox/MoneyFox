b9586a4389134ca562ec0f08d5dc0f1a8ba75b24
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2017-10-11 10:43:01 +0200

    remove unused connectivity plugin.

--- Src/MoneyFox.Ios/Bootstrap/ConnectivityPluginBootstrap.cs
+++
@@ -1,8 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace MoneyFox.Ios.Bootstrap
-{
-    public class ConnectivityPluginBootstrap
-        : MvxLoaderPluginBootstrapAction<Cheesebaron.MvxPlugins.Connectivity.PluginLoader, 
-			Cheesebaron.MvxPlugins.Connectivity.Touch.Plugin> { }
-}
