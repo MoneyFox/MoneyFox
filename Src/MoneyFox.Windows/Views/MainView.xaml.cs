f4a922ae3922e4366164e00b47f5d515fb05f7c9
Nino Padrutt <nino.padrutt@hotmail.com>
2017-12-07 21:44:20 +0100

    update mvvmcross

--- Src/MoneyFox.Windows/Views/MainView.xaml.cs
+++ Src/MoneyFox.Windows/Views/MainView.xaml.cs
@@ -2,12 +2,16 @@
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using MoneyFox.Business.ViewModels;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Uwp.Attributes;
 
 namespace MoneyFox.Windows.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    [MvxPagePresentation]
+    [MvxViewFor(typeof(MenuViewModel))]
     public sealed partial class MainView
     {
         public MainView()
