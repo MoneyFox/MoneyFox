0d92c173f8939be3f6d7ae90f283a63aa0ad8221
Nino Padrutt <nino.padrutt@hotmail.com>
2018-01-05 15:45:04 +0100

    implement scrolling

--- Src/MoneyFox.Windows/Views/UserControls/PaymentListUserControl.xaml.cs
+++ Src/MoneyFox.Windows/Views/UserControls/PaymentListUserControl.xaml.cs
@@ -1,8 +1,10 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Linq;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Input;
 using MoneyFox.Business.ViewModels;
+using MoneyFox.Foundation.Groups;
 
 namespace MoneyFox.Windows.Views.UserControls
 {
@@ -43,6 +45,20 @@ namespace MoneyFox.Windows.Views.UserControls
             var flyoutBase = FlyoutBase.GetAttachedFlyout(senderElement) as MenuFlyout;
 
             flyoutBase.ShowAt(senderElement, e.GetPosition(senderElement));
+        }
+
+        private void PaymentListView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (PaymentListView.Items == null || !PaymentListView.Items.Any()) return;
+
+            // Select first group with a cleared payment in it
+            DateListGroup<PaymentViewModel> selectedGroup = PaymentListView
+                                                            .Items.Select(x => (DateListGroup<PaymentViewModel>)x)
+                                                            .FirstOrDefault(group => group.Any(x => x.IsCleared));
+
+            if (selectedGroup == null) return;
+
+            PaymentListView.ScrollIntoView(selectedGroup, ScrollIntoViewAlignment.Leading);
         }
     }
 }
