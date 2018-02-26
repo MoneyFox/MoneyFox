c10a58717d5eb7e5df6cba810484719b333e1a3e
Nino Padrutt <nino.padrutt@post.ch>
2018-02-26 18:00:35 +0100

    integrate new embedded forms into android

--- Src/MoneyFox.Droid/Fragments/BackupFragment.cs
+++ Src/MoneyFox.Droid/Fragments/BackupFragment.cs
@@ -1,21 +1,35 @@
+using Android.OS;
 using Android.Runtime;
+using Android.Views;
 using MoneyFox.Business.ViewModels;
+using MoneyFox.Business.Views;
 using MoneyFox.Foundation.Resources;
+using MvvmCross.Droid.Support.V4;
 using MvvmCross.Droid.Views.Attributes;
+using Xamarin.Forms.Platform.Android;
 
 namespace MoneyFox.Droid.Fragments
 {
     [MvxFragmentPresentation(typeof(MainViewModel), Resource.Id.content_frame)]
     [Register("moneyfox.droid.fragments.BackupFragment")]
-    public class BackupFragment : BaseFragment<BackupViewModel>
+    public class BackupFragment : MvxFragment<BackupViewModel>
     {
-        protected override int FragmentId => Resource.Layout.fragment_backup;
-        protected override string Title => Strings.BackupLabel;
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = base.OnCreateView(inflater, container, savedInstanceState);
+            Activity.Title = Strings.BackupLabel;
+
+            var fragment = new BackupPage { BindingContext = ViewModel }.CreateSupportFragment(Activity);
+            FragmentManager.BeginTransaction()
+                           .Replace(Resource.Id.content_frame, fragment)
+                           .Commit();
+
+            return view;
+        }
 
         public override async void OnStart()
         {
             base.OnStart();
-
             await ViewModel.LoadedCommand.ExecuteAsync();
         }
     }
