c9b052736e1c78606adaef2c4c78b39be96d24ac
Nino Padrutt <nino.padrutt@hotmail.com>
2018-03-02 13:39:16 +0100

    start with the android XF project.

---
+++ Src/MoneyFox/MoneyFox/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using MoneyFox.Views;
+using Xamarin.Forms;
+
+namespace MoneyFox
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+			InitializeComponent();
+
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Src/MoneyFox/MoneyFox/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoneyFox.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
