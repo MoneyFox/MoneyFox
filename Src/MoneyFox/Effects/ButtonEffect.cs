4b4ccec31669ba4c89e1868dc2c2f5fb354a4acc
Nino Padrutt <nino.padrutt@post.ch>
2018-06-06 17:14:54 +0200

    some design adjustments.

---
+++ Src/MoneyFox/Effects/ButtonEffect.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace MoneyFox.Effects
+{    
+    /// <summary>
+    ///     Routing for the the button effect.
+    /// </summary>
+    public class ButtonEffect : RoutingEffect
+    {
+        public ButtonEffect(string effectId) : base("MoneyFox.ButtonEffect") {
+        }
+    }
+}
