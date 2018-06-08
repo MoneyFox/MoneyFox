5aadb4d22a18339c970d6d4d8346f546858ca275
Nino Padrutt <nino.padrutt@hotmail.com>
2018-06-08 15:51:54 +0200

    fix crash.

--- Src/MoneyFox/Converter/AmountFormatConverter.cs
+++ Src/MoneyFox/Converter/AmountFormatConverter.cs
@@ -18,7 +18,7 @@ namespace MoneyFox.Converter
         /// <param name="parameter">Is not used.</param>
         /// <param name="culture">Culture to use to convert.</param>
         /// <returns>Converted currency string.</returns>
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var noParens = (NumberFormatInfo)culture.NumberFormat.Clone();
             noParens.CurrencyNegativePattern = 1;
@@ -30,6 +30,6 @@ namespace MoneyFox.Converter
         ///     Returns the value.
         /// </summary>
         /// <returns>Passed value.</returns>
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value;
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value;
     }
 }
