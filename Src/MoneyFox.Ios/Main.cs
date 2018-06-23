02a4d2d70bf80e261b6b45b1dcdb2412398b9da4
Nino Padrutt <nino.padrutt@hotmail.com>
2018-06-23 18:00:56 +0200

    some minor things

--- Src/MoneyFox.Ios/Main.cs
+++ Src/MoneyFox.Ios/Main.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+using System.Diagnostics;
+using Microsoft.AppCenter.Crashes;
 using UIKit;
 
 namespace MoneyFox.iOS
@@ -12,9 +10,17 @@ namespace MoneyFox.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            try
+            {
+                // if you want to use a different Application Delegate class from "AppDelegate"
+                // you can specify it here.
+                UIApplication.Main(args, null, "AppDelegate");
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+                Crashes.TrackError(ex);
+            }
         }
     }
 }
