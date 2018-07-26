2df133371b9ae3d0771dc6938ce1b6b0df891f73
Hu Xinpeng <19384512+cloudstalker@users.noreply.github.com>
2018-07-26 20:51:51 +0800

    Minor Visual Change

--- Src/MoneyFox.Windows/Views/GeneralView.xaml.cs
+++ Src/MoneyFox.Windows/Views/GeneralView.xaml.cs
@@ -7,6 +7,9 @@ using Windows.Foundation;
 using Windows.UI.Xaml;
 using Xamarin.Forms.Platform.UWP;
 using MoneyFox.Views;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
 
 namespace MoneyFox.Windows.Views
 {
@@ -15,6 +18,24 @@ namespace MoneyFox.Windows.Views
         public GeneralView()
         {
            InitializeComponent();
+        }
+
+        private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            for (int i = 0; i < GeneralViewPivot.Items.Count; i++)
+            {
+                if (i == GeneralViewPivot.SelectedIndex)
+                {
+                    PivotItem selectedPivotItem = GeneralViewPivot.SelectedItem as PivotItem;
+                    (selectedPivotItem.Header as TextBlock).Foreground = new SolidColorBrush(Color.FromArgb(255, 240, 146, 36));
+
+                }
+                else
+                {
+                    PivotItem unselectedPivotItem = GeneralViewPivot.Items[i] as PivotItem;
+                    (unselectedPivotItem.Header as TextBlock).Foreground = new SolidColorBrush(Colors.DarkGray);
+                }
+            }
         }
     }
 }
