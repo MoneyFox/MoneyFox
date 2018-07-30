9a6885f69136b6a87c200f63491465a161c9d175
Nino Padrutt <nino.padrutt@post.ch>
2018-07-30 13:42:49 +0200

    apply color

--- Src/MoneyFox/Views/MainPage.xaml.cs
+++ Src/MoneyFox/Views/MainPage.xaml.cs
@@ -20,7 +20,7 @@ namespace MoneyFox.Views
 		    On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
 
             // We have to resolve the VM here, since the automagic doesn't yet work the BottomTabbedPage.
-		    ViewModel = Mvx.Resolve<MainViewModel>();
+            ViewModel = Mvx.Resolve<MainViewModel>();
 		}
 
 	    private bool firstTime = true;
