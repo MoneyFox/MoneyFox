2d14cfd37e3d439a61563dd1dcda75e0cc1d9f4f
Isaac Morris <morris.isaac@gmail.com>
2018-10-08 18:25:23 -0600

    continueing to work on live tiles

--- Src/MoneyFox.Windows.LiveandLockScreenTiles/Class1.cs
+++ Src/MoneyFox.Windows.LiveandLockScreenTiles/Class1.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.AppService;
 using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
 
 namespace MoneyFox.Windows.LiveandLockScreenTiles
 {
-    public sealed class UpdateliveandLockscreenTiles:IBackgroundTask
+    public sealed class UpdateliveandLockscreenTiles : IBackgroundTask
     {
         BackgroundTaskDeferral serviceDeferral;
         AppServiceConnection connection;
@@ -21,17 +22,64 @@ namespace MoneyFox.Windows.LiveandLockScreenTiles
             connection = details.AppServiceConnection;
 
             //Listen for incoming app service requests
-            connection.RequestReceived += OnRequestReceived;
+            connection.RequestReceived += OnRequestReceivedAsync;
         }
 
-        private void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        private async Task OnRequestReceivedAsync(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
         {
-            throw new NotImplementedException();
+            var messageDeferral = args.GetDeferral();
+
+            try
+            {
+                var input = args.Request.Message;
+                if (input != null)
+                {
+
+
+                    string command = input["Command"] as string;
+
+                    switch (command)
+                    {
+                        case "GetProductUnitCountForRegion":
+                            {
+                                var productId = (int)input["ProductId"];
+                                var regionId = (int)input["RegionId"];
+                                var inventoryData = GetInventoryData(productId, regionId);
+                                var result = new ValueSet();
+                                result.Add("UnitCount", inventoryData.UnitCount);
+                                result.Add("LastUpdated", inventoryData.LastUpdated.ToString());
+
+                                await args.Request.SendResponseAsync(result);
+                            }
+                            break;
+
+                        //Other commands
+
+                        default:
+                            return;
+                    }
+                }
+            finally
+            {
+                //Complete the message deferral so the operating system knows we're done responding
+                messageDeferral.Complete();
+            }
         }
+
 
         private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
         {
             throw new NotImplementedException();
         }
+        private void UpdatePrimaryLiveTile()
+        {
+
+        }
+        private void UpdateSecondaryLiveTiles()
+        {
+
+        }
+
     }
 }
+
