84e152938f58774502494ab78a81cfc8ef496677
JohnMiz <napstar261@gmail.com>
2018-11-29 13:01:15 +0200

    Added support for android shortcuts.

--- Src/MoneyFox.Android/MainActivity.cs
+++ Src/MoneyFox.Android/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using MoneyFox.Business.ViewModels;
 using MoneyFox.Droid.Jobs;
 using MoneyFox.Foundation.Interfaces;
 using MvvmCross;
@@ -10,72 +11,119 @@ using Rg.Plugins.Popup;
 
 namespace MoneyFox.Droid
 {
-    [Activity(Label = "MoneyFox", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : MvxFormsAppCompatActivity
-    {        
-        /// <summary>
-        ///     Constant for the ClearPayment Service.
-        /// </summary>
-        public const int MESSAGE_SERVICE_CLEAR_PAYMENTS = 1;
+	 [Activity(Label = "MoneyFox", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+		  Exported = true, Name = "com.applysolutions.moneyfox.MainActivity")]
+	 public class MainActivity : MvxFormsAppCompatActivity
+	 {
+		  /// <summary>
+		  ///     Constant for the ClearPayment Service.
+		  /// </summary>
+		  public const int MESSAGE_SERVICE_CLEAR_PAYMENTS = 1;
 
-        /// <summary>
-        ///     Constant for the recurring payment Service.
-        /// </summary>
-        public const int MESSAGE_SERVICE_RECURRING_PAYMENTS = 2;
+		  /// <summary>
+		  ///     Constant for the recurring payment Service.
+		  /// </summary>
+		  public const int MESSAGE_SERVICE_RECURRING_PAYMENTS = 2;
 
-        /// <summary>
-        ///     Constant for the sync backup Service.
-        /// </summary>
-        public const int MESSAGE_SERVICE_SYNC_BACKUP = 3;
+		  /// <summary>
+		  ///     Constant for the sync backup Service.
+		  /// </summary>
+		  public const int MESSAGE_SERVICE_SYNC_BACKUP = 3;
 
-        Handler handler;
-        private ClearPaymentsJob clearPaymentsJob;
-        private RecurringPaymentJob recurringPaymentJob;
+		  /// <summary>
+		  ///    Constant for the add expense shortcut
+		  /// </summary>
+		  const string ACTION_ADD_EXPENSE_VIEW = "com.applysolutions.moneyfox.shortcuts.ADD_EXPENSE";
 
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+		  /// <summary>
+		  ///	  Constant for the add income shortcut
+		  /// </summary>
+		  const string ACTION_ADD_INCOME_VIEW = "com.applysolutions.moneyfox.shortcuts.ADD_INCOME";
 
-            base.OnCreate(bundle);
+		  /// <summary>
+		  ///	  Constant for the add income shortcut
+		  /// </summary>
+		  const string ACTION_ADD_TRANSFER_VIEW = "com.applysolutions.moneyfox.shortcuts.ADD_TRANSFER";
 
-            Popup.Init(this, bundle);
+		  Handler handler;
+		  private ClearPaymentsJob clearPaymentsJob;
+		  private RecurringPaymentJob recurringPaymentJob;
 
-            // Handler to create jobs.
-            handler = new Handler(msg => {
-                switch (msg.What)
-                {
-                    case MESSAGE_SERVICE_CLEAR_PAYMENTS:
-                        clearPaymentsJob = (ClearPaymentsJob)msg.Obj;
-                        clearPaymentsJob.ScheduleTask();
-                        break;
-                    case MESSAGE_SERVICE_RECURRING_PAYMENTS:
-                        recurringPaymentJob = (RecurringPaymentJob)msg.Obj;
-                        recurringPaymentJob.ScheduleTask();
-                        break;
-                }
-            });
 
-            // Start services and provide it a way to communicate with us.
-            var startServiceIntentClearPayment = new Intent(this, typeof(ClearPaymentsJob));
-            startServiceIntentClearPayment.PutExtra("messenger", new Messenger(handler));
-            StartService(startServiceIntentClearPayment);
+		  protected override void OnCreate(Bundle bundle)
+		  {
+			   TabLayoutResource = Resource.Layout.Tabbar;
+			   ToolbarResource = Resource.Layout.Toolbar;
 
-            var startServiceIntentRecurringPayment = new Intent(this, typeof(RecurringPaymentJob));
-            startServiceIntentRecurringPayment.PutExtra("messenger", new Messenger(handler));
-            StartService(startServiceIntentRecurringPayment);
+			   base.OnCreate(bundle);
 
-            if (Mvx.IoCProvider.CanResolve<IBackgroundTaskManager>() && Mvx.IoCProvider.CanResolve<ISettingsManager>())
-            {
-                Mvx.IoCProvider.Resolve<IBackgroundTaskManager>()
-                   .StartBackupSyncTask(Mvx.IoCProvider.Resolve<ISettingsManager>().BackupSyncRecurrence);
-            }
-        }
+			   Popup.Init(this, bundle);
 
-        public override void OnBackPressed()
-        {
-            Popup.SendBackPressed(base.OnBackPressed);
-        }
-    }
+			   // Handler to create jobs.
+			   handler = new Handler(msg =>
+			   {
+					switch (msg.What)
+					{
+						 case MESSAGE_SERVICE_CLEAR_PAYMENTS:
+							  clearPaymentsJob = (ClearPaymentsJob)msg.Obj;
+							  clearPaymentsJob.ScheduleTask();
+							  break;
+						 case MESSAGE_SERVICE_RECURRING_PAYMENTS:
+							  recurringPaymentJob = (RecurringPaymentJob)msg.Obj;
+							  recurringPaymentJob.ScheduleTask();
+							  break;
+					}
+			   });
+
+			   // Start services and provide it a way to communicate with us.
+			   var startServiceIntentClearPayment = new Intent(this, typeof(ClearPaymentsJob));
+			   startServiceIntentClearPayment.PutExtra("messenger", new Messenger(handler));
+			   StartService(startServiceIntentClearPayment);
+
+			   var startServiceIntentRecurringPayment = new Intent(this, typeof(RecurringPaymentJob));
+			   startServiceIntentRecurringPayment.PutExtra("messenger", new Messenger(handler));
+			   StartService(startServiceIntentRecurringPayment);
+
+			   if (Mvx.IoCProvider.CanResolve<IBackgroundTaskManager>() && Mvx.IoCProvider.CanResolve<ISettingsManager>())
+			   {
+					Mvx.IoCProvider.Resolve<IBackgroundTaskManager>()
+					   .StartBackupSyncTask(Mvx.IoCProvider.Resolve<ISettingsManager>().BackupSyncRecurrence);
+			   }
+
+			   // If the user opened the app via one of the shortcuts,
+			   // navigate the user to the right page
+			   NavigateToShortcuts();
+		  }
+
+		  public override void OnBackPressed()
+		  {
+			   Popup.SendBackPressed(base.OnBackPressed);
+		  }
+
+		  private void NavigateToShortcuts()
+		  {
+			   switch (Intent.Action)
+			   {
+					case ACTION_ADD_EXPENSE_VIEW:
+						 {
+							  System.Diagnostics.Debug.WriteLine("AddExpense");
+							  Mvx.IoCProvider.Resolve<AccountListViewActionViewModel>().GoToAddExpenseCommand.Execute(null);
+						 }
+						 break;
+					case ACTION_ADD_INCOME_VIEW:
+						 {
+							  System.Diagnostics.Debug.WriteLine("AddIncome");
+							  Mvx.IoCProvider.Resolve<AccountListViewActionViewModel>().GoToAddIncomeCommand.Execute(null);
+						 }
+						 break;
+					case ACTION_ADD_TRANSFER_VIEW:
+						 {
+							  System.Diagnostics.Debug.WriteLine("AddTransfer");
+							  Mvx.IoCProvider.Resolve<AccountListViewActionViewModel>().GoToAddTransferCommand.Execute(null);
+						 }
+						 break;
+			   }
+		  }
+	 }
 }
 
--- Src/MoneyFox.Android/SplashScreen.cs
+++ Src/MoneyFox.Android/SplashScreen.cs
@@ -1,29 +1,36 @@
 ﻿using System.Threading.Tasks;
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using MoneyFox.Business.ViewModels;
+using MvvmCross;
 using MvvmCross.Forms.Platforms.Android.Views;
 
 namespace MoneyFox.Droid
 {
-    [Activity(
-        Label = "MoneyFox"
-        , MainLauncher = true
-        , Icon = "@drawable/ic_launcher"
-        , Theme = "@style/Theme.Splash"
-        , NoHistory = true
-        , ScreenOrientation = ScreenOrientation.Portrait)]
-    public class SplashScreen : MvxFormsSplashScreenActivity<Setup, CoreApp, App>
-    {
-        public SplashScreen()
-            : base(Resource.Layout.SplashScreen)
-        {
-        }
+	 [Activity(
+		 Label = "MoneyFox"
+		 , MainLauncher = true
+		 , Icon = "@drawable/ic_launcher"
+		 , Theme = "@style/Theme.Splash"
+		 , NoHistory = true
+		 , ScreenOrientation = ScreenOrientation.Portrait,
+		  Name = "com.applysolutions.moneyfox.SplashScreen")]
+	 [MetaData("android.app.shortcuts", Resource = "@xml/shortcuts")]
+	 public class SplashScreen : MvxFormsSplashScreenActivity<Setup, CoreApp, App>
+	 {
+		  public SplashScreen()
+			  : base(Resource.Layout.SplashScreen)
+		  {
+		  }
 
-        protected override Task RunAppStartAsync(Bundle bundle)
-        {
-            StartActivity(typeof(MainActivity));
-            return Task.CompletedTask;
-        }
-    }
+		  protected override Task RunAppStartAsync(Bundle bundle)
+		  {
+			   Intent intent = new Intent(this, typeof(MainActivity));
+			   intent.SetAction(Intent.Action);
+			   StartActivity(intent);
+			   return Task.CompletedTask;
+		  }
+	 }
 }
