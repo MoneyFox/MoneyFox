a549fab8936fdbb90c9ea1a5d8d8d9367c4ad9dd
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-01-26 13:39:06 +0100

    declarations

---
+++ Src/MoneyFox.BusinessLogic/Managers/ClearPaymentManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using MoneyFox.DataAccess.DataServices;
+
+namespace MoneyFox.Business.Manager
+{
+    /// <summary>
+    ///     Manager to handle payment clearing.
+    /// </summary>
+    public interface IClearPaymentManager
+    {
+        /// <summary>
+        ///     Clears all payments up for clearing.
+        /// </summary>
+        void ClearPayments();
+    }
+
+    /// <inheritdoc />
+    public class ClearPaymentManager : IClearPaymentManager
+    {
+        private readonly IPaymentService paymentService;
+
+        public ClearPaymentManager(IPaymentService paymentService)
+        {
+            this.paymentService = paymentService;
+        }
+
+        /// <inheritdoc />
+        public async void ClearPayments()
+        {
+            var payments = await paymentService.GetUnclearedPayments(DateTime.Now);
+            var paymentList = payments.ToList();
+
+            foreach (var payment in paymentList)
+            {
+                payment.ClearPayment();
+            }
+
+            await paymentService.SavePayments(paymentList.ToArray());
+        }
+    }
+}
