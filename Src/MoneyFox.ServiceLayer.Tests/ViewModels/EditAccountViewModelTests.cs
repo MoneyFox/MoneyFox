e5b92a8425f231bdee2a919ca91367ac250928ac
Nino Padrutt <nino.padrutt@post.ch>
2019-01-30 14:43:03 +0100

    add a new unit test.

---
+++ Src/MoneyFox.ServiceLayer.Tests/ViewModels/EditAccountViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿using GenericServices;
+using MoneyFox.ServiceLayer.Parameters;
+using MoneyFox.ServiceLayer.ViewModels;
+using Moq;
+using Xunit;
+
+namespace MoneyFox.ServiceLayer.Tests.ViewModels
+{
+    public class EditAccountViewModelTests
+    {
+        [Fact]
+        public void Prepare_AccountLoaded()
+        {
+            // Arrange
+            const int accoundId = 99;
+            var crudServiceMock = new Mock<ICrudServicesAsync>();
+
+            var editAccountVm = new EditAccountViewModel(crudServiceMock.Object, null, null, null, null, null);
+
+            // Act
+            editAccountVm.Prepare(new ModifyAccountParameter(accoundId));
+
+            // Assert
+            crudServiceMock.Verify(x => x.ReadSingleAsync<AccountViewModel>(accoundId), Times.Once);
+        }
+    }
+}
