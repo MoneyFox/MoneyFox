a8d99eb870ea80ed05b484cb308cf93d1cd8653f
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-02-01 23:49:56 +0100

    remove the custom style and XF use control for categories.

--- Src/MoneyFox.Uwp/Views/EditCategoryView.xaml.cs
+++ Src/MoneyFox.Uwp/Views/EditCategoryView.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace MoneyFox.Uwp.Views
+﻿using Windows.UI.Xaml;
+using MoneyFox.Presentation.Views;
+using Xamarin.Forms.Platform.UWP;
+
+namespace MoneyFox.Uwp.Views
 {
     public sealed partial class EditCategoryView
     {
         public EditCategoryView()
         {
             InitializeComponent();
+        }
+
+        private void EditCategoryView_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ContentGrid.Children.Clear();
+            ContentGrid.Children.Add(new EditCategoryPage { DataContext = ViewModel }.CreateFrameworkElement());
         }
     }
 }
