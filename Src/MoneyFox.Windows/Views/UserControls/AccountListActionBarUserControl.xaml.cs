d7b51ba85c823ac384dc5806098bc3fe4ebfc07e
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-04-20 23:48:32 +0200

    add bindings.

--- Src/MoneyFox.Windows/Views/UserControls/AccountListActionBarUserControl.xaml.cs
+++ Src/MoneyFox.Windows/Views/UserControls/AccountListActionBarUserControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reactive.Disposables;
 using MoneyFox.ServiceLayer.ViewModels;
 using ReactiveUI;
+using Splat;
 
 namespace MoneyFox.Windows.Views.UserControls
 {
@@ -12,6 +13,8 @@ namespace MoneyFox.Windows.Views.UserControls
         public AccountListActionBarUserControl() {
             this.InitializeComponent();
 
+            ViewModel = Locator.Current.GetService<AccountListActionBarViewModel>();
+
             this.WhenActivated(disposables =>
             {
                 this.BindCommand(ViewModel, vm => vm.GoToAddIncomeCommand, v => v.AddIncomeButton.Command)
@@ -22,6 +25,11 @@ namespace MoneyFox.Windows.Views.UserControls
                     .DisposeWith(disposables);
                 this.BindCommand(ViewModel, vm => vm.GoToAddAccountCommand, v => v.AddAccountButton.Command)
                     .DisposeWith(disposables);
+
+                this.OneWayBind(ViewModel, vm => vm.Resources["AddIncomeTitle"], v => v.AddIncomeButton.Label);
+                this.OneWayBind(ViewModel, vm => vm.Resources["AddExpenseTitle"], v => v.AddExpenseButton.Label);
+                this.OneWayBind(ViewModel, vm => vm.Resources["AddTransferTitle"], v => v.AddTransferButton.Label);
+                this.OneWayBind(ViewModel, vm => vm.Resources["AddAccountTitle"], v => v.AddAccountButton.Label);
             });
         }
     }
