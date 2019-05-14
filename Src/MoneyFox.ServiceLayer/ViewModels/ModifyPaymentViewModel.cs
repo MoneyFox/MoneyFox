18d5a4e62e90a74dcb16c673376e043c1b534837
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-05-14 18:08:29 +0200

    first implement select category.

--- Src/MoneyFox.ServiceLayer/ViewModels/ModifyPaymentViewModel.cs
+++ Src/MoneyFox.ServiceLayer/ViewModels/ModifyPaymentViewModel.cs
@@ -67,7 +67,6 @@ namespace MoneyFox.ServiceLayer.ViewModels
                               .Subscribe()
                               .DisposeWith(disposable);
 
-                //SelectedItemChangedCommand = ReactiveCommand.Create(UpdateOtherComboBox).DisposeWith(disposable);
                 SaveCommand = ReactiveCommand.CreateFromTask(SavePaymentBase).DisposeWith(disposable);
                 GoToSelectCategoryDialogCommand =ReactiveCommand.Create(OpenSelectCategoryList).DisposeWith(disposable);
                 CancelCommand = ReactiveCommand.Create(Cancel).DisposeWith(disposable);
@@ -181,39 +180,10 @@ namespace MoneyFox.ServiceLayer.ViewModels
             SelectedPayment.Category = message.SelectedCategory;
         }
 
-        private void OpenSelectCategoryList()
-        {
-            HostScreen.Router.Navigate.Execute(new SelectCategoryListViewModel(HostScreen));
-        }
+        private void OpenSelectCategoryList() => HostScreen.Router.Navigate.Execute(new SelectCategoryListViewModel(HostScreen));
 
-        private void ResetSelection()
-        {
-            SelectedPayment.Category = null;
-        }
+        private void ResetSelection() => SelectedPayment.Category = null;
 
-        private void Cancel()
-        {
-            HostScreen.Router.NavigateBack.Execute();
-        }
-
-        //private void UpdateOtherComboBox()
-        //{
-        //    var tempCollection = new ObservableCollection<AccountViewModel>(ChargedAccounts);
-        //    foreach (AccountViewModel account in TargetAccounts)
-        //    {
-        //        if (!tempCollection.Contains(account)) tempCollection.Add(account);
-        //    }
-
-        //    foreach (AccountViewModel account in tempCollection)
-        //    {
-        //        //fills targetaccounts
-        //        if (!TargetAccounts.Contains(account)) TargetAccounts.Add(account);
-
-        //        //fills chargedaccounts
-        //        if (!ChargedAccounts.Contains(account)) ChargedAccounts.Add(account);
-        //    }
-
-        //    TargetAccounts.Remove(selectedPayment.ChargedAccount);
-        //}
+        private void Cancel() => HostScreen.Router.NavigateBack.Execute();
     }
 }
---
+++ Src/MoneyFox.Windows/Views/SelectCategoryListView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Windows.System;
+using Windows.UI.Xaml;
+using MoneyFox.ServiceLayer.ViewModels;
+using ReactiveUI;
+
+namespace MoneyFox.Windows.Views
+{
+    public class MySelectCategoryListView: ReactiveView<SelectCategoryListViewModel> { }
+
+    public sealed partial class SelectCategoryListView
+    {
+        public SelectCategoryListView()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(disposables =>
+            {
+                CategoryListControl.ViewModel = ViewModel;
+
+                this.OneWayBind(ViewModel, vm => vm.Resources["SelectCategoryTitle"], v => v.TitlePage.Text).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.Resources["AddLabel"], v => v.AddCategoryButton.Label).DisposeWith(disposables); 
+
+                this.Events()
+                    .KeyDown
+                    .Where(e => e.Key == VirtualKey.Enter)
+                    .Do(x => ViewModel.ItemClickCommand.Execute(ViewModel.SelectedCategory));
+            });
+        }
+    }
+}
