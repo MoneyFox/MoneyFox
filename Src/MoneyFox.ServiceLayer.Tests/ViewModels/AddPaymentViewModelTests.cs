1c181c35d2568d42cfe5b862af61193118645cda
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-05-24 13:47:42 +0200

    add and fix unit tests

--- Src/MoneyFox.ServiceLayer.Tests/ViewModels/AddPaymentViewModelTests.cs
+++ Src/MoneyFox.ServiceLayer.Tests/ViewModels/AddPaymentViewModelTests.cs
@@ -129,6 +129,38 @@ namespace MoneyFox.ServiceLayer.Tests.ViewModels
             dialogServiceMock.Verify(x => x.ShowMessage(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
             navigationServiceMock.Verify(x => x.Close(It.IsAny<MvxViewModel>(), CancellationToken.None), Times.Once);
             settingsFacadeMock.VerifySet(x => x.LastExecutionTimeStampSyncBackup = It.IsAny<DateTime>(), Times.Once);
+            backupServiceMock.Verify(x => x.EnqueueBackupTask(0), Times.Never);
+        }
+
+        [Fact]
+        public void SavePayment_ResultSucceededWithBackup_CorrectMethodCalls()
+        {
+            // Arrange
+            paymentServiceMock.Setup(x => x.SavePayment(It.IsAny<PaymentViewModel>()))
+                .ReturnsAsync(OperationResult.Succeeded());
+
+            settingsFacadeMock.SetupGet(x => x.IsBackupAutouploadEnabled).Returns(true);
+
+            var addPaymentVm = new AddPaymentViewModel(paymentServiceMock.Object,
+                crudServiceMock.Object,
+                dialogServiceMock.Object,
+                settingsFacadeMock.Object,
+                new Mock<IMvxMessenger>().Object,
+                backupServiceMock.Object,
+                new Mock<IMvxLogProvider>().Object,
+                navigationServiceMock.Object);
+
+            addPaymentVm.Prepare(new ModifyPaymentParameter());
+            addPaymentVm.SelectedPayment.ChargedAccount = new AccountViewModel();
+
+            // Act
+            addPaymentVm.SaveCommand.Execute();
+
+            // Assert
+            paymentServiceMock.Verify(x => x.SavePayment(It.IsAny<PaymentViewModel>()), Times.Once);
+            dialogServiceMock.Verify(x => x.ShowMessage(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+            navigationServiceMock.Verify(x => x.Close(It.IsAny<MvxViewModel>(), CancellationToken.None), Times.Once);
+            settingsFacadeMock.VerifySet(x => x.LastExecutionTimeStampSyncBackup = It.IsAny<DateTime>(), Times.Once);
             backupServiceMock.Verify(x => x.EnqueueBackupTask(0), Times.Once);
         }
 
@@ -138,6 +170,37 @@ namespace MoneyFox.ServiceLayer.Tests.ViewModels
             // Arrange
             paymentServiceMock.Setup(x => x.SavePayment(It.IsAny<PaymentViewModel>()))
                 .ReturnsAsync(OperationResult.Failed(""));
+
+            var addPaymentVm = new AddPaymentViewModel(paymentServiceMock.Object,
+                crudServiceMock.Object,
+                dialogServiceMock.Object,
+                settingsFacadeMock.Object,
+                new Mock<IMvxMessenger>().Object,
+                backupServiceMock.Object,
+                new Mock<IMvxLogProvider>().Object,
+                navigationServiceMock.Object);
+
+            addPaymentVm.Prepare(new ModifyPaymentParameter());
+            addPaymentVm.SelectedPayment.ChargedAccount = new AccountViewModel();
+
+            // Act
+            addPaymentVm.SaveCommand.Execute();
+
+            // Assert
+            paymentServiceMock.Verify(x => x.SavePayment(It.IsAny<PaymentViewModel>()), Times.Once);
+            dialogServiceMock.Verify(x => x.ShowMessage(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+            navigationServiceMock.Verify(x => x.Close(It.IsAny<MvxViewModel>(), CancellationToken.None), Times.Never);
+            settingsFacadeMock.VerifySet(x => x.LastExecutionTimeStampSyncBackup = It.IsAny<DateTime>(), Times.Once);
+            backupServiceMock.Verify(x => x.EnqueueBackupTask(0), Times.Never);
+        }
+        [Fact]
+        public void SavePayment_ResultFailedWithBackup_CorrectMethodCalls()
+        {
+            // Arrange
+            paymentServiceMock.Setup(x => x.SavePayment(It.IsAny<PaymentViewModel>()))
+                .ReturnsAsync(OperationResult.Failed(""));
+
+            settingsFacadeMock.SetupGet(x => x.IsBackupAutouploadEnabled).Returns(true);
 
             var addPaymentVm = new AddPaymentViewModel(paymentServiceMock.Object,
                 crudServiceMock.Object,
