2f6858f453d3e602a6a5566e0b3f4d82d1e4bb51
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-05-31 22:25:23 +0200

    fix some crashes.

--- Src/MoneyFox.Windows/Views/PaymentListView.xaml.cs
+++ Src/MoneyFox.Windows/Views/PaymentListView.xaml.cs
@@ -19,7 +19,8 @@ namespace MoneyFox.Windows.Views
             {
                 PaymentListUserControl.ViewModel = ViewModel;
 
-                this.OneWayBind(ViewModel, vm => vm.Title, v => TitlePage.Text).DisposeWith(disposables);
+               // this.OneWayBind(ViewModel, vm => vm.Title, v => TitlePage.Text).DisposeWith(disposables);
+
                 this.OneWayBind(ViewModel, vm => vm.Resources["FilterLabel"], v => v.FilterButton.Label).DisposeWith(disposables);
                 this.OneWayBind(ViewModel, vm => vm.Resources["AddIncomeLabel"], v => v.AddIncomeButton.Label).DisposeWith(disposables);
                 this.OneWayBind(ViewModel, vm => vm.Resources["AddExpenseLabel"], v => v.AddExpenseButton.Label).DisposeWith(disposables);
