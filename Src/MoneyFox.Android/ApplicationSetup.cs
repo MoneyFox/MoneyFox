2170d2fb1c5995ed466d29b516a452737c2da168
Nino Padrutt <nino.padrutt@post.ch>
2019-06-03 11:49:02 +0200

    remove serilog from android

--- Src/MoneyFox.Android/ApplicationSetup.cs
+++ Src/MoneyFox.Android/ApplicationSetup.cs
@@ -7,10 +7,7 @@ using MvvmCross;
 using MvvmCross.Forms.Platforms.Android.Core;
 using MvvmCross.Forms.Presenters;
 using MvvmCross.IoC;
-using MvvmCross.Logging;
 using Plugin.SecureStorage;
-using Serilog;
-using Serilog.Events;
 
 namespace MoneyFox.Droid
 {
@@ -36,19 +33,6 @@ namespace MoneyFox.Droid
             var formsPresenter = base.CreateFormsPagePresenter(viewPresenter);
             Mvx.IoCProvider.RegisterSingleton(formsPresenter);
             return formsPresenter;
-        }
-
-        public override MvxLogProviderType GetDefaultLogProviderType() => MvxLogProviderType.Serilog;
-
-        protected override IMvxLogProvider CreateLogProvider()
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.AndroidLog(LogEventLevel.Verbose)
-                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Month)
-                .CreateLogger();
-
-            return base.CreateLogProvider();
         }
     }
 }
