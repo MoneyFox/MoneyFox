4e627db0a3f84aba4eb82c6db9a67442b569fe39
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-06-04 23:06:30 +0200

    add vm locator and navigationservice.

--- Src/MoneyFox.Presentation/App.xaml.cs
+++ Src/MoneyFox.Presentation/App.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using MoneyFox.Presentation.Views;
+using MoneyFox.Views;
 using NLog;
+using Xamarin.Forms;
 
 namespace MoneyFox.Presentation
 {
@@ -14,6 +19,37 @@ namespace MoneyFox.Presentation
             {
                 LogManager.GetCurrentClassLogger().Fatal(e);
             };
+
+            var navigationService = ConfigureNavigation();
+            SimpleIoc.Default.Register<INavigationService>(() => navigationService);
+
+            var mainPage = new NavigationPage(new MainPage());
+            navigationService.Initialize(mainPage);
+            MainPage = mainPage;
+        }
+
+        public NavigationService ConfigureNavigation()
+        {
+            var nav = new NavigationService();
+
+            nav.Configure(Locator.AccountList, typeof(AccountListPage));
+            nav.Configure(Locator.CategoryList, typeof(CategoryListPage));
+            nav.Configure(Locator.SelectCategoryList, typeof(SelectCategoryPage));
+            nav.Configure(Locator.AddAccount, typeof(AddAccountPage));
+            nav.Configure(Locator.AddCategory, typeof(AddCategoryPage));
+            nav.Configure(Locator.AddPayment, typeof(AddPaymentPage));
+            nav.Configure(Locator.EditAccount, typeof(EditAccountPage));
+            nav.Configure(Locator.EditCategory, typeof(EditCategoryPage));
+            nav.Configure(Locator.EditPayment, typeof(EditPaymentPage));
+            nav.Configure(Locator.SettingsBackgroundJob, typeof(BackgroundJobSettingsPage));
+            nav.Configure(Locator.SettingsPersonalization, typeof(SettingsPersonalizationPage));
+            nav.Configure(Locator.StatisticSelector, typeof(StatisticSelectorPage));
+            nav.Configure(Locator.StatisticCashFlow, typeof(StatisticCashFlowPage));
+            nav.Configure(Locator.StatisticCategorySpreading, typeof(StatisticCategorySpreadingPage));
+            nav.Configure(Locator.StatisticCategorySummary, typeof(StatisticCategorySummaryPage));
+            nav.Configure(Locator.About, typeof(AboutPage));
+
+            return nav;
         }
     }
 }
