34e6285b376cb204cc645ed0cc178c6fc0bf996d
Nino Padrutt <nino.padrutt@post.ch>
2019-06-06 15:49:58 +0200

    register more dependencies.

--- Src/MoneyFox.BusinessLogic/BusinessLogicModule.cs
+++ Src/MoneyFox.BusinessLogic/BusinessLogicModule.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 using Autofac;
 using MoneyFox.BusinessDbAccess;
 
@@ -9,6 +9,26 @@ namespace MoneyFox.BusinessLogic
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterModule<BusinessDbAccessModule>();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                   .Where(t => t.Name.EndsWith("Action", StringComparison.CurrentCultureIgnoreCase))
+                   .AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                   .Where(t => t.Name.EndsWith("Provider", StringComparison.CurrentCultureIgnoreCase))
+                   .AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                   .Where(t => t.Name.EndsWith("Manager", StringComparison.CurrentCultureIgnoreCase))
+                   .AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                   .Where(t => t.Name.EndsWith("Service", StringComparison.CurrentCultureIgnoreCase))
+                   .AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                   .Where(t => t.Name.EndsWith("Adapter", StringComparison.CurrentCultureIgnoreCase))
+                   .AsImplementedInterfaces();
         }
     }
 }
--- Src/MoneyFox.ServiceLayer/ServiceModule.cs
+++ Src/MoneyFox.ServiceLayer/ServiceModule.cs
@@ -8,6 +8,14 @@ namespace MoneyFox.ServiceLayer
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterAssemblyTypes(ThisAssembly)
+                   .Where(t => t.Name.EndsWith("Service", StringComparison.CurrentCultureIgnoreCase))
+                   .AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                   .Where(t => t.Name.EndsWith("Facade", StringComparison.CurrentCultureIgnoreCase))
+                   .AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
                    .Where(t => t.Name.EndsWith("ViewModel", StringComparison.CurrentCultureIgnoreCase))
                    .AsImplementedInterfaces();
 
