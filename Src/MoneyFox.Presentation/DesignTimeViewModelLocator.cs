433fbf5a97d8918ceda2e2668adaa574a76496c7
Nino Padrutt <nino.padrutt@post.ch>
2019-06-13 16:16:20 +0200

    add design time vm locator.

---
+++ Src/MoneyFox.Presentation/DesignTimeViewModelLocator.cs
@@ -0,0 +1,13 @@
+﻿using MoneyFox.ServiceLayer.ViewModels.DesignTime;
+
+namespace MoneyFox.Presentation
+{
+    public static class DesignTimeViewModelLocator
+    {
+        static DesignTimeAboutViewModel aboutVm;
+        static DesignTimeAccountListViewModel accountListVm;
+
+        public static DesignTimeAboutViewModel AboutVm => aboutVm ?? (aboutVm = new DesignTimeAboutViewModel());
+        public static DesignTimeAccountListViewModel AccountListVm => accountListVm ?? (accountListVm = new DesignTimeAccountListViewModel());
+    }
+}
