4164890fcdbec5148925153a256dfd618304f0f6
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-06-23 16:55:16 +0200

    some visual adjustments.

--- Src/MoneyFox.Presentation/Controls/CardView.cs
+++ Src/MoneyFox.Presentation/Controls/CardView.cs
@@ -7,6 +7,14 @@ namespace MoneyFox.Presentation.Controls
         public CardView()
         {
             Padding = 0;
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                HasShadow = false;
+                BorderColor = Color.Transparent;
+                BackgroundColor = Color.Transparent;
+                CornerRadius = 0;
+            }
         }
 
         public static readonly BindableProperty ShadowRadiusProperty = BindableProperty.Create(
