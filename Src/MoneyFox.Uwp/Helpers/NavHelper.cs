a76050542cd3324a2c1574306788c26b2b254ba2
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-06-23 21:54:33 +0200

    fix code smell.

--- Src/MoneyFox.Uwp/Helpers/NavHelper.cs
+++ Src/MoneyFox.Uwp/Helpers/NavHelper.cs
@@ -3,7 +3,7 @@ using Windows.UI.Xaml;
 
 namespace MoneyFox.Uwp.Helpers
 {
-    public class NavHelper
+    public static class NavHelper
     {
         public static string GetNavigateTo(NavigationViewItem item)
         {
