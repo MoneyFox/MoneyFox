9a1aa0cb3e7b3db19d7e1ae9319ecaa7920a2491
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-06-23 16:41:06 +0200

    add parameter passings to windows

--- Src/MoneyFox.Uwp/Views/EditCategoryView.xaml.cs
+++ Src/MoneyFox.Uwp/Views/EditCategoryView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
 using MoneyFox.Presentation.Views;
 using Xamarin.Forms.Platform.UWP;
 
@@ -6,12 +7,16 @@ namespace MoneyFox.Uwp.Views
 {
     public sealed partial class EditCategoryView
     {
-        private readonly int categoryId;
+        private int categoryId;
 
-        public EditCategoryView(int categoryId)
+        public EditCategoryView()
         {
             InitializeComponent();
-            this.categoryId = categoryId;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter != null) categoryId = (int)e.Parameter;
         }
 
         private void EditCategoryView_OnSizeChanged(object sender, SizeChangedEventArgs e)
