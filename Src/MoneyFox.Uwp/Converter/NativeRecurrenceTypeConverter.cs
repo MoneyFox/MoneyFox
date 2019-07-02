fe0a9cc3d87e2c55f8022ed1699e0d3a896b9621
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-07-02 19:18:57 +0200

    extract converter logic

--- Src/MoneyFox.Uwp/Converter/NativeRecurrenceTypeConverter.cs
+++ Src/MoneyFox.Uwp/Converter/NativeRecurrenceTypeConverter.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Windows.UI.Xaml.Data;
 using MoneyFox.Foundation;
-using MoneyFox.Foundation.Resources;
+using MoneyFox.Presentation.Converter;
 
 namespace MoneyFox.Uwp.Converter
 {
@@ -9,32 +9,7 @@ namespace MoneyFox.Uwp.Converter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            var passedEnum = (PaymentRecurrence)value;
-
-            switch (passedEnum)
-            {
-                case PaymentRecurrence.Daily:
-                    return Strings.DailyLabel;
-                case PaymentRecurrence.DailyWithoutWeekend:
-                    return Strings.DailyWithoutWeekendLabel;
-                case PaymentRecurrence.Weekly:
-                    return Strings.WeeklyLabel;
-                case PaymentRecurrence.Biweekly:
-                    return Strings.BiweeklyLabel;
-                case PaymentRecurrence.Monthly:
-                    return Strings.MonthlyLabel;
-                case PaymentRecurrence.Bimonthly:
-                    return Strings.BimonthlyLabel;
-                case PaymentRecurrence.Quarterly:
-                    return Strings.QuarterlyLabel;
-                case PaymentRecurrence.Biannually:
-                    return Strings.BiannuallyLabel;
-                case PaymentRecurrence.Yearly:
-                    return Strings.YearlyLabel;
-
-                default:
-                    return string.Empty;
-            }
+            return RecurrenceTypeConverterLogic.GetStringForPaymentRecurrence((PaymentRecurrence)value);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
