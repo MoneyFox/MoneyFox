706d1813c3a0e870c891b24637e89c8e7ec7c9f3
Nino Padrutt <nino.padrutt@post.ch>
2019-07-03 17:16:35 +0200

    Anpassung Test

---
+++ Src/MoneyFox.Application.Tests/Statistics/Queries/GetCashFlowQueryHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using MoneyFox.Application.Statistics.Queries;
+using MoneyFox.Domain;
+using MoneyFox.Domain.Entities;
+using MoneyFox.Domain.Exceptions;
+using MoneyFox.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MoneyFox.Application.Tests.Statistics.Queries
+{
+    [Collection("QueryCollection")]
+    [ExcludeFromCodeCoverage]
+    public class GetCashFlowQueryHandlerTests
+    {
+        private readonly EfCoreContext context;
+
+        public GetCashFlowQueryHandlerTests(EfCoreContext context)
+        {
+            this.context = context;
+
+            context.AddRange(new List<Payment>
+                            {
+                                new Payment(DateTime.Today, 60, PaymentType.Income, new Account("Foo1")),
+                                new Payment(DateTime.Today, 70, PaymentType.Income, new Account("Foo2")),
+                                new Payment(DateTime.Today, 50, PaymentType.Expense, new Account("Foo3")),
+                                new Payment(DateTime.Today, 40, PaymentType.Expense, new Account("Foo3"))
+                            });
+            context.SaveChanges();
+        }
+
+        [Fact]
+        public async Task GetValues_CorrectSums()
+        {
+            // Arrange
+
+            // Act
+            var result = await new CashFlowDataProvider(statisticDbAccess.Object)
+                .GetCashFlow(DateTime.Today.AddDays(-3), DateTime.Today.AddDays(3));
+
+            // Assert
+            result[0].Value.ShouldEqual(130);
+            result[1].Value.ShouldEqual(90);
+            result[2].Value.ShouldEqual(40);
+        }
+    }
+}
