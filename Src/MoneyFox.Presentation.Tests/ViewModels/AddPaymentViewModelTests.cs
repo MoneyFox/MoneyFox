40b78a42f78163e65a45dd19e98e2d3c1f8c32b3
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-07-23 18:55:07 +0200

    code cleanup

--- Src/MoneyFox.Presentation.Tests/ViewModels/AddPaymentViewModelTests.cs
+++ Src/MoneyFox.Presentation.Tests/ViewModels/AddPaymentViewModelTests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading.Tasks;
-using GalaSoft.MvvmLight.Views;
+﻿using GalaSoft.MvvmLight.Views;
 using GenericServices;
 using MockQueryable.Moq;
-using MoneyFox.BusinessLogic;
 using MoneyFox.Foundation;
 using MoneyFox.Foundation.Resources;
 using MoneyFox.Presentation.Facades;
@@ -14,13 +8,18 @@ using MoneyFox.Presentation.Services;
 using MoneyFox.Presentation.ViewModels;
 using Moq;
 using Should;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 using IDialogService = MoneyFox.Presentation.Interfaces.IDialogService;
 
 namespace MoneyFox.Presentation.Tests.ViewModels
 {
     [ExcludeFromCodeCoverage]
-    public class AddPaymentViewModelTests 
+    public class AddPaymentViewModelTests
     {
         private readonly Mock<IPaymentService> paymentServiceMock;
         private readonly Mock<ICrudServicesAsync> crudServiceMock;
@@ -135,7 +134,7 @@ namespace MoneyFox.Presentation.Tests.ViewModels
                                                        crudServiceMock.Object,
                                                        dialogServiceMock.Object,
                                                        settingsFacadeMock.Object,
-                                                       backupServiceMock.Object, 
+                                                       backupServiceMock.Object,
                                                        navigationServiceMock.Object);
 
             await addPaymentVm.InitializeCommand.ExecuteAsync();
