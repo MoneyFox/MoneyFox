251da9b9987af7c331054764aea964b564a2b26e
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-07-24 17:34:22 +0200

    mapper infrastructure

---
+++ Src/MoneyFox.Presentation.Tests/Collections/MapperCollection.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MoneyFox.Application.Infrastructure;
+using System;
+using Xunit;
+
+namespace MoneyFox.Presentation.Tests.Collections
+{
+    [CollectionDefinition("AutoMapperCollection")]
+    public class MapperCollection : ICollectionFixture<MapperCollectionFixture>
+    {
+    }
+
+    public class MapperCollectionFixture : IDisposable
+    {
+        public IMapper mapper { get; set; }
+
+        public MapperCollectionFixture() => mapper = AutoMapperFactory.Create();
+
+        public void Dispose() => mapper = null;
+    }
+}
---
+++ Src/MoneyFox.Presentation.Tests/Mapper/CategoryViewModelMapperTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MoneyFox.Domain.Entities;
+using MoneyFox.Presentation.Tests.Collections;
+using MoneyFox.Presentation.ViewModels;
+using Should;
+using Xunit;
+
+namespace MoneyFox.Presentation.Tests.Mapper
+{
+    [Collection("AutoMapperCollection")]
+    public class CategoryViewModelMapperTests
+    {
+        private readonly IMapper mapper;
+
+        public CategoryViewModelMapperTests(MapperCollectionFixture fixture)
+        {
+            mapper = fixture.mapper;
+        }
+
+        [Fact]
+        public void CategoryMapToViewModel()
+        {
+            // Arrange
+            var category = new Category("test");
+            
+            // Act
+            var result = mapper.Map<CategoryViewModel>(category);
+
+            // Assert
+            result.ShouldBeType<CategoryViewModel>();
+        }
+    }
+}
