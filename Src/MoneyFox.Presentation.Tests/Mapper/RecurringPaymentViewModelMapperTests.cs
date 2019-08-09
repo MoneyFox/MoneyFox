b77c7b41f160d559ddbfe268a608c5286d6136b6
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-08-09 22:19:20 +0200

    add unit test

--- Src/MoneyFox.Presentation.Tests/Mapper/RecurringPaymentViewModelMapperTests.cs
+++ Src/MoneyFox.Presentation.Tests/Mapper/RecurringPaymentViewModelMapperTests.cs
@@ -33,5 +33,25 @@ namespace MoneyFox.Presentation.Tests.Mapper
             // Assert
             result.ShouldBeType<RecurringPaymentViewModel>();
         }
+
+        [Fact]
+        public void RecurringPaymentFieldsCorrectlyMapped()
+        {
+            // Arrange
+            var recurringPayment = new RecurringPayment(DateTime.Now, 123, PaymentType.Expense, PaymentRecurrence.Daily, new Account("asdf"), "asdf", DateTime.Today, new Account("fasdf"), new Category("gfds"));
+
+            // Act
+            var result = mapper.Map<RecurringPaymentViewModel>(recurringPayment);
+
+            // Assert
+            result.EndDate.ShouldEqual(recurringPayment.EndDate);
+            result.Amount.ShouldEqual(recurringPayment.Amount);
+            result.Type.ShouldEqual(recurringPayment.Type);
+            result.Recurrence.ShouldEqual(recurringPayment.Recurrence);
+            result.Note.ShouldEqual(recurringPayment.Note);
+            result.ChargedAccount.Name.ShouldEqual(recurringPayment.ChargedAccount.Name);
+            result.Category.Name.ShouldEqual(recurringPayment.Category.Name);
+            result.EndDate.ShouldEqual(recurringPayment.EndDate);
+        }
     }
 }
