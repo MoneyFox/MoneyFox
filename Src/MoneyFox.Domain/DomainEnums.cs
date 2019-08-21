efd8a83553d87bfc33b335b5f5e97f778be8e0e6
Nino Padrutt <nino.padrutt@post.ch>
2019-08-21 14:53:49 +0200

    adjustments after merge.

--- Src/MoneyFox.Domain/DomainEnums.cs
+++ Src/MoneyFox.Domain/DomainEnums.cs
@@ -36,30 +36,4 @@
         BackgroundJob,
         About
     }
-
-    public enum TyleType
-    {
-        Income,
-        Expense,
-        Transfer
-    }
-
-    public enum ModifyOperation
-    {
-        Edit,
-        Delete
-    }
-
-    public enum AppTheme
-    {
-        Dark,
-        Light
-    }
-
-    public enum AppPlatform
-    {
-        Android,
-        iOS,
-        UWP
-    }
 }
