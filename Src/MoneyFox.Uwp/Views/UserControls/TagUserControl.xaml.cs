4576e55da71255d04589be391e21b122e36ff7b0
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-09-18 21:02:54 +0200

    add basic tag UI

---
+++ Src/MoneyFox.Uwp/Views/UserControls/TagUserControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MoneyFox.Uwp.Views.UserControls
+{
+    public sealed partial class TagUserControl : UserControl
+    {
+        public TagUserControl()
+        {
+            InitializeComponent();
+
+            WrapPanelContainer.Items.Add(new Button());
+            WrapPanelContainer.Items.Add(new Button());
+            WrapPanelContainer.Items.Add(new Button());
+        }
+    }
+}
