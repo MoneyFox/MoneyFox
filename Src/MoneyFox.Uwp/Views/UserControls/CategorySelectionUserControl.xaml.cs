0e49d8e2454bc452511e1374175ca209f0997644
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-09-18 20:30:30 +0200

    extract category selectoin box to user control.

---
+++ Src/MoneyFox.Uwp/Views/UserControls/CategorySelectionUserControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MoneyFox.Uwp.Views.UserControls
+{
+    public sealed partial class CategorySelectionUserControl : UserControl
+    {
+        public CategorySelectionUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
