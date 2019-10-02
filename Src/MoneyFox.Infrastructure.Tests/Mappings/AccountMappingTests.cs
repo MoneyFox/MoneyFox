811e1de68c31ed2f6f928e82bc46abb6325b05e6
Nino Padrutt <nino.padrutt@post.ch>
2019-10-02 12:21:58 +0200

    add mapping test for account

---
+++ Src/MoneyFox.Infrastructure.Tests/Mappings/AccountMappingTests.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MoneyFox.Domain.Entities;
+using MoneyFox.Presentation.Tests.Collections;
+using MoneyFox.Presentation.ViewModels;
+using Should;
+using Xunit;
+using MapperCollectionFixture = MoneyFox.Infrastructure.Tests.Collections.MapperCollectionFixture;
+
+namespace MoneyFox.Infrastructure.Tests.Mappings
+{
+    [Collection("AutoMapperCollection")]
+    public class AccountMappingTests
+    {
+        private IMapper mapper;
+
+        public AccountMappingTests(MapperCollectionFixture fixture) 
+        {
+            mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public void MapToViewModel() 
+        {
+            // Arrange
+            var account = new Account("Testname", 40, "My Note", true);
+
+            // Act
+            var result = mapper.Map<AccountViewModel>(account);
+
+            // Assert
+            result.Name.ShouldEqual(account.Name);
+            result.CurrentBalance.ShouldEqual(account.CurrentBalance);
+            result.Note.ShouldEqual(account.Note);
+            result.IsExcluded.ShouldEqual(account.IsExcluded);
+        }
+    }
+}
