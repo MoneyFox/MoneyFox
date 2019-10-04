1cc01714bf8eb7c6b2983478d5d4d717ecda80b9
Nino Padrutt <nino.padrutt@post.ch>
2019-10-04 13:09:10 +0200

    tests

--- Src/MoneyFox.Application.Tests/Payments/Query/GetPaymentsForAccountId/GetPaymentsForAccountIdQueryTests.cs
+++ Src/MoneyFox.Application.Tests/Payments/Query/GetPaymentsForAccountId/GetPaymentsForAccountIdQueryTests.cs
@@ -28,7 +28,7 @@ namespace MoneyFox.Application.Tests.Payments.Query.GetPaymentsForAccountId
         }
         
         [Fact]
-        public async Task GetPaymentsForAccountId_CategoryFound()
+        public async Task GetPaymentsForAccountId_CorrectAccountId_PaymentFound()
         {
             // Arrange
             var account = new Account("test", 80);
@@ -40,10 +40,31 @@ namespace MoneyFox.Application.Tests.Payments.Query.GetPaymentsForAccountId
             await context.SaveChangesAsync();
 
             // Act
-            var result = await new GetPaymentsForAccountIdQuery.Handler(context).Handle(new GetPaymentsForAccountIdQuery(account.Id), default);
+            var result = await new GetPaymentsForAccountIdQuery.Handler(context).Handle(
+                new GetPaymentsForAccountIdQuery(account.Id, DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1)), default);
 
             // Assert
             result.First().Id.ShouldEqual(payment1.Id);
+        }
+        
+        [Fact]
+        public async Task GetPaymentsForAccountId_CorrectDateRange_PaymentFound()
+        {
+            // Arrange
+            var account = new Account("test", 80);
+
+            var payment1 = new Payment(DateTime.Now.AddDays(-2), 20, PaymentType.Expense, account);
+            var payment2 = new Payment(DateTime.Now, 20, PaymentType.Expense, account);
+            await context.AddAsync(payment1);
+            await context.AddAsync(payment2);
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await new GetPaymentsForAccountIdQuery.Handler(context).Handle(
+                new GetPaymentsForAccountIdQuery(account.Id, DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1)), default);
+
+            // Assert
+            result.First().Id.ShouldEqual(payment2.Id);
         }
     }
 }
