8b0710613194da473188dfbbc994d49570747755
Nino Padrutt <nino.padrutt@post.ch>
2019-10-10 10:02:57 +0200

    start adding the time range to the statistic pages.

---
+++ Src/MoneyFox.Presentation/UserControls/StatisticDateRangeUserControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoneyFox.Presentation.UserControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StatisticDateRangeUserControl : ContentView
+    {
+        public StatisticDateRangeUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Src/MoneyFox.Uwp/Converter/ShortDateFormatConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml.Data;
+
+namespace MoneyFox.Uwp.Converter
+{
+    public class ShortDateFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            return ((DateTime)value).ToString("d", CultureInfo.InvariantCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
+    }
+}
