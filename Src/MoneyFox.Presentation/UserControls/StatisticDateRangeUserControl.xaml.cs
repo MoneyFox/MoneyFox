e8d02dbb7f1f624c40012304c7427ac38148a916
Nino Padrutt <nino.padrutt@post.ch>
2019-10-10 10:41:47 +0200

    implement date range in uwp where not already the case.

--- Src/MoneyFox.Presentation/UserControls/StatisticDateRangeUserControl.xaml.cs
+++ Src/MoneyFox.Presentation/UserControls/StatisticDateRangeUserControl.xaml.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms.Xaml;
 
 namespace MoneyFox.Presentation.UserControls
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class StatisticDateRangeUserControl : ContentView
+    public partial class StatisticDateRangeUserControl
     {
         public StatisticDateRangeUserControl()
         {
