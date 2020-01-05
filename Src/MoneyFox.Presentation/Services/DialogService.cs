882505412558a2d5d1f1ec0ec249dae82979f4bc
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-01-05 18:39:49 +0100

    fix issue with dialog

--- Src/MoneyFox.Presentation/Services/DialogService.cs
+++ Src/MoneyFox.Presentation/Services/DialogService.cs
@@ -1,7 +1,8 @@
 ﻿using MoneyFox.Application.Common.Interfaces;
-using System.Threading.Tasks;
 using MoneyFox.Application.Resources;
+using System.Threading.Tasks;
 using Xamarin.Forms;
+using XF.Material.Forms;
 using XF.Material.Forms.Resources;
 using XF.Material.Forms.UI.Dialogs;
 using XF.Material.Forms.UI.Dialogs.Configurations;
@@ -10,61 +11,79 @@ namespace MoneyFox.Presentation.Services
 {
     public class DialogService : IDialogService
     {
-        public async Task ShowMessage(string title, string message) {
-            await MaterialDialog.Instance.AlertAsync(message,
-                                                     title,
-                                                     Strings.OkLabel,
-                                                     GetAlertDialogConfiguration());
+        public async Task ShowMessage(string title, string message)
+        {
+            if(loadingDialog != null)
+            {
+                await HideLoadingDialogAsync();
+            }
+
+            await MaterialDialog.Instance.AlertAsync(message, title, Strings.OkLabel, GetAlertDialogConfiguration());
         }
 
-        public async Task<bool> ShowConfirmMessageAsync(string title, string message, string positiveButtonText = null, string negativeButtonText = null) {
+        public async Task<bool> ShowConfirmMessageAsync(string title,
+                                                        string message,
+                                                        string positiveButtonText = null,
+                                                        string negativeButtonText = null)
+        {
+            if(loadingDialog != null)
+            {
+                await HideLoadingDialogAsync();
+            }
 
-            bool? wasConfirmed = await MaterialDialog.Instance.ConfirmAsync(message,
-                                                     title,
-                                                     Strings.OkLabel,
-                                                     Strings.CancelLabel,
-                                                     GetAlertDialogConfiguration());
+            bool? wasConfirmed = await MaterialDialog.Instance
+                                                     .ConfirmAsync(message,
+                                                                   title,
+                                                                   Strings.OkLabel,
+                                                                   Strings.CancelLabel,
+                                                                   GetAlertDialogConfiguration());
 
             return wasConfirmed ?? false;
         }
 
         private IMaterialModalPage loadingDialog;
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         public async Task ShowLoadingDialogAsync(string message = null)
         {
-            loadingDialog = await MaterialDialog.Instance.LoadingDialogAsync(message: message ?? Strings.LoadingLabel, GetLoadingDialogConfiguration());
+            if(loadingDialog != null)
+            {
+                await HideLoadingDialogAsync();
+            }
+
+            loadingDialog = await MaterialDialog.Instance
+                                                .LoadingDialogAsync(message: message ?? Strings.LoadingLabel, GetLoadingDialogConfiguration());
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         public async Task HideLoadingDialogAsync()
         {
             await loadingDialog.DismissAsync();
         }
 
-        private static MaterialAlertDialogConfiguration GetAlertDialogConfiguration() 
+        private static MaterialAlertDialogConfiguration GetAlertDialogConfiguration()
         {
-            return new MaterialAlertDialogConfiguration 
-            {
-                BackgroundColor = XF.Material.Forms.Material.GetResource<Color>(MaterialConstants.Color.BACKGROUND),
-                TitleTextColor = XF.Material.Forms.Material.GetResource<Color>(MaterialConstants.Color.ON_PRIMARY),
-                MessageTextColor = XF.Material.Forms.Material.GetResource<Color>(MaterialConstants.Color.ON_PRIMARY).MultiplyAlpha(0.8),
-                TintColor = XF.Material.Forms.Material.GetResource<Color>(MaterialConstants.Color.ON_PRIMARY),
-                CornerRadius = 8,
-                ScrimColor = Color.FromHex("#232F34").MultiplyAlpha(0.32),
-                ButtonAllCaps = false
-            };
+            return new MaterialAlertDialogConfiguration
+                   {
+                       BackgroundColor = Material.GetResource<Color>(MaterialConstants.Color.BACKGROUND),
+                       TitleTextColor = Material.GetResource<Color>(MaterialConstants.Color.ON_PRIMARY),
+                       MessageTextColor = Material.GetResource<Color>(MaterialConstants.Color.ON_PRIMARY).MultiplyAlpha(0.8),
+                       TintColor = Material.GetResource<Color>(MaterialConstants.Color.ON_PRIMARY),
+                       CornerRadius = 8,
+                       ScrimColor = Color.FromHex("#232F34").MultiplyAlpha(0.32),
+                       ButtonAllCaps = false
+                   };
         }
-        private static MaterialLoadingDialogConfiguration GetLoadingDialogConfiguration() 
+        private static MaterialLoadingDialogConfiguration GetLoadingDialogConfiguration()
         {
             return new MaterialLoadingDialogConfiguration
-            {
-                BackgroundColor = XF.Material.Forms.Material.GetResource<Color>(MaterialConstants.Color.BACKGROUND),
-                MessageTextColor = XF.Material.Forms.Material.GetResource<Color>(MaterialConstants.Color.ON_PRIMARY).MultiplyAlpha(0.8),
-                TintColor = XF.Material.Forms.Material.GetResource<Color>(MaterialConstants.Color.ON_PRIMARY),
-                CornerRadius = 8,
-                ScrimColor = Color.FromHex("#232F34").MultiplyAlpha(0.32)
-            };
+                   {
+                       BackgroundColor = Material.GetResource<Color>(MaterialConstants.Color.BACKGROUND),
+                       MessageTextColor = Material.GetResource<Color>(MaterialConstants.Color.ON_PRIMARY).MultiplyAlpha(0.8),
+                       TintColor = Material.GetResource<Color>(MaterialConstants.Color.ON_PRIMARY),
+                       CornerRadius = 8,
+                       ScrimColor = Color.FromHex("#232F34").MultiplyAlpha(0.32)
+                   };
         }
     }
 }
