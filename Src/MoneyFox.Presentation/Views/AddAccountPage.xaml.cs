db6597e989ccbe0ec82adc16336bf5541d862b35
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-01-11 14:13:01 +0100

    replace icons with normal text buttons.

--- Src/MoneyFox.Presentation/Views/AddAccountPage.xaml.cs
+++ Src/MoneyFox.Presentation/Views/AddAccountPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using MoneyFox.Presentation.Utilities;
+﻿using MoneyFox.Application.Resources;
+using MoneyFox.Presentation.Utilities;
 using MoneyFox.Presentation.ViewModels;
+using Xamarin.Forms;
 
 namespace MoneyFox.Presentation.Views
 {
@@ -10,6 +12,17 @@ namespace MoneyFox.Presentation.Views
         public AddAccountPage()
         {
             InitializeComponent();
+
+            var saveItem = new ToolbarItem
+            {
+                Command = new Command(async () => await ViewModel.SaveCommand.ExecuteAsync()),
+                Text = Strings.SaveLabel,
+                Priority = 0,
+                Order = ToolbarItemOrder.Primary
+            };
+
+            ToolbarItems.Add(saveItem);
+
             BindingContext = ViewModelLocator.AddAccountVm;
             ViewModel.InitializeCommand.ExecuteAsync().FireAndForgetSafeAsync();
         }
