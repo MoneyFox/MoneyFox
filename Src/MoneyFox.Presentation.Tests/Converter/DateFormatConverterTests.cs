694480ff8ad6244962e3a8ae9038fc80147e5e36
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-01-22 17:59:35 +0100

    adjust langVersion

--- Src/MoneyFox.Presentation.Tests/Converter/DateFormatConverterTests.cs
+++ Src/MoneyFox.Presentation.Tests/Converter/DateFormatConverterTests.cs
@@ -14,7 +14,7 @@ namespace MoneyFox.Presentation.Tests.Converter
         public void Convert_DateTime_ValidString()
         {
             var date = new DateTime(2015, 09, 15, 14, 56, 48);
-            new DateTimeFormatConverter().Convert(date, null, null, null).ShouldEqual(date.ToString("D", CultureInfo.CurrentUICulture));
+            new DateTimeFormatConverter().Convert(date, null, null, null).ShouldEqual(date.ToString("D", CultureHelper.CurrentCulture));
         }
 
         [Fact]
