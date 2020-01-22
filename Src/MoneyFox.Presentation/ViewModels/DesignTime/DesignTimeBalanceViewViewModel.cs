9add8b1b10d9fbec8200c3fefac0c9e666771dee
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-01-22 19:43:13 +0100

    fix compile issue;

--- Src/MoneyFox.Presentation/ViewModels/DesignTime/DesignTimeBalanceViewViewModel.cs
+++ Src/MoneyFox.Presentation/ViewModels/DesignTime/DesignTimeBalanceViewViewModel.cs
@@ -12,6 +12,6 @@ namespace MoneyFox.Presentation.ViewModels.DesignTime
         public decimal EndOfMonthBalance => 9784;
 
         /// <inheritdoc />
-        public AsyncCommand UpdateBalanceCommand { get; };
+        public AsyncCommand UpdateBalanceCommand { get; }
     }
 }
