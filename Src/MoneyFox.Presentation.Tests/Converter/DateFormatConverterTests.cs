5a1e1b075fdb4f122c5bcb5ce235566718e1de5e
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-02-17 20:01:18 +0100

    add culture helper

--- Src/MoneyFox.Presentation.Tests/Converter/DateFormatConverterTests.cs
+++ Src/MoneyFox.Presentation.Tests/Converter/DateFormatConverterTests.cs
@@ -14,6 +14,7 @@ namespace MoneyFox.Presentation.Tests.Converter
         [Fact]
         public void Convert_DateTime_ValidString()
         {
+            CultureHelper.CurrentCulture = new CultureInfo("en-US");
             var date = new DateTime(2015, 09, 15, 14, 56, 48);
             new DateTimeFormatConverter().Convert(date, null, null, null).ShouldEqual(date.ToString("D", CultureHelper.CurrentCulture));
         }
