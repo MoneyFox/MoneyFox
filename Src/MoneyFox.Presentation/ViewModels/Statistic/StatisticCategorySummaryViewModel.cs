adc35e3d816880333264ab31d14ac902914df063
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-03-15 14:21:41 +0100

    add logging

--- Src/MoneyFox.Presentation/ViewModels/Statistic/StatisticCategorySummaryViewModel.cs
+++ Src/MoneyFox.Presentation/ViewModels/Statistic/StatisticCategorySummaryViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using MoneyFox.Application.Common.Facades;
 using MoneyFox.Application.Statistics.Queries.GetCategorySummary;
+using NLog;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +12,7 @@ namespace MoneyFox.Presentation.ViewModels.Statistic
     /// <inheritdoc cref="IStatisticCategorySummaryViewModel"/>
     public class StatisticCategorySummaryViewModel : StatisticViewModel, IStatisticCategorySummaryViewModel
     {
+        private Logger logger = LogManager.GetCurrentClassLogger();
         private ObservableCollection<CategoryOverviewViewModel> categorySummary;
 
         public StatisticCategorySummaryViewModel(IMediator mediator,
@@ -26,7 +29,7 @@ namespace MoneyFox.Presentation.ViewModels.Statistic
             get => incomeExpenseBalance;
             set
             {
-                if(incomeExpenseBalance == value)
+                if (incomeExpenseBalance == value)
                     return;
                 incomeExpenseBalance = value;
                 RaisePropertyChanged();
@@ -52,25 +55,29 @@ namespace MoneyFox.Presentation.ViewModels.Statistic
         /// </summary>
         protected override async Task Load()
         {
-            CategorySummaryModel categorySummaryModel =
-                await Mediator.Send(new GetCategorySummaryQuery { EndDate = EndDate, StartDate = StartDate });
-
-            CategorySummary = new ObservableCollection<CategoryOverviewViewModel>(
-                                                                                  categorySummaryModel
-                                                                                     .CategoryOverviewItems
-                                                                                     .Select(x => new CategoryOverviewViewModel
-                                                                                                  {
-                                                                                                      Value = x.Value,
-                                                                                                      Average = x.Average,
-                                                                                                      Label = x.Label,
-                                                                                                      Percentage = x.Percentage
-                                                                                                  }));
-
-            IncomeExpenseBalance = new IncomeExpenseBalanceViewModel
+            try
             {
-                TotalEarned = categorySummaryModel.TotalEarned,
-                TotalSpent = categorySummaryModel.TotalSpent
-            };
+                CategorySummaryModel categorySummaryModel = await Mediator.Send(new GetCategorySummaryQuery { EndDate = EndDate, StartDate = StartDate });
+
+                CategorySummary = new ObservableCollection<CategoryOverviewViewModel>(categorySummaryModel.CategoryOverviewItems.Select(x => new CategoryOverviewViewModel
+                {
+                    Value = x.Value,
+                    Average = x.Average,
+                    Label = x.Label,
+                    Percentage = x.Percentage
+                }));
+
+                IncomeExpenseBalance = new IncomeExpenseBalanceViewModel
+                {
+                    TotalEarned = categorySummaryModel.TotalEarned,
+                    TotalSpent = categorySummaryModel.TotalSpent
+                };
+            }
+            catch(Exception ex)
+            {
+                logger.Error(ex);
+                throw;
+            }
         }
     }
 }
