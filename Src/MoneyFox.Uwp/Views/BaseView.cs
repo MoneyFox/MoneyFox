edbb2085551e2b0337f04a41511f124a7bd30a1e
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-03-22 22:55:50 +0100

    add property to show header

--- Src/MoneyFox.Uwp/Views/BaseView.cs
+++ Src/MoneyFox.Uwp/Views/BaseView.cs
@@ -5,5 +5,6 @@ namespace MoneyFox.Uwp.Views
     public class BaseView : Page
     {
         public virtual string Header => string.Empty;
+        public virtual bool ShowHeader => false;
     }
 }
