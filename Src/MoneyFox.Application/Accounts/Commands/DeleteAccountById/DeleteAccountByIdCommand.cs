c4fcc60a606fd5fa9de1571f49d3238a9967c2e5
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-03-23 23:28:50 +0100

    fire and forgot for more.

--- Src/MoneyFox.Application/Accounts/Commands/DeleteAccountById/DeleteAccountByIdCommand.cs
+++ Src/MoneyFox.Application/Accounts/Commands/DeleteAccountById/DeleteAccountByIdCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using MoneyFox.Application.Common;
 using MoneyFox.Application.Common.CloudBackup;
 using MoneyFox.Application.Common.Facades;
 using MoneyFox.Application.Common.Interfaces;
@@ -39,7 +40,7 @@ namespace MoneyFox.Application.Accounts.Commands.DeleteAccountById
                 await contextAdapter.Context.SaveChangesAsync(cancellationToken);
 
                 settingsFacade.LastDatabaseUpdate = DateTime.Now;
-                await backupService.UploadBackupAsync();
+                backupService.UploadBackupAsync().FireAndForgetSafeAsync();
 
                 return Unit.Value;
             }
