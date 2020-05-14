1f99a0391ab5b378ec9abc0a059f1a1450a2c691
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-05-14 23:24:03 +0200

    start implementing new query for selecting payments

---
+++ Src/MoneyFox.Application/Payments/Queries/GetPaymentsForCategory/GetPaymentsForCategoryQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MoneyFox.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MoneyFox.Application.Payments.Queries.GetPaymentsForCategory
+{
+    public class GetPaymentsForCategoryQuery : IRequest<List<Payment>>
+    {
+        public GetPaymentsForCategoryQuery(int categoryId, DateTime dateRangeFrom, DateTime dateRangeTo)
+        {
+            CategoryId = categoryId;
+            DateRangeFrom = dateRangeFrom;
+            DateRangeTo = dateRangeTo;
+        }
+
+        public int CategoryId { get; set; }
+        public DateTime DateRangeFrom { get; set; }
+        public DateTime DateRangeTo { get; set; }
+
+        public class Handler : IRequestHandler<GetPaymentsForCategoryQuery, List<Payment>>
+        {
+            public Task<List<Payment>> Handle(GetPaymentsForCategoryQuery request, CancellationToken cancellationToken) => throw new NotImplementedException();
+        }
+    }
+}
