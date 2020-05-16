b8fb54948436fa2843ff8a748932a965f62aba60
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-05-16 17:14:19 +0200

    use dialogs instead of pages.

--- Src/MoneyFox.Presentation/Views/EditPaymentPage.xaml.cs
+++ Src/MoneyFox.Presentation/Views/EditPaymentPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MoneyFox.Application.Common;
 using MoneyFox.Application.Resources;
 using MoneyFox.Presentation.ViewModels;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MoneyFox.Presentation.Views
@@ -14,18 +15,32 @@ namespace MoneyFox.Presentation.Views
             InitializeComponent();
             BindingContext = ViewModelLocator.EditPaymentVm;
 
+            var cancelItem = new ToolbarItem
+            {
+                Command = new Command(async () => await Close()),
+                Text = Strings.CancelLabel,
+                Priority = -1,
+                Order = ToolbarItemOrder.Primary
+            };
+
             var saveItem = new ToolbarItem
                            {
                                Command = new Command(async() => await ViewModel.SaveCommand.ExecuteAsync()),
                                Text = Strings.SaveLabel,
-                               Priority = 0,
+                               Priority = 1,
                                Order = ToolbarItemOrder.Primary
                            };
 
+            ToolbarItems.Add(cancelItem);
             ToolbarItems.Add(saveItem);
 
             ViewModel.PaymentId = paymentId;
             ViewModel.InitializeCommand.ExecuteAsync().FireAndForgetSafeAsync();
+        }
+
+        private async Task Close()
+        {
+            await Navigation.PopModalAsync();
         }
     }
 }
