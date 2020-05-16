713a5ac49c61753f75a7cb14a874623d6cbc7a9c
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-05-16 17:17:36 +0200

    use modal for account creation and modifying

--- Src/MoneyFox.Presentation/Views/EditAccountPage.xaml.cs
+++ Src/MoneyFox.Presentation/Views/EditAccountPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MoneyFox.Application.Common;
 using MoneyFox.Application.Resources;
 using MoneyFox.Presentation.ViewModels;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MoneyFox.Presentation.Views
@@ -14,18 +15,32 @@ namespace MoneyFox.Presentation.Views
             InitializeComponent();
             BindingContext = ViewModelLocator.EditAccountVm;
 
-            var saveItem = new ToolbarItem
-                           {
-                               Command = new Command(async() => await ViewModel.SaveCommand.ExecuteAsync()),
-                               Text = Strings.SaveLabel,
-                               Priority = 0,
-                               Order = ToolbarItemOrder.Primary
-                           };
+            var cancelItem = new ToolbarItem
+            {
+                Command = new Command(async () => await Close()),
+                Text = Strings.CancelLabel,
+                Priority = -1,
+                Order = ToolbarItemOrder.Primary
+            };
 
+            var saveItem = new ToolbarItem
+            {
+                Command = new Command(async () => await ViewModel.SaveCommand.ExecuteAsync()),
+                Text = Strings.SaveLabel,
+                Priority = 1,
+                Order = ToolbarItemOrder.Primary
+            };
+
+            ToolbarItems.Add(cancelItem);
             ToolbarItems.Add(saveItem);
 
             ViewModel.AccountId = accountId;
             ViewModel.InitializeCommand.ExecuteAsync().FireAndForgetSafeAsync();
+        }
+
+        private async Task Close()
+        {
+            await Navigation.PopModalAsync();
         }
     }
 }
