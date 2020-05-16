b5851d445b0d11861bf4648801dbe383ff060011
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-05-16 22:53:56 +0200

    use combobox for category selection

--- Src/MoneyFox.Uwp/ViewModels/ModifyPaymentViewModel.cs
+++ Src/MoneyFox.Uwp/ViewModels/ModifyPaymentViewModel.cs
@@ -4,6 +4,7 @@ using GalaSoft.MvvmLight.Command;
 using MediatR;
 using MoneyFox.Application.Accounts.Queries.GetAccounts;
 using MoneyFox.Application.Categories.Queries.GetCategoryById;
+using MoneyFox.Application.Categories.Queries.GetCategoryBySearchTerm;
 using MoneyFox.Application.Common.Interfaces;
 using MoneyFox.Application.Common.Messages;
 using MoneyFox.Application.Resources;
@@ -175,6 +176,17 @@ namespace MoneyFox.Uwp.ViewModels
             }
         }
 
+        private ObservableCollection<CategoryViewModel> categories;
+        public ObservableCollection<CategoryViewModel> Categories
+        {
+            get => categories;
+            private set
+            {
+                categories = value;
+                RaisePropertyChanged();
+            }
+        }
+
         public virtual string Title
         {
             get => title;
@@ -203,6 +215,7 @@ namespace MoneyFox.Uwp.ViewModels
 
             ChargedAccounts = new ObservableCollection<AccountViewModel>(accounts);
             TargetAccounts = new ObservableCollection<AccountViewModel>(accounts);
+            Categories = new ObservableCollection<CategoryViewModel>(mapper.Map<List<CategoryViewModel>>(await mediator.Send(new GetCategoryBySearchTermQuery())));
         }
 
         private async Task SavePaymentBaseAsync()
--- Src/MoneyFox.Uwp/Views/AddPaymentView.xaml.cs
+++ Src/MoneyFox.Uwp/Views/AddPaymentView.xaml.cs
@@ -1,43 +1,20 @@
-﻿using MoneyFox.Application.Common;
-using MoneyFox.Domain;
+﻿using MoneyFox.Domain;
+using MoneyFox.Application.Common;
 using MoneyFox.Uwp.ViewModels;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
 
 namespace MoneyFox.Uwp.Views
 {
-    public sealed partial class AddPaymentView
+    public sealed partial class AddPaymentView : ContentDialog
     {
-        public override string Header => ViewModelLocator.AddPaymentVm.Title;
-
         private AddPaymentViewModel ViewModel => DataContext as AddPaymentViewModel;
 
-        public AddPaymentView()
+        public AddPaymentView(PaymentType paymentType)
         {
             InitializeComponent();
-        }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            if(e.Parameter != null && e.NavigationMode != NavigationMode.Back)
-            {
-                ViewModel.PaymentType = (PaymentType) e.Parameter;
-                ViewModel.InitializeCommand.ExecuteAsync().FireAndForgetSafeAsync();
-            }
-        }
-
-        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
-        {
-            base.OnNavigatingFrom(e);
-            if(e.NavigationMode == NavigationMode.Back)
-                ResetPageCache();
-        }
-
-        private void ResetPageCache()
-        {
-            int cacheSize = ((Frame) Parent).CacheSize;
-            ((Frame) Parent).CacheSize = 0;
-            ((Frame) Parent).CacheSize = cacheSize;
+            ViewModel.PaymentType = paymentType;
+            ViewModel.InitializeCommand.ExecuteAsync().FireAndForgetSafeAsync();
         }
     }
 }
