01eda4e790ba185aea91feb6b226812ef2a45cf4
Nino Padrutt <nino.padrutt@hotmail.com>
2020-07-24 19:37:34 +0200

    add migration startup

---
+++ Src/NewMobile/MoneyFox.Migrations.Startup/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MoneyFox.Migrations.Startup
+{
+    [ExcludeFromCodeCoverage]
+    [SuppressMessage("Major Code Smell", "S1118:Utility classes should not have public constructors", Justification = "RunTimeComponent")]
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
