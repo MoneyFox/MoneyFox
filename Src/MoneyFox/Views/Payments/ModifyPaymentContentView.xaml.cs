935a2a44acc77aa0ea3932cfd6bf2d50d92e4fd2
Nino Padrutt <nino.padrutt@hotmail.com>
2020-07-27 21:19:25 +0200

    add new items.

---
+++ Src/MoneyFox/Views/Payments/ModifyPaymentContentView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MoneyFox.Views.Payments
+{
+    public partial class ModifyPaymentContentView : ContentView
+    {
+        public ModifyPaymentContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
