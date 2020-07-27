bed0360694c389dccc53dbf5f2daecef1e1a4933
Nino Padrutt <nino.padrutt@hotmail.com>
2020-07-27 17:10:32 +0200

    test commit

--- Src/MoneyFox.Migrations.Startup/Program.cs
+++ Src/MoneyFox.Migrations.Startup/Program.cs
@@ -9,7 +9,7 @@ namespace MoneyFox.Migrations.Startup
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Migrating");
         }
     }
 }
