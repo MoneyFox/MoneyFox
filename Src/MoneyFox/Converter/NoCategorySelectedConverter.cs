bc076edd31d1bcf5f15a57af50b6bb11b8eef6e1
Nino Padrutt <nino.padrutt@hotmail.com>
2020-07-28 21:50:18 +0200

    create payment control

---
+++ Src/MoneyFox/Converter/NoCategorySelectedConverter.cs
@@ -0,0 +1,24 @@
+﻿using MoneyFox.Application.Resources;
+using MoneyFox.ViewModels.Category;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MoneyFox.Converter
+{
+    public class NoCategorySelectedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var category = (CategoryViewModel)value;
+
+            return string.IsNullOrEmpty(category?.Name)
+                   ? Strings.SelectCategoryTitle : category.Name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
--- Src/MoneyFox/Views/Payments/ModifyPaymentContentView.xaml.cs
+++ Src/MoneyFox/Views/Payments/ModifyPaymentContentView.xaml.cs
@@ -8,5 +8,14 @@ namespace MoneyFox.Views.Payments
         {
             InitializeComponent();
         }
+
+        private void AmountFieldGotFocus(object sender, FocusEventArgs e)
+        {
+            Dispatcher.BeginInvokeOnMainThread(() =>
+            {
+                AmountEntry.CursorPosition = 0;
+                AmountEntry.SelectionLength = AmountEntry.Text != null ? AmountEntry.Text.Length : 0;
+            });
+        }
     }
 }
