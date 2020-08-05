3f0612d1c12da096a532d5c71cef070cf5c314a9
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-08-05 23:10:44 +0200

    readd mediatr

--- Src/MoneyFox/MoneyFoxModule.cs
+++ Src/MoneyFox/MoneyFoxModule.cs
@@ -1,9 +1,14 @@
 ﻿using Autofac;
+using MediatR;
+using MediatR.Pipeline;
 using Microsoft.Identity.Client;
 using MoneyFox.Application;
+using MoneyFox.Application.Accounts.Queries.GetAccounts;
 using MoneyFox.Application.Common.Constants;
 using MoneyFox.Persistence;
 using System;
+using System.Reflection;
+using Module = Autofac.Module;
 
 namespace MoneyFox
 {
@@ -11,6 +16,8 @@ namespace MoneyFox
     {
         protected override void Load(ContainerBuilder builder)
         {
+            RegisterMediatr(builder);
+
             builder.RegisterModule<ApplicationModule>();
             builder.RegisterModule<PersistenceModule>();
 
@@ -24,6 +31,39 @@ namespace MoneyFox
                    .Where(t => !t.Name.StartsWith("DesignTime", StringComparison.CurrentCultureIgnoreCase))
                    .Where(t => t.Name.EndsWith("ViewModel", StringComparison.CurrentCultureIgnoreCase))
                    .AsSelf();
+        }
+
+        private void RegisterMediatr(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly).AsImplementedInterfaces();
+
+            var mediatrOpenTypes = new[]
+            {
+                typeof(IRequestHandler<,>),
+                typeof(IRequestExceptionHandler<,,>),
+                typeof(IRequestExceptionAction<,>),
+                typeof(INotificationHandler<>),
+            };
+
+            foreach(var mediatrOpenType in mediatrOpenTypes)
+            {
+                builder
+                    .RegisterAssemblyTypes(typeof(GetAccountsQuery).GetTypeInfo().Assembly)
+                    .AsClosedTypesOf(mediatrOpenType)
+                    .AsImplementedInterfaces();
+            }
+
+            // It appears Autofac returns the last registered types first
+            builder.RegisterGeneric(typeof(RequestPostProcessorBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+            builder.RegisterGeneric(typeof(RequestPreProcessorBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+            builder.RegisterGeneric(typeof(RequestExceptionActionProcessorBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+            builder.RegisterGeneric(typeof(RequestExceptionProcessorBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+
+            builder.Register<ServiceFactory>(ctx =>
+            {
+                var c = ctx.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
         }
     }
 }
