f5efbed57625027831a3996ad9237af1d08187ef
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-08-05 22:15:27 +0200

    fix route

--- Src/MoneyFox/ViewModelLocator.cs
+++ Src/MoneyFox/ViewModelLocator.cs
@@ -43,7 +43,7 @@ namespace MoneyFox
         public static string PaymentListRoute => $"Payment/{nameof(PaymentListPage)}";
         public static string AddPaymentRoute => $"Payment/{nameof(AddPaymentPage)}";
         public static string CategoryListRoute => $"Category/{nameof(CategoryListPage)}";
-        public static string AddCategoryRoute => $"Category/{nameof(AddCategoryRoute)}";
+        public static string AddCategoryRoute => $"Category/{nameof(AddCategoryPage)}";
 
         // ViewModels
         public static DashboardViewModel DashboardViewModel => ServiceLocator.Current.GetInstance<DashboardViewModel>();
