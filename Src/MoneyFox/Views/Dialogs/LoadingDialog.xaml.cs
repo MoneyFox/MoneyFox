3551a1ea787ee918cc59b3d6b8d2bb604c840a1d
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-08-05 23:33:55 +0200

    implement loading dialog

---
+++ Src/MoneyFox/Views/Dialogs/LoadingDialog.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace MoneyFox.Views.Dialogs
+{
+    public partial class LoadingDialog
+    {
+        public LoadingDialog()
+        {
+            InitializeComponent();
+        }
+
+        internal static async Task<LoadingDialog> Loading()
+        {
+            var dialog = new LoadingDialog();
+            await dialog.ShowAsync();
+
+            return dialog;
+        }
+
+        public async Task ShowAsync()
+        {
+            await App.Current.MainPage.Navigation.PushModalAsync(this);
+        }
+
+        public async Task DismissAsync()
+        {
+            await App.Current.MainPage.Navigation.PopModalAsync();
+        }
+    }
+}
