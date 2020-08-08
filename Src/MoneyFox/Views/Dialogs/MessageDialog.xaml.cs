972ecbf001cf27ceac57cf2e8ecd349c1a9bfd1f
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-08-08 13:17:36 +0200

    message dialog

---
+++ Src/MoneyFox/Views/Dialogs/MessageDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MoneyFox.Views.Dialogs
+{
+    public partial class MessageDialog
+    {
+        public MessageDialog(string title, string message)
+        {
+            InitializeComponent();
+
+            Title = title;
+            Message = message;
+        }
+
+        public string Title { get; set; }
+        public string Message { get; set; }
+
+        public async Task ShowAsync()
+        {
+            await App.Current.MainPage.Navigation.PushModalAsync(this);
+        }
+
+        public async Task DismissAsync()
+        {
+            await App.Current.MainPage.Navigation.PopModalAsync();
+        }
+    }
+}
