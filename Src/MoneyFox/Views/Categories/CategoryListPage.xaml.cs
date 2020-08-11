cdf87ed3b9bdee8272ad3d1da197d6170e387732
Nino Padrutt <nino.padrutt@hotmail.com>
2020-08-11 21:05:35 +0200

    ui adjustments

--- Src/MoneyFox/Views/Categories/CategoryListPage.xaml.cs
+++ Src/MoneyFox/Views/Categories/CategoryListPage.xaml.cs
@@ -1,13 +1,18 @@
-﻿using Xamarin.Forms;
+﻿using MoneyFox.ViewModels.Categories;
+using Xamarin.Forms;
 
 namespace MoneyFox.Views.Categories
 {
     public partial class CategoryListPage : ContentPage
     {
+        private CategoryListViewModel ViewModel => BindingContext as CategoryListViewModel;
+
         public CategoryListPage()
         {
             InitializeComponent();
             BindingContext = ViewModelLocator.CategoryListViewModel;
         }
+
+        protected override async void OnAppearing() => await ViewModel.OnAppearingAsync();
     }
 }
