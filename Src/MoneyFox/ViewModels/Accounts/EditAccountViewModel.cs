a1c1856b1c021539eb9ae33b70cc1fdf5c692b4b
Nino Padrutt <nino.padrutt@hotmail.com>
2020-08-11 22:18:03 +0200

    start implementing add account

--- Src/MoneyFox/ViewModels/Accounts/EditAccountViewModel.cs
+++ Src/MoneyFox/ViewModels/Accounts/EditAccountViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System.Diagnostics;
+﻿using AutoMapper;
+using MediatR;
+using MoneyFox.Application.Accounts.Queries.GetAccountById;
+using System.Threading.Tasks;
 
 namespace MoneyFox.ViewModels.Accounts
 {
     public class EditAccountViewModel : ModifyAccountViewModel
     {
-        public void Init(int accountId)
+        private readonly IMediator mediator;
+        private readonly IMapper mapper;
+
+        public EditAccountViewModel(IMediator mediator, IMapper mapper)
         {
-            Debug.Write($"Account Id passed {accountId}");
+            this.mediator = mediator;
+            this.mapper = mapper;
+        }
+
+
+        public async Task Init(int accountId)
+        {
+            SelectedAccountVm = mapper.Map<AccountViewModel>(await mediator.Send(new GetAccountByIdQuery(accountId)));
         }
     }
 }
--- Src/MoneyFox/ViewModels/Accounts/ModifyAccountViewModel.cs
+++ Src/MoneyFox/ViewModels/Accounts/ModifyAccountViewModel.cs
@@ -5,6 +5,21 @@ namespace MoneyFox.ViewModels.Accounts
 {
     public class ModifyAccountViewModel : BaseViewModel
     {
+        private AccountViewModel selectedAccountVm = new AccountViewModel();
+
+        /// <summary>
+        /// The currently selected CategoryViewModel
+        /// </summary>
+        public AccountViewModel SelectedAccountVm
+        {
+            get => selectedAccountVm;
+            set
+            {
+                selectedAccountVm = value;
+                RaisePropertyChanged();
+            }
+        }
+
         public RelayCommand SaveCommand => new RelayCommand(async () => await SaveAccountBaseAsync());
 
         private async Task SaveAccountBaseAsync()
