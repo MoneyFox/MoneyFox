52047006bbec5fc64f121eb4071d95f9b9db4e50
Nino Padrutt <nino.padrutt@hotmail.com>
2020-08-19 21:36:02 +0200

    implement delete account

--- Src/MoneyFox/ViewModels/Accounts/AccountListViewModel.cs
+++ Src/MoneyFox/ViewModels/Accounts/AccountListViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using AutoMapper;
 using GalaSoft.MvvmLight.Command;
 using MediatR;
+using MoneyFox.Application.Accounts.Commands.DeleteAccountById;
 using MoneyFox.Application.Accounts.Queries.GetAccounts;
 using MoneyFox.Application.Common.Messages;
 using MoneyFox.Application.Resources;
 using MoneyFox.Common;
 using MoneyFox.Extensions;
 using MoneyFox.Groups;
+using MoneyFox.Services;
 using MoneyFox.Views.Accounts;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -18,14 +20,17 @@ namespace MoneyFox.ViewModels.Accounts
 {
     public class AccountListViewModel : BaseViewModel
     {
-        private readonly IMediator mediator;
-        private readonly IMapper mapper;
         private ObservableCollection<AlphaGroupListGroupCollection<AccountViewModel>> accounts = new ObservableCollection<AlphaGroupListGroupCollection<AccountViewModel>>();
 
-        public AccountListViewModel(IMediator mediator, IMapper mapper)
+        private readonly IMediator mediator;
+        private readonly IMapper mapper;
+        private readonly IDialogService dialogService;
+
+        public AccountListViewModel(IMediator mediator, IMapper mapper, IDialogService dialogService)
         {
             this.mediator = mediator;
             this.mapper = mapper;
+            this.dialogService = dialogService;
 
             MessengerInstance.Register<ReloadMessage>(this, async (m) => await OnAppearingAsync());
         }
@@ -69,9 +74,24 @@ namespace MoneyFox.ViewModels.Accounts
             => new RelayCommand<AccountViewModel>(async (accountViewModel)
                 => await Shell.Current.Navigation.PushModalAsync(
                     new NavigationPage(new EditAccountPage(accountViewModel.Id)) { BarBackgroundColor = Color.Transparent, BarTextColor = ResourceHelper.GetCurrentTextColor() }));
-
         public RelayCommand<AccountViewModel> GoToTransactionListCommand
             => new RelayCommand<AccountViewModel>(async (accountViewModel)
                 => await Shell.Current.GoToAsync($"{ViewModelLocator.PaymentListRoute}?accountId={accountViewModel.Id}"));
+
+        public RelayCommand<AccountViewModel> DeleteAccountCommand
+            => new RelayCommand<AccountViewModel>(async (accountViewModel)
+                => await DeleteAccountAsync(accountViewModel));
+
+        private async Task DeleteAccountAsync(AccountViewModel accountViewModel)
+        {
+            if(await dialogService.ShowConfirmMessageAsync(Strings.DeleteTitle,
+                Strings.DeleteAccountConfirmationMessage,
+                Strings.YesLabel,
+                Strings.NoLabel))
+            {
+                await mediator.Send(new DeleteAccountByIdCommand(accountViewModel.Id));
+                await OnAppearingAsync();
+            }
+        }
     }
 }
