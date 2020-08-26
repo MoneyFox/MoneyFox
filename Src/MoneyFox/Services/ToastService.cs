92ae658a58c2b4e6c5e64797366e571cfc8b98db
Nino Padrutt <nino.padrutt@hotmail.com>
2020-08-26 09:37:08 +0200

    replace toast service

--- Src/MoneyFox/Services/ToastService.cs
+++ Src/MoneyFox/Services/ToastService.cs
@@ -1,10 +1,27 @@
 ﻿using Plugin.Toast;
+using Plugin.Toasts;
+using System.Threading.Tasks;
 
 namespace MoneyFox.Services
 {
     public class ToastService : IToastService
     {
-        public void ShowToast(string text)
-            => CrossToastPopUp.Current.ShowToastMessage(text);
+        private readonly IToastNotificator toastNotificator;
+
+        public ToastService(IToastNotificator toastNotificator)
+        {
+            this.toastNotificator = toastNotificator;
+        }
+
+        public async Task ShowToastAsync(string title, string text)
+        {
+            var options = new NotificationOptions()
+            {
+                Title = title,
+                Description = text
+            };
+
+            await toastNotificator.Notify(options);
+        }
     }
 }
