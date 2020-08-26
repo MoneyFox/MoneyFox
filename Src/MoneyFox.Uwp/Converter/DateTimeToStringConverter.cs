240c1b48cae081ded661584eb3024690caa56060
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-08-26 19:32:11 +0200

    add new converter to csproj and use culture helper

--- Src/MoneyFox.Uwp/Converter/DateTimeToStringConverter.cs
+++ Src/MoneyFox.Uwp/Converter/DateTimeToStringConverter.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using MoneyFox.Application;
+using System;
 using Windows.UI.Xaml.Data;
 
 namespace MoneyFox.Uwp.Converter
 {
     public class DateTimeToStringConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, string language) => ((DateTime)value).ToString("d");
+        public object Convert(object value, Type targetType, object parameter, string language) => ((DateTime)value).ToString("d", CultureHelper.CurrentCulture);
         public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
     }
 }
