5558390de1ce12190ca1af13bb1f677b9d54e007
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-09-10 18:31:51 +0200

    remove wrong bindings.

--- Src/MoneyFox.Uwp/Views/Statistics/StatisticCategorySummary/PaymentListUserControl.xaml.cs
+++ Src/MoneyFox.Uwp/Views/Statistics/StatisticCategorySummary/PaymentListUserControl.xaml.cs
@@ -1,10 +1,4 @@
-﻿using MoneyFox.Uwp.ViewModels;
-using MoneyFox.Ui.Shared.Groups;
-using System;
-using System.Linq;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using MoneyFox.Uwp.ViewModels.Statistic.StatisticCategorySummary;
+﻿using MoneyFox.Uwp.ViewModels.Statistic.StatisticCategorySummary;
 
 namespace MoneyFox.Uwp.Views.Statistics.StatisticCategorySummary
 {
