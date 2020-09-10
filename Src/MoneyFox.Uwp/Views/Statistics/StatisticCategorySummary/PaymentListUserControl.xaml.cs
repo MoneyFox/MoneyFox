4a26739ee54497510c78f1d65e0d7efd496f3424
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-09-10 18:21:43 +0200

    remove unused code

--- Src/MoneyFox.Uwp/Views/Statistics/StatisticCategorySummary/PaymentListUserControl.xaml.cs
+++ Src/MoneyFox.Uwp/Views/Statistics/StatisticCategorySummary/PaymentListUserControl.xaml.cs
@@ -16,33 +16,5 @@ namespace MoneyFox.Uwp.Views.Statistics.StatisticCategorySummary
         }
 
         public CategoryOverviewViewModel ViewModel => (CategoryOverviewViewModel)DataContext;
-
-        private void PaymentListView_OnLoaded(object sender, RoutedEventArgs e)
-        {
-            if(PaymentListView.Items == null || !PaymentListView.Items.Any())
-                return;
-
-            // Select first group with a cleared payment in it
-            DateListGroupCollection<PaymentViewModel> selectedGroupCollection = PaymentListView
-                                                                               .Items
-                                                                               .Select(x => (DateListGroupCollection<PaymentViewModel>) x)
-                                                                               .FirstOrDefault(group => group.Any(x => x.IsCleared));
-
-            if(selectedGroupCollection == null) return;
-
-            PaymentListView.ScrollIntoView(selectedGroupCollection, ScrollIntoViewAlignment.Leading);
-        }
-
-        private async void ListView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            var viewModel = (PaymentViewModel)e.ClickedItem;
-            await new EditPaymentView(viewModel.Id).ShowAsync();
-        }
-
-        private async void EditPaymentClick(object sender, RoutedEventArgs e)
-        {
-            var viewModel = ((MenuFlyoutItem)sender).CommandParameter as PaymentViewModel;
-            await new EditPaymentView(viewModel.Id).ShowAsync();
-        }
     }
 }
