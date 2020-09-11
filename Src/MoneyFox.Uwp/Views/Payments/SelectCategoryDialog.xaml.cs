a675c0a68fdc267b8e1e3049540962ae833a95f6
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-09-11 22:15:33 +0200

    fix loading of categories

--- Src/MoneyFox.Uwp/Views/Payments/SelectCategoryDialog.xaml.cs
+++ Src/MoneyFox.Uwp/Views/Payments/SelectCategoryDialog.xaml.cs
@@ -1,14 +1,19 @@
 ﻿using CommonServiceLocator;
 using MoneyFox.Uwp.ViewModels;
+using Windows.UI.Xaml;
 
 namespace MoneyFox.Uwp.Views.Payments
 {
     public sealed partial class SelectCategoryDialog
     {
+        private SelectCategoryListViewModel ViewModel => (SelectCategoryListViewModel)DataContext;
+
         public SelectCategoryDialog()
         {
             InitializeComponent();
             DataContext = ServiceLocator.Current.GetInstance<SelectCategoryListViewModel>();
         }
+
+        protected override void OnBringIntoViewRequested(BringIntoViewRequestedEventArgs e) => ViewModel.AppearingCommand.Execute(null);
     }
 }
