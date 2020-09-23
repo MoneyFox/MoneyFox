ea39330c78b5253ecbd075575eccceb9b9ca9606
Nino Padrutt <nino.padrutt@hotmail.com>
2020-09-23 21:26:07 +0200

    include in startup

--- Src/MoneyFox/ViewModels/SetupAssistant/WelcomeViewModel.cs
+++ Src/MoneyFox/ViewModels/SetupAssistant/WelcomeViewModel.cs
@@ -1,12 +1,29 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using MoneyFox.Application.Common.Facades;
 using MoneyFox.Extensions;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MoneyFox.ViewModels.SetupAssistant
 {
     public class WelcomeViewModel : ViewModelBase
     {
+        private readonly ISettingsFacade settingsFacade;
+
+        public WelcomeViewModel(ISettingsFacade settingsFacade)
+        {
+            this.settingsFacade = settingsFacade;
+        }
+
+        public async Task InitAsync()
+        {
+            if(settingsFacade.IsSetupCompleted)
+            {
+                await Shell.Current.GoToAsync(ViewModelLocator.DashboardRoute);
+            }
+        }
+
         public RelayCommand GoToAddAccountCommand
             => new RelayCommand(async () => await Shell.Current.GoToModalAsync(ViewModelLocator.AddAccountRoute));
 
--- Src/MoneyFox/Views/SetupAssistant/WelcomePage.xaml.cs
+++ Src/MoneyFox/Views/SetupAssistant/WelcomePage.xaml.cs
@@ -8,10 +8,17 @@ namespace MoneyFox.Views.SetupAssistant
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WelcomePage : ContentPage
     {
+        private WelcomeViewModel ViewModel => (WelcomeViewModel)BindingContext;
+
         public WelcomePage()
         {
             InitializeComponent();
             BindingContext = ServiceLocator.Current.GetInstance<WelcomeViewModel>();
+        }
+
+        protected override async void OnAppearing()
+        {
+            await ViewModel.InitAsync();
         }
     }
 }
