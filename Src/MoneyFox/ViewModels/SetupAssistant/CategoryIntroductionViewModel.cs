3999b9f7d3dadba24fa7a6b67d698a34a265b6e3
Nino Padrutt <nino.padrutt@hotmail.com>
2020-09-24 17:03:29 +0200

    adjust navigation in setup

--- Src/MoneyFox/ViewModels/SetupAssistant/CategoryIntroductionViewModel.cs
+++ Src/MoneyFox/ViewModels/SetupAssistant/CategoryIntroductionViewModel.cs
@@ -14,6 +14,6 @@ namespace MoneyFox.ViewModels.SetupAssistant
             => new RelayCommand(async () => await Shell.Current.GoToAsync(ViewModelLocator.SetupCompletionRoute));
 
         public RelayCommand BackCommand
-            => new RelayCommand(async () => await Shell.Current.GoToAsync(ViewModelLocator.WelcomeViewRoute));
+            => new RelayCommand(async () => await Shell.Current.Navigation.PopAsync());
     }
 }
