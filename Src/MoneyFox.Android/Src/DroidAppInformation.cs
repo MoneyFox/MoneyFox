edb178665523db9c416bff8c16ec0d6853397807
Nino Padrutt <nino.padrutt@hotmail.com>
2020-10-02 15:01:55 +0200

    fix nullable

--- Src/MoneyFox.Android/Src/DroidAppInformation.cs
+++ Src/MoneyFox.Android/Src/DroidAppInformation.cs
@@ -1,13 +1,26 @@
 using MoneyFox.Application.Common.Interfaces;
 
+#nullable enable
 namespace MoneyFox.Droid.Src
 {
     public class DroidAppInformation : IAppInformation
     {
-        public string GetVersion => Android.App.Application
-                              .Context
-                              .PackageManager
-                              .GetPackageInfo(Android.App.Application.Context.PackageName, 0)
-                              .VersionName;
+        public string GetVersion
+        {
+            get
+            {
+                var context = Android.App.Application.Context;
+
+                if(context == null)
+                {
+                    return string.Empty;
+                }
+
+                return context.PackageManager
+                              ?.GetPackageInfo(context.PackageName ?? string.Empty, 0)
+                              ?.VersionName
+                              ?? string.Empty;
+            }
+        }
     }
 }
