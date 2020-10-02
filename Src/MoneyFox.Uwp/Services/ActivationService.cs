a10d4204457e38d9de8d2b03f4e1f1f5517eae4e
Nino Padrutt <nino.padrutt@hotmail.com>
2020-10-02 16:57:05 +0200

    extract register logic

--- Src/MoneyFox.Uwp/Services/ActivationService.cs
+++ Src/MoneyFox.Uwp/Services/ActivationService.cs
@@ -1,21 +1,7 @@
 ﻿using Autofac;
 using MoneyFox.Application.Common;
 using MoneyFox.Domain.Exceptions;
-using MoneyFox.Presentation.ViewModels.Statistic;
-using MoneyFox.Ui.Shared.ViewModels.Backup;
-using MoneyFox.Ui.Shared.ViewModels.Settings;
 using MoneyFox.Uwp.Activation;
-using MoneyFox.Uwp.ViewModels;
-using MoneyFox.Uwp.ViewModels.Payments;
-using MoneyFox.Uwp.ViewModels.Settings;
-using MoneyFox.Uwp.ViewModels.Statistic;
-using MoneyFox.Uwp.ViewModels.Statistic.StatisticCategorySummary;
-using MoneyFox.Uwp.Views;
-using MoneyFox.Uwp.Views.Accounts;
-using MoneyFox.Uwp.Views.Payments;
-using MoneyFox.Uwp.Views.Settings;
-using MoneyFox.Uwp.Views.Statistics;
-using MoneyFox.Uwp.Views.Statistics.StatisticCategorySummary;
 using PCLAppConfig;
 using PCLAppConfig.FileSystemStream;
 using System;
@@ -88,9 +74,9 @@ namespace MoneyFox.Uwp.Services
 
             LoggerService.Initialize();
 
-            ConfigureNavigation();
             RegisterServices();
 
+            NavigationServiceInitializerService.Initialize();
             await JumpListService.InitializeAsync();
             ThemeSelectorService.Initialize();
         }
@@ -110,22 +96,6 @@ namespace MoneyFox.Uwp.Services
 
             builder.RegisterModule<WindowsModule>();
             ViewModelLocator.RegisterServices(builder);
-        }
-
-        private void ConfigureNavigation()
-        {
-            NavigationService.Register<AccountListViewModel, AccountListView>();
-            NavigationService.Register<PaymentListViewModel, PaymentListView>();
-            NavigationService.Register<AddPaymentViewModel, AddPaymentView>();
-            NavigationService.Register<EditPaymentViewModel, EditPaymentView>();
-            NavigationService.Register<CategoryListViewModel, CategoryListView>();
-            NavigationService.Register<SettingsViewModel, SettingsView>();
-            NavigationService.Register<StatisticCashFlowViewModel, StatisticCashFlowView>();
-            NavigationService.Register<StatisticCategorySpreadingViewModel, StatisticCategorySpreadingView>();
-            NavigationService.Register<StatisticCategorySummaryViewModel, StatisticCategorySummaryView>();
-            NavigationService.Register<StatisticSelectorViewModel, StatisticSelectorView>();
-            NavigationService.Register<BackupViewModel, BackupView>();
-            NavigationService.Register<WindowsSettingsViewModel, SettingsView>();
         }
 
         private async Task HandleActivationAsync(object activationArgs)
