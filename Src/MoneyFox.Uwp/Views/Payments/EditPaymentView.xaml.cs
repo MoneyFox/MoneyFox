83b87e6764a34ed01839e267953867bb2c40b9c6
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2021-01-10 16:53:45 +0100

    add missing contexts

--- Src/MoneyFox.Uwp/Views/Payments/EditPaymentView.xaml.cs
+++ Src/MoneyFox.Uwp/Views/Payments/EditPaymentView.xaml.cs
@@ -12,6 +12,7 @@ namespace MoneyFox.Uwp.Views.Payments
         public EditPaymentView()
         {
             InitializeComponent();
+            DataContext = ViewModelLocator.EditPaymentVm;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
