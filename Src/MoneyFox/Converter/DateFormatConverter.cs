20ae609956e36e32173dd305372f92c272e1fae4
Nino Padrutt <nino.padrutt@hotmail.com>
2021-01-22 10:54:56 +0100

    add new converter and use in lists.

--- Src/MoneyFox/Converter/DateFormatConverter.cs
+++ Src/MoneyFox/Converter/DateFormatConverter.cs
@@ -1,18 +1,18 @@
-﻿using System;
+﻿using MoneyFox.Application;
+using System;
 using System.Globalization;
 using Xamarin.Forms;
 
 namespace MoneyFox.Converter
 {
     /// <summary>
-    /// Hides the date if it is equal to the default DateTime Value.
+    ///     Formats the date with the culture in the CultureHelper.
     /// </summary>
-    public class DateVisibilityConverter : IValueConverter
+    public class DateFormatConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => (DateTime)value != new DateTime();
-
+            => ((DateTime)value).ToString("d", CultureHelper.CurrentCulture);
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            => (DateTime)value != new DateTime();
+            => throw new NotSupportedException();
     }
 }
