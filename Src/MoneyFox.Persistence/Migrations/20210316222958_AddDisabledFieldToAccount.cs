4154fdaa96f86c48cb7ad03a6ce37953f7736fe2
Nino Padrutt <nino.padrutt@hotmail.com>
2021-03-16 23:30:50 +0100

    create migration

---
+++ Src/MoneyFox.Persistence/Migrations/20210316222958_AddDisabledFieldToAccount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoneyFox.Persistence.Migrations
+{
+    public partial class AddDisabledFieldToAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDisabled",
+                table: "Accounts",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDisabled",
+                table: "Accounts");
+        }
+    }
+}
