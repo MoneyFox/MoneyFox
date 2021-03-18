bb197770fe0fae4f0af844017a8a0d034f5e5e72
Nino Padrutt <nino.padrutt@hotmail.com>
2021-03-18 14:47:06 +0100

    make a better test

---
+++ Src/MoneyFox.UWP.Tests/Extensions/SortDirectionExtensionTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Microsoft.Toolkit.Uwp.UI.Controls;
+using MoneyFox.Ui.Shared.PaymentSorting;
+using MoneyFox.Uwp.Extensions;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace MoneyFox.Uwp.Tests.Extensions
+{
+    [ExcludeFromCodeCoverage]
+    public class SortDirectionExtensionTests
+    {
+        [Theory]
+        [InlineData(SortDirection.Ascending, DataGridSortDirection.Ascending)]
+        [InlineData(SortDirection.Descending, DataGridSortDirection.Descending)]
+        public void DirectionMappedCorrectly(SortDirection inputDirection, DataGridSortDirection expectedResult)
+        {
+            // Arrange
+            // Act
+            var result = inputDirection.ToDataGridDirection();
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
