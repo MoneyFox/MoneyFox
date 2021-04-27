f5ac4543c1ee91cfe3d697e21695c1573c218048
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2021-04-27 17:06:41 +0200

    fix smells

--- Src/MoneyFox.Application/Common/Constants/AppConstants.cs
+++ Src/MoneyFox.Application/Common/Constants/AppConstants.cs
@@ -5,7 +5,6 @@ namespace MoneyFox.Application.Common.Constants
     /// <summary>
     /// String Constants for usage in the app
     /// </summary>
-    [SuppressMessage("Major Code Smell", "S3996:URI properties should not be strings", Justification = "We use the string to display as well.")]
     public static class AppConstants
     {
         /// <summary>
--- Src/MoneyFox.Application/Common/TaskUtilities.cs
+++ Src/MoneyFox.Application/Common/TaskUtilities.cs
@@ -9,10 +9,7 @@ namespace MoneyFox.Application.Common
     {
         private static readonly Logger Logger = LogManager.GetLogger("TaskLogger");
 
-        [SuppressMessage("Minor Code Smell", "S4261:Methods should be named according to their synchronicities", Justification = "Wanted")]
         [SuppressMessage("Major Bug", "S3168:\"async\" methods should not return \"void\"", Justification = "Wanted")]
-        [SuppressMessage("Minor Code Smell", "S2221:\"Exception\" should not be caught when not required by called methods",
-                         Justification = "Catch all exception")]
         public static async void FireAndForgetSafeAsync(this Task task)
         {
             try
