7d785f678a2633ee916f77fcb4a6e859db5ddbb8
Nino Padrutt <nino.padrutt@hotmail.com>
2021-11-20 18:39:02 +0100

    remove shadows

--- Src/MoneyFox.Uwp/Views/Statistics/StatisticSelectorView.xaml.cs
+++ Src/MoneyFox.Uwp/Views/Statistics/StatisticSelectorView.xaml.cs
@@ -1,6 +1,6 @@
-﻿using MoneyFox.Application.Resources;
+﻿#nullable enable
+using MoneyFox.Application.Resources;
 
-#nullable enable
 namespace MoneyFox.Uwp.Views.Statistics
 {
     public sealed partial class StatisticSelectorView
