0a481b94c3a7f2a7c821768cf8b47d705f8df923
Nino Padrutt <nino.padrutt@hotmail.com>
2021-11-21 13:28:50 +0100

    cleanup

--- Src/MoneyFox.Uwp/Views/Statistics/StatisticAccountMonthlyCashflowView.xaml.cs
+++ Src/MoneyFox.Uwp/Views/Statistics/StatisticAccountMonthlyCashflowView.xaml.cs
@@ -1,9 +1,7 @@
-﻿using MoneyFox.Application.Resources;
+﻿#nullable enable
+using MoneyFox.Application.Resources;
 using MoneyFox.Uwp.ViewModels.Statistics;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls.Primitives;
 
-#nullable enable
 namespace MoneyFox.Uwp.Views.Statistics
 {
     public sealed partial class StatisticAccountMonthlyCashflowView
@@ -18,8 +16,5 @@ namespace MoneyFox.Uwp.Views.Statistics
             InitializeComponent();
             DataContext = ViewModelLocator.StatisticAccountMonthlyCashFlowVm;
         }
-
-        private void OpenFilterFlyout(object sender, RoutedEventArgs e) =>
-            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
     }
 }
