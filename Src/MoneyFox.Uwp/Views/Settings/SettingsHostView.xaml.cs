2c5a43731b9346c0b3d36677befb28ce3d930523
Nino Padrutt <nino.padrutt@hotmail.com>
2021-11-21 14:20:26 +0100

    adjust settings page

--- Src/MoneyFox.Uwp/Views/Settings/SettingsHostView.xaml.cs
+++ Src/MoneyFox.Uwp/Views/Settings/SettingsHostView.xaml.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.UI.Xaml.Controls;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Media.Animation;
-
-#nullable enable
+﻿#nullable enable
 namespace MoneyFox.Uwp.Views.Settings
 {
     public sealed partial class SettingsHostView
@@ -12,30 +8,6 @@ namespace MoneyFox.Uwp.Views.Settings
         public SettingsHostView()
         {
             InitializeComponent();
-        }
-
-        private void OnLoad(object sender, RoutedEventArgs e)
-        {
-            ContentFrame.Navigate(typeof(SettingsView));
-            SettingsView.SelectedItem = SettingsView.MenuItems[0];
-        }
-
-        private void OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
-        {
-            if(args.InvokedItemContainer.Tag.ToString() == "settings")
-            {
-                ContentFrame.Navigate(
-                    typeof(SettingsView),
-                    null,
-                    new SlideNavigationTransitionInfo {Effect = SlideNavigationTransitionEffect.FromRight});
-            }
-            else
-            {
-                ContentFrame.Navigate(
-                    typeof(AboutView),
-                    null,
-                    new SlideNavigationTransitionInfo {Effect = SlideNavigationTransitionEffect.FromLeft});
-            }
         }
     }
 }
