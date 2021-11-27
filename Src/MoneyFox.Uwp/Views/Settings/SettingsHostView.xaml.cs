96c73e7a838b1e7a692083729e59b78372bbc1da
Nino Padrutt <nino.padrutt@hotmail.com>
2021-11-27 12:57:39 +0100

    adjust settings page a bit

--- Src/MoneyFox.Uwp/Views/Settings/SettingsHostView.xaml.cs
+++ Src/MoneyFox.Uwp/Views/Settings/SettingsHostView.xaml.cs
@@ -1,11 +1,9 @@
-﻿using MoneyFox.Application.Resources;
-
-#nullable enable
+﻿#nullable enable
 namespace MoneyFox.Uwp.Views.Settings
 {
     public sealed partial class SettingsHostView
     {
-        public override string Header => Strings.SettingsTitle;
+        public override bool ShowHeader => false;
 
         public SettingsHostView()
         {
