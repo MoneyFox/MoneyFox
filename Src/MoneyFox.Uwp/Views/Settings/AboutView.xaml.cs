4b982b9338d0baa91db03010432bece8669f2c59
Nino Padrutt <nino.padrutt@hotmail.com>
2021-12-06 23:41:47 +0100

    remove store engagnement

--- Src/MoneyFox.Uwp/Views/Settings/AboutView.xaml.cs
+++ Src/MoneyFox.Uwp/Views/Settings/AboutView.xaml.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.Services.Store.Engagement;
+﻿#nullable enable
 using MoneyFox.Uwp.ViewModels.About;
-using System;
-using Windows.UI.Xaml;
 
-#nullable enable
 namespace MoneyFox.Uwp.Views.Settings
 {
     public sealed partial class AboutView
@@ -14,17 +11,6 @@ namespace MoneyFox.Uwp.Views.Settings
         {
             InitializeComponent();
             DataContext = ViewModelLocator.AboutVm;
-
-            if(StoreServicesFeedbackLauncher.IsSupported())
-            {
-                FeedbackButton.Visibility = Visibility.Visible;
-            }
-        }
-
-        private async void FeedbackButton_Click(object sender, RoutedEventArgs e)
-        {
-            StoreServicesFeedbackLauncher? launcher = StoreServicesFeedbackLauncher.GetDefault();
-            await launcher.LaunchAsync();
         }
     }
 }
