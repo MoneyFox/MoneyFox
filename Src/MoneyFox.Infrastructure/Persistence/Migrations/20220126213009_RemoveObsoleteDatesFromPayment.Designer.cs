af0c617bad2f6973e821a57d49d0ad0dde9be519
Nino Padrutt <nino.padrutt@hotmail.com>
2022-01-26 22:30:22 +0100

    remove obsolete dates from payment

--- Src/MoneyFox.Infrastructure/Persistence/Migrations/20220126213009_RemoveObsoleteDatesFromPayment.Designer.cs
+++ Src/MoneyFox.Infrastructure/Persistence/Migrations/20220126213009_RemoveObsoleteDatesFromPayment.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoneyFox.Infrastructure.Persistence;
 
 namespace MoneyFox.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220126213009_RemoveObsoleteDatesFromPayment")]
+    partial class RemoveObsoleteDatesFromPayment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
