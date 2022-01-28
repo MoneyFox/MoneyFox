188908609b7ebbcc33624062e1900032478bfca1
Nino Padrutt <nino.padrutt@hotmail.com>
2022-01-28 22:37:19 +0100

    move automapper over

---
+++ Src/MoneyFox.Win/MoneyFox.Win/AutoMapper/AutoMapperFactory.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+#nullable enable
+namespace MoneyFox.Win.AutoMapper
+{
+    public static class AutoMapperFactory
+    {
+        public static IMapper Create()
+        {
+            // Auto Mapper Configurations
+            var mappingConfig = new MapperConfiguration(
+                mc =>
+                {
+                    mc.AddProfile(new AutoMapperProfile());
+                });
+
+            return mappingConfig.CreateMapper();
+        }
+    }
+}
