c6cb96cd0402c991b71fab8b00e28ded6c3cb215
Nino Padrutt <nino.padrutt@hotmail.com>
2022-01-28 12:02:06 +0100

    add basic appshell page

---
+++ Src/MoneyFox.Win/MoneyFox.Win/Pages/ShellPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace MoneyFox.Win.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShellPage : Page
+    {
+        public ShellPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void AddPaymentItemTapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
