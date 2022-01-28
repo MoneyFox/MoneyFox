52a7454216448ceb9eb4f71ea261a9c98fd364fb
Nino Padrutt <nino.padrutt@hotmail.com>
2022-01-28 17:45:03 +0100

    cleanout shell

--- Src/MoneyFox.Win/MoneyFox.Win/Pages/ShellPage.xaml.cs
+++ Src/MoneyFox.Win/MoneyFox.Win/Pages/ShellPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Input;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
 using MoneyFox.Win.ViewModels;
+using Windows.System;
+using Windows.UI.Core;
 using static MoneyFox.Win.ViewModels.ShellViewModel;
 
 namespace MoneyFox.Win.Pages
@@ -19,6 +23,8 @@ namespace MoneyFox.Win.Pages
 
             MainContentFrame.Navigated += MainContentFrame_Navigated;
             NavView.BackRequested += MenuNav_BackRequested;
+
+            PointerPressed += OnMouseButtonClicked;
         }
 
         private void AddPaymentItemTapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
@@ -37,6 +43,21 @@ namespace MoneyFox.Win.Pages
         private void MainContentFrame_Navigated(object sender, NavigationEventArgs e)
         {
             NavView.IsBackEnabled = MainContentFrame.CanGoBack;
+        }
+
+        private void OnMouseButtonClicked(object sender, PointerRoutedEventArgs e)
+        {
+            if(InputKeyboardSource.GetKeyStateForCurrentThread(VirtualKey.XButton1) == CoreVirtualKeyStates.Down)
+            {
+                MainContentFrame.GoBack();
+                e.Handled = true;
+            }
+
+            if(InputKeyboardSource.GetKeyStateForCurrentThread(VirtualKey.XButton1) == CoreVirtualKeyStates.Down)
+            {
+                MainContentFrame.GoForward();
+                e.Handled = true;
+            }
         }
 
         private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
--- Src/MoneyFox.Win/MoneyFox.Win/ViewModels/ShellViewModel.cs
+++ Src/MoneyFox.Win/MoneyFox.Win/ViewModels/ShellViewModel.cs
@@ -1,33 +1,15 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Input;
 using MoneyFox.Win.Services;
 using NLog;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Threading.Tasks;
 using System.Windows.Input;
-using Windows.System;
-using Windows.UI.Core;
-using Windows.UI.Input;
-using WinUI = Microsoft.UI.Xaml.Controls;
 
 namespace MoneyFox.Win.ViewModels
 {
     public class ShellViewModel : ObservableObject
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-
-        private readonly KeyboardAccelerator altLeftKeyboardAccelerator =
-            BuildKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu);
-
-        private readonly KeyboardAccelerator backKeyboardAccelerator = BuildKeyboardAccelerator(VirtualKey.GoBack);
-
-        private bool isBackEnabled;
-        private IList<KeyboardAccelerator> keyboardAccelerators;
-        private NavigationViewItem selected;
-
         private readonly INavigationService navigationService;
 
         public ShellViewModel(INavigationService navigationService)
@@ -46,19 +28,6 @@ namespace MoneyFox.Win.ViewModels
             //menuItems.Add(new MenuItem { Name = Strings.StatisticsTitle, Icon = "\uE904", ViewModelType = nameof(StatisticSelectorViewModel) });
             //menuItems.Add(new MenuItem { Name = Strings.CategoriesTitle, Icon = "\uE8EC", ViewModelType = nameof(CategoryListViewModel) });
             //menuItems.Add(new MenuItem { Name = Strings.BackupTitle, Icon = "\uEA35", ViewModelType = nameof(BackupViewModel) });
-        }
-
-        public bool IsBackEnabled
-        {
-            get => isBackEnabled;
-            set => SetProperty(ref isBackEnabled, value);
-        }
-
-
-        public WinUI.NavigationViewItem Selected
-        {
-            get => selected;
-            set => SetProperty(ref selected, value);
         }
 
         public ICommand SelectedPageChangedCommand => new RelayCommand<MenuItem>((value) => NavigateToMenuItem(value));
@@ -80,54 +49,6 @@ namespace MoneyFox.Win.ViewModels
             //        break;
             //}
         }
-
-        private void On_PointerPressed(CoreWindow sender, PointerEventArgs e)
-        {
-            bool isXButton1Pressed = e.CurrentPoint.Properties.PointerUpdateKind == PointerUpdateKind.XButton1Pressed;
-
-            if(isXButton1Pressed)
-            {
-                e.Handled = navigationService.GoBack();
-            }
-
-            bool isXButton2Pressed = e.CurrentPoint.Properties.PointerUpdateKind == PointerUpdateKind.XButton2Pressed;
-
-            if(isXButton2Pressed)
-            {
-                e.Handled = navigationService.GoForward();
-            }
-        }
-
-        private async Task OnLoadedAsync()
-        {
-            if(keyboardAccelerators == null)
-            {
-                return;
-            }
-
-            // Keyboard accelerators are added here to avoid showing 'Alt + left' tooltip on the page.
-            // More info on tracking issue https://github.com/Microsoft/microsoft-ui-xaml/issues/8
-            keyboardAccelerators.Add(altLeftKeyboardAccelerator);
-            keyboardAccelerators.Add(backKeyboardAccelerator);
-            await Task.CompletedTask;
-        }
-
-        private static KeyboardAccelerator BuildKeyboardAccelerator(VirtualKey key,
-            VirtualKeyModifiers? modifiers = null)
-        {
-            var keyboardAccelerator = new KeyboardAccelerator { Key = key };
-            if(modifiers.HasValue)
-            {
-                keyboardAccelerator.Modifiers = modifiers.Value;
-            }
-
-            keyboardAccelerator.Invoked += OnKeyboardAcceleratorInvoked;
-
-            return keyboardAccelerator;
-        }
-
-        private static void OnKeyboardAcceleratorInvoked(KeyboardAccelerator sender,
-            KeyboardAcceleratorInvokedEventArgs args) => args.Handled = true;
 
         public class MenuItem
         {
