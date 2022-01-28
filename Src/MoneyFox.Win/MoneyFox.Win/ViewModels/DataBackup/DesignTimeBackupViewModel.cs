0ef4f805d313a1c0cf708705c33000ec6d27a6eb
Nino Padrutt <nino.padrutt@hotmail.com>
2022-01-28 23:09:14 +0100

    move code

---
+++ Src/MoneyFox.Win/MoneyFox.Win/ViewModels/DataBackup/DesignTimeBackupViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+
+namespace MoneyFox.Win.ViewModels.DataBackup
+{
+    public class DesignTimeBackupViewModel : IBackupViewModel
+    {
+        public AsyncRelayCommand InitializeCommand { get; } = null!;
+
+        public AsyncRelayCommand LoginCommand { get; } = null!;
+
+        public AsyncRelayCommand LogoutCommand { get; } = null!;
+
+        public AsyncRelayCommand BackupCommand { get; } = null!;
+
+        public AsyncRelayCommand RestoreCommand { get; } = null!;
+
+        public DateTime BackupLastModified { get; }
+
+        public bool IsLoadingBackupAvailability { get; }
+
+        public bool IsLoggedIn { get; } = true;
+
+        public bool BackupAvailable { get; }
+
+        public bool IsAutoBackupEnabled { get; }
+    }
+}
