7c13709e993317897458a4ffa160b88472c936d9
Nino Padrutt <nino.padrutt@hotmail.com>
2022-01-29 00:58:01 +0100

    copy over code

---
+++ Src/MoneyFox.Win/MoneyFox.Win/Pages/BackupView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MoneyFox.Win.ViewModels.DataBackup;
+
+namespace MoneyFox.Win.Pages
+{
+    public sealed partial class BackupView
+    {
+        public override bool ShowHeader => false;
+        public BackupViewModel ViewModel => (BackupViewModel)DataContext;
+
+        public BackupView()
+        {
+            InitializeComponent();
+            DataContext = ViewModelLocator.BackupVm;
+        }
+    }
+}
--- Src/MoneyFox.Win/MoneyFox.Win/WindowsModule.cs
+++ Src/MoneyFox.Win/MoneyFox.Win/WindowsModule.cs
@@ -3,8 +3,19 @@ using MoneyFox.Core;
 using MoneyFox.Core.Interfaces;
 using MoneyFox.Win.Infrastructure;
 using MoneyFox.Win.Pages;
+using MoneyFox.Win.Pages.Accounts;
+using MoneyFox.Win.Pages.Categories;
+using MoneyFox.Win.Pages.Payments;
+using MoneyFox.Win.Pages.Settings;
+using MoneyFox.Win.Pages.Statistics;
 using MoneyFox.Win.Services;
 using MoneyFox.Win.ViewModels;
+using MoneyFox.Win.ViewModels.Accounts;
+using MoneyFox.Win.ViewModels.Categories;
+using MoneyFox.Win.ViewModels.DataBackup;
+using MoneyFox.Win.ViewModels.Payments;
+using MoneyFox.Win.ViewModels.Settings;
+using MoneyFox.Win.ViewModels.Statistics;
 using System;
 
 namespace MoneyFox.Win
@@ -32,20 +43,20 @@ namespace MoneyFox.Win
                 .AsSelf();
 
             NavigationService.Register<ShellViewModel, ShellPage>();
-            //NavigationService.Register<AccountListViewModel, AccountListView>();
-            //NavigationService.Register<PaymentListViewModel, PaymentListView>();
-            //NavigationService.Register<AddPaymentViewModel, AddPaymentView>();
-            //NavigationService.Register<EditPaymentViewModel, EditPaymentView>();
-            //NavigationService.Register<CategoryListViewModel, CategoryListView>();
-            //NavigationService.Register<SettingsViewModel, SettingsHostView>();
+            NavigationService.Register<AccountListViewModel, AccountListView>();
+            NavigationService.Register<PaymentListViewModel, PaymentListView>();
+            NavigationService.Register<AddPaymentViewModel, AddPaymentView>();
+            NavigationService.Register<EditPaymentViewModel, EditPaymentView>();
+            NavigationService.Register<CategoryListViewModel, CategoryListView>();
+            NavigationService.Register<SettingsViewModel, SettingsHostView>();
             //NavigationService.Register<StatisticCashFlowViewModel, StatisticCashFlowView>();
             //NavigationService.Register<StatisticCategoryProgressionViewModel, StatisticCategoryProgressionView>();
             //NavigationService.Register<StatisticAccountMonthlyCashflowViewModel, StatisticAccountMonthlyCashflowView>();
             //NavigationService.Register<StatisticCategorySpreadingViewModel, StatisticCategorySpreadingView>();
             //NavigationService.Register<StatisticCategorySummaryViewModel, StatisticCategorySummaryView>();
-            //NavigationService.Register<StatisticSelectorViewModel, StatisticSelectorView>();
-            //NavigationService.Register<BackupViewModel, BackupView>();
-            //NavigationService.Register<WindowsSettingsViewModel, SettingsHostView>();
+            NavigationService.Register<StatisticSelectorViewModel, StatisticSelectorView>();
+            NavigationService.Register<BackupViewModel, BackupView>();
+            NavigationService.Register<WindowsSettingsViewModel, SettingsHostView>();
         }
     }
 }
