143f4dd50f05b1b99acbeb2e07ad0fd8968f05ab
Nino Padrutt <nino.padrutt@hotmail.com>
2022-01-29 01:00:35 +0100

    include some pages.

--- Src/MoneyFox.Win/MoneyFox.Win/ViewModels/ShellViewModel.cs
+++ Src/MoneyFox.Win/MoneyFox.Win/ViewModels/ShellViewModel.cs
@@ -1,6 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MoneyFox.Core.Resources;
 using MoneyFox.Win.Services;
+using MoneyFox.Win.ViewModels.Accounts;
+using MoneyFox.Win.ViewModels.Categories;
+using MoneyFox.Win.ViewModels.DataBackup;
+using MoneyFox.Win.ViewModels.Statistics;
 using NLog;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
@@ -24,30 +29,30 @@ namespace MoneyFox.Win.ViewModels
         public void LoadMenuItems()
         {
             menuItems = new ObservableCollection<MenuItem>();
-            //menuItems.Add(new MenuItem { Name = Strings.AccountsTitle, Icon = "\uE80F", ViewModelType = nameof(AccountListViewModel) });
-            //menuItems.Add(new MenuItem { Name = Strings.StatisticsTitle, Icon = "\uE904", ViewModelType = nameof(StatisticSelectorViewModel) });
-            //menuItems.Add(new MenuItem { Name = Strings.CategoriesTitle, Icon = "\uE8EC", ViewModelType = nameof(CategoryListViewModel) });
-            //menuItems.Add(new MenuItem { Name = Strings.BackupTitle, Icon = "\uEA35", ViewModelType = nameof(BackupViewModel) });
+            menuItems.Add(new MenuItem { Name = Strings.AccountsTitle, Icon = "\uE80F", ViewModelType = nameof(AccountListViewModel) });
+            menuItems.Add(new MenuItem { Name = Strings.StatisticsTitle, Icon = "\uE904", ViewModelType = nameof(StatisticSelectorViewModel) });
+            menuItems.Add(new MenuItem { Name = Strings.CategoriesTitle, Icon = "\uE8EC", ViewModelType = nameof(CategoryListViewModel) });
+            menuItems.Add(new MenuItem { Name = Strings.BackupTitle, Icon = "\uEA35", ViewModelType = nameof(BackupViewModel) });
         }
 
         public ICommand SelectedPageChangedCommand => new RelayCommand<MenuItem>((value) => NavigateToMenuItem(value));
         private void NavigateToMenuItem(MenuItem item)
         {
-            //switch(item.ViewModelType)
-            //{
-            //    case nameof(AccountListViewModel):
-            //        navigationService.NavigateTo<AccountListViewModel>();
-            //        break;
-            //    case nameof(StatisticSelectorViewModel):
-            //        navigationService.NavigateTo<StatisticSelectorViewModel>();
-            //        break;
-            //    case nameof(CategoryListViewModel):
-            //        navigationService.NavigateTo<CategoryListViewModel>();
-            //        break;
-            //    case nameof(BackupViewModel):
-            //        navigationService.NavigateTo<BackupViewModel>();
-            //        break;
-            //}
+            switch(item.ViewModelType)
+            {
+                case nameof(AccountListViewModel):
+                    navigationService.Navigate<AccountListViewModel>();
+                    break;
+                case nameof(StatisticSelectorViewModel):
+                    navigationService.Navigate<StatisticSelectorViewModel>();
+                    break;
+                case nameof(CategoryListViewModel):
+                    navigationService.Navigate<CategoryListViewModel>();
+                    break;
+                case nameof(BackupViewModel):
+                    navigationService.Navigate<BackupViewModel>();
+                    break;
+            }
         }
 
         public class MenuItem
