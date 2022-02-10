631e149a24b60a6322c5af6b98bfe658bddd6781
Nino Padrutt <nino.padrutt@hotmail.com>
2022-02-10 21:57:15 +0100

    remove unused date fields

--- Src/MoneyFox.Core/Aggregates/Account.cs
+++ Src/MoneyFox.Core/Aggregates/Account.cs
@@ -17,15 +17,11 @@ namespace MoneyFox.Core.Aggregates
         {
             Guard.Argument(name, nameof(name)).NotNull().NotWhiteSpace();
 
-            ModificationDate = DateTime.Now;
-            CreationTime = DateTime.Now;
-
             Name = name;
             CurrentBalance = currentBalance;
             Note = note;
             IsExcluded = isExcluded;
             IsOverdrawn = currentBalance < 0;
-            ModificationDate = DateTime.Now;
         }
 
         [Key]
@@ -45,10 +41,6 @@ namespace MoneyFox.Core.Aggregates
 
         public bool IsDeactivated { get; private set; }
 
-        public DateTime ModificationDate { get; private set; }
-
-        public DateTime CreationTime { get; [UsedImplicitly] private set;}
-
         public void UpdateAccount(string name, decimal currentBalance = 0m, string note = "", bool isExcluded = false)
         {
             Guard.Argument(name, nameof(name)).NotNull().NotWhiteSpace();
@@ -58,8 +50,6 @@ namespace MoneyFox.Core.Aggregates
             Note = note;
             IsExcluded = isExcluded;
             IsOverdrawn = currentBalance < 0;
-
-            ModificationDate = DateTime.Now;
         }
 
         public void AddPaymentAmount(Payment payment)
@@ -78,8 +68,6 @@ namespace MoneyFox.Core.Aggregates
             {
                 CurrentBalance += payment.Amount;
             }
-
-            ModificationDate = DateTime.Now;
         }
 
         public void RemovePaymentAmount(Payment payment)
@@ -101,8 +89,6 @@ namespace MoneyFox.Core.Aggregates
             {
                 CurrentBalance += -payment.Amount;
             }
-
-            ModificationDate = DateTime.Now;
         }
 
         public void Deactivate()
