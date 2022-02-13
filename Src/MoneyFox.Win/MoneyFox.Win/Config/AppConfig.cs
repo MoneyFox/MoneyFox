d88dfbb1decce1ada87a892896c36068f1be04d5
Nino Padrutt <nino.padrutt@hotmail.com>
2022-02-13 15:43:16 +0100

    add config file and appcenter registration

---
+++ Src/MoneyFox.Win/MoneyFox.Win/Config/AppConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Windows.ApplicationModel;
+
+namespace MoneyFox.Win;
+
+internal partial class AppConfig
+{
+    private readonly IConfigurationRoot configurationRoot;
+
+    public AppConfig()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Package.Current.InstalledLocation.Path)
+            .AddJsonFile("appsettings.json", optional: false);
+
+        configurationRoot = builder.Build();
+    }
+
+    public AppCenterConfig AppCenter => GetSection<AppCenterConfig>(nameof(AppCenter));
+
+    private T GetSection<T>(string key) => configurationRoot.GetSection(key).Get<T>();
+
+}
