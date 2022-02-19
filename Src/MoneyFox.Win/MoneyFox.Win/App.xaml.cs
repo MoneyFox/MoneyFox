cf44d6de38326c593a2ceda9b9853416d929ef7d
Nino Padrutt <nino.padrutt@hotmail.com>
2022-02-19 13:47:54 +0100

    add startup tasks to win

--- Src/MoneyFox.Win/MoneyFox.Win/App.xaml.cs
+++ Src/MoneyFox.Win/MoneyFox.Win/App.xaml.cs
@@ -1,7 +1,16 @@
 ﻿namespace MoneyFox.Win
 {
     using Autofac;
+    using CommonServiceLocator;
+    using MediatR;
     using Microsoft.UI.Xaml;
+    using MoneyFox.Core._Pending_.Common.Facades;
+    using MoneyFox.Core.Commands.Payments.ClearPayments;
+    using MoneyFox.Core.Commands.Payments.CreateRecurringPayments;
+    using MoneyFox.Core.Interfaces;
+    using NLog;
+    using System;
+    using System.Threading.Tasks;
 
 #if !DEBUG
     using Microsoft.AppCenter;
@@ -11,6 +20,9 @@
 
     public partial class App : Application
     {
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+        private bool isRunning;
+
         public App()
         {
             InitializeComponent();
@@ -29,6 +41,52 @@
             var appConfig = new AppConfig();
             AppCenter.Start(appConfig.AppCenter.Secret, typeof(Analytics), typeof(Crashes));
 #endif
+            ExecuteStartupTasks();
+        }
+
+        private void ExecuteStartupTasks() =>
+            Task.Run(
+                    async () =>
+                    {
+                        await StartupTasksAsync();
+                    })
+                .ConfigureAwait(false);
+
+        private async Task StartupTasksAsync()
+        {
+            // Don't execute this again when already running
+            if(isRunning)
+            {
+                return;
+            }
+
+            isRunning = true;
+
+            var settingsFacade = ServiceLocator.Current.GetInstance<ISettingsFacade>();
+            var mediator = ServiceLocator.Current.GetInstance<IMediator>();
+
+            try
+            {
+                if(settingsFacade.IsBackupAutouploadEnabled && settingsFacade.IsLoggedInToBackupService)
+                {
+                    var backupService = ServiceLocator.Current.GetInstance<IBackupService>();
+                    await backupService.RestoreBackupAsync();
+
+                    logger.Info("Backup synced.");
+                }
+
+                await mediator.Send(new ClearPaymentsCommand());
+                await mediator.Send(new CreateRecurringPaymentsCommand());
+            }
+            catch(Exception ex)
+            {
+                logger.Fatal(ex);
+            }
+            finally
+            {
+                settingsFacade.LastExecutionTimeStampSyncBackup = DateTime.Now;
+                isRunning = false;
+            }
         }
     }
 }
