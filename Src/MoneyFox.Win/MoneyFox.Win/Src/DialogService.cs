09582dcf91d3475de567557bf9f8293f64f5a365
Nino Padrutt <nino.padrutt@hotmail.com>
2022-04-08 19:27:55 +0200

    cleanup

--- Src/MoneyFox.Win/MoneyFox.Win/Src/DialogService.cs
+++ Src/MoneyFox.Win/MoneyFox.Win/Src/DialogService.cs
@@ -1,19 +1,17 @@
 ﻿namespace MoneyFox.Win;
 
+using System;
+using System.Threading.Tasks;
 using Core.Common.Interfaces;
 using Core.Resources;
 using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Media;
 using Pages.Dialogs;
-using System;
-using System.Threading.Tasks;
 
 public class DialogService : IDialogService
 {
     private LoadingDialog? loadingDialog;
-    private ContentDialog? openContentDialog;
 
     /// <summary>
     ///     Show a dialog with two buttons with customizable Texts. Returns the answer.
@@ -22,16 +20,18 @@ public class DialogService : IDialogService
     /// <param name="message">Text for the dialog.</param>
     /// <param name="positiveButtonText">Text for the yes button.</param>
     /// <param name="negativeButtonText">Text for the no button.</param>
-    public async Task<bool> ShowConfirmMessageAsync(string title,
-        string message,
-        string? positiveButtonText = null,
-        string? negativeButtonText = null)
+    public async Task<bool> ShowConfirmMessageAsync(string title, string message, string? positiveButtonText = null, string? negativeButtonText = null)
     {
-        var dialog = new ContentDialog {XamlRoot = MainWindow.RootFrame.XamlRoot, Title = title, Content = message};
-        dialog.PrimaryButtonText = positiveButtonText ?? Strings.YesLabel;
-        dialog.SecondaryButtonText = negativeButtonText ?? Strings.NoLabel;
+        var dialog = new ContentDialog
+        {
+            XamlRoot = MainWindow.RootFrame.XamlRoot,
+            Title = title,
+            Content = message,
+            PrimaryButtonText = positiveButtonText ?? Strings.YesLabel,
+            SecondaryButtonText = negativeButtonText ?? Strings.NoLabel
+        };
 
-        ContentDialogResult result = await dialog.ShowAsync();
+        var result = await dialog.ShowAsync();
 
         return result == ContentDialogResult.Primary;
     }
@@ -43,9 +43,8 @@ public class DialogService : IDialogService
     /// <param name="message">Text to display.</param>
     public async Task ShowMessageAsync(string title, string message)
     {
-        var dialog = new ContentDialog {XamlRoot = MainWindow.RootFrame.XamlRoot, Title = title, Content = message};
+        var dialog = new ContentDialog { XamlRoot = MainWindow.RootFrame.XamlRoot, Title = title, Content = message };
         dialog.PrimaryButtonText = Strings.OkLabel;
-
         await dialog.ShowAsync();
     }
 
@@ -54,13 +53,9 @@ public class DialogService : IDialogService
     /// </summary>
     public Task ShowLoadingDialogAsync(string? message = null)
     {
-        loadingDialog = new LoadingDialog {Text = message ?? Strings.LoadingLabel};
-
-        DispatcherQueue dispatcherQueue = DispatcherQueue.GetForCurrentThread();
-        dispatcherQueue.TryEnqueue(async () =>
-        {
-            await loadingDialog.ShowAsync();
-        });
+        loadingDialog = new() { Text = message ?? Strings.LoadingLabel };
+        var dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+        dispatcherQueue.TryEnqueue(async () => { await loadingDialog.ShowAsync(); });
 
         return Task.CompletedTask;
     }
@@ -70,25 +65,27 @@ public class DialogService : IDialogService
     /// </summary>
     public Task HideLoadingDialogAsync()
     {
-        DispatcherQueue dispatcherQueue = DispatcherQueue.GetForCurrentThread();
-        dispatcherQueue.TryEnqueue(() =>
-        {
-            loadingDialog?.Hide();
-            loadingDialog = null;
-        });
+        var dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+        dispatcherQueue.TryEnqueue(
+            () =>
+            {
+                loadingDialog?.Hide();
+                loadingDialog = null;
+            });
+
         return Task.CompletedTask;
     }
 
     /// <summary>
-    ///    Returns the currently open ContentDialog object if one exists, null otherwise.
+    ///     Returns the currently open ContentDialog object if one exists, null otherwise.
     /// </summary>
     public static ContentDialog? GetOpenContentDialog()
     {
-        foreach(Popup? popup in VisualTreeHelper.GetOpenPopupsForXamlRoot(MainWindow.RootFrame.XamlRoot))
+        foreach (var popup in VisualTreeHelper.GetOpenPopupsForXamlRoot(MainWindow.RootFrame.XamlRoot))
         {
-            if(popup.Child is ContentDialog)
+            if (popup.Child is ContentDialog child)
             {
-                return (ContentDialog)popup.Child;
+                return child;
             }
         }
 
@@ -96,22 +93,22 @@ public class DialogService : IDialogService
     }
 
     /// <summary>
-    ///    Hides the open ContentDialog object, if one exists, so another dialog can be displayed.
+    ///     Hides the open ContentDialog object, if one exists, so another dialog can be displayed.
     /// </summary>
     public static void HideContentDialog(ContentDialog? contentDialog)
     {
-        if(contentDialog != null)
+        if (contentDialog != null)
         {
             contentDialog.Hide();
         }
     }
 
     /// <summary>
-    ///    Restores visibilty of the ContentDialog object that was previously hidden
+    ///     Restores visibilty of the ContentDialog object that was previously hidden
     /// </summary>
     public static async Task ShowContentDialog(ContentDialog? contentDialog)
     {
-        if(contentDialog != null)
+        if (contentDialog != null)
         {
             await contentDialog.ShowAsync();
         }
