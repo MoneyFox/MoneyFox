13350b76268987527fd29b01cf15b8d040b7e1a3
Nino Padrutt <nino.padrutt@hotmail.com>
2022-05-28 21:13:03 +0200

    add icons to statistic selector page

--- Src/MoneyFox/ViewModels/Statistics/StatisticSelectorTypeViewModel.cs
+++ Src/MoneyFox/ViewModels/Statistics/StatisticSelectorTypeViewModel.cs
@@ -3,8 +3,9 @@
 
     using Core.Enums;
 
-    public class StatisticSelectorType
+    public class StatisticSelectorTypeViewModel
     {
+        public string IconGlyph { get; set; } = "";
         public string Name { get; set; } = "";
         public string Description { get; set; } = "";
         public StatisticType Type { get; set; }
