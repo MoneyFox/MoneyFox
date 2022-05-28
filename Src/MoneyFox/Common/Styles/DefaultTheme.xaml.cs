a4c7ce477cc5bb50720bf9075edb91d9c7ea0b94
Nino Padrutt <nino.padrutt@hotmail.com>
2022-05-28 20:15:02 +0200

    add material design icon font and replace appshell icons

--- Src/MoneyFox/Common/Styles/DefaultTheme.xaml.cs
+++ Src/MoneyFox/Common/Styles/DefaultTheme.xaml.cs
@@ -1,6 +1,7 @@
 [assembly: ExportFont("ProductSans-Regular.ttf", Alias = "Product")]
 [assembly: ExportFont("RobotoMono-Regular.ttf", Alias = "Roboto")]
 [assembly: ExportFont("norwester.otf", Alias = "Norwester")]
+[assembly: ExportFont("MaterialIconsRound-Regular.otf", Alias = "MaterialIconsRound")]
 
 namespace MoneyFox.Common.Styles
 {
