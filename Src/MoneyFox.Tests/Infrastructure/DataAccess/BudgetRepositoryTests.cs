fec91792c25da14a9b274ccd135d128030128423
Nino Padrutt <nino.padrutt@hotmail.com>
2022-05-29 22:43:49 +0200

    restructure tests

--- Src/MoneyFox.Tests/Infrastructure/DataAccess/BudgetRepositoryTests.cs
+++ Src/MoneyFox.Tests/Infrastructure/DataAccess/BudgetRepositoryTests.cs
@@ -17,75 +17,82 @@
         private readonly BudgetRepository budgetRepository;
         private readonly AppDbContext appDbContext;
 
-        public BudgetRepositoryTests()
+        protected BudgetRepositoryTests()
         {
             appDbContext = InMemoryAppDbContextFactory.Create();
             budgetRepository = new BudgetRepository(appDbContext);
         }
 
-        [Fact]
-        public async Task AddsBudget()
+        public sealed class AddAsync : BudgetRepositoryTests
         {
-            // Arrange
-            var testBudget = new TestData.DefaultBudget();
-
-            // Act
-            await budgetRepository.AddAsync(testBudget.CreateDbBudget());
-
-            // Assert
-            appDbContext.Budgets.Should().ContainSingle();
-            var loadedBudget = appDbContext.Budgets.Single();
-            AssertBudget(actual: loadedBudget, expected: testBudget);
-        }
-
-        [Fact]
-        public async Task AddsCategoryOnlyOnce_WhenAddIsCalledMultipleTimes()
-        {
-            // Arrange
-            var testBudget = new TestData.DefaultBudget();
-            var testDbCategory = appDbContext.RegisterBudget(testBudget);
-
-            // Act
-            Func<Task> act = async () => await budgetRepository.AddAsync(testDbCategory);
-
-            // Assert
-            await act.Should().ThrowAsync<ArgumentException>().WithMessage("An item with the same key has already been added. Key: 1");
-        }
-
-        [Fact]
-        public async Task ReturnsBudgetForId()
-        {
-            // Arrange
-            var testBudget = new TestData.DefaultBudget();
-            var dbBudget = testBudget.CreateDbBudget();
-            await budgetRepository.AddAsync(dbBudget);
-
-            // Act
-            var result = await budgetRepository.GetAsync(dbBudget.Id);
-
-            // Assert
-            AssertBudget(actual: result, expected: testBudget);
-        }
-
-        [Fact]
-        public async Task ReturnsAllBudgets()
-        {
-            // Arrange
-            var testBudget = new TestData.DefaultBudget();
-            var dbBudget1 = testBudget.CreateDbBudget();
-            var dbBudget2 = testBudget.CreateDbBudget();
-            await budgetRepository.AddAsync(dbBudget1);
-            await budgetRepository.AddAsync(dbBudget2);
-
-            // Act
-            var budgetList = await budgetRepository.GetAsync();
-
-            // Assert
-            budgetList.Should().HaveCount(2);
-            foreach (var budget in budgetList)
+            [Fact]
+            public async Task AddsBudget()
             {
-                AssertBudget(actual: budget, expected: testBudget);
+                // Arrange
+                var testBudget = new TestData.DefaultBudget();
+
+                // Act
+                await budgetRepository.AddAsync(testBudget.CreateDbBudget());
+
+                // Assert
+                appDbContext.Budgets.Should().ContainSingle();
+                var loadedBudget = appDbContext.Budgets.Single();
+                AssertBudget(actual: loadedBudget, expected: testBudget);
+            }
+
+            [Fact]
+            public async Task AddsCategoryOnlyOnce_WhenAddIsCalledMultipleTimes()
+            {
+                // Arrange
+                var testBudget = new TestData.DefaultBudget();
+                var testDbCategory = appDbContext.RegisterBudget(testBudget);
+
+                // Act
+                Func<Task> act = async () => await budgetRepository.AddAsync(testDbCategory);
+
+                // Assert
+                await act.Should().ThrowAsync<ArgumentException>().WithMessage("An item with the same key has already been added. Key: 1");
+            }
+        }
+
+        public sealed class GetAsync : BudgetRepositoryTests
+        {
+            [Fact]
+            public async Task ReturnsBudgetForId()
+            {
+                // Arrange
+                var testBudget = new TestData.DefaultBudget();
+                var dbBudget = testBudget.CreateDbBudget();
+                await budgetRepository.AddAsync(dbBudget);
+
+                // Act
+                var result = await budgetRepository.GetAsync(dbBudget.Id);
+
+                // Assert
+                AssertBudget(actual: result, expected: testBudget);
+            }
+
+            [Fact]
+            public async Task ReturnsAllBudgets()
+            {
+                // Arrange
+                var testBudget = new TestData.DefaultBudget();
+                var dbBudget1 = testBudget.CreateDbBudget();
+                var dbBudget2 = testBudget.CreateDbBudget();
+                await budgetRepository.AddAsync(dbBudget1);
+                await budgetRepository.AddAsync(dbBudget2);
+
+                // Act
+                var budgetList = await budgetRepository.GetAsync();
+
+                // Assert
+                budgetList.Should().HaveCount(2);
+                foreach (var budget in budgetList)
+                {
+                    AssertBudget(actual: budget, expected: testBudget);
+                }
             }
         }
     }
+
 }
--- Src/MoneyFox.Tests/Infrastructure/DataAccess/CategoryRepositoryTests.cs
+++ Src/MoneyFox.Tests/Infrastructure/DataAccess/CategoryRepositoryTests.cs
@@ -17,39 +17,43 @@
         private readonly CategoryRepository categoryRepository;
         private readonly AppDbContext appDbContext;
 
-        public CategoryRepositoryTests()
+        protected CategoryRepositoryTests()
         {
             appDbContext = InMemoryAppDbContextFactory.Create();
             categoryRepository = new CategoryRepository(appDbContext);
         }
 
-        [Fact]
-        public async Task CategoryAdded_WhenNewlyCreated()
+        public sealed class AddAsync : CategoryRepositoryTests
         {
-            // Arrange
-            var testCategory = new TestData.DefaultCategory();
+            [Fact]
+            public async Task CategoryAdded_WhenNewlyCreated()
+            {
+                // Arrange
+                var testCategory = new TestData.DefaultCategory();
 
-            // Act
-            await categoryRepository.AddAsync(testCategory.CreateDbCategory());
+                // Act
+                await categoryRepository.AddAsync(testCategory.CreateDbCategory());
 
-            // Assert
-            appDbContext.Categories.Should().ContainSingle();
-            var loadedCategory = appDbContext.Categories.Single();
-            AssertCategory(actual: loadedCategory, expected: testCategory);
-        }
+                // Assert
+                appDbContext.Categories.Should().ContainSingle();
+                var loadedCategory = appDbContext.Categories.Single();
+                AssertCategory(actual: loadedCategory, expected: testCategory);
+            }
 
-        [Fact]
-        public async Task AddsCategoryOnlyOnce_WhenAddIsCalledMultipleTimes()
-        {
-            // Arrange
-            var testCategory = new TestData.DefaultCategory();
-            var testDbCategory = appDbContext.RegisterCategory(testCategory);
+            [Fact]
+            public async Task AddsCategoryOnlyOnce_WhenAddIsCalledMultipleTimes()
+            {
+                // Arrange
+                var testCategory = new TestData.DefaultCategory();
+                var testDbCategory = appDbContext.RegisterCategory(testCategory);
 
-            // Act
-            Func<Task> act = async () => await categoryRepository.AddAsync(testDbCategory);
+                // Act
+                Func<Task> act = async () => await categoryRepository.AddAsync(testDbCategory);
 
-            // Assert
-            await act.Should().ThrowAsync<ArgumentException>().WithMessage("An item with the same key has already been added. Key: 1");
+                // Assert
+                await act.Should().ThrowAsync<ArgumentException>().WithMessage("An item with the same key has already been added. Key: 1");
+            }
         }
     }
+
 }
