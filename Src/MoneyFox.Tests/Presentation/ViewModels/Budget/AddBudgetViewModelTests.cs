4d6648d345606434e5e716dccc69ffe9e0354f44
Nino Padrutt <nino.padrutt@hotmail.com>
2022-05-29 22:29:13 +0200

    add test for vm

---
+++ Src/MoneyFox.Tests/Presentation/ViewModels/Budget/AddBudgetViewModelTests.cs
@@ -0,0 +1,53 @@
+﻿namespace MoneyFox.Tests.Presentation.ViewModels.Budget
+{
+
+    using FluentAssertions;
+    using MediatR;
+    using MoneyFox.Core._Pending_.Common.Messages;
+    using MoneyFox.ViewModels.Budget;
+    using NSubstitute;
+    using Xunit;
+
+    public class AddBudgetViewModelTests
+    {
+        private const int CATEGORY_ID = 10;
+        private readonly ISender sender;
+
+        public AddBudgetViewModelTests()
+        {
+            sender = Substitute.For<ISender>();
+        }
+
+        [Fact]
+        public void AddsSelectedCategoryToList()
+        {
+            // Arrange
+            var vm = new AddBudgetViewModel(sender);
+
+            // Act
+            var categorySelectedMessage = new CategorySelectedMessage(new CategorySelectedDataSet(categoryId: CATEGORY_ID, name: "Beer"));
+            vm.Receive(categorySelectedMessage);
+
+            // Assert
+            vm.SelectedCategories.Should().HaveCount(1);
+            vm.SelectedCategories.Should().Contain(c => c.CategoryId == CATEGORY_ID);
+        }
+
+        [Fact]
+        public void IgnoresSelectedCategory_WhenEntryWithSameIdAlreadyInList()
+        {
+            // Arrange
+            var vm = new AddBudgetViewModel(sender);
+
+            // Act
+            var categorySelectedMessage = new CategorySelectedMessage(new CategorySelectedDataSet(categoryId: CATEGORY_ID, name: "Beer"));
+            vm.Receive(categorySelectedMessage);
+            vm.Receive(categorySelectedMessage);
+
+            // Assert
+            vm.SelectedCategories.Should().HaveCount(1);
+            vm.SelectedCategories.Should().Contain(c => c.CategoryId == CATEGORY_ID);
+        }
+    }
+
+}
