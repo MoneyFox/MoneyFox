8244db919aa1fbd7ba790ca471ee5d9626e7af4d
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-01 13:38:35 +0200

    write a first test for new command

--- Src/MoneyFox.Core/ApplicationCore/UseCases/DbBackup/BackupUpload/UploadBackupCommand.cs
+++ Src/MoneyFox.Core/ApplicationCore/UseCases/DbBackup/BackupUpload/UploadBackupCommand.cs
@@ -1,12 +1,8 @@
 ﻿namespace MoneyFox.Core.ApplicationCore.UseCases.DbBackup.BackupUpload
 {
 
-    using System;
-    using System.IO;
     using System.Threading;
     using System.Threading.Tasks;
-    using _Pending_.Common.Facades;
-    using Interfaces;
     using JetBrains.Annotations;
     using MediatR;
 
@@ -28,76 +24,6 @@
 
                 return Unit.Value;
             }
-        }
-    }
-
-    public static class UploadBackup
-    {
-        public sealed class Command : IRequest { }
-
-        public class Handler : IRequestHandler<Command, Unit>
-        {
-            private readonly IBackupServiceNEW backupService;
-            private readonly ISettingsFacade settingsFacade;
-            private readonly IFileStore fileStore;
-            private readonly IDbPathProvider dbPathProvider;
-
-            public Handler(IBackupServiceNEW backupService, ISettingsFacade settingsFacade, IFileStore fileStore, IDbPathProvider dbPathProvider)
-            {
-                this.backupService = backupService;
-                this.settingsFacade = settingsFacade;
-                this.fileStore = fileStore;
-                this.dbPathProvider = dbPathProvider;
-            }
-
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
-            {
-                if (backupService.IsLoggedIn is false)
-                {
-                    return Unit.Value;
-                }
-
-                var backupDate = await backupService.GetBackupDateAsync();
-                if (settingsFacade.LastDatabaseUpdate > backupDate)
-                {
-                    var dbAsStream = await fileStore.OpenReadAsync(dbPathProvider.GetDbPath());
-                    await backupService.UploadAsync(dbAsStream);
-                    settingsFacade.LastDatabaseUpdate = backupDate;
-                }
-
-                return Unit.Value;
-            }
-        }
-    }
-
-    public interface IBackupServiceNEW
-    {
-        bool IsLoggedIn { get; }
-
-        Task<DateTime> GetBackupDateAsync();
-
-        Task UploadAsync(Stream dataToUpload);
-    }
-
-    public class OneDriveBackupService : IBackupServiceNEW
-    {
-        private readonly IOneDriveAuthenticationService oneDriveAuthenticationService;
-
-        public OneDriveBackupService(IOneDriveAuthenticationService oneDriveAuthenticationService)
-        {
-            this.oneDriveAuthenticationService = oneDriveAuthenticationService;
-        }
-
-        public bool IsLoggedIn => oneDriveAuthenticationService.IsLoggedIn;
-
-        public Task<DateTime> GetBackupDateAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UploadAsync(Stream dataToUpload)
-        {
-            throw new NotImplementedException();
         }
     }
 
