d3aba26b16823750318b95b38a7eb74245aa0a58
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-04 12:31:11 +0200

    make test cases a bit more hierarchical

--- Src/MoneyFox.Tests/Core/ApplicationCore/UseCases/BackupUpload/UploadBackupShould.cs
+++ Src/MoneyFox.Tests/Core/ApplicationCore/UseCases/BackupUpload/UploadBackupShould.cs
@@ -25,25 +25,32 @@ namespace MoneyFox.Tests.Core.ApplicationCore.UseCases.BackupUpload
             fileStore = Substitute.For<IFileStore>();
         }
 
-        [Fact]
-        public async Task DoNothing_When_NotLoggedIn_ToBackupLocation()
+        public class NotLoggedInBackupService : UploadBackupShould
         {
-            // Assert
-            backupService.GetBackupDateAsync().Returns(DateTime.Today.AddMinutes(-2));
-            settingsFacade.IsLoggedInToBackupService.Returns(false);
-            settingsFacade.LastDatabaseUpdate.Returns(DateTime.Today);
-            var handler = new UploadBackup.Handler(
-                backupUploadService: backupService,
-                settingsFacade: settingsFacade,
-                fileStore: fileStore,
-                dbPathProvider: Substitute.For<IDbPathProvider>());
+            public NotLoggedInBackupService()
+            {
+                settingsFacade.IsLoggedInToBackupService.Returns(false);
+            }
 
-            // Act
-            var command = new UploadBackup.Command();
-            var result = await handler.Handle(request: command, cancellationToken: CancellationToken.None);
+            [Fact]
+            public async Task DoNothing_When_NotLoggedIn_ToBackupLocation()
+            {
+                // Assert
+                backupService.GetBackupDateAsync().Returns(DateTime.Today.AddMinutes(-2));
+                settingsFacade.LastDatabaseUpdate.Returns(DateTime.Today);
+                var handler = new UploadBackup.Handler(
+                    backupUploadService: backupService,
+                    settingsFacade: settingsFacade,
+                    fileStore: fileStore,
+                    dbPathProvider: Substitute.For<IDbPathProvider>());
 
-            // Assert
-            result.Should().Be(UploadBackup.UploadResult.Skipped);
+                // Act
+                var command = new UploadBackup.Command();
+                var result = await handler.Handle(request: command, cancellationToken: CancellationToken.None);
+
+                // Assert
+                result.Should().Be(UploadBackup.UploadResult.Skipped);
+            }
         }
 
         public class LoggedInBackupService : UploadBackupShould
@@ -92,58 +99,58 @@ namespace MoneyFox.Tests.Core.ApplicationCore.UseCases.BackupUpload
                 // Assert
                 result.Should().Be(UploadBackup.UploadResult.Skipped);
             }
-        }
 
-        [Fact]
-        public async Task Upload_FileStream_When_LoggedIn_And_LocalBackup_Newer()
-        {
-            // Assert
-            backupService.GetBackupDateAsync().Returns(returnThis: x => DateTime.Today.AddMinutes(-2), x => DateTime.Now);
-            backupService.GetBackupCount().Returns(3);
-            settingsFacade.IsLoggedInToBackupService.Returns(true);
-            settingsFacade.LastDatabaseUpdate.Returns(DateTime.Today.AddMinutes(-1));
-            fileStore.OpenReadAsync(Arg.Any<string>()).Returns(Stream.Null);
-            var handler = new UploadBackup.Handler(
-                backupUploadService: backupService,
-                settingsFacade: settingsFacade,
-                fileStore: fileStore,
-                dbPathProvider: Substitute.For<IDbPathProvider>());
+            [Fact]
+            public async Task Upload_FileStream_When_LoggedIn_And_LocalBackup_Newer()
+            {
+                // Assert
+                backupService.GetBackupDateAsync().Returns(returnThis: x => DateTime.Today.AddMinutes(-2), x => DateTime.Now);
+                backupService.GetBackupCount().Returns(3);
+                settingsFacade.IsLoggedInToBackupService.Returns(true);
+                settingsFacade.LastDatabaseUpdate.Returns(DateTime.Today.AddMinutes(-1));
+                fileStore.OpenReadAsync(Arg.Any<string>()).Returns(Stream.Null);
+                var handler = new UploadBackup.Handler(
+                    backupUploadService: backupService,
+                    settingsFacade: settingsFacade,
+                    fileStore: fileStore,
+                    dbPathProvider: Substitute.For<IDbPathProvider>());
 
-            // Act
-            var command = new UploadBackup.Command();
-            var result = await handler.Handle(request: command, cancellationToken: CancellationToken.None);
+                // Act
+                var command = new UploadBackup.Command();
+                var result = await handler.Handle(request: command, cancellationToken: CancellationToken.None);
 
-            // Assert
-            await backupService.Received(1).UploadAsync(backupName: Arg.Is<string>(s => s.StartsWith("backupmoneyfox3_")), dataToUpload: Arg.Any<Stream>());
-            await backupService.Received(0).DeleteOldest();
-            settingsFacade.LastDatabaseUpdate.Should().BeWithin(TimeSpan.FromSeconds(3)).Before(DateTime.Now);
-            result.Should().Be(UploadBackup.UploadResult.Successful);
-        }
+                // Assert
+                await backupService.Received(1).UploadAsync(backupName: Arg.Is<string>(s => s.StartsWith("backupmoneyfox3_")), dataToUpload: Arg.Any<Stream>());
+                await backupService.Received(0).DeleteOldest();
+                settingsFacade.LastDatabaseUpdate.Should().BeWithin(TimeSpan.FromSeconds(3)).Before(DateTime.Now);
+                result.Should().Be(UploadBackup.UploadResult.Successful);
+            }
 
-        [Fact]
-        public async Task Upload_FileStream_AndDeleteOldestEntry_When_LoggedIn_And_LocalBackup_Newer_And_ArchiveThreshold_Reached()
-        {
-            // Assert
-            backupService.GetBackupDateAsync().Returns(returnThis: x => DateTime.Today.AddMinutes(-2), x => DateTime.Now);
-            backupService.GetBackupCount().Returns(15);
-            settingsFacade.IsLoggedInToBackupService.Returns(true);
-            settingsFacade.LastDatabaseUpdate.Returns(DateTime.Today.AddMinutes(-1));
-            fileStore.OpenReadAsync(Arg.Any<string>()).Returns(Stream.Null);
-            var handler = new UploadBackup.Handler(
-                backupUploadService: backupService,
-                settingsFacade: settingsFacade,
-                fileStore: fileStore,
-                dbPathProvider: Substitute.For<IDbPathProvider>());
+            [Fact]
+            public async Task Upload_FileStream_AndDeleteOldestEntry_When_LoggedIn_And_LocalBackup_Newer_And_ArchiveThreshold_Reached()
+            {
+                // Assert
+                backupService.GetBackupDateAsync().Returns(returnThis: x => DateTime.Today.AddMinutes(-2), x => DateTime.Now);
+                backupService.GetBackupCount().Returns(15);
+                settingsFacade.IsLoggedInToBackupService.Returns(true);
+                settingsFacade.LastDatabaseUpdate.Returns(DateTime.Today.AddMinutes(-1));
+                fileStore.OpenReadAsync(Arg.Any<string>()).Returns(Stream.Null);
+                var handler = new UploadBackup.Handler(
+                    backupUploadService: backupService,
+                    settingsFacade: settingsFacade,
+                    fileStore: fileStore,
+                    dbPathProvider: Substitute.For<IDbPathProvider>());
 
-            // Act
-            var command = new UploadBackup.Command();
-            var result = await handler.Handle(request: command, cancellationToken: CancellationToken.None);
+                // Act
+                var command = new UploadBackup.Command();
+                var result = await handler.Handle(request: command, cancellationToken: CancellationToken.None);
 
-            // Assert
-            await backupService.Received(1).UploadAsync(backupName: Arg.Is<string>(s => s.StartsWith("backupmoneyfox3_")), dataToUpload: Arg.Any<Stream>());
-            await backupService.Received(1).DeleteOldest();
-            settingsFacade.LastDatabaseUpdate.Should().BeWithin(TimeSpan.FromSeconds(3)).Before(DateTime.Now);
-            result.Should().Be(UploadBackup.UploadResult.Successful);
+                // Assert
+                await backupService.Received(1).UploadAsync(backupName: Arg.Is<string>(s => s.StartsWith("backupmoneyfox3_")), dataToUpload: Arg.Any<Stream>());
+                await backupService.Received(1).DeleteOldest();
+                settingsFacade.LastDatabaseUpdate.Should().BeWithin(TimeSpan.FromSeconds(3)).Before(DateTime.Now);
+                result.Should().Be(UploadBackup.UploadResult.Successful);
+            }
         }
     }
 
