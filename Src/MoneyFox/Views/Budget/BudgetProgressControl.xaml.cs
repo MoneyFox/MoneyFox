dfaac0ef59e3317c973e340f50768787772fe173
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-06 21:01:03 +0200

    extract progress control

---
+++ Src/MoneyFox/Views/Budget/BudgetProgressControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MoneyFox.Views.Budget
+{
+
+    using Xamarin.Forms;
+
+    public partial class BudgetProgressControl
+    {
+        public static readonly BindableProperty CurrentSpendingProperty = BindableProperty.Create(
+            propertyName: "CurrentSpending",
+            returnType: typeof(decimal),
+            declaringType: typeof(BudgetProgressControl));
+
+        public static readonly BindableProperty SpendingLimitProperty = BindableProperty.Create(
+            propertyName: "SpendingLimit",
+            returnType: typeof(decimal),
+            declaringType: typeof(BudgetProgressControl));
+
+        public BudgetProgressControl()
+        {
+            InitializeComponent();
+        }
+
+        public decimal CurrentSpending
+        {
+            get => (decimal)GetValue(CurrentSpendingProperty);
+            set => SetValue(property: CurrentSpendingProperty, value: value);
+        }
+
+        public decimal SpendingLimit
+        {
+            get => (decimal)GetValue(SpendingLimitProperty);
+            set => SetValue(property: SpendingLimitProperty, value: value);
+        }
+    }
+
+}
