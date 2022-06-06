b7a6c50701c8f3aaa5ad07d1982328477a48e9c0
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-07 00:02:35 +0200

    update progression after dependency property is updated.

--- Src/MoneyFox/Views/Budget/BudgetProgressControl.xaml.cs
+++ Src/MoneyFox/Views/Budget/BudgetProgressControl.xaml.cs
@@ -10,13 +10,15 @@
             propertyName: "CurrentSpending",
             returnType: typeof(decimal),
             declaringType: typeof(BudgetProgressControl),
-            defaultValue: 0m,);
+            defaultValue: 0m,
+            propertyChanged: CurrentSpendingChanged);
 
         public static readonly BindableProperty SpendingLimitProperty = BindableProperty.Create(
             propertyName: "SpendingLimit",
             returnType: typeof(decimal),
             declaringType: typeof(BudgetProgressControl),
-            defaultValue: 0m);
+            defaultValue: 0m,
+            propertyChanged: CurrentSpendingChanged);
 
         public BudgetProgressControl()
         {
@@ -26,23 +28,19 @@
         public decimal CurrentSpending
         {
             get => (decimal)GetValue(CurrentSpendingProperty);
-
-            set
-            {
-                SetValue(property: CurrentSpendingProperty, value: value);
-                UpdateProgressBar();
-            }
+            set => SetValue(property: CurrentSpendingProperty, value: value);
         }
 
         public decimal SpendingLimit
         {
             get => (decimal)GetValue(SpendingLimitProperty);
+            set => SetValue(property: SpendingLimitProperty, value: value);
+        }
 
-            set
-            {
-                SetValue(property: SpendingLimitProperty, value: value);
-                UpdateProgressBar();
-            }
+        private static void CurrentSpendingChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            var control = bindable as BudgetProgressControl;
+            control.UpdateProgressBar();
         }
 
         private void UpdateProgressBar()
