4b49cbc996b1fc93759af6826cc1503099f65367
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-06 23:09:07 +0200

    fix issue with ef core query translation.

--- Src/MoneyFox.Core/ApplicationCore/Queries/BudgetListLoading/LoadBudgets.cs
+++ Src/MoneyFox.Core/ApplicationCore/Queries/BudgetListLoading/LoadBudgets.cs
@@ -30,16 +30,17 @@
                 var dbContext = contextAdapter.Context;
                 var budgets = await dbContext.Budgets.ToListAsync(cancellationToken);
                 var budgetListDataList = new List<BudgetListData>();
-
                 var firstDayOfCurrentMonth = DateTime.Now.GetFirstDayOfMonth();
                 var lastDayOfCurrentMonth = DateTime.Now.GetLastDayOfMonth();
                 foreach (var budget in budgets)
                 {
-                    var currentSpending = await dbContext.Payments.Where(p => p.CategoryId != null)
+                    var payments = await dbContext.Payments.Where(p => p.CategoryId != null)
                         .Where(p => p.Date >= firstDayOfCurrentMonth && p.Date <= lastDayOfCurrentMonth)
                         .Where(p => budget.IncludedCategories.Contains(p.CategoryId!.Value))
-                        .SumAsync(selector: p => p.Type == PaymentType.Expense ? p.Amount : -p.Amount, cancellationToken: cancellationToken);
+                        .ToListAsync(cancellationToken);
 
+                    // Since sum is not supported for decimal in Ef Core with SQLite we have to do this in two steps
+                    var currentSpending = payments.Sum(selector: p => p.Type == PaymentType.Expense ? p.Amount : -p.Amount);
                     budgetListDataList.Add(
                         new BudgetListData(id: budget.Id, name: budget.Name, spendingLimit: budget.SpendingLimit, currentSpending: currentSpending));
                 }
