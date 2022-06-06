5a4ccd51aec7cb81c8613d35bfc41ab1501eaf4a
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-06 16:11:46 +0200

    call initialize command

--- Src/MoneyFox/Views/Budget/BudgetListPage.xaml.cs
+++ Src/MoneyFox/Views/Budget/BudgetListPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿namespace MoneyFox.Views.Budget
 {
 
+    using ViewModels.Budget;
     using Xamarin.Forms;
 
     public partial class BudgetListPage : ContentPage
@@ -9,6 +10,14 @@
         {
             InitializeComponent();
             BindingContext = ViewModelLocator.BudgetListViewModel;
+        }
+
+        private BudgetListViewModel ViewModel => (BudgetListViewModel)BindingContext;
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await ViewModel.InitializeCommand.ExecuteAsync(null);
         }
     }
 
