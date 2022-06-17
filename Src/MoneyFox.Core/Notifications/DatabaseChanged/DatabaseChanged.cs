f88ac17b11ee3b928db9a109f4c60c3955bb7a55
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-17 22:14:10 +0200

    add notification handler

--- Src/MoneyFox.Core/Notifications/DatabaseChanged/DatabaseChanged.cs
+++ Src/MoneyFox.Core/Notifications/DatabaseChanged/DatabaseChanged.cs
@@ -1,15 +1,33 @@
 ﻿namespace MoneyFox.Core.Notifications.DatabaseChanged
 {
 
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using _Pending_.Common.Facades;
+    using ApplicationCore.UseCases.BackupUpload;
     using MediatR;
 
     public static class DataBaseChanged
     {
         public sealed class Notification : INotification { }
 
-        public class Handler
+        public class Handler : INotificationHandler<Notification>
         {
+            private readonly ISettingsFacade settingsFacade;
+            private readonly ISender sender;
 
+            public Handler(ISender sender, ISettingsFacade settingsFacade)
+            {
+                this.sender = sender;
+                this.settingsFacade = settingsFacade;
+            }
+
+            public async Task Handle(Notification notification, CancellationToken cancellationToken)
+            {
+                settingsFacade.LastDatabaseUpdate = DateTime.Now;
+                await sender.Send(new UploadBackup.Command(), cancellationToken);
+            }
         }
     }
 
--- Src/MoneyFox.Tests/Notification/DatabaseChanged/DatabaseChangedNotificationHandlerShould.cs
+++ Src/MoneyFox.Tests/Notification/DatabaseChanged/DatabaseChangedNotificationHandlerShould.cs
@@ -1,21 +1,44 @@
 ﻿namespace MoneyFox.Tests.Notification.DatabaseChanged
 {
 
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using MediatR;
+    using MoneyFox.Core._Pending_.Common.Facades;
+    using MoneyFox.Core.ApplicationCore.UseCases.BackupUpload;
     using MoneyFox.Core.Notifications.DatabaseChanged;
+    using NSubstitute;
+    using NSubstitute.ReceivedExtensions;
     using Xunit;
 
     public sealed class DatabaseChangedNotificationHandlerShould
     {
+        private readonly ISender sender;
+        private readonly ISettingsFacade settingsFacade;
+        private readonly DataBaseChanged.Handler handler;
+
+        public DatabaseChangedNotificationHandlerShould()
+        {
+            sender = Substitute.For<ISender>();
+            settingsFacade = Substitute.For<ISettingsFacade>();
+            handler = new DataBaseChanged.Handler(sender, settingsFacade);
+        }
+
         [Fact]
-        public void UpdateDatabaseChangedTimeStamp_InSettings()
+        public async Task UploadBackup_WhenLoggedIn_And_BackupAutoUpload_Enabled()
         {
             // Arrange
-            var handler = new DataBaseChanged.Handler();
+            settingsFacade.IsBackupAutoUploadEnabled.Returns(true);
 
             // Act
+            var notification = new DataBaseChanged.Notification();
+            await handler.Handle(notification: notification, cancellationToken: CancellationToken.None);
 
             // Assert
-
+            await sender.Received().Send(Arg.Any<UploadBackup.Command>(), CancellationToken.None);
+            settingsFacade.LastDatabaseUpdate.Should().BeCloseTo(nearbyTime: DateTime.Now, precision: TimeSpan.FromSeconds(5));
         }
     }
 
