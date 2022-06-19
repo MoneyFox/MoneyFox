78d933504f603acea6120af20e7c5947d5883d0d
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-19 14:25:07 +0200

    replace context adapter

--- Src/MoneyFox.Tests/Core/ApplicationCore/Queries/Accounts/GetIfAccountWithNameExists/GetIfAccountWithNameExistsQueryTests.cs
+++ Src/MoneyFox.Tests/Core/ApplicationCore/Queries/Accounts/GetIfAccountWithNameExists/GetIfAccountWithNameExistsQueryTests.cs
@@ -1,40 +1,25 @@
 ﻿namespace MoneyFox.Tests.Core.ApplicationCore.Queries.Accounts.GetIfAccountWithNameExists
 {
 
-    using System;
     using System.Diagnostics.CodeAnalysis;
     using System.Threading.Tasks;
     using FluentAssertions;
     using MoneyFox.Core.ApplicationCore.Domain.Aggregates.AccountAggregate;
     using MoneyFox.Core.ApplicationCore.Queries;
-    using MoneyFox.Core.Common.Interfaces;
     using MoneyFox.Infrastructure.Persistence;
-    using Moq;
     using TestFramework;
     using Xunit;
 
     [ExcludeFromCodeCoverage]
-    public class GetIfAccountWithNameExistsQueryTests : IDisposable
+    public class GetIfAccountWithNameExistsQueryTests
     {
         private readonly AppDbContext context;
-        private readonly Mock<IContextAdapter> contextAdapterMock;
+        private readonly GetIfAccountWithNameExistsQuery.Handler handler;
 
         public GetIfAccountWithNameExistsQueryTests()
         {
             context = InMemoryAppDbContextFactory.Create();
-            contextAdapterMock = new Mock<IContextAdapter>();
-            contextAdapterMock.SetupGet(x => x.Context).Returns(context);
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            InMemoryAppDbContextFactory.Destroy(context);
+            handler = new GetIfAccountWithNameExistsQuery.Handler(context);
         }
 
         [Theory]
@@ -51,9 +36,7 @@
             await context.SaveChangesAsync();
 
             // Act
-            var result = await new GetIfAccountWithNameExistsQuery.Handler(contextAdapterMock.Object).Handle(
-                request: new GetIfAccountWithNameExistsQuery(accountName: name, accountId: 0),
-                cancellationToken: default);
+            var result = await handler.Handle(request: new GetIfAccountWithNameExistsQuery(accountName: name, accountId: 0), cancellationToken: default);
 
             // Assert
             result.Should().Be(expectedResult);
