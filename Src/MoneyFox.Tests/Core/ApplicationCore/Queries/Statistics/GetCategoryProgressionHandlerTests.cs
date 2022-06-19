6fc998bb62b8f21320088cdfb0998e9eb3ff171d
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-19 03:00:43 +0200

    remove context adapter

--- Src/MoneyFox.Tests/Core/ApplicationCore/Queries/Statistics/GetCategoryProgressionHandlerTests.cs
+++ Src/MoneyFox.Tests/Core/ApplicationCore/Queries/Statistics/GetCategoryProgressionHandlerTests.cs
@@ -21,12 +21,14 @@
     {
         private readonly AppDbContext context;
         private readonly Mock<IContextAdapter> contextAdapterMock;
+        private readonly GetCategoryProgressionHandler handler;
 
         public GetCategoryProgressionHandlerTests()
         {
             context = InMemoryAppDbContextFactory.Create();
             contextAdapterMock = new Mock<IContextAdapter>();
             contextAdapterMock.SetupGet(x => x.Context).Returns(context);
+            handler = new GetCategoryProgressionHandler(contextAdapterMock.Object);
         }
 
         [Fact]
@@ -66,10 +68,10 @@
 
             context.Add(account);
             context.Add(category);
-            context.SaveChanges();
+            await context.SaveChangesAsync();
 
             // Act
-            var result = await new GetCategoryProgressionHandler(contextAdapterMock.Object).Handle(
+            var result = await handler.Handle(
                 request: new GetCategoryProgressionQuery(categoryId: category.Id, startDate: DateTime.Today.AddYears(-1), endDate: DateTime.Today.AddDays(3)),
                 cancellationToken: default);
 
@@ -77,56 +79,6 @@
             result[12].Value.Should().Be(40);
             result[11].Value.Should().Be(-50);
             result[10].Value.Should().Be(-40);
-        }
-
-        [Fact]
-        public async Task GetValues_CorrectSums()
-        {
-            // Arrange
-            var account = new Account("Foo1");
-            var category = new Category("abcd");
-            context.AddRange(
-                new List<Payment>
-                {
-                    new Payment(
-                        date: DateTime.Today,
-                        amount: 60,
-                        type: PaymentType.Income,
-                        chargedAccount: account,
-                        category: category),
-                    new Payment(
-                        date: DateTime.Today,
-                        amount: 20,
-                        type: PaymentType.Expense,
-                        chargedAccount: account,
-                        category: category),
-                    new Payment(
-                        date: DateTime.Today.AddMonths(-1),
-                        amount: 50,
-                        type: PaymentType.Expense,
-                        chargedAccount: account,
-                        category: category),
-                    new Payment(
-                        date: DateTime.Today.AddMonths(-2),
-                        amount: 40,
-                        type: PaymentType.Expense,
-                        chargedAccount: account,
-                        category: category)
-                });
-
-            context.Add(account);
-            context.Add(category);
-            context.SaveChanges();
-
-            // Act
-            var result = await new GetAccountProgressionHandler(contextAdapterMock.Object).Handle(
-                request: new GetAccountProgressionQuery(accountId: account.Id, startDate: DateTime.Today.AddYears(-1), endDate: DateTime.Today.AddDays(3)),
-                cancellationToken: default);
-
-            // Assert
-            result[0].Color.Should().Be("#87cefa");
-            result[1].Color.Should().Be("#cd3700");
-            result[2].Color.Should().Be("#cd3700");
         }
     }
 
