c1f7bef1287c6a1b38c5e6df5cbc987196dbb37c
Nino Padrutt <nino.padrutt@hotmail.com>
2022-08-15 20:20:58 +0200

    add migration

---
+++ Src/MoneyFox.Infrastructure/Persistence/Migrations/20220815181711_AddTimeRangeColumn.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoneyFox.Persistence.Migrations
+{
+    public partial class AddTimeRangeColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Budgets",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BudgetTimeRange",
+                table: "Budgets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BudgetTimeRange",
+                table: "Budgets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Budgets",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
