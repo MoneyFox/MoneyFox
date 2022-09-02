ccb4fa9c4ffea4921c326d7f511ebc36445cf50d
Nino Padrutt <nino.padrutt@hotmail.com>
2022-09-02 23:09:59 +0200

    include all files

---
+++ Src/MoneyFox.Ui/Views/Backup/BackupPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MoneyFox.Views.Backup;
+
+using ViewModels.DataBackup;
+
+public partial class BackupPage
+{
+    public BackupPage()
+    {
+        InitializeComponent();
+        BindingContext = App.GetViewModel<BackupViewModel>();
+    }
+
+    internal BackupViewModel ViewModel => (BackupViewModel)BindingContext;
+
+    protected override void OnAppearing()
+    {
+        ViewModel.InitializeCommand.Execute(null);
+    }
+}
