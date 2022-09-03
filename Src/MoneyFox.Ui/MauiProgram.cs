8e397db6d6bb90ba22695e74bea02b0a751dbf66
Nino Padrutt <nino.padrutt@hotmail.com>
2022-09-03 15:14:19 +0200

    fix styles and add skia

--- Src/MoneyFox.Ui/MauiProgram.cs
+++ Src/MoneyFox.Ui/MauiProgram.cs
@@ -1,6 +1,7 @@
 namespace MoneyFox.Ui;
 
 using CommunityToolkit.Maui;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 public static class MauiProgram
 {
@@ -15,8 +16,8 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont(filename: "ProductSans-Regular.ttf", alias: "Product");
                 fonts.AddFont(filename: "MaterialIconsRound-Regular.otf", alias: "MaterialIconsRound");
-                fonts.AddFont(filename: "RobotoMono-Regular.ttf", alias: "Roboto");
             })
+            .UseSkiaSharp(true)
             .UseMauiCommunityToolkit();
 
         return builder.Build();
