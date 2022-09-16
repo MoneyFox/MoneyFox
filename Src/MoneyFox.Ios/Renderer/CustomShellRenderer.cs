7b3909d2a89cc2ff79c7ad5a39e9fde9082212f3
NPadrutt <nino.padrutt@hotmail.com>
2022-09-16 17:04:08 +0200

    add custom renderer for ios appshell

---
+++ Src/MoneyFox.Ios/Renderer/CustomShellRenderer.cs
@@ -0,0 +1,59 @@
+using MoneyFox;
+using MoneyFox.iOS.Renderer;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(AppShell), typeof(CustomShellRenderer))]
+namespace MoneyFox.iOS.Renderer
+{
+    using CoreGraphics;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Platform.iOS;
+    public class CustomShellRenderer : ShellRenderer
+    {
+        protected override IShellPageRendererTracker CreatePageRendererTracker()
+        {
+            return new CustomShellPageRendererTracker(this);
+        }
+    }
+
+    public class CustomShellPageRendererTracker : ShellPageRendererTracker
+    {
+        public CustomShellPageRendererTracker(IShellContext context)
+            : base(context)
+        {
+
+        }
+
+        protected override void UpdateTitleView()
+        {
+            var titleView = Shell.GetTitleView(Page);
+
+            if (titleView == null)
+            {
+                var view = ViewController.NavigationItem.TitleView;
+                ViewController.NavigationItem.TitleView = null;
+                view?.Dispose();
+            }
+            else
+            {
+                var view = new CustomTitleViewContainer(titleView);
+                ViewController.NavigationItem.TitleView = view;
+            }
+        }
+    }
+
+    public class CustomTitleViewContainer : UIContainerView
+    {
+        public CustomTitleViewContainer(View view) : base(view)
+        {
+            TranslatesAutoresizingMaskIntoConstraints = false;
+        }
+
+        public override CGSize IntrinsicContentSize => UILayoutFittingExpandedSize;
+
+        public override CGSize SizeThatFits(CGSize size)
+        {
+            return size;
+        }
+    }
+}
