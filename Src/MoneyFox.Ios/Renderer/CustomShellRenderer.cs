f5de934a48fb84647e9e78df48d39ac554e4fefc
Nino Padrutt <nino.padrutt@hotmail.com>
2022-09-20 22:24:44 +0200

    merge shell renderers

--- Src/MoneyFox.Ios/Renderer/CustomShellRenderer.cs
+++ Src/MoneyFox.Ios/Renderer/CustomShellRenderer.cs
@@ -1,17 +1,29 @@
-using CoreGraphics;
 using MoneyFox;
 using MoneyFox.iOS.Renderer;
 using Xamarin.Forms;
-using Xamarin.Forms.Platform.iOS;
 
 [assembly: ExportRenderer(typeof(AppShell), typeof(CustomShellRenderer))]
 namespace MoneyFox.iOS.Renderer
 {
+    using CoreGraphics;
+    using UIKit;
+
+    using Xamarin.Essentials;
+
+    using Xamarin.Forms;
+
+    using Xamarin.Forms.Platform.iOS;
+
     public class CustomShellRenderer : ShellRenderer
     {
         protected override IShellPageRendererTracker CreatePageRendererTracker()
         {
             return new CustomShellPageRendererTracker(this);
+        }
+
+        protected override IShellNavBarAppearanceTracker CreateNavBarAppearanceTracker()
+        {
+            return new NoLineAppearanceTracker();
         }
     }
 
@@ -52,5 +64,40 @@ namespace MoneyFox.iOS.Renderer
         }
 
         public override CGSize IntrinsicContentSize => UILayoutFittingExpandedSize;
+    }
+
+    public class NoLineAppearanceTracker : IShellNavBarAppearanceTracker
+    {
+        public void SetAppearance(UINavigationController controller, ShellAppearance appearance)
+        {
+            var tintColor = AppInfo.RequestedTheme == AppTheme.Dark ? Color.White : Color.FromHex("#323130");
+            var navBar = controller.NavigationBar;
+            navBar.TintColor = tintColor.ToUIColor();
+            var navigationBarAppearance = new UINavigationBarAppearance();
+            navigationBarAppearance.ConfigureWithOpaqueBackground();
+            navigationBarAppearance.ShadowColor = UIColor.Clear;
+            navigationBarAppearance.BackgroundColor = UIColor.Clear;
+            navBar.ScrollEdgeAppearance = navBar.StandardAppearance = navigationBarAppearance;
+        }
+
+        public void SetHasShadow(UINavigationController controller, bool hasShadow)
+        {
+            // Only needed for interface implementation.
+        }
+
+        public void UpdateLayout(UINavigationController controller)
+        {
+            // Only needed for interface implementation.
+        }
+
+        public void ResetAppearance(UINavigationController controller)
+        {
+            // Only needed for interface implementation.
+        }
+
+        public void Dispose()
+        {
+            // Only needed for interface implementation.
+        }
     }
 }
