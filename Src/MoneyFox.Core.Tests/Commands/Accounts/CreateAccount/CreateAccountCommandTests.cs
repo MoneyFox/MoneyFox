b726ede0a053b1bdb39a6346198e11e3f79a6a38
Nino Padrutt <nino.padrutt@hotmail.com>
2022-09-25 17:31:59 +0200

    add core test project

---
+++ Src/MoneyFox.Core.Tests/Commands/Accounts/CreateAccount/CreateAccountCommandTests.cs
@@ -0,0 +1,33 @@
+namespace MoneyFox.Core.Tests.Commands.Accounts.CreateAccount;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using MoneyFox.Core.Commands.Accounts.CreateAccount;
+using MoneyFox.Infrastructure.Persistence;
+using MoneyFox.Tests.TestFramework;
+using Xunit;
+
+[ExcludeFromCodeCoverage]
+public class CreateAccountCommandTests
+{
+    private readonly AppDbContext context;
+    private readonly CreateAccountCommand.Handler handler;
+
+    public CreateAccountCommandTests()
+    {
+        context = InMemoryAppDbContextFactory.Create();
+        handler = new CreateAccountCommand.Handler(context);
+    }
+
+    [Fact]
+    public async Task GetAccountQuery_CorrectNumberLoaded()
+    {
+        // Arrange
+        // Act
+        _ = await handler.Handle(request: new CreateAccountCommand(name: "test", currentBalance: 80), cancellationToken: default);
+
+        // Assert
+        _ = Assert.Single(context.Accounts);
+    }
+}
+
