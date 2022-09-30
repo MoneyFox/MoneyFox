c6c8640f1201dbe517d783d16b5befd85729cd46
Nino Padrutt <nino.padrutt@hotmail.com>
2022-09-30 14:25:53 +0200

    fix startup on android

--- Src/MoneyFox.Ui/Platforms/Android/MainActivity.cs
+++ Src/MoneyFox.Ui/Platforms/Android/MainActivity.cs
@@ -14,7 +14,7 @@ using Microsoft.Maui.ApplicationModel;
 using MoneyFox.Infrastructure.DbBackup.Legacy;
 using MoneyFox.Ui.Platforms.Android.Resources.Src;
 
-[Activity(Label = "MoneyFox", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.UiMode | ConfigChanges.Orientation)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
     protected override void OnCreate(Bundle? savedInstanceState)
