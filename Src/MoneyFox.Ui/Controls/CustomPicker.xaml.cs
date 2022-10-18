da8c46e90089f114c2b560c0487302db0920624a
Nino Padrutt <nino.padrutt@hotmail.com>
2022-10-18 22:23:55 +0200

    try adding custom picker

---
+++ Src/MoneyFox.Ui/Controls/CustomPicker.xaml.cs
@@ -0,0 +1,47 @@
+namespace MoneyFox.Ui.Controls;
+
+using System.Collections;
+
+public partial class CustomPicker : ContentView
+{
+    public CustomPicker()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty PickerTitleProperty = BindableProperty.Create(nameof(PickerTitle), typeof(string), typeof(CustomPicker), string.Empty);
+    public static readonly BindableProperty PickerItemsSourceProperty = BindableProperty.Create(nameof(PickerItemsSource), typeof(IList), typeof(CustomPicker));
+    public static readonly BindableProperty PickerSelectedItemProperty = BindableProperty.Create(nameof(PickerSelectedItem), typeof(object), typeof(CustomPicker));
+
+    public string PickerTitle
+    {
+        get => (string)GetValue(CustomPicker.PickerTitleProperty);
+        set => SetValue(CustomPicker.PickerTitleProperty, value);
+    }
+    public IList PickerItemsSource
+    {
+        get => (IList)GetValue(CustomPicker.PickerItemsSourceProperty);
+        set => SetValue(CustomPicker.PickerItemsSourceProperty, value);
+    }
+
+    public object PickerSelectedItem
+    {
+        get => GetValue(CustomPicker.PickerSelectedItemProperty);
+        set => SetValue(CustomPicker.PickerSelectedItemProperty, value);
+    }
+
+
+    BindingBase itemDisplayBinding;
+    public BindingBase PickerItemDisplayBinding
+    {
+        get { return itemDisplayBinding; }
+        set
+        {
+            if (itemDisplayBinding == value)
+                return;
+
+            OnPropertyChanging();
+            itemDisplayBinding = value;
+        }
+    }
+}
