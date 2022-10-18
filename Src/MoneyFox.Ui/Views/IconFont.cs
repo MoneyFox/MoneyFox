e783cbcc0ad1a84a142af5a90b5337c124d4950a
NPadrutt <nino.padrutt@hotmail.com>
2022-10-18 17:57:49 +0200

    start adding custom controls

---
+++ Src/MoneyFox.Ui/Views/IconFont.cs
@@ -0,0 +1,12 @@
+namespace MoneyFox.Ui.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+static class IconFont
+{
+    public const string Expand_more = "\ue5cf";
+    public const string Exposure_neg_1 = "\ue3cb";
+}
