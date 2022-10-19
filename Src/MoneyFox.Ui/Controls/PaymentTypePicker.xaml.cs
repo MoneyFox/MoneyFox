03b12b8d67b5f43943980842e547d7b7baa57231
Nino Padrutt <nino.padrutt@hotmail.com>
2022-10-19 19:10:24 +0200

    add payment type picker

---
+++ Src/MoneyFox.Ui/Controls/PaymentTypePicker.xaml.cs
@@ -0,0 +1,32 @@
+namespace MoneyFox.Ui.Controls;
+
+using System.Collections;
+
+public partial class PaymentTypePicker : ContentView
+{
+    public PaymentTypePicker()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty PickerTitleProperty = BindableProperty.Create(nameof(PickerTitle), typeof(string), typeof(PaymentTypePicker), string.Empty);
+    public static readonly BindableProperty PickerItemsSourceProperty = BindableProperty.Create(nameof(PickerItemsSource), typeof(IList), typeof(PaymentTypePicker));
+    public static readonly BindableProperty PickerSelectedItemProperty = BindableProperty.Create(nameof(PickerSelectedItem), typeof(object), typeof(PaymentTypePicker));
+
+    public string PickerTitle
+    {
+        get => (string)GetValue(PaymentTypePicker.PickerTitleProperty);
+        set => SetValue(PaymentTypePicker.PickerTitleProperty, value);
+    }
+    public IList PickerItemsSource
+    {
+        get => (IList)GetValue(PaymentTypePicker.PickerItemsSourceProperty);
+        set => SetValue(PaymentTypePicker.PickerItemsSourceProperty, value);
+    }
+
+    public object PickerSelectedItem
+    {
+        get => GetValue(PaymentTypePicker.PickerSelectedItemProperty);
+        set => SetValue(PaymentTypePicker.PickerSelectedItemProperty, value);
+    }
+}
