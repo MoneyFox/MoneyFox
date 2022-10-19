b6e3d9b7d88d2005c9760207cb45e22a3d261841
Nino Padrutt <nino.padrutt@hotmail.com>
2022-10-19 23:28:28 +0200

    rename bindings

--- Src/MoneyFox.Ui/Controls/AccountPicker.xaml.cs
+++ Src/MoneyFox.Ui/Controls/AccountPicker.xaml.cs
@@ -10,23 +10,23 @@ public partial class AccountPicker : ContentView
     }
 
     public static readonly BindableProperty PickerTitleProperty = BindableProperty.Create(nameof(PickerTitle), typeof(string), typeof(AccountPicker), string.Empty, BindingMode.OneWay);
-    public static readonly BindableProperty PickerItemsSourceProperty = BindableProperty.Create(nameof(PickerItemsSource), typeof(IList), typeof(AccountPicker));
-    public static readonly BindableProperty PickerSelectedItemProperty = BindableProperty.Create(nameof(PickerSelectedItem), typeof(object), typeof(AccountPicker), BindingMode.TwoWay);
+    public static readonly BindableProperty AccountsSourceProperty = BindableProperty.Create(nameof(AccountsSource), typeof(IList), typeof(AccountPicker));
+    public static readonly BindableProperty SelectedAccountProperty = BindableProperty.Create(nameof(SelectedAccount), typeof(object), typeof(AccountPicker), BindingMode.TwoWay);
 
     public string PickerTitle
     {
         get => (string)GetValue(AccountPicker.PickerTitleProperty);
         set => SetValue(AccountPicker.PickerTitleProperty, value);
     }
-    public IList PickerItemsSource
+    public IList AccountsSource
     {
-        get => (IList)GetValue(AccountPicker.PickerItemsSourceProperty);
-        set => SetValue(AccountPicker.PickerItemsSourceProperty, value);
+        get => (IList)GetValue(AccountPicker.AccountsSourceProperty);
+        set => SetValue(AccountPicker.AccountsSourceProperty, value);
     }
 
-    public object PickerSelectedItem
+    public object SelectedAccount
     {
-        get => GetValue(AccountPicker.PickerSelectedItemProperty);
-        set => SetValue(AccountPicker.PickerSelectedItemProperty, value);
+        get => GetValue(AccountPicker.SelectedAccountProperty);
+        set => SetValue(AccountPicker.SelectedAccountProperty, value);
     }
 }
