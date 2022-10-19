b32fe52447165f0b080d02aa1a3953d1c50fde06
Nino Padrutt <nino.padrutt@hotmail.com>
2022-10-19 20:53:29 +0200

    extend custom controls

--- Src/MoneyFox.Ui/Controls/AmountEntry.xaml.cs
+++ Src/MoneyFox.Ui/Controls/AmountEntry.xaml.cs
@@ -9,8 +9,6 @@ public partial class AmountEntry : ContentView
 
     public static readonly BindableProperty EntryTextProperty = BindableProperty.Create(nameof(EntryText), typeof(string), typeof(AmountEntry), string.Empty, BindingMode.TwoWay);
     public static readonly BindableProperty EntryPlaceholderProperty = BindableProperty.Create(nameof(EntryPlaceholder), typeof(string), typeof(AmountEntry), string.Empty);
-    public static readonly BindableProperty EntryKeyboardProperty = BindableProperty.Create(nameof(EntryKeyboard), typeof(Keyboard), typeof(AmountEntry), Keyboard.Default);
-    public static readonly BindableProperty EntryHorizontalTextAlignmentProperty = BindableProperty.Create(nameof(EntryHorizontalTextAlignment), typeof(TextAlignment), typeof(AmountEntry), TextAlignment.Start);
 
     public string EntryText
     {
@@ -24,15 +22,9 @@ public partial class AmountEntry : ContentView
         set => SetValue(AmountEntry.EntryPlaceholderProperty, value);
     }
 
-    public Keyboard EntryKeyboard
+    private void AmountFieldGotFocus(object sender, FocusEventArgs e)
     {
-        get => (Keyboard)GetValue(AmountEntry.EntryKeyboardProperty);
-        set => SetValue(AmountEntry.EntryKeyboardProperty, value);
-    }
-
-    public TextAlignment EntryHorizontalTextAlignment
-    {
-        get => (TextAlignment)GetValue(AmountEntry.EntryHorizontalTextAlignmentProperty);
-        set => SetValue(AmountEntry.EntryHorizontalTextAlignmentProperty, value);
+        AmountField.CursorPosition = 0;
+        AmountField.SelectionLength = AmountField.Text != null ? AmountField.Text.Length : 0;
     }
 }
--- Src/MoneyFox.Ui/Views/Payments/ModifyPaymentContentView.xaml.cs
+++ Src/MoneyFox.Ui/Views/Payments/ModifyPaymentContentView.xaml.cs
@@ -1,14 +1,9 @@
 namespace MoneyFox.Ui.Views.Payments;
+
 public partial class ModifyPaymentContentView
 {
     public ModifyPaymentContentView()
     {
         InitializeComponent();
-    }
-
-    private void AmountFieldGotFocus(object sender, FocusEventArgs e)
-    {
-        //AmountEntry.CursorPosition = 0;
-        //AmountEntry.SelectionLength = AmountEntry.Text?.Length ?? 0;
     }
 }
