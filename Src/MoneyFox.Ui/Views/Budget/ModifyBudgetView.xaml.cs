93cf41dfb1e067d8d170e907e834dcf506a9a011
NPadrutt <nino.padrutt@hotmail.com>
2022-10-21 15:55:02 +0200

    remove unused method

--- Src/MoneyFox.Ui/Views/Budget/ModifyBudgetView.xaml.cs
+++ Src/MoneyFox.Ui/Views/Budget/ModifyBudgetView.xaml.cs
@@ -1,15 +1,9 @@
-﻿namespace MoneyFox.Ui.Views.Budget;
+namespace MoneyFox.Ui.Views.Budget;
 
 public partial class ModifyBudgetView
 {
     public ModifyBudgetView()
     {
         InitializeComponent();
-    }
-
-    private void AmountFieldGotFocus(object sender, FocusEventArgs e)
-    {
-        AmountEntry.CursorPosition = 0;
-        AmountEntry.SelectionLength = AmountEntry.Text?.Length ?? 0;
     }
 }
