5d1c89d4ba82c37fb34bc3c2db5c6307d09eb7ff
Nino Padrutt <nino.padrutt@hotmail.com>
2022-10-22 17:21:12 +0200

    add custom datepicker

---
+++ Src/MoneyFox.Ui/Controls/CustomDatePicker.xaml.cs
@@ -0,0 +1,37 @@
+namespace MoneyFox.Ui.Controls;
+
+using System.Windows.Input;
+
+public partial class CustomDatePicker : ContentView
+{
+    public static readonly BindableProperty DatePickerTitleProperty = BindableProperty.Create(
+        propertyName: nameof(DatePickerTitle),
+        returnType: typeof(string),
+        declaringType: typeof(CustomDatePicker),
+        defaultValue: string.Empty);
+
+    public static readonly BindableProperty DateFieldProperty = BindableProperty.Create(
+        propertyName: nameof(DateField),
+        returnType: typeof(string),
+        declaringType: typeof(CustomDatePicker),
+        defaultValue: string.Empty);
+
+    public CustomDatePicker()
+    {
+        InitializeComponent();
+    }
+
+    public string DatePickerTitle
+    {
+        get => (string)GetValue(DatePickerTitleProperty);
+        set => SetValue(property: DatePickerTitleProperty, value: value);
+    }
+
+    public string DateField
+    {
+        get => (string)GetValue(DateFieldProperty);
+        set => SetValue(property: DateFieldProperty, value: value);
+    }
+}
+
+
