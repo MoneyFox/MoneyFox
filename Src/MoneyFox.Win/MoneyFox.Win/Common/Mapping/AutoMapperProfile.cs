617f134ff5db157db4119133502f98a8c2433863
Nino Padrutt <nino.padrutt@hotmail.com>
2022-10-22 11:03:27 +0200

    adjust profiles

--- Src/MoneyFox.Win/MoneyFox.Win/Common/Mapping/AutoMapperProfile.cs
+++ Src/MoneyFox.Win/MoneyFox.Win/Common/Mapping/AutoMapperProfile.cs
@@ -1,26 +1,14 @@
 namespace MoneyFox.Win.Common.Mapping;
 
 using AutoMapper;
-using ViewModels.Accounts;
 using ViewModels.Payments;
 
 public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
-        LoadCustomMappings();
-    }
-
-    private void LoadCustomMappings()
-    {
-        var mapsFromNotShared = MapperProfileHelper.LoadCustomMappings(typeof(PaymentViewModel).Assembly);
-        foreach (var map in mapsFromNotShared)
-        {
-            map.CreateMappings(this);
-        }
-
-        var mapsFromShared = MapperProfileHelper.LoadCustomMappings(typeof(AccountViewModel).Assembly);
-        foreach (var map in mapsFromShared)
+        var mappings = MapperProfileHelper.LoadCustomMappings(typeof(PaymentViewModel).Assembly);
+        foreach (var map in mappings)
         {
             map.CreateMappings(this);
         }
