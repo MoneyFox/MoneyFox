e702438886c1cb0d8175ff200dc063dc33a007eb
NPadrutt <nino.padrutt@hotmail.com>
2022-10-25 15:15:31 +0200

    add unit test and command to delete category

--- Src/MoneyFox.Ui/ViewModels/Categories/EditCategoryViewModel.cs
+++ Src/MoneyFox.Ui/ViewModels/Categories/EditCategoryViewModel.cs
@@ -1,21 +1,29 @@
-﻿namespace MoneyFox.Ui.ViewModels.Categories;
+namespace MoneyFox.Ui.ViewModels.Categories;
 
 using AutoMapper;
+using CommunityToolkit.Mvvm.Input;
 using Core.ApplicationCore.Domain.Aggregates.CategoryAggregate;
 using Core.ApplicationCore.Queries;
 using Core.Commands.Categories.UpdateCategory;
 using Core.Common.Interfaces;
 using MediatR;
+using MoneyFox.Core.Commands.Categories.DeleteCategoryById;
+using MoneyFox.Core.Interfaces;
+using MoneyFox.Core.Resources;
 
-internal sealed class EditCategoryViewModel : ModifyCategoryViewModel
+internal sealed partial class EditCategoryViewModel : ModifyCategoryViewModel
 {
     private readonly IMapper mapper;
     private readonly IMediator mediator;
+    private readonly IDialogService dialogService;
+    private readonly INavigationService navigationService;
 
-    public EditCategoryViewModel(IMediator mediator, IMapper mapper, IDialogService dialogService) : base(mediator: mediator, dialogService: dialogService)
+    public EditCategoryViewModel(IMediator mediator, IMapper mapper, IDialogService dialogService, INavigationService navigationService) : base(mediator: mediator, dialogService: dialogService)
     {
         this.mediator = mediator;
         this.mapper = mapper;
+        this.dialogService = dialogService;
+        this.navigationService = navigationService;
     }
 
     public async Task InitializeAsync(int categoryId)
@@ -27,6 +35,15 @@ internal sealed class EditCategoryViewModel : ModifyCategoryViewModel
     {
         await mediator.Send(new UpdateCategoryCommand(mapper.Map<Category>(SelectedCategory)));
     }
-}
 
+    [RelayCommand]
+    private async Task Delete()
+    {
+        if (await dialogService.ShowConfirmMessageAsync(title: Strings.DeleteTitle, message: Strings.DeleteCategoryConfirmationMessage))
+        {
+            await mediator.Send(new DeleteCategoryByIdCommand(SelectedCategory.Id));
+            await navigationService.GoBackFromModalAsync();
+        }
+    }
+}
 
