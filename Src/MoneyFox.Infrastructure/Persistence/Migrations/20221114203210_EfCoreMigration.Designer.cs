3f9be5756e3aaf06212453acf835631f557871b7
Nino Padrutt <nino.padrutt@hotmail.com>
2022-11-14 21:36:04 +0100

    add migration after update

--- Src/MoneyFox.Infrastructure/Persistence/Migrations/20221114203210_EfCoreMigration.Designer.cs
+++ Src/MoneyFox.Infrastructure/Persistence/Migrations/20221114203210_EfCoreMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoneyFox.Infrastructure.Persistence;
 
@@ -10,9 +11,11 @@ using MoneyFox.Infrastructure.Persistence;
 namespace MoneyFox.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20221114203210_EfCoreMigration")]
+    partial class EfCoreMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0");
---
+++ Src/MoneyFox.Infrastructure/Persistence/Migrations/20221114203210_EfCoreMigration.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyFox.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EfCoreMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Accounts_ChargedAccountId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecurringPayments_Accounts_ChargedAccountId",
+                table: "RecurringPayments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChargedAccountId",
+                table: "RecurringPayments",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChargedAccountId",
+                table: "Payments",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Accounts_ChargedAccountId",
+                table: "Payments",
+                column: "ChargedAccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecurringPayments_Accounts_ChargedAccountId",
+                table: "RecurringPayments",
+                column: "ChargedAccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Accounts_ChargedAccountId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecurringPayments_Accounts_ChargedAccountId",
+                table: "RecurringPayments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChargedAccountId",
+                table: "RecurringPayments",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChargedAccountId",
+                table: "Payments",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Accounts_ChargedAccountId",
+                table: "Payments",
+                column: "ChargedAccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecurringPayments_Accounts_ChargedAccountId",
+                table: "RecurringPayments",
+                column: "ChargedAccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+        }
+    }
+}
