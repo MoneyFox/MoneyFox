5cc3d33386b44a738f7e0a984eefd4f45dddaec1
kg8z <81488531+kg8z@users.noreply.github.com>
2022-11-14 20:36:41 -0800

    Set transaction group card opacity to 40% for future dates. Format Income/Expense values in transaction group header as currency.

--- Src/MoneyFox.Ui/Common/Groups/DateListGroupCollection.cs
+++ Src/MoneyFox.Ui/Common/Groups/DateListGroupCollection.cs
@@ -1,4 +1,4 @@
-﻿namespace MoneyFox.Ui.Common.Groups;
+namespace MoneyFox.Ui.Common.Groups;
 
 using CommunityToolkit.Mvvm.Input;
 using Core.ApplicationCore.Domain.Exceptions;
@@ -31,10 +31,11 @@ public class DateListGroupCollection<T> : List<T>
     /// </summary>
     /// <param name="key">The key for this group.</param>
     /// <param name="itemClickCommand">The command to execute on click</param>
-    private DateListGroupCollection(string key, RelayCommand<T>? itemClickCommand = null)
+    private DateListGroupCollection(string key, RelayCommand<T>? itemClickCommand = null, bool isFutureDate = false)
     {
         Key = key;
         ItemClickCommand = itemClickCommand;
+        IsFutureDate = isFutureDate;
     }
 
     /// <summary>
@@ -46,6 +47,8 @@ public class DateListGroupCollection<T> : List<T>
     ///     The Title of this group.
     /// </summary>
     public string Subtitle { get; set; } = "";
+
+    public bool IsFutureDate { get; } = false;
 
     /// <summary>
     ///     The command to execute on a click.
@@ -73,9 +76,10 @@ public class DateListGroupCollection<T> : List<T>
         foreach (var item in items)
         {
             var index = getKey(item);
+            var infuture = getSortKey(item).Date > DateTime.Today;
             if (list.All(a => a.Key != index))
             {
-                list.Add(new(key: index, itemClickCommand: itemClickCommand));
+                list.Add(new(key: index, itemClickCommand: itemClickCommand, isFutureDate: infuture));
             }
 
             if (!string.IsNullOrEmpty(index))
---
+++ Src/MoneyFox.Ui/Converter/DateGroupCardOpacityConverter.cs
@@ -0,0 +1,18 @@
+namespace MoneyFox.Ui.Converter;
+
+using System;
+using System.Globalization;
+
+public class DateGroupCardOpacityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // Returns 40% opacity if the date is in the future, 100% if it's not
+        return value is bool b && b ? 0.4 : 1.0;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
