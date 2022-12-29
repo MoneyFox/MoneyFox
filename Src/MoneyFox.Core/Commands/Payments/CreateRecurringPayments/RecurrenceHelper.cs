563d325af4bf503723bb51d7aec6eb269bb5b98b
Nino Padrutt <nino.padrutt@hotmail.com>
2022-12-29 20:36:25 +0100

    move translation files

--- Src/MoneyFox.Core/Commands/Payments/CreateRecurringPayments/RecurrenceHelper.cs
+++ Src/MoneyFox.Core/Commands/Payments/CreateRecurringPayments/RecurrenceHelper.cs
@@ -5,7 +5,6 @@ using ApplicationCore.Domain.Aggregates;
 using ApplicationCore.Domain.Aggregates.AccountAggregate;
 using ApplicationCore.Domain.Exceptions;
 using Common.Extensions;
-using Resources;
 
 internal static class RecurrenceHelper
 {
@@ -70,7 +69,8 @@ internal static class RecurrenceHelper
             case PaymentRecurrence.DailyWithoutWeekend:
                 if (currDate.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday)
                 {
-                    throw new InvalidOperationException($"Unable to create a {Translations.DailyWithoutWeekendLabel} recurring payment on a {currDate.DayOfWeek}");
+                    throw new InvalidOperationException(
+                        $"Unable to create a {Translations.DailyWithoutWeekendLabel} recurring payment on a {currDate.DayOfWeek}");
                 }
 
                 return currDate;
