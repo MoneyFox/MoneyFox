9092c6a702569f6ad9aedac56b53a9705531ea12
NPadrutt <nino.padrutt@hotmail.com>
2023-01-12 12:03:12 +0100

    remove work around for titleview on ios

--- Src/MoneyFox.Ui/Platforms/iOS/Renderer/CustomShellRenderer.cs
+++ Src/MoneyFox.Ui/Platforms/iOS/Renderer/CustomShellRenderer.cs
@@ -1,57 +1,15 @@
 namespace MoneyFox.Ui.Platforms.iOS.Renderer;
 
-using CoreGraphics;
 using Microsoft.Maui.Controls.Handlers.Compatibility;
 using Microsoft.Maui.Controls.Platform.Compatibility;
 using UIKit;
 
 public class CustomShellRenderer : ShellRenderer
 {
-    protected override IShellPageRendererTracker CreatePageRendererTracker()
-    {
-        return new CustomShellPageRendererTracker(this);
-    }
-
     protected override IShellNavBarAppearanceTracker CreateNavBarAppearanceTracker()
     {
         return new NoLineAppearanceTracker();
     }
-}
-
-public class CustomShellPageRendererTracker : ShellPageRendererTracker
-{
-    public CustomShellPageRendererTracker(IShellContext context) : base(context) { }
-
-    protected override void UpdateTitleView()
-    {
-        if (ViewController == null || ViewController.NavigationItem == null)
-        {
-            return;
-        }
-
-        var titleView = Shell.GetTitleView(Page);
-        if (titleView == null)
-        {
-            var view = ViewController.NavigationItem.TitleView;
-            ViewController.NavigationItem.TitleView = null;
-            view?.Dispose();
-        }
-        else
-        {
-            var view = new CustomTitleViewContainer(titleView);
-            ViewController.NavigationItem.TitleView = view;
-        }
-    }
-}
-
-public class CustomTitleViewContainer : UIContainerView
-{
-    public CustomTitleViewContainer(View view) : base(view)
-    {
-        TranslatesAutoresizingMaskIntoConstraints = false;
-    }
-
-    public override CGSize IntrinsicContentSize => UILayoutFittingExpandedSize;
 }
 
 public class NoLineAppearanceTracker : IShellNavBarAppearanceTracker
