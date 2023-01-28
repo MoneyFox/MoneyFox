17d85068b531e85adbc2c8dcb185bebd6f612ec2
Nino Padrutt <nino.padrutt@hotmail.com>
2023-01-28 18:33:18 +0100

    simplify code

--- Src/MoneyFox.Ui/Converter/NoCategorySelectedConverter.cs
+++ Src/MoneyFox.Ui/Converter/NoCategorySelectedConverter.cs
@@ -2,15 +2,13 @@ namespace MoneyFox.Ui.Converter;
 
 using System.Globalization;
 using Resources.Strings;
-using Views.Categories;
 using Views.Categories.ModifyCategory;
 
 public class NoCategorySelectedConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        var category = value as CategoryViewModel;
-        return category == null ? Translations.SelectCategoryLabel : category.Name;
+        return value is not CategoryViewModel category ? Translations.SelectCategoryLabel : category.Name;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
