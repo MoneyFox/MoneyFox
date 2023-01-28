a1a70d170ce891115e497f62a29e8a95aaa2593d
Nino Padrutt <nino.padrutt@hotmail.com>
2023-01-28 14:56:18 +0100

    remove category repository

--- Src/MoneyFox.Infrastructure/InversionOfControl/InfrastructureConfig.cs
+++ Src/MoneyFox.Infrastructure/InversionOfControl/InfrastructureConfig.cs
@@ -1,12 +1,9 @@
 namespace MoneyFox.Infrastructure.InversionOfControl;
 
-using Core.ApplicationCore.Domain.Aggregates.BudgetAggregate;
-using Core.ApplicationCore.Domain.Aggregates.CategoryAggregate;
 using Core.ApplicationCore.UseCases.BackupUpload;
 using Core.ApplicationCore.UseCases.DbBackup;
 using Core.Common.Interfaces;
 using Core.Interfaces;
-using DataAccess;
 using DbBackup;
 using DbBackup.Legacy;
 using Microsoft.EntityFrameworkCore;
@@ -21,13 +18,7 @@ public static class InfrastructureConfig
             sp => new DbContextOptionsBuilder<AppDbContext>().UseSqlite($"Data Source={sp.GetService<IDbPathProvider>().GetDbPath()}").Options);
 
         _ = serviceCollection.AddTransient<IAppDbContext, AppDbContext>();
-        RegisterRepositories(serviceCollection);
         RegisterBackupServices(serviceCollection);
-    }
-
-    private static void RegisterRepositories(IServiceCollection serviceCollection)
-    {
-        _ = serviceCollection.AddTransient<ICategoryRepository, CategoryRepository>();
     }
 
     private static void RegisterBackupServices(IServiceCollection serviceCollection)
