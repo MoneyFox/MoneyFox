9f38534dcf453d8fa6e25a58d7621f43e0738471
Nino Padrutt <nino.padrutt@hotmail.com>
2023-01-28 14:20:29 +0100

    use in memory db directly

---
+++ Src/MoneyFox.Core.Tests/InMemoryTestBase.cs
@@ -0,0 +1,27 @@
+﻿namespace MoneyFox.Core.Tests;
+
+using Infrastructure.Persistence;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+public abstract class InMemoryTestBase : IDisposable
+{
+    private readonly SqliteConnection connection;
+
+    protected InMemoryTestBase()
+    {
+        connection = new("Filename=:memory:");
+        connection.Open();
+        var options = new DbContextOptionsBuilder<AppDbContext>().UseSqlite(connection).Options;
+        Context = new(options: options, publisher: null!, settingsFacade: null!);
+        Context.Database.EnsureCreated();
+    }
+
+    protected AppDbContext Context { get; }
+
+    public void Dispose()
+    {
+        connection.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
