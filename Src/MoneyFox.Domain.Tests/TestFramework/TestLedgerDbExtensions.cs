08fc5428fa9fddd2e634c9ee7a993f963bea292e
Nino Padrutt <nino.padrutt@hotmail.com>
2023-01-29 11:16:54 +0100

    testframework

---
+++ Src/MoneyFox.Domain.Tests/TestFramework/TestLedgerDbExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace MoneyFox.Domain.Tests.TestFramework;
+
+using Domain.Aggregates.LedgerAggregate;
+using Infrastructure.Persistence;
+
+internal static class TestLedgerDbExtensions
+{
+    public static void RegisterLedgers(this AppDbContext db, params TestData.ILedger[] testLedgers)
+    {
+        foreach (var ledger in testLedgers)
+        {
+            db.Add(ledger.CreateDbLedger());
+        }
+
+        db.SaveChanges();
+    }
+
+    public static Ledger RegisterLedger(this AppDbContext db, TestData.ILedger testLedger)
+    {
+        var dbLedger = testLedger.CreateDbLedger();
+        db.Add(dbLedger);
+        db.SaveChanges();
+
+        return dbLedger;
+    }
+}
---
+++ Src/MoneyFox.Domain.Tests/TestFramework/TestLedgerDbFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace MoneyFox.Domain.Tests.TestFramework;
+
+using Domain.Aggregates.CategoryAggregate;
+using Domain.Aggregates.LedgerAggregate;
+
+internal static class TestLedgerDbFactory
+{
+    internal static Ledger CreateDbLedger(this TestData.ILedger ledger)
+    {
+        return Ledger.Create(ledger.Name, ledger.CurrentBalance, ledger.Note, ledger.IsExcluded);
+    }
+}
