258a02070a74b963abc40eb7705c9ecac3406bcd
Nino Padrutt <nino.padrutt@hotmail.com>
2023-02-02 22:59:30 +0100

    add new setting

--- Src/MoneyFox.Core/Common/Facades/SettingsFacade.cs
+++ Src/MoneyFox.Core/Common/Facades/SettingsFacade.cs
@@ -16,27 +16,22 @@ public interface ISettingsFacade
 
     bool IsSetupCompleted { get; set; }
 
+    bool IsCategoryCleanupExecuted { get; set; }
+
     DateTime LastExecutionTimeStampSyncBackup { get; set; }
 }
 
 public class SettingsFacade : ISettingsFacade
 {
-    private const string AUTO_UPLOAD_BACKUP_KEY_NAME = "AutoUploadBackup";
-    private const bool AUTO_UPLOAD_BACKUP_KEY_DEFAULT = false;
-
-    private const string BACKUP_LOGGED_IN_KEY_NAME = "BackupLoggedIn";
-    private const bool BACKUP_LOGGED_IN_KEY_DEFAULT = false;
-
-    private const string LAST_EXECUTION_TIME_STAMP_SYNC_BACKUP_KEY_NAME = "LastExecutionTimeStampSyncBackup";
-    private const string LAST_EXECUTION_TIME_STAMP_SYNC_BACKUP_KEY_DEFAULT = "";
-
-    private const string DEFAULT_CULTURE_KEY_NAME = "DefaultCulture";
-    private const string DATABASE_LAST_UPDATE_KEY_NAME = "DatabaseLastUpdate";
-
-    private const string IS_SETUP_COMPLETED_KEY_NAME = "IsSetupCompleted";
-    private const bool IS_SETUP_COMPLETED_KEY_DEFAULT = false;
-
     private readonly string defaultCultureKeyDefault = CultureInfo.CurrentCulture.Name;
+    private const string DEFAULT_CULTURE_KEY_NAME = "DefaultCulture";
+
+    private const string AUTO_UPLOAD_BACKUP_KEY_NAME = "AutoUploadBackup";
+    private const string BACKUP_LOGGED_IN_KEY_NAME = "BackupLoggedIn";
+    private const string LAST_EXECUTION_TIME_STAMP_SYNC_BACKUP_KEY_NAME = "LastExecutionTimeStampSyncBackup";
+    private const string DATABASE_LAST_UPDATE_KEY_NAME = "DatabaseLastUpdate";
+    private const string IS_SETUP_COMPLETED_KEY_NAME = "IsSetupCompleted";
+    private const string IS_CATEGORY_CLEANUP_EXECUTED_KEY_NAME = "CategoryCleanupExecuted";
 
     private readonly ISettingsAdapter settingsAdapter;
 
@@ -47,7 +42,7 @@ public class SettingsFacade : ISettingsFacade
 
     public bool IsBackupAutoUploadEnabled
     {
-        get => settingsAdapter.GetValue(key: AUTO_UPLOAD_BACKUP_KEY_NAME, defaultValue: AUTO_UPLOAD_BACKUP_KEY_DEFAULT);
+        get => settingsAdapter.GetValue(key: AUTO_UPLOAD_BACKUP_KEY_NAME, defaultValue: false);
         set => settingsAdapter.AddOrUpdate(key: AUTO_UPLOAD_BACKUP_KEY_NAME, value: value);
     }
 
@@ -65,7 +60,7 @@ public class SettingsFacade : ISettingsFacade
 
     public bool IsLoggedInToBackupService
     {
-        get => settingsAdapter.GetValue(key: BACKUP_LOGGED_IN_KEY_NAME, defaultValue: BACKUP_LOGGED_IN_KEY_DEFAULT);
+        get => settingsAdapter.GetValue(key: BACKUP_LOGGED_IN_KEY_NAME, defaultValue: false);
         set => settingsAdapter.AddOrUpdate(key: BACKUP_LOGGED_IN_KEY_NAME, value: value);
     }
 
@@ -73,7 +68,7 @@ public class SettingsFacade : ISettingsFacade
     {
         get
             => DateTime.TryParse(
-                s: settingsAdapter.GetValue(key: LAST_EXECUTION_TIME_STAMP_SYNC_BACKUP_KEY_NAME, defaultValue: LAST_EXECUTION_TIME_STAMP_SYNC_BACKUP_KEY_DEFAULT),
+                s: settingsAdapter.GetValue(key: LAST_EXECUTION_TIME_STAMP_SYNC_BACKUP_KEY_NAME, defaultValue: string.Empty),
                 provider: CultureInfo.InvariantCulture,
                 styles: DateTimeStyles.None,
                 result: out var outValue)
@@ -91,7 +86,13 @@ public class SettingsFacade : ISettingsFacade
 
     public bool IsSetupCompleted
     {
-        get => settingsAdapter.GetValue(key: IS_SETUP_COMPLETED_KEY_NAME, defaultValue: IS_SETUP_COMPLETED_KEY_DEFAULT);
+        get => settingsAdapter.GetValue(key: IS_SETUP_COMPLETED_KEY_NAME, defaultValue: false);
         set => settingsAdapter.AddOrUpdate(key: IS_SETUP_COMPLETED_KEY_NAME, value: value);
+    }
+
+    public bool IsCategoryCleanupExecuted
+    {
+        get => settingsAdapter.GetValue(key: IS_CATEGORY_CLEANUP_EXECUTED_KEY_NAME, defaultValue: false);
+        set => settingsAdapter.AddOrUpdate(key: IS_CATEGORY_CLEANUP_EXECUTED_KEY_NAME, value: value);
     }
 }
