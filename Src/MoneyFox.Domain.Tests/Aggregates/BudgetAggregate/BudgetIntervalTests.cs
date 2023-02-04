d5c9d95c426adfb03aca5fa3e61a3d1907ab8b42
Nino Padrutt <nino.padrutt@hotmail.com>
2023-02-04 17:02:55 +0100

    validation for interval

---
+++ Src/MoneyFox.Domain.Tests/Aggregates/BudgetAggregate/BudgetIntervalTests.cs
@@ -0,0 +1,33 @@
+﻿namespace MoneyFox.Domain.Tests.Aggregates.BudgetAggregate;
+
+using Domain.Aggregates.BudgetAggregate;
+using Exceptions;
+using FluentAssertions;
+
+public sealed class BudgetIntervalTests
+{
+    [Fact]
+    public void BeCorrectlyCreated()
+    {
+        // Arrange
+        const int numberOfMonths = 10;
+
+        // Act
+        var budgetInterval = new BudgetInterval(numberOfMonths);
+
+        // Assert
+        budgetInterval.NumberOfMonths.Should().Be(numberOfMonths);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void ThrowWhenInvalidNumberOfMonthsPassed(int numberOfMonths)
+    {
+        // Act
+        Action act = () => _ = new BudgetInterval(numberOfMonths);
+
+        // Assert
+        act.Should().Throw<InvalidArgumentException>();
+    }
+}
--- Src/MoneyFox.Domain/Aggregates/BudgetAggregate/BudgetInterval.cs
+++ Src/MoneyFox.Domain/Aggregates/BudgetAggregate/BudgetInterval.cs
@@ -1,7 +1,21 @@
 ﻿namespace MoneyFox.Domain.Aggregates.BudgetAggregate;
 
-public sealed record BudgetInterval(int NumberOfMonths)
+using Exceptions;
+
+public sealed record BudgetInterval
 {
+    public BudgetInterval(int numberOfMonths)
+    {
+        if (numberOfMonths < 1)
+        {
+            throw new InvalidArgumentException(paramName: nameof(numberOfMonths), message: "Value can't be 0 or negative.");
+        }
+
+        NumberOfMonths = numberOfMonths;
+    }
+
+    public int NumberOfMonths { get; }
+
     public static implicit operator decimal(BudgetInterval budgetInterval)
     {
         return budgetInterval.NumberOfMonths;
