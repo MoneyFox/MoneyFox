127b5134ddb015e7d7f7dc448c4906cc9aa0747f
Nino Padrutt <nino.padrutt@hotmail.com>
2023-02-05 18:06:34 +0100

    add to git

---
+++ Src/MoneyFox.Ui/Views/Backup/DateVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace MoneyFox.Ui.Views.Backup;
+
+using System.Globalization;
+
+/// <summary>
+///     Hides the date if it is equal to the default DateTime Value.
+/// </summary>
+public class DateVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (DateTime)value != new DateTime();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (DateTime)value != new DateTime();
+    }
+}
