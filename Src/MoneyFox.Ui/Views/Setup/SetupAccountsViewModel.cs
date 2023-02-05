c6ce5ccf15f01412cd84704ce4967819f02892f3
Nino Padrutt <nino.padrutt@hotmail.com>
2023-02-05 14:47:27 +0100

    ui adjustments

--- Src/MoneyFox.Ui/Views/Setup/SetupAccountsViewModel.cs
+++ Src/MoneyFox.Ui/Views/Setup/SetupAccountsViewModel.cs
@@ -8,4 +8,6 @@ public sealed class SetupAccountsViewModel : BasePageViewModel
     public AsyncRelayCommand GoToAddAccountCommand => new(async () => await Shell.Current.GoToModalAsync(Routes.AddAccountRoute));
 
     public AsyncRelayCommand NextStepCommand => new(async () => await Shell.Current.GoToAsync(Routes.CategoryIntroductionRoute));
+
+    public AsyncRelayCommand BackCommand => new(Shell.Current.Navigation.PopAsync);
 }
