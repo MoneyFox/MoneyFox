f068301a185fdffa56c37209a8e6c8097dbbc043
Nino Padrutt <nino.padrutt@hotmail.com>
2023-02-28 21:10:22 +0100

    create new baseviewmodel

---
+++ Src/MoneyFox.Ui/Views/ObservableViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿namespace MoneyFox.Ui.Views;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class ObservableViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void RaisePropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    protected bool SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(property, value))
+        {
+            return false;
+        }
+
+        property = value;
+        RaisePropertyChanged(propertyName);
+        return true;
+    }
+}
