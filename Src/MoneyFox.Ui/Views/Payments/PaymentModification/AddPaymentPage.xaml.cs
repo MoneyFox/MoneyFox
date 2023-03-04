0f298625432752ef784568ca63b942535eb09ffd
Nino Padrutt <nino.padrutt@hotmail.com>
2023-03-04 13:09:31 +0100

    add on appearing and disappearing

--- Src/MoneyFox.Ui/Views/Payments/PaymentModification/AddPaymentPage.xaml.cs
+++ Src/MoneyFox.Ui/Views/Payments/PaymentModification/AddPaymentPage.xaml.cs
@@ -7,4 +7,18 @@ public partial class AddPaymentPage
         InitializeComponent();
         BindingContext = App.GetViewModel<AddPaymentViewModel>();
     }
+
+    private AddPaymentViewModel ViewModel => (AddPaymentViewModel)BindingContext;
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        ViewModel.IsActive = true;
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        ViewModel.IsActive = false;
+    }
 }
