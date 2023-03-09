765cd5acc6e2a62142918121f67685db575c0b19
Nino Padrutt <nino.padrutt@hotmail.com>
2023-03-09 23:56:12 +0100

    add group footer

--- Src/MoneyFox.Ui/Views/Payments/PaymentList/PaymentDayGroup.cs
+++ Src/MoneyFox.Ui/Views/Payments/PaymentList/PaymentDayGroup.cs
@@ -2,10 +2,12 @@ namespace MoneyFox.Ui.Views.Payments.PaymentList;
 
 public class PaymentDayGroup : List<PaymentListItemViewModel>
 {
-    public DateOnly Date { get; }
-
     public PaymentDayGroup(DateOnly date, List<PaymentListItemViewModel> payments) : base(payments)
     {
         Date = date;
     }
+
+    public DateOnly Date { get; }
+
+    public decimal Sum => this.Sum(p => p.Amount);
 }
