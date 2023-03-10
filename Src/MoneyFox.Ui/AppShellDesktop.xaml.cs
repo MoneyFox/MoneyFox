7740205a8579c591c80fc4817878c011ca73acc3
NPadrutt <nino.padrutt@hotmail.com>
2023-03-10 13:55:05 +0100

    start adjusting account list page

--- Src/MoneyFox.Ui/AppShellDesktop.xaml.cs
+++ Src/MoneyFox.Ui/AppShellDesktop.xaml.cs
@@ -53,8 +53,7 @@ public partial class AppShellDesktop
         Routing.RegisterRoute(route: Routes.AddBudgetRoute, type: typeof(AddBudgetPage));
         Routing.RegisterRoute(route: Routes.EditBudgetRoute, type: typeof(EditBudgetPage));
         Routing.RegisterRoute(route: Routes.CategoryListRoute, type: typeof(DesktopCategoryListPage));
-        Routing.RegisterRoute(route: Routes.AccountListRoute, type: typeof(DesktopAccountListPage));
+        Routing.RegisterRoute(route: Routes.AccountListRoute, type: typeof(AccountListPage));
         Routing.RegisterRoute(route: Routes.SelectCategoryRoute, type: typeof(DesktopSelectedCategoryPage));
-
     }
 }
