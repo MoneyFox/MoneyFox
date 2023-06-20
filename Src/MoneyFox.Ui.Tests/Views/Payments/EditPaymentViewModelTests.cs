274ef705a0dc24b39c7bb5340b313c81be035259
Nino Padrutt <nino.padrutt@hotmail.com>
2023-06-20 19:25:51 +0100

    add failing test for mapping

--- Src/MoneyFox.Ui.Tests/Views/Payments/EditPaymentViewModelTests.cs
+++ Src/MoneyFox.Ui.Tests/Views/Payments/EditPaymentViewModelTests.cs
@@ -3,7 +3,12 @@ namespace MoneyFox.Ui.Tests.Views.Payments;
 using AutoMapper;
 using Core.Common.Interfaces;
 using Core.Common.Settings;
+using Core.Queries;
 using Domain;
+using Domain.Aggregates.AccountAggregate;
+using Domain.Tests.TestFramework;
+using FluentAssertions;
+using Mapping;
 using MediatR;
 using NSubstitute;
 using Ui.Views.Payments.PaymentModification;
@@ -11,6 +16,38 @@ using Xunit;
 
 public sealed class EditPaymentViewModelTests
 {
+    [Fact]
+    public async Task SelectedPaymentSet_AfterInitialization()
+    {
+        // Arrange
+        var dialogService = Substitute.For<IDialogService>();
+        var toastService = Substitute.For<IToastService>();
+        var mediator = Substitute.For<IMediator>();
+
+        var settingsFacade = Substitute.For<ISettingsFacade>();
+        settingsFacade.DefaultCurrency.Returns("CHF");
+
+        var dbPayment = new TestData.DefaultExpense().CreateDbPayment();
+        var dbAccount = new TestData.DefaultAccount().CreateDbAccount();
+        mediator.Send(Arg.Any<GetAccountsQuery>(), Arg.Any<CancellationToken>()).Returns(new List<Account> { dbAccount });
+        mediator.Send(Arg.Any<GetPaymentByIdQuery>(), Arg.Any<CancellationToken>()).Returns(dbPayment);
+        var vm = new EditPaymentViewModel(
+            mediator: mediator,
+            mapper: AutoMapperFactory.Create(),
+            dialogService: dialogService,
+            toastService: toastService,
+            settingsFacade: settingsFacade,
+            categorySelectionViewModel: new(navigationService: Substitute.For<INavigationService>()));
+
+        dialogService.ShowConfirmMessageAsync(title: Arg.Any<string>(), message: Arg.Any<string>()).Returns(true);
+
+        // Act
+        await vm.InitializeAsync(dbPayment.Id);
+
+        // Assert
+        vm.SelectedPayment.Should().NotBeNull();
+    }
+
     [Fact]
     public void ShowToast_WhenExceptionThrownDuringSave()
     {
