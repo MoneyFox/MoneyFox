5a174048c28800e735bfb2a78995a97667421d95
Nino Padrutt <nino.padrutt@hotmail.com>
2023-06-26 21:23:44 +0200

    remove unused field on budget

--- Src/MoneyFox.Domain.Tests/TestFramework/TestData.Budget.cs
+++ Src/MoneyFox.Domain.Tests/TestFramework/TestData.Budget.cs
@@ -8,13 +8,12 @@ internal static partial class TestData
     internal sealed class DefaultBudget : IBudget
     {
         public int Id { get; set; } = 10;
-        public string Name { get; set; } = "Beverages";
-        public SpendingLimit SpendingLimit { get; set; } = new(100.50m);
-        public decimal CurrentSpending { get; set; } = 60.20m;
+        public string Name { get; init; } = "Beverages";
+        public SpendingLimit SpendingLimit { get; init; } = new(100.50m);
+        public decimal CurrentSpending { get; init; } = 60.20m;
         public decimal MonthlyBudget => SpendingLimit / Interval;
-        public BudgetInterval Interval { get; set; } = new(1);
-        public BudgetTimeRange BudgetTimeRange { get; set; } = BudgetTimeRange.Last2Years;
-        public IReadOnlyList<int> Categories { get; set; } = ImmutableList.Create(11);
+        public BudgetInterval Interval { get; init; } = new(1);
+        public IReadOnlyList<int> Categories { get; init; } = ImmutableList.Create(11);
     }
 
     internal interface IBudget
@@ -25,7 +24,6 @@ internal static partial class TestData
         decimal CurrentSpending { get; }
         decimal MonthlyBudget { get; }
         BudgetInterval Interval { get; }
-        BudgetTimeRange BudgetTimeRange { get; }
         IReadOnlyList<int> Categories { get; }
     }
 }
