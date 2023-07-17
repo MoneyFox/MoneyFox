05b682a5a996638d6add67fd8d9d9a23713a295a
Nino Padrutt <nino.padrutt@hotmail.com>
2023-07-17 08:27:20 +0100

    Revert "Revert "add total expenses and revenue""
    This reverts commit 756c85efb129c3d4a2c1351ade1fd4e9780fc32b.

--- Src/MoneyFox.Ui/Views/Statistics/CategorySummary/PaymentForCategoryListViewModel.cs
+++ Src/MoneyFox.Ui/Views/Statistics/CategorySummary/PaymentForCategoryListViewModel.cs
@@ -5,6 +5,7 @@ using AutoMapper;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using Core.Queries;
+using Domain.Aggregates.AccountAggregate;
 using MediatR;
 using Resources.Strings;
 
@@ -13,7 +14,7 @@ internal sealed class PaymentForCategoryListViewModel : BasePageViewModel, IReci
     private readonly IMapper mapper;
     private readonly IMediator mediator;
 
-    private ReadOnlyObservableCollection<PaymentDayGroup> paymentDayGroups = null!;
+    private ReadOnlyObservableCollection<PaymentDayGroup> paymentDayGroups = new ReadOnlyObservableCollection<PaymentDayGroup>(new());
 
     private string title = string.Empty;
 
@@ -34,6 +35,10 @@ internal sealed class PaymentForCategoryListViewModel : BasePageViewModel, IReci
         get => paymentDayGroups;
         private set => SetProperty(field: ref paymentDayGroups, newValue: value);
     }
+
+    public decimal TotalRevenue => PaymentDayGroups.Sum(pdg => pdg.TotalRevenue);
+
+    public decimal TotalExpenses => PaymentDayGroups.Sum(pdg => pdg.TotalExpense);
 
     public AsyncRelayCommand<PaymentListItemViewModel> GoToEditPaymentCommand
         => new(async pvm => await Shell.Current.GoToAsync($"{Routes.EditPaymentRoute}?paymentId={pvm!.Id}"));
