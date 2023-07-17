615a8c025493e89fa143e8040608357fc26988f2
Nino Padrutt <nino.padrutt@hotmail.com>
2023-07-17 18:04:29 +0100

    cleanup

--- Src/MoneyFox.Ui.Tests/Views/Statistics/CategorySummary/PaymentDayGroupTests.cs
+++ Src/MoneyFox.Ui.Tests/Views/Statistics/CategorySummary/PaymentDayGroupTests.cs
@@ -11,24 +11,25 @@ public sealed class PaymentDayGroupTests
     public void CalculatesTotalExpenseAndRevenueCorrectly_WhenGroupContainsNoItems()
     {
         // Arrange
-        var group = new PaymentDayGroup(DateOnly.FromDateTime(DateTime.Today), new List<PaymentListItemViewModel>());
+        var group = new PaymentDayGroup(date: DateOnly.FromDateTime(DateTime.Today), payments: new List<PaymentListItemViewModel>());
 
         // Act / Assert
         group.TotalExpense.Should().Be(0);
         group.TotalRevenue.Should().Be(0);
     }
-    
+
     [Fact]
     public void CalculatesTotalExpenseAndRevenueCorrectly_WithTransferInList()
     {
         // Arrange
         var paymentListItemViewModels = new List<PaymentListItemViewModel>
         {
-            new PaymentListItemViewModel { Amount = 10, Type = PaymentType.Expense },
-            new PaymentListItemViewModel { Amount = 20, Type = PaymentType.Income },
-            new PaymentListItemViewModel { Amount = 30, Type = PaymentType.Transfer }
+            new() { Amount = 10, Type = PaymentType.Expense },
+            new() { Amount = 20, Type = PaymentType.Income },
+            new() { Amount = 30, Type = PaymentType.Transfer }
         };
-        var group = new PaymentDayGroup(DateOnly.FromDateTime(DateTime.Today), paymentListItemViewModels);
+
+        var group = new PaymentDayGroup(date: DateOnly.FromDateTime(DateTime.Today), payments: paymentListItemViewModels);
 
         // Act / Assert
         group.TotalExpense.Should().Be(10);
