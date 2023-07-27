cec86a843f0417fc7e029b7f014502c58767c9e3
NPadrutt <nino.padrutt@hotmail.com>
2023-07-27 14:37:30 +0200

    extract assertion

--- Src/MoneyFox.Domain.Tests/Aggregates/RecurringTransactionAggregate/RecurringTransactionTests.cs
+++ Src/MoneyFox.Domain.Tests/Aggregates/RecurringTransactionAggregate/RecurringTransactionTests.cs
@@ -1,8 +1,8 @@
 namespace MoneyFox.Domain.Tests.Aggregates.RecurringTransactionAggregate;
 
 using Domain.Aggregates.RecurringTransactionAggregate;
-using FluentAssertions;
 using TestFramework;
+using static TestFramework.RecurringTransactionAssertion;
 
 public sealed class RecurringTransactionTests
 {
@@ -27,17 +27,6 @@ public sealed class RecurringTransactionTests
             isLastDayOfMonth: testRecurringTransfer.IsLastDayOfMonth);
 
         // Assert
-        recurringTransaction.Id.Should().Be(testRecurringTransfer.Id);
-        recurringTransaction.StartDate.Should().Be(testRecurringTransfer.StartDate);
-        recurringTransaction.EndDate.Should().Be(testRecurringTransfer.EndDate);
-        recurringTransaction.Amount.Should().Be(testRecurringTransfer.Amount);
-        recurringTransaction.Type.Should().Be(testRecurringTransfer.Type);
-        recurringTransaction.Note.Should().Be(testRecurringTransfer.Note);
-        recurringTransaction.ChargedAccount.Should().Be(testRecurringTransfer.ChargedAccount);
-        recurringTransaction.TargetAccount.Should().Be(testRecurringTransfer.TargetAccount);
-        recurringTransaction.CategoryId.Should().Be(testRecurringTransfer.CategoryId);
-        recurringTransaction.Recurrence.Should().Be(testRecurringTransfer.Recurrence);
-        recurringTransaction.IsLastDayOfMonth.Should().Be(testRecurringTransfer.IsLastDayOfMonth);
-        recurringTransaction.LastRecurrence.Should().Be(DateOnly.FromDateTime(DateTime.Today));
+        AssertRecurringTransaction(recurringTransaction: recurringTransaction, testRecurringTransfer: testRecurringTransfer);
     }
 }
