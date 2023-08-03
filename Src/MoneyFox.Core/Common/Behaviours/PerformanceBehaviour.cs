481bdab1e32b0b731e263dfa8365d1017aca32d9
Nino Padrutt <nino.padrutt@hotmail.com>
2023-08-03 11:45:37 +0100

    cleanup

--- Src/MoneyFox.Core/Common/Behaviours/PerformanceBehaviour.cs
+++ Src/MoneyFox.Core/Common/Behaviours/PerformanceBehaviour.cs
@@ -11,12 +11,7 @@ using Serilog;
 public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
     private const int THRESHOLD_LONG_RUNNING_REQUEST_MS = 500;
-    private readonly Stopwatch timer;
-
-    public PerformanceBehaviour()
-    {
-        timer = new();
-    }
+    private readonly Stopwatch timer = new();
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
