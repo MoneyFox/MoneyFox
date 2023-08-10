8d048b50b6da7679c73a479862eb875b934f678d
NPadrutt <nino.padrutt@hotmail.com>
2023-08-10 14:10:15 +0200

    cleanup about page and viewmodel

--- Src/MoneyFox.Ui/Views/About/AboutViewModel.cs
+++ Src/MoneyFox.Ui/Views/About/AboutViewModel.cs
@@ -9,11 +9,6 @@ using Resources.Strings;
 public class AboutViewModel : BasePageViewModel
 {
     private const string SUPPORT_MAIL = "mobile.support@apply-solutions.ch";
-    private static readonly Uri projectUri = new("https://github.com/MoneyFox/MoneyFox");
-    private static readonly Uri translationUri = new("https://crowdin.com/project/money-fox");
-    private static readonly Uri iconDesignerUrl = new("https://twitter.com/vandert9");
-    private static readonly Uri contributorUrl = new("https://github.com/MoneyFox/MoneyFox/graphs/contributors");
-    private static readonly Uri websiteUri = new(uriString: "https://www.apply-solutions.ch", uriKind: UriKind.Absolute);
 
     private readonly IBrowserAdapter browserAdapter;
     private readonly IEmailAdapter emailAdapter;
@@ -28,20 +23,10 @@ public class AboutViewModel : BasePageViewModel
 
     public static string Version => AppInfo.VersionString;
 
-    public AsyncRelayCommand GoToWebsiteCommand => new(GoToWebsiteAsync);
     public AsyncRelayCommand SendMailCommand => new(SendMailAsync);
     public AsyncRelayCommand RateAppCommand => new(RateAppAsync);
-    public AsyncRelayCommand GoToRepositoryCommand => new(GoToRepositoryAsync);
-    public AsyncRelayCommand GoToTranslationProjectCommand => new(GoToTranslationProjectAsync);
-    public AsyncRelayCommand GoToDesignerTwitterAccountCommand => new(GoToDesignerTwitterAccountAsync);
-    public AsyncRelayCommand GoToContributionPageCommand => new(GoToContributionPageAsync);
     public AsyncRelayCommand OpenLogFileCommand => new(OpenLogFile);
     public AsyncRelayCommand<string> OpenUrlCommand => new(url => browserAdapter.OpenWebsiteAsync(new(url ?? string.Empty)));
-
-    private async Task GoToWebsiteAsync()
-    {
-        await browserAdapter.OpenWebsiteAsync(websiteUri);
-    }
 
     private async Task SendMailAsync()
     {
@@ -63,26 +48,6 @@ public class AboutViewModel : BasePageViewModel
     private async Task RateAppAsync()
     {
         await CrossStoreReview.Current.RequestReview(false);
-    }
-
-    private async Task GoToRepositoryAsync()
-    {
-        await browserAdapter.OpenWebsiteAsync(projectUri);
-    }
-
-    private async Task GoToTranslationProjectAsync()
-    {
-        await browserAdapter.OpenWebsiteAsync(translationUri);
-    }
-
-    private async Task GoToDesignerTwitterAccountAsync()
-    {
-        await browserAdapter.OpenWebsiteAsync(iconDesignerUrl);
-    }
-
-    private async Task GoToContributionPageAsync()
-    {
-        await browserAdapter.OpenWebsiteAsync(contributorUrl);
     }
 
     private async Task OpenLogFile()
