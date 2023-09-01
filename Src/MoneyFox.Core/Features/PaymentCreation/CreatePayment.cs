1c38b941b30f1decdc415fce592f0e60973ac547
Nino Padrutt <nino.padrutt@hotmail.com>
2023-09-01 22:42:19 +0200

    cleanup

--- Src/MoneyFox.Core/Features/PaymentCreation/CreatePayment.cs
+++ Src/MoneyFox.Core/Features/PaymentCreation/CreatePayment.cs
@@ -3,16 +3,24 @@ namespace MoneyFox.Core.Features.PaymentCreation;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Common.Interfaces;
+using Domain;
+using Domain.Aggregates.AccountAggregate;
+using Domain.Aggregates.CategoryAggregate;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using MoneyFox.Core.Common.Interfaces;
-using MoneyFox.Domain;
-using MoneyFox.Domain.Aggregates.AccountAggregate;
-using MoneyFox.Domain.Aggregates.CategoryAggregate;
 
 public static class CreatePayment
 {
-    public record Command(int ChargedAccountId, int? TargetAccountId, Money Amount, PaymentType Type, DateOnly Date, int? CategoryId, Guid? RecurringTransactionId, string Note) : IRequest;
+    public record Command(
+        int ChargedAccountId,
+        int? TargetAccountId,
+        Money Amount,
+        PaymentType Type,
+        DateOnly Date,
+        int? CategoryId,
+        Guid? RecurringTransactionId,
+        string Note) : IRequest;
 
     public class Handler : IRequestHandler<Command>
     {
@@ -25,21 +33,28 @@ public static class CreatePayment
 
         public async Task Handle(Command command, CancellationToken cancellationToken)
         {
-            var chargedAccount = await context.Accounts.SingleAsync(a => a.Id == command.ChargedAccountId, cancellationToken: cancellationToken);
-
+            var chargedAccount = await context.Accounts.SingleAsync(predicate: a => a.Id == command.ChargedAccountId, cancellationToken: cancellationToken);
             Account? targetAccount = null;
             if (command.TargetAccountId.HasValue)
             {
-                targetAccount = await context.Accounts.SingleAsync(a => a.Id == command.TargetAccountId, cancellationToken: cancellationToken);
+                targetAccount = await context.Accounts.SingleAsync(predicate: a => a.Id == command.TargetAccountId, cancellationToken: cancellationToken);
             }
 
             Category? category = null;
             if (command.CategoryId.HasValue)
             {
-                category = await context.Categories.SingleAsync(a => a.Id == command.CategoryId, cancellationToken: cancellationToken);
+                category = await context.Categories.SingleAsync(predicate: a => a.Id == command.CategoryId, cancellationToken: cancellationToken);
             }
 
-            var payment = new Payment(command.Date.ToDateTime(TimeOnly.MinValue), command.Amount.Amount, command.Type, chargedAccount, targetAccount, category, command.Note, command.RecurringTransactionId);
+            var payment = new Payment(
+                date: command.Date.ToDateTime(TimeOnly.MinValue),
+                amount: command.Amount.Amount,
+                type: command.Type,
+                chargedAccount: chargedAccount,
+                targetAccount: targetAccount,
+                category: category,
+                note: command.Note,
+                recurringTransactionId: command.RecurringTransactionId);
 
             context.Add(payment);
             await context.SaveChangesAsync(cancellationToken);
