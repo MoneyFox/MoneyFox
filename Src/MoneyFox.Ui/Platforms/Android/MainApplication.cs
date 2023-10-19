af8cf37e7580ae125588b400d3403f3f27e01b4e
Nino Padrutt <nino.padrutt@hotmail.com>
2023-10-19 21:57:29 +0200

    add migration of db file for android

--- Src/MoneyFox.Ui/Platforms/Android/MainApplication.cs
+++ Src/MoneyFox.Ui/Platforms/Android/MainApplication.cs
@@ -18,9 +18,35 @@ public class MainApplication : MauiApplication
 
     protected override MauiApp CreateMauiApp()
     {
+        CreatePersonalFolder();
+        MigrateAndroidDatabaseFile();
         MauiProgram.AddPlatformServicesAction = AddServices;
 
         return MauiProgram.CreateMauiApp();
+    }
+
+    private static void CreatePersonalFolder()
+    {
+        // This seems to be necessary with .net 8 since the folder does not exist
+        var documentDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        if (Directory.Exists(documentDirectoryPath) is false)
+        {
+            Directory.CreateDirectory(documentDirectoryPath);
+        }
+    }
+
+    /// <summary>
+    ///     Migrates the db file, since there was a breaking change in .net 8 in the paths returned by special folder
+    /// </summary>
+    private static void MigrateAndroidDatabaseFile()
+    {
+        var databaseName = "moneyfox3.db";
+        var oldDbPath = Path.Combine(path1: Environment.GetEnvironmentVariable("HOME") ?? "", path2: databaseName);
+        if (File.Exists(oldDbPath))
+        {
+            var dbPath = Path.Combine(path1: Environment.GetFolderPath(Environment.SpecialFolder.Personal), path2: databaseName);
+            File.Move(sourceFileName: oldDbPath, destFileName: dbPath);
+        }
     }
 
     public override void OnCreate()
