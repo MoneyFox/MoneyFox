9160a91c962a5467dd3f6d88dabe3d28dc3fd992
Nino Padrutt <nino.padrutt@hotmail.com>
2023-10-26 11:15:45 +0100

    return readonly list

--- Src/MoneyFox.Core/Queries/GetAccountsQuery.cs
+++ Src/MoneyFox.Core/Queries/GetAccountsQuery.cs
@@ -9,9 +9,9 @@ using Domain.Aggregates.AccountAggregate;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-public class GetAccountsQuery : IRequest<List<Account>>
+public class GetAccountsQuery : IRequest<IReadOnlyList<Account>>
 {
-    public class Handler : IRequestHandler<GetAccountsQuery, List<Account>>
+    public class Handler : IRequestHandler<GetAccountsQuery, IReadOnlyList<Account>>
     {
         private readonly IAppDbContext appDbContext;
 
@@ -20,7 +20,7 @@ public class GetAccountsQuery : IRequest<List<Account>>
             this.appDbContext = appDbContext;
         }
 
-        public async Task<List<Account>> Handle(GetAccountsQuery request, CancellationToken cancellationToken)
+        public async Task<IReadOnlyList<Account>> Handle(GetAccountsQuery request, CancellationToken cancellationToken)
         {
             var accounts = await appDbContext.Accounts.AreActive().OrderByInclusion().OrderByName().ToListAsync(cancellationToken);
 
