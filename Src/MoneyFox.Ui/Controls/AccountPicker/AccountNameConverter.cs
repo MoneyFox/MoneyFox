e6d0212dabd26f82cc3ed64d96f6926cb3f16c3d
Nino Padrutt <nino.padrutt@hotmail.com>
2023-11-29 22:30:31 +0000

    fix warnings

--- Src/MoneyFox.Ui/Controls/AccountPicker/AccountNameConverter.cs
+++ Src/MoneyFox.Ui/Controls/AccountPicker/AccountNameConverter.cs
@@ -5,12 +5,12 @@ using Core.Common.Extensions;
 
 public class AccountNameConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return value is not AccountPickerViewModel account ? string.Empty : $"{account.Name} ({account.CurrentBalance.FormatCurrency()})";
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotSupportedException();
     }
