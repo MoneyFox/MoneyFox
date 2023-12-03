4673f8cddb36fda5bf61cf42bee70187499bfb61
Nino Padrutt <nino.padrutt@hotmail.com>
2023-12-03 19:02:47 +0000

    add routes

--- Src/MoneyFox.Ui/Common/Navigation/ViewLocator.cs
+++ Src/MoneyFox.Ui/Common/Navigation/ViewLocator.cs
@@ -1,19 +1,67 @@
 namespace MoneyFox.Ui.Common.Navigation;
 
 using Extensions;
+using Views.About;
 using Views.Accounts.AccountList;
+using Views.Accounts.AccountModification;
+using Views.Backup;
+using Views.Budget;
+using Views.Budget.BudgetModification;
+using Views.Categories;
+using Views.Categories.CategorySelection;
+using Views.Categories.ModifyCategory;
 using Views.Dashboard;
+using Views.Payments.PaymentList;
+using Views.Payments.PaymentModification;
+using Views.Settings;
+using Views.Setup;
+using Views.Setup.SelectCurrency;
+using Views.Statistics.CashFlow;
+using Views.Statistics.CategoryProgression;
+using Views.Statistics.CategorySpreading;
+using Views.Statistics.CategorySummary;
+using Views.Statistics.MonthlyAccountCashFlow;
+using Views.Statistics.Selector;
 
 internal class ViewLocator(IServiceProvider serviceProvider) : IViewLocator
 {
-    private static readonly List<(Type ViewModelType, Type ViewType)> ViewLocatorDictionary = new()
-    {
-        (typeof(DashboardViewModel), typeof(DashboardPage)), (typeof(AccountListViewModel), typeof(AccountListPage))
-    };
+    private static readonly List<(Type ViewModelType, Type ViewType)> ViewLocatorDictionary =
+    [
+        (typeof(DashboardViewModel), typeof(DashboardPage)),
+        (typeof(AccountListViewModel), typeof(AccountListPage)),
+        (typeof(AddAccountViewModel), typeof(AddAccountPage)),
+        (typeof(EditAccountViewModel), typeof(EditAccountPage)),
+        (typeof(BudgetListViewModel), typeof(BudgetListPage)),
+        (typeof(PaymentListViewModel), typeof(PaymentListPage)),
+        (typeof(AddPaymentViewModel), typeof(AddPaymentPage)),
+        (typeof(EditPaymentViewModel), typeof(EditPaymentPage)),
+        (typeof(CategoryListViewModel), typeof(CategoryListPage)),
+        (typeof(SelectCategoryViewModel), typeof(SelectCategoryPage)),
+        (typeof(AddCategoryViewModel), typeof(AddCategoryPage)),
+        (typeof(EditCategoryViewModel), typeof(EditCategoryPage)),
+        (typeof(BackupViewModel), typeof(BackupPage)),
+        (typeof(SettingsViewModel), typeof(SettingsPage)),
+        (typeof(AboutViewModel), typeof(AboutPage)),
+        (typeof(StatisticCashFlowViewModel), typeof(StatisticCashFlowPage)),
+        (typeof(StatisticCategorySpreadingViewModel), typeof(StatisticCategorySpreadingPage)),
+        (typeof(StatisticCategorySummaryViewModel), typeof(StatisticCategorySummaryPage)),
+        (typeof(StatisticAccountMonthlyCashFlowViewModel), typeof(StatisticAccountMonthlyCashFlowPage)),
+        (typeof(StatisticCategoryProgressionViewModel), typeof(StatisticCategoryProgressionPage)),
+        (typeof(StatisticSelectorViewModel), typeof(SelectCategoryPage)),
+        (typeof(PaymentForCategoryListViewModel), typeof(PaymentForCategoryListPage)),
+        (typeof(AddBudgetViewModel), typeof(AddBudgetPage)),
+        (typeof(EditBudgetViewModel), typeof(EditBudgetPage)),
+        (typeof(WelcomeViewModel), typeof(WelcomePage)),
+        (typeof(SetupCurrencyViewModel), typeof(SetupCurrencyPage)),
+        (typeof(SetupAccountsViewModel), typeof(SetupAccountPage)),
+        (typeof(SetupCategoryViewModel), typeof(SetupCategoryPage)),
+        (typeof(SetupCompletionViewModel), typeof(SetupCompletionPage)),
+    ];
 
     private static readonly List<(Type ViewModelType, Type ViewType)> DesktopViewLocatorDictionary = new()
     {
-        (typeof(DashboardViewModel), typeof(DashboardPage)), (typeof(AccountListViewModel), typeof(DesktopAccountListPage))
+        (typeof(DashboardViewModel), typeof(DashboardPage)),
+        (typeof(AccountListViewModel), typeof(DesktopAccountListPage))
     };
 
     public IBindablePage GetViewFor<TViewModel>() where TViewModel : NavigableViewModel
