496adf873781604b2a716fbdb7850412b2032830
Nino Padrutt <nino.padrutt@hotmail.com>
2023-12-03 22:02:18 +0000

    add new on navigated back method

--- Src/MoneyFox.Ui/INavigationService.cs
+++ Src/MoneyFox.Ui/INavigationService.cs
@@ -24,10 +24,18 @@ public class NavigableViewModel : ObservableRecipient
     public virtual Task OnNavigatedAsync(object? parameter)
     {
         OnNavigated(parameter);
+
         return Task.CompletedTask;
     }
 
-    public virtual void OnNavigated(object? parameter)
+    protected virtual void OnNavigated(object? parameter) { }
+
+    public virtual Task OnNavigatedBackAsync(object? parameter)
     {
+        OnNavigatedBack(parameter);
+
+        return Task.CompletedTask;
     }
+
+    protected virtual void OnNavigatedBack(object? parameter) { }
 }
