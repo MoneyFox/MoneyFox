209da370c43c1c3e1caa4df9cec532fe33797504
Nino Padrutt <nino.padrutt@hotmail.com>
2023-12-16 18:09:15 +0100

    statisticselectorview

---
+++ Src/MoneyFox.Ui/Views/Statistics/Selector/StatisticSelectorView.xaml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFox.Ui.Views.Statistics.Selector;
+
+public partial class StatisticSelectorView : ContentView
+{
+    public StatisticSelectorView()
+    {
+        InitializeComponent();
+    }
+}
+
