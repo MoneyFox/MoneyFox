e6dbdc99858c61cdf50fce1ce46fedba4d733a51
Nino Padrutt <nino.padrutt@hotmail.com>
2024-01-12 22:01:12 +0100

    fix can execute

--- Src/MoneyFox.Ui/Views/Setup/SetupAccounts/SetupAccountsViewModel.cs
+++ Src/MoneyFox.Ui/Views/Setup/SetupAccounts/SetupAccountsViewModel.cs
@@ -1,8 +1,10 @@
 namespace MoneyFox.Ui.Views.Setup.SetupAccounts;
 
+using System.Collections.ObjectModel;
 using Accounts.AccountModification;
 using Common.Navigation;
 using CommunityToolkit.Mvvm.Input;
+using Core.Common.Extensions;
 using Core.Queries;
 using MediatR;
 
@@ -10,14 +12,32 @@ public sealed class SetupAccountsViewModel(INavigationService navigationService,
 {
     public AsyncRelayCommand GoToAddAccountCommand => new(() => navigationService.GoTo<AddAccountViewModel>());
 
-    public AsyncRelayCommand NextStepCommand => new(execute: () => navigationService.GoTo<SetupCategoryViewModel>(), canExecute: CheckIfAccountWasMade);
+    public AsyncRelayCommand NextStepCommand
+        => new(execute: () => navigationService.GoTo<SetupCategoryViewModel>(), canExecute: () => IsNextStepAvailable);
 
     public AsyncRelayCommand BackCommand => new(() => navigationService.GoBack());
 
-    private bool CheckIfAccountWasMade()
-    {
-        var accountVms = mediator.Send(new GetAccountsQuery()).GetAwaiter().GetResult();
+    public ObservableCollection<AccountViewModel> ExistingAccounts { get; } = [];
 
-        return accountVms.Any();
+    public bool IsNextStepAvailable => ExistingAccounts.Count > 0;
+
+    public override Task OnNavigatedAsync(object? parameter)
+    {
+        return LoadExistingAccounts();
+    }
+
+    public override Task OnNavigatedBackAsync(object? parameter)
+    {
+        return LoadExistingAccounts();
+    }
+
+    private async Task LoadExistingAccounts()
+    {
+        var accounts = await mediator.Send(new GetAccountsQuery());
+        ExistingAccounts.Clear();
+        ExistingAccounts.AddRange(accounts.Select(a => new AccountViewModel(Id: a.Id, Name: a.Name)));
+        OnPropertyChanged(nameof(IsNextStepAvailable));
     }
 }
+
+public record AccountViewModel(int Id, string Name);
