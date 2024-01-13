6f0c267fdbc9251f6073df1eb8035643402dffda
Nino Padrutt <nino.padrutt@hotmail.com>
2024-01-13 01:44:30 +0100

    skip compilation

--- Src/MoneyFox.Ui/Views/Accounts/AccountModification/ModifyAccountContentView.xaml.cs
+++ Src/MoneyFox.Ui/Views/Accounts/AccountModification/ModifyAccountContentView.xaml.cs
@@ -1,5 +1,7 @@
 namespace MoneyFox.Ui.Views.Accounts.AccountModification;
 
+// Compilation needs to be disabled here, since otherwise the binding does not work on Android
+[XamlCompilation(XamlCompilationOptions.Skip)]
 public partial class ModifyAccountContentView
 {
     public ModifyAccountContentView()
