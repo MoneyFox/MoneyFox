6f1fd620b4bbc743b8afc4cac0de3556305ad61f
Nino Padrutt <nino.padrutt@hotmail.com>
2024-01-16 15:02:33 +0100

    set payment sign

--- Src/MoneyFox.Core/Queries/GetPaymentsInBudget.cs
+++ Src/MoneyFox.Core/Queries/GetPaymentsInBudget.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Common;
 using Common.Extensions;
 using Common.Interfaces;
+using Domain.Aggregates.AccountAggregate;
 using Domain.Aggregates.BudgetAggregate;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -31,18 +32,18 @@ public static class GetPaymentsInBudget
         public async Task<IReadOnlyList<Data>> Handle(Query query, CancellationToken cancellationToken)
         {
             var budget = await dbContext.Budgets.Where(b => b.Id == query.BudgetId).SingleAsync(cancellationToken);
-
             var thresholdDate = systemDateHelper.Today.GetFirstDayOfMonth().AddMonths(-(budget.Interval.NumberOfMonths - 1));
             var endOfMonth = systemDateHelper.Today.GetLastDayOfMonth();
-            return await dbContext.Payments
-                .Where(p => p.Category != null && budget.IncludedCategories.Contains(p.Category!.Id))
+
+            return await dbContext.Payments.Where(p => p.Category != null && budget.IncludedCategories.Contains(p.Category!.Id))
                 .Where(p => p.Date >= thresholdDate)
                 .Where(p => p.Date <= endOfMonth)
+                .Where(p => p.Type != PaymentType.Transfer)
                 .Select(
                     p => new Data(
                         p.Id,
                         p.Date,
-                        p.Amount,
+                        p.Type == PaymentType.Expense ? -p.Amount : p.Amount,
                         p.ChargedAccount.Name,
                         p.Category!.Name,
                         p.Note ?? "",
